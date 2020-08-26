890a51b6bb75ea2efc4adcfe355b4e3763d63cb1
Elena-brv <leena.brv@gmail.com>
2020-08-26 21:25:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead tr');
+
+thead.addEventListener('click', event => {
+  const index = [...thead.children].indexOf(event.target);
+  const tbody = document.querySelectorAll('table tbody tr');
+
+  const cells = [...tbody]
+    .sort((a, b) => {
+      const innerTextA = a.children[index].innerText
+        .replace(/[^0-9a-z]/gi, '');
+      const innerTextB = b.children[index].innerText
+        .replace(/[^0-9a-z]/gi, '');
+
+      return !isNaN(+innerTextA)
+        ? (+innerTextA) - (+innerTextB)
+        : innerTextA.localeCompare(innerTextB);
+    });
+
+  for (const cell of cells) {
+    document.querySelector('table tbody').append(cell);
+  }
+});
