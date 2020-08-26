76701603b9dc3f3cdd944de63e4956562a88c72e
nanccyy <lidiapriymak@gmail.com>
2020-08-26 17:03:14 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+
+const employees = tbody.children;
+const headings = thead.firstElementChild.children;
+
+[...headings].map((heading, index) =>
+  heading.addEventListener('click', () => {
+    const sorted = [...employees].sort((a, b) => {
+      const first = a.children[index].innerHTML;
+      const second = b.children[index].innerHTML;
+
+      return (index === 3)
+        ? convertCurrencyToNumber(first) - convertCurrencyToNumber(second)
+        : first.localeCompare(second);
+    });
+
+    tbody.append(...sorted);
+  })
+);
+
+function convertCurrencyToNumber(number) {
+  return +number.replace(/[$,]+/g, '');
+}
