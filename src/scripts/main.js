63e9509ab5d6086921122be26f29e53a0cb7e7e7
Mikle25 <mikle2593@gmail.com>
2020-08-26 19:24:27 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+(function() {
+  const table = document.querySelector('tbody');
+  const headings = document.querySelectorAll('th');
+
+  [...headings].forEach((heading, index) => {
+    heading.addEventListener('click', () => {
+      const sortList = [...table.querySelectorAll('tr')].sort((a, b) => {
+        const elem1 = a
+          .children[index]
+          .innerHTML
+          .replace(/[$,]/g, '');
+
+        const elem2 = b
+          .children[index]
+          .innerHTML
+          .replace(/[$,]/g, '');
+
+        if (isFinite(elem1)) {
+          return (+elem1) - (+elem2);
+        } else {
+          return elem1 > elem2 ? 1 : -1;
+        }
+      });
+
+      table.append(...sortList);
+    });
+  });
+}());
