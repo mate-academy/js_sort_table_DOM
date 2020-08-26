a8b3b15e3277bcfb5ae294ca8a95072cbb140130
Anatasia <krajnova.stasy@gmail.com>
2020-08-26 23:39:10 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('tbody');
+
+const listHeader = [ ...document.querySelectorAll('th') ];
+
+listHeader.map((item, index) => {
+  item.addEventListener('click', () => {
+    const rows = [ ...list.querySelectorAll('tr') ];
+
+    rows.sort((a, b) => {
+      const itemA = a.children[index].innerHTML.replace(/[^A-Z0-9]/gi, '');
+      const itemB = b.children[index].innerHTML.replace(/[^A-Z0-9]/gi, '');
+
+      if (!isNaN(itemA)) {
+        return (+itemA) - (+itemB);
+      }
+
+      return itemA.localeCompare(itemB);
+    });
+
+    list.append(...rows);
+  });
+});
