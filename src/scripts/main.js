bad1f56eec8a7a5c32865b3e911921f70f278687
DmJSCourse <dimonua@gmail.com>
2020-08-26 21:20:45 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
 'use strict';
 
-// write code here
+const data = [...document.querySelector('tbody').children].map(element => {
+  return {
+    name: element.children[0].textContent,
+    position: element.children[1].textContent,
+    age: Number(element.children[2].textContent),
+    salary: element.children[3].textContent,
+    salaryNum: formatSalary(element.children[3].textContent),
+  };
+});
+const nameHeader = document.querySelector('table thead tr').children[0];
+const positionHeader = document.querySelector('table thead tr').children[1];
+const ageHeader = document.querySelector('table thead tr').children[2];
+const salaryHeader = document.querySelector('table thead tr').children[3];
+const tableContent = document.querySelector('tbody');
+
+function formatSalary(string) {
+  let checkingArray = string.split('');
+
+  checkingArray = checkingArray.filter(element => !isNaN(element));
+
+  return Number(checkingArray.join(''));
+}
+
+function sortData(array, field) {
+  if (field === 'name' || field === 'position') {
+    array.sort((a, b) => a[field].localeCompare(b[field]));
+  } else {
+    array.sort((a, b) => a[field] - b[field]);
+  }
+}
+
+function buildTable(array) {
+  let insertedCode = '';
+
+  array.forEach(employee => {
+    insertedCode += `
+      <tr>
+        <td>${employee.name}</td>
+        <td>${employee.position}</td>
+        <td>${employee.age}</td>
+        <td>${employee.salary}</td>
+      </tr>
+    `;
+  });
+
+  tableContent.innerHTML = insertedCode;
+}
+
+nameHeader.addEventListener('click', event => {
+  sortData(data, 'name');
+  buildTable(data);
+});
+
+positionHeader.addEventListener('click', event => {
+  sortData(data, 'position');
+  buildTable(data);
+});
+
+ageHeader.addEventListener('click', event => {
+  sortData(data, 'age');
+  buildTable(data);
+});
+
+salaryHeader.addEventListener('click', event => {
+  sortData(data, 'salaryNum');
+  buildTable(data);
+});
