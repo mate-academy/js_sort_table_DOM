2f86c86e1cd1c72f83ebe607b5451db3be62dae9
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-28 02:34:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
 // write code here
+
+const peopleContainer = document.querySelector('tbody');
+const people = [...peopleContainer.children];
+
+const tableHeader = document.querySelector('thead');
+
+tableHeader.addEventListener('click', (event) => {
+  const nameColumn = tableHeader.firstElementChild.children[0];
+  const positionColumn = tableHeader.firstElementChild.children[1];
+  const ageColumn = tableHeader.firstElementChild.children[2];
+  const salaryColumn = tableHeader.firstElementChild.children[3];
+
+  switch (event.target) {
+    case nameColumn:
+      people.sort((person1, person2) => {
+        return (person1.children[0].innerText).localeCompare(
+          person2.children[0].innerText
+        );
+      }).forEach(elem => peopleContainer.append(elem));
+      break;
+
+    case positionColumn:
+      people.sort((person1, person2) => {
+        return (person1.children[1].innerText).localeCompare(
+          person2.children[1].innerText
+        );
+      }).forEach(elem => peopleContainer.append(elem));
+      break;
+
+    case ageColumn:
+      people.sort((person1, person2) => {
+        return (person1.children[2].innerText)
+          - person2.children[2].innerText;
+      }).forEach(elem => peopleContainer.append(elem));
+      break;
+
+    case salaryColumn:
+      people.sort((person1, person2) => {
+        return +(person1.children[3].innerText
+          .slice(1)
+          .split(',')
+          .join(''))
+        - +(person2.children[3].innerText
+          .slice(1)
+          .split(',')
+          .join(''));
+      }).forEach(elem => peopleContainer.append(elem));
+      break;
+  }
+});
