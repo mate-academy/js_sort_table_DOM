4f463640fbab3ebefbeea7ec6c2a537517026fc3
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-08-27 19:52:23 +0300

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+[...document.querySelectorAll('th')]
+  .map(th => {
+    th.dataset.name = th.innerText;
+  });
+
+function sortByName(a, b) {
+  return a > b ? 1 : -1;
+}
+
+function sortByPosition(a, b) {
+  return a > b ? 1 : -1;
+}
+
+function sortByAge(a, b) {
+  return a - b;
+}
+
+function sortBySalary(a, b) {
+  const first = a.replace(/[$,]/gi, '');
+  const second = b.replace(/[$,]/gi, '');
+
+  return first - second;
+}
+
+tableHead.addEventListener('click', event => {
+  const heading = event.target.dataset.name;
+  const column = event.target.cellIndex;
+  const rows = [...tableBody.children];
+
+  rows.sort((rowA, rowB) => {
+    const valueA = rowA.cells[column].textContent;
+    const valueB = rowB.cells[column].textContent;
+
+    switch (heading) {
+      case 'Name':
+        return sortByName(valueA, valueB);
+
+      case 'Position':
+        return sortByPosition(valueA, valueB);
+
+      case 'Age':
+        return sortByAge(valueA, valueB);
+
+      case 'Salary':
+        return sortBySalary(valueA, valueB);
+
+      default:
+        return 0;
+    }
+  });
+
+  for (const row of rows) {
+    tableBody.appendChild(row);
+  }
+});
