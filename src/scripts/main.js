9a44c929e38b358266c4cd663170e32a18a19b09
oivannikov <alex0502ivannikov@gmail.com>
2020-08-27 18:11:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tr = [...document.querySelectorAll('tbody tr')];
+const thead = document.querySelector('thead tr');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (event) => {
+  const indexHead = [...thead.children].indexOf(event.target);
+  let findSorting = tr[indexHead].children[indexHead];
+
+  if (findSorting.textContent[0] === '$'
+    || !isNaN(parseFloat(findSorting.textContent))) {
+
+    const sorted = [...tr].sort((a, b) => {
+      return replacementSigns(a.children[indexHead].textContent)
+        - replacementSigns(b.children[indexHead].textContent);
+    });
+
+    tbody.append(...sorted);
+  } else {
+    const sorted = [...tr].sort((a, b) => {
+      return a.children[indexHead].textContent
+        .localeCompare(b.children[indexHead].textContent);
+    });
+
+    tbody.append(...sorted);
+  }
+
+  function replacementSigns(string) {
+    return string.replace('$', '').replace(',', '');
+  }
+});
