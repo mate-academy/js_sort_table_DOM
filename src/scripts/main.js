d806f5a23cec6667c7953215ab6537a10ee974dc
artsashko <artyomsashko@gmail.com>
2020-08-27 23:46:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headerRow = table.tHead.children[0];
+const people = [...table.tBodies[0].querySelectorAll('tr')];
+
+headerRow.addEventListener('click', handler);
+
+function handler(event) {
+  const header = event.target;
+
+  switch (header.innerText) {
+    case ('Name') :
+      tableSort(people, 0);
+      break;
+    case ('Position') :
+      tableSort(people, 1);
+      break;
+    case ('Age') :
+      tableSort(people, 2);
+      break;
+    case ('Salary') :
+      tableSort(people, 3);
+      break;
+    default:
+      break;
+  }
+}
+
+function tableSort(rows, sortIndex) {
+  rows.sort((employee1, employee2) => {
+    const value1 = [...employee1.children][sortIndex].textContent;
+    const value2 = [...employee2.children][sortIndex].textContent;
+
+    return (value1[0] === '$')
+      ? salaryConverter(value1) - salaryConverter(value2)
+      : value1.localeCompare(value2);
+  });
+  table.tBodies[0].append(...rows);
+}
+
+function salaryConverter(salary) {
+  return +salary
+    .slice(1)
+    .split(',')
+    .join('');
+}
