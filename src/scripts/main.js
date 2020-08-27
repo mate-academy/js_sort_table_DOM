90b29b87e3bd42721523fbbc8d2c7ef38b6487d8
karinaKudatska <karina.kudatska@gmail.com>
2020-08-27 16:51:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const people = Array.from(document.querySelectorAll('tbody tr'));
+const categories = document.querySelector('thead');
+const table = document.querySelector('tbody');
+
+categories.addEventListener('click', (event) => {
+  const column = event.target.cellIndex;
+  const sortTable = people.sort((a, b) => {
+    const first = a.children[column].innerHTML;
+    const second = b.children[column].innerHTML;
+
+    if (column === 2) {
+      return first - second;
+    } else if (column === 3) {
+      return +first.replace('$', '').replace(',', '.')
+      - +second.replace('$', '').replace(',', '.');
+    } else {
+      return first.localeCompare(second);
+    }
+  });
+
+  table.append(...sortTable);
+});
