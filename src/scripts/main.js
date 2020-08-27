e757134954d37614f75ea775572e8371fdc71262
martakupka <marta-k2008@ukr.net>
2020-08-27 17:56:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,62 @@
 'use strict';
 
-// write code here
+const tableHeadRow = document.querySelector('thead');
+const tableHeadContent = [];
+
+const tableBody = document.querySelector('tbody');
+const tableRows = tableBody.querySelectorAll('tr');
+
+const employees = [];
+
+for (const element of tableHeadRow.querySelectorAll('th')) {
+  tableHeadContent.push(element.innerText);
+}
+
+for (const element of tableRows) {
+  const row = element.querySelectorAll('td');
+  const rowContent = [];
+  const employee = {};
+
+  for (const item of row) {
+    rowContent.push(item.innerText);
+  }
+
+  for (let i = 0; i < rowContent.length; i++) {
+    employee[tableHeadContent[i]] = rowContent[i];
+  }
+
+  employees.push(employee);
+}
+
+tableHeadRow.addEventListener('click', (event) => {
+  switch (event.target.innerText) {
+    case tableHeadContent[0]:
+      employees.sort((a, b) => a.Name.localeCompare(b.Name));
+      break;
+    case tableHeadContent[1]:
+      employees.sort((a, b) => a.Position.localeCompare(b.Position));
+      break;
+    case tableHeadContent[2]:
+      employees.sort((a, b) => (+a.Age) - (+b.Age));
+      break;
+    case tableHeadContent[3]:
+      employees.sort((a, b) => (+a.Salary.replace(',', '').slice(1))
+        - (+b.Salary.replace(',', '').slice(1)));
+      break;
+  }
+
+  tableBody.innerHTML = '';
+
+  for (const employee of employees) {
+    const newRow = document.createElement('tr');
+
+    newRow.insertAdjacentHTML('afterbegin', `
+      <td>${employee.Name}</td>
+      <td>${employee.Position}</td>
+      <td>${employee.Age}</td>
+      <td>${employee.Salary}</td>
+    `);
+
+    tableBody.append(newRow);
+  }
+});
