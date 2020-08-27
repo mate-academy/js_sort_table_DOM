8ae34024f62f417fbc715c43f597cd2ada48fa25
Elena-brv <leena.brv@gmail.com>
2020-08-27 15:47:46 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,17 @@
 const thead = document.querySelector('thead tr');
 
 thead.addEventListener('click', event => {
-  const index = [...thead.children].indexOf(event.target);
-  const tbody = document.querySelectorAll('table tbody tr');
+  const categoryIndex = [...thead.children].indexOf(event.target);
+  const tbodyRows = document.querySelectorAll('table tbody tr');
 
-  const sortedEmployees = [...tbody]
+  const sortedEmployees = [...tbodyRows]
     .sort((a, b) => {
-      const emloyeeA = a.children[index].innerText
+      const emloyeeA = a.children[categoryIndex].innerText
         .replace(/[^0-9a-z]/gi, '');
-      const emloyeeB = b.children[index].innerText
+      const emloyeeB = b.children[categoryIndex].innerText
         .replace(/[^0-9a-z]/gi, '');
 
-      return !isNaN(+emloyeeA)
+      return !Number.isNaN(+emloyeeA)
         ? (+emloyeeA) - (+emloyeeB)
         : emloyeeA.localeCompare(emloyeeB);
     });
