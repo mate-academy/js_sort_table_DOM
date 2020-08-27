4b099a19582cd66b3034dcdf41503ffd976df8ca
Roman <romikkus@gmail.com>
2020-08-27 12:00:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write code here
+const tBody = document.querySelector('tbody');
+
+const tableHead = document.querySelector('thead');
+
+const tableHeaders = [...tableHead.children[0].children];
+
+const rows = [...tBody.querySelectorAll('tr')];
+
+function toNumber(value) {
+  return Number(value
+    .replace(/\$/g, '')
+    .split(',')
+    .join(''));
+}
+
+function sortTable(tblBody, columnIndex) {
+  const sortedRows = rows.sort((a, b) => {
+    const aColumnText = a.children[columnIndex]
+      .textContent
+      .trim();
+    const bColumnText = b.children[columnIndex]
+      .textContent
+      .trim();
+
+    if (aColumnText.includes('$')) {
+      return toNumber(aColumnText) - toNumber(bColumnText);
+    }
+
+    if (!isNaN(parseInt(aColumnText))) {
+      return parseInt(aColumnText) - parseInt(bColumnText);
+    }
+
+    return aColumnText.localeCompare(bColumnText);
+  });
+
+  while (tblBody.firstChild) {
+    tblBody.removeChild(tblBody.firstChild);
+  }
+
+  tblBody.append(...sortedRows);
+}
+
+document.addEventListener('click', e => {
+  const element = e.target;
+
+  if (element.parentNode.parentNode.tagName === 'THEAD') {
+    const headerIndex = tableHeaders.indexOf(element);
+
+    sortTable(tBody, headerIndex);
+  }
+});
