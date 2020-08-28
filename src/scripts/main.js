1564d6f11f463326bc2e784bb9b7112df43a6dd8
JuliaNastenko <juli.nastenko@gmail.com>
2020-08-28 16:28:45 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
 // write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (event) => {
+  const th = event.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const column = th.cellIndex;
+  const rows = [...tbody.children];
+
+  rows.sort((a, b) => {
+    const A = a.cells[column].innerText;
+    const B = b.cells[column].innerText;
+
+    if (column === 3) {
+      return +(A.replace(/[$,]/g, '')) - +(B.replace(/[$,]/g, ''));
+    } else {
+      return A.localeCompare(B);
+    }
+  });
+
+  for (const row of rows) {
+    tbody.appendChild(row);
+  };
+});
