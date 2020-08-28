92f9bcbafbe9647b018945887a350d2e44e82395
Gabbasova Oksana <oksana.gabba@gmail.com>
2020-08-28 19:23:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const tableHeadRow = document.querySelector('thead tr');
+
+tableHeadRow.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'TH') {
+    return;
+  }
+
+  const sortBy = event.target.textContent;
+  const tableBody = document.querySelector('tbody');
+  const tableRows = [...document.querySelectorAll('tbody tr')];
+
+  switch (sortBy) {
+    case 'Name':
+      tableRows.sort((a, b) => {
+        return a.children[0].textContent
+          .localeCompare(b.children[0].textContent);
+      });
+      break;
+
+    case 'Position':
+      tableRows.sort((a, b) => {
+        return a.children[1].textContent
+          .localeCompare(b.children[1].textContent);
+      });
+      break;
+
+    case 'Age':
+      tableRows.sort((a, b) => {
+        return Number(a.children[2].textContent)
+          - Number(b.children[2].textContent);
+      });
+      break;
+
+    case 'Salary':
+      tableRows.sort((a, b) => {
+        return Number(a.children[3].textContent
+          .replace('$', '').replace(',', ''))
+          - Number(b.children[3].textContent
+            .replace('$', '').replace(',', ''));
+      });
+      break;
+
+    default:
+
+      return;
+  }
+
+  tableBody.innerHTML = '';
+
+  tableRows.forEach(tr => {
+    tableBody.append(tr);
+  });
+});
