9e7fa01fad4748093eb52c6dfa4e9f8d2a9de231
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-30 22:11:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const people = [...document.querySelectorAll('tbody tr')];
+const head = document.querySelector('thead tr');
+
+head.addEventListener('click', (event) => {
+  const index = [...head.children].indexOf(event.target);
+
+  const sorted = people.sort((a, b) => {
+    const first = a.children[index].innerText.replace(/[$,]/g, '');
+    const second = b.children[index].innerText.replace(/[$,]/g, '');
+
+    if (isNaN(+first)) {
+      return first.localeCompare(second);
+    };
+
+    return first - second;
+  });
+
+  document.querySelector('tbody').append(...sorted);
+});
