ed14c5f49815b513968ec91da0f0d360554a1973
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-31 11:23:01 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead tr');
+
+thead.addEventListener('click', event => {
+  const colIndex = [...thead.children].indexOf(event.target);
+  const peoples = document.querySelectorAll('tbody tr');
+
+  const sortedEmployees = [...peoples]
+    .sort((a, b) => {
+      const humanA = a.children[colIndex].innerText.replace(/[^0-9a-z]/gi, '');
+      const humanB = b.children[colIndex].innerText.replace(/[^0-9a-z]/gi, '');
+
+      return !Number.isNaN(+humanA)
+        ? (+humanA) - (+humanB)
+        : humanA.localeCompare(humanB);
+    });
+
+  document.querySelector('tbody').append(...sortedEmployees);
+});
