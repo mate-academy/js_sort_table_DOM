cb5b7cc5363f8c2a8c435b3eac0ea547df10b5e9
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-31 13:55:39 +0300

    vers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+function tableSorter(index, elem) {
+  const result = [...table.tBodies[0].rows].sort((a, b) => {
+    let x = a.cells[index].textContent;
+    let y = b.cells[index].textContent;
+
+    if (elem === 'Salary') {
+      x = x.replace(/[$,]/g, '');
+      y = y.replace(/[$,]/g, '');
+    }
+
+    if (isNaN(+x)) {
+      return x.localeCompare(y);
+    } else {
+      return (+x) - (+y);
+    };
+  });
+
+  table.tBodies[0].append(...result);
+}
+
+table.addEventListener('click', (event) => {
+  tableSorter(event.target.cellIndex, event.target.textContent);
+});
