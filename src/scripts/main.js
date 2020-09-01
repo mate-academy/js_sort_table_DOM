fd572633aaa5c621da806a1eb9ea78128de217e1
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-01 13:50:05 +0300

    The best Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,6 @@ const headerIndex = {
   Age: 2,
   Salary: 3,
 };
-const person = [...table.tBodies[0].children];
 
 table.addEventListener('click', (event) => {
   const item = event.target;
@@ -26,9 +25,7 @@ table.addEventListener('click', (event) => {
       handleTableSort(headerIndex.Age);
       break;
     case 'Salary':
-      formatNumber(headerIndex.Salary);
       handleTableSort(headerIndex.Salary);
-      formatSalary(headerIndex.Salary);
       break;
   }
 });
@@ -36,7 +33,8 @@ table.addEventListener('click', (event) => {
 function handleTableSort(column) {
   const rows = [...table.tBodies[0].children].sort(function(a, b) {
     if (column === headerIndex.Salary) {
-      return a.children[column].textContent - b.children[column].textContent;
+      return formatNumber(a.children[column].textContent)
+       - formatNumber(b.children[column].textContent);
     } else {
       return a.children[column].textContent
         .localeCompare(b.children[column].textContent);
@@ -46,17 +44,6 @@ function handleTableSort(column) {
   table.tBodies[0].append(...rows);
 }
 
-function formatNumber(column) {
-  for (let i = 0; i < person.length; i++) {
-    person[i].children[column].textContent = +person[i].children[column]
-      .textContent.substr(1).split(',').join('');
-  }
-  table.tBodies[0].append(...person);
-}
-
-function formatSalary(column) {
-  for (let i = 0; i < person.length; i++) {
-    person[i].children[column].textContent = `$${new Intl.NumberFormat('en-us')
-      .format(person[i].children[column].textContent)}`;
-  }
+function formatNumber(salary) {
+  return +salary.substr(1).split(',').join('');
 }
