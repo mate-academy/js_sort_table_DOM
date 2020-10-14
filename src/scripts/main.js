3b8cc61e041e59e6777c682ea19d0e30b580f7f3
Alona <aionovaionova@gmail.com>
2020-10-14 23:15:25 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const table = document.querySelector('table');
+const headers = [...head.firstElementChild.children];
+const rows = [...table.children[1].querySelectorAll('tr')];
+
+head.addEventListener('click', (event) => {
+  if (event.target.tagName !== 'TH') {
+    return;
+  };
+
+  // eslint-disable-next-line max-len
+  const index = headers.indexOf(event.target);
+
+  // eslint-disable-next-line max-len
+  if (parseFloat([...rows[0].children][index].textContent.match(/\w/g).join(''))) {
+    // eslint-disable-next-line max-len
+    rows.sort((a, b) => +([...a.children][index].textContent.match(/\w/g).join('')) - +([...b.children][index].textContent.match(/\w/g).join('')));
+  } else {
+    // eslint-disable-next-line max-len
+    rows.sort((a, b) => [...a.children][index].textContent.localeCompare([...b.children][index].textContent));
+  }
+
+  table.children[1].append(...rows);
+});
