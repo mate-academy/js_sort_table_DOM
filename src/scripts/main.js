7107fdeca00067cbdb337323232a44b3e7df5fcd
Anastasia Chervona <av.chervona@gmail.com>
2020-10-16 00:53:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = table.tBodies[0];
+
+table.tHead.addEventListener('click', (event) => {
+  const sortedCellIndex = event.target.cellIndex;
+
+  const sortedRows = [...tableBody.rows].sort((firstRow, secondRow) => {
+    const firstRowText = firstRow.cells[sortedCellIndex].textContent;
+    const secondRowText = secondRow.cells[sortedCellIndex].textContent;
+
+    if (+firstRowText.replace(/\D*/g, '')) {
+      const firstNumber = +firstRowText.replace(/\D*/g, '');
+      const secondNumber = +secondRowText.replace(/\D*/g, '');
+
+      return firstNumber - secondNumber;
+    }
+
+    return firstRowText.localeCompare(secondRowText);
+  });
+
+  tableBody.append(...sortedRows);
+});
