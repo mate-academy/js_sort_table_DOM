4749b0b877349101a3cd7ca2c26c2cfcf9e97091
Alina <alina90565@gmail.com>
2020-10-16 15:26:39 +0300

    update sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,21 @@ const titles = headTable.firstElementChild.children;
 const rows = [...bodyTable.children];
 
 headTable.addEventListener('click', ({ target }) => {
-  const columnIndex = [...titles]
-    .findIndex(title => title === target);
-
-  const contentType = titles[columnIndex].textContent;
+  const columnIndex = [...titles].indexOf(target);
 
   rows.sort((prevRow, nextRow) => {
     const prevContent = prevRow.children[columnIndex].textContent;
     const nextContent = nextRow.children[columnIndex].textContent;
 
-    if (contentType === 'Name' || contentType === 'Position') {
+    if (prevContent.startsWith('$')) {
+      return parseNumber(prevContent) - parseNumber(nextContent);
+    }
+
+    if (isNaN(+prevContent)) {
       return prevContent.localeCompare(nextContent);
     }
 
-    if (contentType === 'Age') {
-      return +prevContent - +nextContent;
-    }
-
-    return parseNumber(prevContent) - parseNumber(nextContent);
+    return +prevContent - +nextContent;
   });
 
   bodyTable.append(...rows);
