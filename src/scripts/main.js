41c097052343888579926b78c75756e5f9e2f181
Artur Bespalyy <zkhomers@gmail.com>
2020-10-17 13:04:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('tbody');
+
+const listHeader = [ ...document.querySelectorAll('th') ];
+// console.log(listHeader)
+listHeader.map((item, index) => {
+  // debugger
+  item.addEventListener('click', () => {
+    const rows = [ ...list.querySelectorAll('tr') ];
+
+    rows.sort((a, b) => {
+      const itemA = a.children[index].innerText.replace(/[^A-Z0-9]/gi, '');
+      const itemB = b.children[index].innerText.replace(/[^A-Z0-9]/gi, '');
+
+      if (!isNaN(itemA)) {
+        return (+itemA) - (+itemB);
+      }
+
+      return itemA.localeCompare(itemB);
+    });
+
+    list.append(...rows);
+  });
+});
