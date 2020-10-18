85334256b5454e5ac60022050ba9bb5b881d23dc
goncharukop <goncharukop@gmail.com>
2020-10-18 22:36:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = document.querySelector('tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
+const tableTitle = ['Name', 'Position', 'Age', 'Salary'];
+
+table.addEventListener('click', event => {
+  const title = event.target.closest('th');
+
+  const sortBy = title.textContent;
+
+  rows.sort((a, b) => {
+    const index = tableTitle.indexOf(sortBy);
+    const prevItem = a.children[index].textContent;
+    const nextItem = b.children[index].textContent;
+
+    if (prevItem.includes('$')) {
+      return parseFloat(prevItem.replace(',', '.').replace('$', ''))
+        - parseFloat(nextItem.replace(',', '.').replace('$', ''));
+    }
+
+    if (+prevItem.typeOf !== 'number') {
+      return prevItem.localeCompare(nextItem);
+    }
+
+    return +prevItem - +nextItem;
+  });
+
+  tableBody.append(...rows);
+});
