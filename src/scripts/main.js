f94264b1e4a88a31f73010610a068c77d068fd7d
goncharukop <goncharukop@gmail.com>
2020-10-18 23:29:09 +0300

    del 1 replace, change find index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,18 @@
 const table = document.querySelector('table');
 const tableBody = document.querySelector('tbody');
 const rows = [...tableBody.querySelectorAll('tr')];
-const tableTitle = ['Name', 'Position', 'Age', 'Salary'];
 
 table.addEventListener('click', event => {
   const title = event.target.closest('th');
 
-  const sortBy = title.textContent;
-
   rows.sort((a, b) => {
-    const index = tableTitle.indexOf(sortBy);
+    const index = title.cellIndex;
     const prevItem = a.children[index].textContent;
     const nextItem = b.children[index].textContent;
 
     if (prevItem.includes('$')) {
-      return parseFloat(prevItem.replace(',', '.').replace('$', ''))
-        - parseFloat(nextItem.replace(',', '.').replace('$', ''));
+      return parseFloat(prevItem.replace('$', ''))
+        - parseFloat(nextItem.replace('$', ''));
     }
 
     if (+prevItem.typeOf !== 'number') {
