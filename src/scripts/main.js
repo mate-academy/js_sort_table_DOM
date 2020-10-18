87defba6fea36d7d5d6e8826cd92b4fc5500eb48
VoduDay <andriy.maniak@gmail.com>
2020-10-18 18:53:07 +0300

    sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableHead = table.tHead;
+const tableBody = table.tBodies[0];
+
+tableHead.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName !== 'TH') {
+    return;
+  }
+
+  const indexOfCellToSort = target.cellIndex;
+  const tableRows = [ ...tableBody.rows ];
+
+  sortTable(tableRows, indexOfCellToSort);
+});
+
+function sortTable(list, indexOfCellToSort) {
+  list.sort((previous, current) => {
+    let previousContent = previous.cells[indexOfCellToSort].textContent;
+    let currentContent = current.cells[indexOfCellToSort].textContent;
+
+    switch (indexOfCellToSort) {
+      case 2:
+        previousContent = parseInt(previousContent);
+        currentContent = parseInt(currentContent);
+
+        return previousContent - currentContent;
+      case 3:
+        previousContent = parseInt(
+          previousContent.slice(1).split(',').join('')
+        );
+
+        currentContent = parseInt(
+          currentContent.slice(1).split(',').join('')
+        );
+
+        return previousContent - currentContent;
+    }
+
+    return previousContent.localeCompare(currentContent);
+  });
+
+  tableBody.append(...list);
+}
