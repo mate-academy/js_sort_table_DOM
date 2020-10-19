547c8d05d06a46cfc77de88adc82e8ea4657210e
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-19 17:51:14 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', function(event) {
+  const index = event.target.cellIndex;
+
+  const sortedInfo = [...tbody.children].sort((current, next) => {
+    const convertCurrent = current.cells[index].innerText.replace(/[$,]/g, '');
+    const convertNext = next.cells[index].innerText.replace(/[$,]/g, '');
+
+    if (isNaN(convertCurrent)) {
+      return convertCurrent.localeCompare(convertNext);
+    }
+
+    return convertCurrent - convertNext;
+  });
+
+  tbody.append(...sortedInfo);
+});
