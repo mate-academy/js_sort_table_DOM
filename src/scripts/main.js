0323f1daa570729977432660421997f45424f378
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-19 17:30:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const rows = [...table.tBodies[0].rows];
+
+table.addEventListener('click', event => {
+  const sortBy = event.target.textContent;
+  const index = event.target.cellIndex;
+
+  sortTable(index, sortBy);
+});
+
+function sortTable(index, sortBy) {
+  const sortedRows = rows.sort((a, b) => {
+    let firstRow = a.cells[index].textContent;
+    let secondRow = b.cells[index].textContent;
+
+    if (sortBy === 'Salary') {
+      firstRow = firstRow.replace(/[$,]/g, '');
+      secondRow = secondRow.replace(/[$,]/g, '');
+
+      return firstRow - secondRow;
+    }
+
+    return firstRow.localeCompare(secondRow, { numeric: true });
+  });
+
+  for (const row of sortedRows) {
+    table.tBodies[0].append(row);
+  }
+}
