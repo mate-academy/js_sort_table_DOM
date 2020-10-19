2e18766e0e1f5fa9675fe3d09930fc2867611c5d
milla-romankova <milla-romankova@gmail.com>
2020-10-19 20:13:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const titles = [...table.rows[0].cells];
+const tableMain = document.querySelector('tbody');
+const rows = [...tableMain.rows];
+
+function convertToNumber(string) {
+  return Number(string.replace(/[^\d]/g, ''));
+}
+
+table.addEventListener('click', event => {
+  const index = [...titles].indexOf(event.target);
+
+  rows.sort((a, b) => {
+    const prevRow = a.cells[index].textContent;
+    const nextRow = b.cells[index].textContent;
+
+    if (prevRow.includes('$')) {
+      return convertToNumber(prevRow) - convertToNumber(nextRow);
+    } else if (Number(prevRow)) {
+      return Number(prevRow) - Number(nextRow);
+    } else {
+      return prevRow.localeCompare(nextRow);
+    }
+  });
+
+  table.append(...rows);
+});
