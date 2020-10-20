732f85ec4e1699abdd40adc439377d14c5891e51
Liza <elizabethhoncharenko@gmail.com>
2020-10-20 16:37:01 +0300

    added function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,14 @@
 
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
-const data = [...tbody.children];
+const data = [...tbody.rows];
 
 thead.addEventListener('click', (event) => {
   const cellIndex = event.target.cellIndex;
 
   const sortedData = data.sort((prev, cur) => {
-    const firstData = prev.cells[cellIndex].textContent.replace(/[,$]/g, '');
-    const secondData = cur.cells[cellIndex].textContent.replace(/[,$]/g, '');
+    const firstData = fixedData(prev);
+    const secondData = fixedData(cur);
 
     if (+firstData) {
       return firstData - secondData;
@@ -17,6 +17,10 @@ thead.addEventListener('click', (event) => {
 
     return firstData.localeCompare(secondData);
   });
+
+  function fixedData(dataString) {
+    return dataString.cells[cellIndex].textContent.replace(/[,$]/g, '');
+  }
 
   tbody.append(...sortedData);
 });
