a0d864e687412919dd215ba15f06f66bcbd47757
Yuriy Putsan <putsan57@gmail.com>
2020-10-20 07:46:05 +0300

    code optimization & refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,26 @@
 'use strict';
 
 const table = document.querySelector('table');
+const tHead = table.querySelector('thead');
 const tBodyRows = [ ...table.tBodies[0].rows ];
 
 function takeDigit(string) {
-  return string.match(/[0-9]/g).join('');
+  return string.length ? string.match(/[0-9]/g).join('') : string;
 }
 
-table.addEventListener('click', event => {
-  if (event.target.parentNode.parentNode.tagName !== 'THEAD') {
-    return;
-  }
-
-  const targetCollumn = event.target.textContent;
-  let haveChildren = table.children[1].children.length;
-
+tHead.addEventListener('click', event => {
   tBodyRows.sort((a, b) => {
     const rowA = event.target.orderASC ? b : a;
     const rowB = event.target.orderASC ? a : b;
+    const columnIndex = event.target.cellIndex;
+    const dataA = rowA.cells[columnIndex].textContent;
+    const dataB = rowB.cells[columnIndex].textContent;
 
-    switch (targetCollumn) {
-      case 'Name':
-        return rowA.cells[0].textContent
-          .localeCompare(rowB.cells[0].textContent);
-
-      case 'Position':
-        return rowA.cells[1].textContent
-          .localeCompare(rowB.cells[1].textContent);
-
-      case 'Age':
-        return rowA.cells[2].textContent - rowB.cells[2].textContent;
-
-      case 'Salary':
-        return takeDigit(rowA.cells[3].innerText)
-        - takeDigit(rowB.cells[3].innerText);
-    }
+    return Number.isInteger(a)
+      ? dataA.localeCompare(dataB)
+      : takeDigit(dataA) - takeDigit(dataB);
   });
 
-  while (haveChildren) {
-    table.children[1].children[haveChildren - 1].remove();
-    haveChildren--;
-  };
-
-  tBodyRows.forEach(row => (
-    table.children[1].insertAdjacentHTML('beforeend', row.innerHTML)
-  ));
+  tBodyRows.forEach(row => table.tBodies[0].append(row));
   event.target.orderASC = !event.target.orderASC;
 });
