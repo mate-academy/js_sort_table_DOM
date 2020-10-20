68db6755a2a86ee5c0c8365b355856cbd48ae387
Liza <elizabethhoncharenko@gmail.com>
2020-10-20 15:39:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const data = [...tbody.children];
+
+thead.addEventListener('click', (event) => {
+  const cellIndex = event.target.cellIndex;
+
+  const sortedData = data.sort((prev, cur) => {
+    const firstData = prev.cells[cellIndex].textContent.replace(/[,$]/g, '');
+    const secondData = cur.cells[cellIndex].textContent.replace(/[,$]/g, '');
+
+    if (+firstData) {
+      return firstData - secondData;
+    }
+
+    return firstData.localeCompare(secondData);
+  });
+
+  tbody.append(...sortedData);
+});
