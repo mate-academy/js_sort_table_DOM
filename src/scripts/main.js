0ad9aac7264b43e4d24efcc69d751058f9724497
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-20 15:36:53 +0300

    fixed names var

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const table = document.querySelector('table');
 const head = document.querySelector('thead');
-const row = [...table.tHead.rows[0].children];
+const tableHeaders = [...table.tHead.rows[0].children];
 
 function toNumber(string) {
   const searchNumber = string.match(/[0-9]/g);
@@ -17,20 +17,22 @@ function toNumber(string) {
 head.addEventListener('click', event => {
   const target = event.target;
 
-  if (row.includes(target)) {
-    const findIndex = target.cellIndex;
-
-    const people = table.tBodies[0].rows;
-
-    const sortedPeople = [...people].sort((a, b) => {
-      const prev = a.cells[findIndex].textContent;
-      const next = b.cells[findIndex].textContent;
-
-      return sorted[typeof toNumber(prev)](prev, next);
-    });
-
-    table.tBodies[0].append(...sortedPeople);
+  if (!tableHeaders.includes(target)) {
+    return;
   }
+
+  const columnIndex = target.cellIndex;
+
+  const people = table.tBodies[0].rows;
+
+  const sortedPeople = [...people].sort((a, b) => {
+    const prev = a.cells[columnIndex].textContent;
+    const next = b.cells[columnIndex].textContent;
+
+    return sorted[typeof toNumber(prev)](prev, next);
+  });
+
+  table.tBodies[0].append(...sortedPeople);
 });
 
 const sorted = {
