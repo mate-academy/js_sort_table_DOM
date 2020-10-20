ba4c7eebc54378009ffc4b01e84eed081db92b83
Evgeniy <more.inspirationn@gmail.com>
2020-10-20 11:54:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const people = [...tbody.children];
+
+thead.addEventListener('click', ({ target }) => {
+  const targetIndex = target.cellIndex;
+
+  const peopleSorted = people.sort((a, b) => {
+    const firstElement = a.cells[targetIndex].textContent.replace(/[,$]/g, '');
+    const secondElement = b.cells[targetIndex].textContent.replace(/[,$]/g, '');
+
+    if (!isNaN(+firstElement)) {
+      return firstElement - secondElement;
+    }
+
+    return firstElement.localeCompare(secondElement);
+  });
+
+  tbody.append(...peopleSorted);
+});
