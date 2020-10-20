4d3645c19337fdf23e8ded9e48250724bf14e123
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-20 18:49:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const people = [...document.querySelectorAll('tbody tr')];
+const titleList = [...document.querySelectorAll(`thead th`)];
+
+function sortType(index) {
+  return function(a, b) {
+    let itemA = a.children[index]
+      .innerHTML
+      .replace(/[$,]/g, '');
+
+    let itemB = b.children[index]
+      .innerHTML
+      .replace(/[$,]/g, '');
+
+    if (!isNaN(itemA)) {
+      itemA = +itemA;
+      itemB = +itemB;
+    };
+
+    return itemA > itemB ? 1 : -1;
+  };
+};
+
+for (const title of titleList) {
+  title.onclick = () => {
+    people.sort(sortType(titleList.indexOf(title)));
+    document.querySelector('tbody').append(...people);
+  };
+};
