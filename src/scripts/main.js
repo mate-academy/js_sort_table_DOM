2fa81356deff3654e9a8b050e20278924fc65f63
andlap24 <andrew.laptev24@gmail.com>
2020-10-20 14:35:15 +0300

    solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,50 +1,26 @@
 'use strict';
 
 const table = document.querySelector('table');
-const tableHead = [...table.rows[0].cells];
 const tableRows = [...table.tBodies[0].rows];
 
-table.addEventListener('click', (event) => {
-  if (tableHead.includes(event.target)) {
-    const sortString = (a, b) => {
-      return a.localeCompare(b);
-    };
+table.tHead.addEventListener('click', ({ target }) => {
+  const index = target.cellIndex;
 
-    const sortAge = (a, b) => {
-      return +a - +b;
-    };
+  tableRows.sort((a, b) => {
+    const columnA = a.children[index].textContent;
+    const columnB = b.children[index].textContent;
 
-    const sortSalary = (a, b) => {
-      return +(a.replace(/[\s.,$]/g, '')) - +(b.replace(/[\s.,$]/g, ''));
-    };
-
-    const selectedRow = event.target.textContent;
-
-    if (selectedRow === 'Name' || selectedRow === 'Position') {
-      return sortTable(event.target, sortString);
+    if (columnA.includes('$')) {
+      return +(columnA.replace(/[\s.,$]/g, ''))
+      - +(columnB.replace(/[\s.,$]/g, ''));
     }
 
-    if (selectedRow === 'Age') {
-      return sortTable(event.target, sortAge);
+    if (isNaN(+columnA)) {
+      return columnA.localeCompare(columnB);
     }
 
-    if (selectedRow === 'Salary') {
-      return sortTable(event.target, sortSalary);
-    }
-  }
-});
-
-function sortTable(columnHeader, compareFunction) {
-  const index = tableHead.indexOf(columnHeader);
-
-  const sortedRows = tableRows.sort((a, b) => {
-    const columnA = a.querySelectorAll('td')[index].textContent;
-    const columnB = b.querySelectorAll('td')[index].textContent;
-
-    return compareFunction(columnA, columnB);
+    return +columnA - +columnB;
   });
 
-  for (const row of sortedRows) {
-    table.tBodies[0].append(row);
-  }
-}
+  table.tBodies[0].append(...tableRows);
+});
