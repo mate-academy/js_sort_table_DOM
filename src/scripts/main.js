af1f946188040bc03b6e9c481fd6dc809368d0ad
Olena kuzii <okuziy@gmail.com>
2020-10-21 00:08:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.rows];
+
+function getReplace(data, i) {
+  return data.cells[i].innerText.replace(/[$,]/g, '');
+}
+
+thead.addEventListener('click', event => {
+  const index = event.target.cellIndex;
+
+  const sortedRows = rows.sort((a, b) => (
+    (typeof getReplace(a, index) === 'string')
+      ? getReplace(a, index).localeCompare(getReplace(b, index))
+      : getReplace(a, index) - getReplace(b, index)
+  ));
+
+  tbody.append(...sortedRows);
+});
