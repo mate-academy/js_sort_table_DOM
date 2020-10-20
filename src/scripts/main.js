fd0fc68b0c8eca9d2b3d25f5797cc30de67f8ad7
Yurii <yurastt1@gmail.com>
2020-10-20 21:52:21 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tablePeople = document.querySelector('tbody');
+
+tableHead.addEventListener('click', (event) => {
+  const target = event.target.cellIndex;
+
+  const sortedInfo = [...tablePeople.children].sort((a, b) => {
+    const firstElement = convertText(a);
+    const secondElement = convertText(b);
+
+    if (isNaN(firstElement)) {
+      return firstElement.localeCompare(secondElement);
+    }
+
+    return firstElement - secondElement;
+  });
+
+  function convertText(tr) {
+    return tr.cells[target].innerText.replace(/[$,]/g, '');
+  }
+
+  tablePeople.append(...sortedInfo);
+});
