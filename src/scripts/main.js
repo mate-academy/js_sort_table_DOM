86b2a88ce21dee0df59e5e4c68d738de74151edd
Dmitry Morozov <10mdv11@gmail.com>
2020-10-21 16:32:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+tableHead.addEventListener('click', e => {
+  const rows = [...tableBody.rows];
+  const target = e.target;
+  const index = target.cellIndex;
+
+  rows.sort((prev, curr) => {
+    let prevText = prev.cells[index].textContent;
+    let currText = curr.cells[index].textContent;
+
+    if (prevText[0] === '$') {
+      prevText = prevText.slice(1).split(',').join('');
+      currText = currText.slice(1).split(',').join('');
+    }
+
+    if (isNaN(prevText)) {
+      return prevText.localeCompare(currText);
+    }
+
+    return prevText - currText;
+  });
+
+  tableBody.append(...rows);
+});
