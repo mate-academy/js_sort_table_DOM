0aecded25b9be37d29ae22d3f06b2d0ff943852d
Eugene <evolokhin@gmail.com>
2020-10-21 10:35:19 +0300

    add cellIndex, make destructure event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,15 @@
 // write code here
 
 const thead = document.querySelector('thead');
-const headItems = thead.querySelectorAll('th');
 const tbody = document.querySelector('tbody');
 const tableItems = document.querySelector('tbody').querySelectorAll('tr');
 
-thead.addEventListener('click', (e) => {
-  const columnName = e.target.innerText;
-  const index = findIndex(columnName);
+thead.addEventListener('click', ({ target }) => {
+  const index = target.cellIndex;
   const sorted = sortData(tableItems, index);
 
   tbody.append(...sorted);
 });
-
-function findIndex(data) {
-  for (let i = 0; i < headItems.length; i++) {
-    if (headItems[i].innerText === data) {
-      return i;
-    }
-  };
-};
 
 function normalizeNum(input, i) {
   return input.cells[i].textContent.replace(/[,$]/g, '');
