e4513e0dce310fa985d5eecd25753eb74138bf75
dmitry <akulinind@gmail.com>
2020-10-24 14:31:01 +0300

    add number sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const tHeader = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+tHeader.addEventListener('click', (event) => {
+  const rows = [...tBody.rows];
+  const caption = event.target;
+  const columnIndex = caption.cellIndex;
+
+  rows.sort((prev, curr) => {
+    const prevText = prev.cells[columnIndex].textContent;
+    const currText = curr.cells[columnIndex].textContent;
+
+    return +prevText - +currText;
+  });
+
+  tBody.append(...rows);
+});
