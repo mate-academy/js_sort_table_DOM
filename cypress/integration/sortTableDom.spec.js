6939571a217c857f2173a579e56438c4ac563331
y.kostenko <y.kostenko@mate.academy>
2020-11-20 20:31:05 +0200

    small changes to readme and test

--- cypress/integration/sortTableDom.spec.js
+++ cypress/integration/sortTableDom.spec.js
@@ -1,34 +1,36 @@
-/* eslint-disable strict */
-/* eslint-disable max-len */
-///<reference types="Cypress"/>
+'use strict';
 
 describe('Sorting table app', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
-  it(`should sort in ASC order, by clicking on 'Name' header`, () => {
+  it('should sort in ASC order, by clicking on `Name` header', () => {
     cy.contains('Name').click();
     cy.get('tbody tr:nth-child(1) td:nth-child(1)').contains('Airi Satou');
     cy.get('tbody tr:nth-child(2) td:nth-child(1)').contains('Colleen Hurst');
     cy.get('tbody tr:nth-child(12) td:nth-child(1)').contains('Zorita Serrano');
   });
 
-  it(`should sort in ASC order, by clicking on 'Position' header`, () => {
+  it('should sort in ASC order, by clicking on `Position` header', () => {
     cy.contains('Position').click();
     cy.get('tbody tr:nth-child(1) td:nth-child(2)').contains('Accountant');
-    cy.get('tbody tr:nth-child(2) td:nth-child(2)').contains('Data Coordinator');
-    cy.get('tbody tr:nth-child(12) td:nth-child(2)').contains('Technical Author');
+
+    cy.get('tbody tr:nth-child(2) td:nth-child(2)')
+      .contains('Data Coordinator');
+
+    cy.get('tbody tr:nth-child(12) td:nth-child(2)')
+      .contains('Technical Author');
   });
 
-  it(`should sort in ASC order, by clicking on 'Age' header`, () => {
+  it('should sort in ASC order, by clicking on `Age` header', () => {
     cy.contains('Age').click();
     cy.get('tbody tr:nth-child(1) td:nth-child(3)').contains('20');
     cy.get('tbody tr:nth-child(2) td:nth-child(3)').contains('27');
     cy.get('tbody tr:nth-child(12) td:nth-child(3)').contains('66');
   });
 
-  it(`should sort in ASC order, by clicking on 'Salary' header`, () => {
+  it('should sort in ASC order, by clicking on `Salary` header', () => {
     cy.contains('Salary').click();
     cy.get('tbody tr:nth-child(1) td:nth-child(4)').contains('$98,540');
     cy.get('tbody tr:nth-child(2) td:nth-child(4)').contains('$115,000');
