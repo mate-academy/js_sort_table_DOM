951dee40d23982e836f1b852835f6860b3998762
Dmitrii <dmitrii.zinchuk@dstarlab.com>
2020-11-20 15:18:27 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,24 @@
 'use strict';
 
-const ths = document.querySelectorAll('th');
-const table = document.querySelector('table');
+const ths = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
+let sort = 1;
 
-let sort = false;
+ths.addEventListener('click', (event) => {
+  const indexColumn = event.target.cellIndex;
 
-ths.forEach(th => th.addEventListener('click', event => {
-  const cellsTable = [...table.rows[0].cells];
-  let rowsAr = [];
-
-  for (let i = 0; i < cellsTable.length; i++) {
-    if (cellsTable[i] === event.currentTarget) {
-      if (sort) {
-        rowsAr = ([...tbody.rows].sort((rowA, rowB) => {
-          if (event.currentTarget === cellsTable[3]) {
-            return Number.parseFloat(rowA.cells[i].innerText.replace('$', ''))
-             - Number.parseFloat(rowB.cells[i].innerText.replace('$', ''));
-          } else {
-            return rowA.cells[i].innerText
-              .localeCompare(rowB.cells[i].innerText);
-          }
-        }));
-      } else {
-        rowsAr = ([...tbody.rows].sort((rowA, rowB) => {
-          if (event.currentTarget === cellsTable[3]) {
-            return Number.parseFloat(rowB.cells[i].innerText.replace('$', ''))
-             - Number.parseFloat(rowA.cells[i].innerText.replace('$', ''));
-          } else {
-            return rowB.cells[i].innerText
-              .localeCompare(rowA.cells[i].innerText);
-          }
-        }));
-      }
-      sort = !sort;
-      tbody.append(...rowsAr);
+  const sortedRows = [...tbody.rows].sort((a, b) => {
+    if (a.cells[indexColumn].innerText.startsWith('$')) {
+      return (a.cells[indexColumn].innerText.replace('$', '').replace(',', '')
+       - b.cells[indexColumn].innerText.replace('$', '').replace(',', ''))
+        * sort;
+    } else {
+      return a.cells[indexColumn].innerText
+        .localeCompare(b.cells[indexColumn].innerText)
+         * sort;
     }
-  }
-}));
+  });
+
+  sort *= -1;
+  tbody.append(...sortedRows);
+});
