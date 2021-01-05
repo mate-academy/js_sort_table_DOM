6b86d09b38625b4bf6a3c01afeb13b084ef9c0de
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-01-05 22:54:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBody = document.querySelector('tbody');
+const headCells = table.rows[0].children;
+
+table.tHead.addEventListener('click', action => {
+  const index = [...headCells].indexOf(action.target);
+  const newOrder = [...tBody.children];
+
+  newOrder.sort((a, b) => {
+    const contentA = a.children[index].textContent;
+    const contentB = b.children[index].textContent;
+
+    if (contentA[0] === '$') {
+      const numA = contentA.replace(/\D/g, '');
+      const numB = contentB.replace(/\D/g, '');
+
+      return numA - numB;
+    }
+
+    if (isNaN(contentA)) {
+      return contentA.localeCompare(contentB);
+    }
+
+    return +contentA - +contentB;
+  });
+
+  tBody.append(...newOrder);
+});
