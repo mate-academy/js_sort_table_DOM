6214f544481983e5c6dc4ed31da2f10f7b709272
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-05 17:53:51 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const theadRow = thead.querySelector('tr');
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.querySelectorAll('tr')];
+
+for (let i = 0; i < theadRow.children.length; i++) {
+  theadRow.children[i].onclick = () => {
+    if (isNaN(+(rows[0].children[i]
+      .textContent
+      .replace(/\D/, '')
+      .replace(',', '')))) {
+      rows.sort((a, b) => a.children[i]
+        .textContent
+        .localeCompare(b.children[i].textContent));
+      rows.forEach(row => tbody.append(row));
+    } else {
+      rows.sort((a, b) => {
+        return +(a.children[i].textContent
+          .replace(/\D/, '')
+          .replace(',', ''))
+            - +(b.children[i].textContent
+              .replace(/\D/, '')
+              .replace(',', ''));
+      });
+      rows.forEach(row => tbody.append(row));
+    }
+  };
+}
