bf5b46ef3e09cf1bd84a975fb86b3fe34f6e9196
Your Name <mogolgogol.90@gmail.com>
2021-01-06 12:29:21 +0200

    replaced mouseEvent with e

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,29 +14,29 @@ salary.classList.add('salary');
 
 const header = document.querySelector('thead > tr');
 
-header.addEventListener('click', (mouseEvent) => {
-  if (mouseEvent.target.classList.contains('full-name')) {
+header.addEventListener('click', (e) => {
+  if (e.target.classList.contains('full-name')) {
     const sortedByName = [...people]
       .sort((a, b) => a.cells[0].innerText.localeCompare(b.cells[0].innerText));
 
     document.querySelector('tbody').append(...sortedByName);
   }
 
-  if (mouseEvent.target.classList.contains('position')) {
+  if (e.target.classList.contains('position')) {
     const sortedByPosition = [...people]
       .sort((a, b) => a.cells[1].innerText.localeCompare(b.cells[1].innerText));
 
     document.querySelector('tbody').append(...sortedByPosition);
   }
 
-  if (mouseEvent.target.classList.contains('age')) {
+  if (e.target.classList.contains('age')) {
     const sortedByAge = [...people]
       .sort((a, b) => +a.cells[2].innerText - +b.cells[2].innerText);
 
     document.querySelector('tbody').append(...sortedByAge);
   }
 
-  if (mouseEvent.target.classList.contains('salary')) {
+  if (e.target.classList.contains('salary')) {
     const sortedBySalary = [...people].sort((a, b) => +(a.cells[3].innerText)
       .replace(/\$/, '').replace(/,/, '') - +(b.cells[3].innerText)
       .replace(/\$/, '').replace(/,/, ''));
