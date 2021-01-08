2569f8aa1685b6bd3b40574942b0f22304c7ce95
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-08 16:27:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,67 @@
 'use strict';
 
-// write code here
+const clickArea = document.querySelector('thead');
+const dataTD = [...document.querySelectorAll('td')];
+const dataArray = [];
+const allTagsTD = dataTD.slice();
+
+function createData() {
+  const parseData = () => {
+    return allTagsTD.splice(0, 1)[0].innerText;
+  };
+
+  while (allTagsTD.length > 0) {
+    const person = {};
+
+    person.name = parseData();
+    person.position = parseData();
+    person.age = +parseData();
+    // person.salary = parseData();
+    person.salary = +parseData().match(/\d/g).join('');
+    dataArray.push(person);
+  }
+}
+
+function rebuild() {
+  const result = [];
+
+  for (let i = 0; i < dataArray.length; i++) {
+    for (const key in dataArray[i]) {
+      if (key === 'salary') {
+        result.push(`$${dataArray[i][key].toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`);
+      } else {
+        result.push(dataArray[i][key]);
+      }
+    }
+  }
+
+  for (let i = 0; i < dataTD.length; i++) {
+    dataTD[i].innerText = result[i];
+  }
+}
+
+function userSort(param = 'name') {
+  createData();
+
+  dataArray.sort((a, b) => {
+    if (a[param] > b[param]) {
+      return 1;
+    } else if (a[param] < b[param]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  rebuild();
+}
+
+clickArea.addEventListener('click', (e) => {
+  const typeSort = e.target.innerText.toLowerCase();
+
+  if (!typeSort) {
+    return;
+  }
+  userSort(typeSort);
+});
