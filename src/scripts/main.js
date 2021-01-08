b13767fe7e9908986c2b55f78fd509d8f0fce348
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-01-09 00:34:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const heads = document.querySelector('thead tr');
+const list = [...document.querySelectorAll('tbody tr')];
+const names = [...document.querySelectorAll('thead tr th')]
+  .map(cell => cell.innerText);
+
+function ascending(e) {
+  const i = names.indexOf(e.target.innerText);
+
+  if (i === 3) {
+    list.sort((a, b) =>
+      +a.children[i].innerText.replace('$', '').replace(',', '')
+    - +b.children[i].innerText.replace('$', '').replace(',', ''));
+  } else {
+    list.sort((a, b) =>
+      a.children[i].innerText.localeCompare(b.children[i].innerText));
+  }
+
+  list.forEach(row => document.querySelector('tbody').append(row));
+}
+
+heads.addEventListener('click', ascending);
