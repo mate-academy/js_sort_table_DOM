e1866c7a9116f898765328e32f1826e0614bcc35
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-09 15:48:47 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write code here
+const tableHeaderItems = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const rows = [...document.querySelectorAll('tbody > tr')];
+
+tableHeaderItems.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  if (index === 3) {
+    rows.map(item => item.setAttribute('data-sort',
+      `${convertToNumber(item.children[index].innerText)}`));
+
+    rows.sort((value1, value2) => {
+      return value1.dataset.sort - value2.dataset.sort;
+    });
+  } else {
+    rows.map(item => item.setAttribute('data-sort',
+      `${item.children[index].innerText}`));
+
+    rows.sort((value1, value2) => {
+      return (value1.dataset.sort > value2.dataset.sort) ? 1 : -1;
+    });
+  }
+
+  tableBody.append(...rows);
+});
+
+function convertToNumber(string) {
+  return +string.split('$').join('').split(',').join('');
+}
