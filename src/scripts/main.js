24f34313c2408372abbb373681c36ef06b1759fa
vovador <vovadorko@gmail.com>
2021-01-10 23:02:34 +0200

    The code was refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,44 +4,41 @@ function MoneyStringToNumber(string) {
   return +string.slice(1).split(',').join('');
 }
 
-const peopleDataRows = document.querySelectorAll('tbody > tr');
 const tableHead = document.querySelector('thead');
 const tableBody = document.querySelector('tbody');
-const peopleSortedByName = [...peopleDataRows].sort(
-  (prevPerson, curPerson) => {
-    return prevPerson.children[0].textContent.localeCompare(
-      curPerson.children[0].textContent);
-  });
-const peopleSortedByPosition = [...peopleDataRows].sort(
-  (prevPerson, curPerson) => {
-    return prevPerson.children[1].textContent.localeCompare(
-      curPerson.children[1].textContent);
-  });
-const peopleSortedByAge = [...peopleDataRows].sort(
-  (prevPerson, curPerson) => {
-    return +prevPerson.children[2].textContent
-    - +curPerson.children[2].textContent;
-  });
-const peopleSortedBySalary = [...peopleDataRows].sort(
-  (prevPerson, curPerson) => {
-    return MoneyStringToNumber(prevPerson.children[3].textContent)
-    - MoneyStringToNumber(curPerson.children[3].textContent);
-  });
 
 tableHead.addEventListener('click', (e) => {
+  const peopleDataRows = document.querySelectorAll('tbody > tr');
+
   if (e.target.tagName === 'TH') {
     switch (e.target.innerText) {
       case 'Name' :
-        tableBody.append(...peopleSortedByName);
+        tableBody.append(...[...peopleDataRows].sort(
+          (prevPerson, curPerson) => {
+            return prevPerson.children[0].textContent.localeCompare(
+              curPerson.children[0].textContent);
+          }));
         break;
       case 'Position' :
-        tableBody.append(...peopleSortedByPosition);
+        tableBody.append(...[...peopleDataRows].sort(
+          (prevPerson, curPerson) => {
+            return prevPerson.children[1].textContent.localeCompare(
+              curPerson.children[1].textContent);
+          }));
         break;
       case 'Age' :
-        tableBody.append(...peopleSortedByAge);
+        tableBody.append(...[...peopleDataRows].sort(
+          (prevPerson, curPerson) => {
+            return +prevPerson.children[2].textContent
+            - +curPerson.children[2].textContent;
+          }));
         break;
       case 'Salary' :
-        tableBody.append(...peopleSortedBySalary);
+        tableBody.append(...[...peopleDataRows].sort(
+          (prevPerson, curPerson) => {
+            return MoneyStringToNumber(prevPerson.children[3].textContent)
+            - MoneyStringToNumber(curPerson.children[3].textContent);
+          }));
         break;
     }
   }
