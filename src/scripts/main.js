d2503eaa1a25f879a21ebe6effe7af63be873131
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-10 21:33:29 +0200

    Solution improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,63 +8,49 @@ headers.forEach(item => {
 });
 
 function sorted(ev) {
+  let sorter = null;
+
   if (ev.target.innerText === 'Name') {
-    const sortedElements = [...list].sort((a, b) => {
+    sorter = (a, b) => {
       const prev = a.children[0].textContent;
       const next = b.children[0].textContent;
 
       return prev.localeCompare(next);
-    });
-
-    list.forEach(x => x.remove());
-
-    const tbody = document.querySelector('tbody');
-
-    sortedElements.forEach(x => tbody.append(x));
+    };
   }
 
   if (ev.target.innerText === 'Position') {
-    const sortedElements = [...list].sort((a, b) => {
+    sorter = (a, b) => {
       const prev = a.children[1].textContent;
       const next = b.children[1].textContent;
 
       return prev.localeCompare(next);
-    });
-
-    list.forEach(x => x.remove());
-
-    const tbody = document.querySelector('tbody');
-
-    sortedElements.forEach(x => tbody.append(x));
+    };
   }
 
   if (ev.target.innerText === 'Age') {
-    const sortedElements = [...list].sort((a, b) => {
+    sorter = (a, b) => {
       const prev = parseFloat(a.children[2].textContent);
       const next = parseFloat(b.children[2].textContent);
 
-      return next - prev;
-    });
-
-    list.forEach(x => x.remove());
-
-    const tbody = document.querySelector('tbody');
-
-    sortedElements.forEach(x => tbody.append(x));
+      return prev - next;
+    };
   }
 
   if (ev.target.innerText === 'Salary') {
-    const sortedElements = [...list].sort((a, b) => {
+    sorter = (a, b) => {
       const prev = parseFloat(a.children[3].textContent.replace(/\D/, ''));
       const next = parseFloat(b.children[3].textContent.replace(/\D/, ''));
 
-      return next - prev;
-    });
-
-    list.forEach(x => x.remove());
-
-    const tbody = document.querySelector('tbody');
-
-    sortedElements.forEach(x => tbody.append(x));
+      return prev - next;
+    };
   }
+
+  list.forEach(x => x.remove());
+
+  const tbody = document.querySelector('tbody');
+
+  const sortedElements = [...list].sort(sorter);
+
+  sortedElements.forEach(x => tbody.append(x));
 }
