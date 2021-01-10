d4ea34c2a0238fc35d60bc42d39947c123127741
Your Name <mogolgogol.90@gmail.com>
2021-01-10 15:44:28 +0200

    modified sort method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,31 @@
 'use strict';
 
-// write code here
-
 const people = document.querySelector('tbody').children;
-
-const titles = document.querySelectorAll('thead > tr > th');
-const [fullName, position, age, salary] = titles;
-
-fullName.classList.add('full-name');
-position.classList.add('position');
-age.classList.add('age');
-salary.classList.add('salary');
-
 const header = document.querySelector('thead > tr');
+let columnIndex = 0;
 
 header.addEventListener('click', (e) => {
-  if (e.target.classList.contains('full-name')) {
-    const sortedByName = [...people]
-      .sort((a, b) => a.cells[0].innerText.localeCompare(b.cells[0].innerText));
+  const sorted = [...people].sort((a, b) => {
+    columnIndex = e.target.cellIndex;
 
-    document.querySelector('tbody').append(...sortedByName);
-  }
+    if (isNaN(Number(a.cells[columnIndex].innerText))
+    && !a.cells[columnIndex].innerText.startsWith('$')) {
+      return (
+        a.cells[columnIndex].innerText
+          .localeCompare(b.cells[columnIndex].innerText)
+      );
+    }
 
-  if (e.target.classList.contains('position')) {
-    const sortedByPosition = [...people]
-      .sort((a, b) => a.cells[1].innerText.localeCompare(b.cells[1].innerText));
+    if (a.cells[columnIndex].innerText.startsWith('$')) {
+      return +(a.cells[columnIndex].innerText)
+        .replace(/\$/, '').replace(/,/, '') - +(b.cells[columnIndex].innerText)
+        .replace(/\$/, '').replace(/,/, '');
+    }
 
-    document.querySelector('tbody').append(...sortedByPosition);
-  }
+    if (!isNaN(Number(a.cells[columnIndex].innerText))) {
+      return +a.cells[columnIndex].innerText - +b.cells[columnIndex].innerText;
+    }
+  });
 
-  if (e.target.classList.contains('age')) {
-    const sortedByAge = [...people]
-      .sort((a, b) => +a.cells[2].innerText - +b.cells[2].innerText);
-
-    document.querySelector('tbody').append(...sortedByAge);
-  }
-
-  if (e.target.classList.contains('salary')) {
-    const sortedBySalary = [...people].sort((a, b) => +(a.cells[3].innerText)
-      .replace(/\$/, '').replace(/,/, '') - +(b.cells[3].innerText)
-      .replace(/\$/, '').replace(/,/, ''));
-
-    document.querySelector('tbody').append(...sortedBySalary);
-  }
+  document.querySelector('tbody').append(...sorted);
 });
