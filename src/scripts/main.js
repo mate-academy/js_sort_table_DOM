055c538fa53dd539167aa4c1851f5711e6223b78
DarMatt <daryamatviienko@gmail.com>
2021-01-11 20:30:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const title = document.querySelector('thead');
+const el = [...document.querySelectorAll('tbody tr')];
+const body = document.querySelector('tbody');
+
+title.addEventListener('click', (e) => {
+  const indexCol = e.target.cellIndex;
+
+  el.sort((a, b) => {
+    let first = a.children[indexCol].textContent;
+    let second = b.children[indexCol].textContent;
+
+    if (first.includes('$')) {
+      first = parseFloat(first.replace(/\D/, ''));
+      second = parseFloat(second.replace(/\D/, ''));
+    }
+
+    return isNaN(first) ? first.localeCompare(second) : first - second;
+  });
+
+  body.append(...el);
+});
