7db3f1c9533ca82b7bd9eb53e450d2ebe84346dd
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-11 17:38:38 +0200

    Rewrited with switch case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,46 +4,48 @@ const list = document.querySelectorAll('tbody > tr');
 const headers = document.querySelectorAll('thead > tr > th');
 
 headers.forEach(item => {
-  item.addEventListener('click', sorted);
+  item.addEventListener('click', eventListener);
 });
 
-function sorted(ev) {
+function eventListener(ev) {
   let sorter = null;
 
-  if (ev.target.innerText === 'Name') {
-    sorter = (a, b) => {
-      const prev = a.children[0].textContent;
-      const next = b.children[0].textContent;
+  switch (ev.target.innerText) {
+    case 'Name':
+      sorter = (a, b) => {
+        const prev = a.children[0].textContent;
+        const next = b.children[0].textContent;
 
-      return prev.localeCompare(next);
-    };
-  }
+        return prev.localeCompare(next);
+      };
+      break;
 
-  if (ev.target.innerText === 'Position') {
-    sorter = (a, b) => {
-      const prev = a.children[1].textContent;
-      const next = b.children[1].textContent;
+    case 'Position':
+      sorter = (a, b) => {
+        const prev = a.children[1].textContent;
+        const next = b.children[1].textContent;
 
-      return prev.localeCompare(next);
-    };
-  }
+        return prev.localeCompare(next);
+      };
+      break;
 
-  if (ev.target.innerText === 'Age') {
-    sorter = (a, b) => {
-      const prev = parseFloat(a.children[2].textContent);
-      const next = parseFloat(b.children[2].textContent);
+    case 'Age':
+      sorter = (a, b) => {
+        const prev = parseFloat(a.children[2].textContent);
+        const next = parseFloat(b.children[2].textContent);
 
-      return prev - next;
-    };
-  }
+        return prev - next;
+      };
+      break;
 
-  if (ev.target.innerText === 'Salary') {
-    sorter = (a, b) => {
-      const prev = parseFloat(a.children[3].textContent.replace(/\D/, ''));
-      const next = parseFloat(b.children[3].textContent.replace(/\D/, ''));
+    case 'Salary':
+      sorter = (a, b) => {
+        const prev = parseFloat(a.children[3].textContent.replace(/\D/, ''));
+        const next = parseFloat(b.children[3].textContent.replace(/\D/, ''));
 
-      return prev - next;
-    };
+        return prev - next;
+      };
+      break;
   }
 
   list.forEach(x => x.remove());
