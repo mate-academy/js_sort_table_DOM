ca4b425dc9151c8b22efbf66c5e0fd8c801321ed
Layrovell <marina95prodan@gmail.com>
2021-01-11 18:37:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thItems = document.querySelectorAll('th');
+const trItems = tbody.querySelectorAll('tr');
+
+function toNum(str) {
+  return +str.replace(/[$,]/g, '');
+};
+
+[...thItems].map((el, i) => {
+  el.addEventListener('click', () => {
+    const sorted = [...trItems].sort((prev, curr) => {
+      const a = prev.children[i].innerText;
+      const b = curr.children[i].innerText;
+
+      if (!isNaN(toNum(a))) {
+        return toNum(a) - toNum(b);
+      } else {
+        return a.localeCompare(b);
+      }
+    });
+
+    tbody.append(...sorted);
+  });
+});
