b047b79a436e42bbe5fe4c160017d3626b2e38e1
Layrovell <marina95prodan@gmail.com>
2021-01-13 11:44:17 +0200

    fixed after a code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,24 @@
 'use strict';
 
 const tbody = document.querySelector('tbody');
-const thItems = document.querySelectorAll('th');
+const thItems = document.querySelector('thead').children[0];
 const trItems = tbody.querySelectorAll('tr');
 
 function toNum(str) {
   return +str.replace(/[$,]/g, '');
-};
+}
 
-[...thItems].map((el, i) => {
-  el.addEventListener('click', () => {
-    const sorted = [...trItems].sort((prev, curr) => {
-      const a = prev.children[i].innerText;
-      const b = curr.children[i].innerText;
+thItems.addEventListener('click', (e) => {
+  const sorted = [...trItems].sort((prev, curr) => {
+    const a = prev.children[e.target.cellIndex].innerText;
+    const b = curr.children[e.target.cellIndex].innerText;
 
-      if (!isNaN(toNum(a))) {
-        return toNum(a) - toNum(b);
-      } else {
-        return a.localeCompare(b);
-      }
-    });
-
-    tbody.append(...sorted);
+    if (!isNaN(toNum(a))) {
+      return toNum(a) - toNum(b);
+    } else {
+      return a.localeCompare(b);
+    }
   });
+
+  tbody.append(...sorted);
 });
