35fd29dde320a9a8ebc5a9c6f5a32d95d35daedb
Bohdan Dobak <chudolipton@gmail.com>
2021-02-27 23:00:36 +0200

    made task sort table by using sort array method and forEach method in the end remove old rows and append new rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tbody = document.querySelector('tbody');
+const rows = tbody.querySelectorAll('tr');
+
+headers.forEach(header => header.addEventListener('click', sortCell));
+
+function converter(string) {
+  return string.replace('$', '').replace(',', '');
+}
+
+function sortCell(element) {
+  const targetIndex = element.target.cellIndex;
+  const newRows = [...rows];
+
+  newRows.sort((a, b) => (
+    targetIndex === 0 || targetIndex === 1
+      ? a.cells[targetIndex].innerText
+        .localeCompare(b.cells[targetIndex].innerText)
+      : converter(a.cells[targetIndex].innerText)
+      - converter(b.cells[targetIndex].innerText)
+  ));
+
+  rows.forEach(row => tbody.removeChild(row));
+
+  newRows.forEach(newRow => tbody.appendChild(newRow));
+}
