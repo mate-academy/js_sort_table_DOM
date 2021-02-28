989dfcfc73e873d0e1ff400a2fc41801f0b5d825
chri911 <delightsome@bigmir.net>
2021-02-28 12:07:48 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const table = document.querySelectorAll('table tbody tr');
+const tbody = document.querySelector('table tbody');
+const thead = document.querySelectorAll('table thead th');
+const rows = [...table];
+
+const convert = (string) => {
+  return Number(string.replace(/[$,]/g, ''));
+};
+
+thead.forEach((title, i) => title.addEventListener('click', () => {
+  const sorted = rows.sort(
+    (a, b) => {
+      return convert(rows[0].cells[i].innerText) > 0
+        ? convert(a.cells[i].innerText) - convert(b.cells[i].innerText)
+        : a.cells[i].innerText.localeCompare(b.cells[i].innerText);
+    });
+
+  tbody.append(...sorted);
+}));
