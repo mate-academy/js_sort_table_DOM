71e56b68fed83640f6d9e7e248f3d8fc4e3f0de1
chri911 <delightsome@bigmir.net>
2021-03-01 21:49:07 +0200

    solution with added delegation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,35 @@
 'use strict';
 
-const table = document.querySelectorAll('table tbody tr');
-const tbody = document.querySelector('table tbody');
-const thead = document.querySelectorAll('table thead th');
-const rows = [...table];
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.rows];
 
 const convert = (string) => {
   return Number(string.replace(/[$,]/g, ''));
 };
 
-thead.forEach((title, index) => title.addEventListener('click', () => {
-  const sorted = rows.sort(
-    (currentRow, nextRow) => {
+const sortColumn = (e) => {
+  const index = e.target.cellIndex;
+  let sorted;
+
+  if (Number.isNaN(convert(rows[0].cells[index].innerText))) {
+    sorted = rows.sort(
+      (currentRow, nextRow) => {
+        const currentValue = currentRow.cells[index].innerText;
+        const nextValue = nextRow.cells[index].innerText;
+
+        return currentValue.localeCompare(nextValue);
+      });
+  } else {
+    sorted = rows.sort((currentRow, nextRow) => {
       const currentValue = currentRow.cells[index].innerText;
       const nextValue = nextRow.cells[index].innerText;
 
-      if (Number.isNaN(convert(rows[0].cells[index].innerText))) {
-        return currentValue.localeCompare(nextValue);
-      }
-
       return convert(currentValue) - convert(nextValue);
     });
+  }
 
   tbody.append(...sorted);
-}));
+};
+
+thead.addEventListener('click', sortColumn);
