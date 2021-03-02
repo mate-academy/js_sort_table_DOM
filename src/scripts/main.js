d58f1eac23d4c38b40e855afe923fbca5f5e527c
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-02 15:11:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector('thead');
+
+tableHead.addEventListener('click', e => {
+  const column = e.target.closest('th');
+
+  if (!column) {
+    return;
+  }
+
+  const columnIndex = [...column.parentElement.children]
+    .findIndex(child => child === column);
+
+  const sortedRows = [...tableBody.children].sort((currentRow, nextRow) => {
+    const firstOperand = formatNumber(
+      currentRow.children[columnIndex].textContent
+    );
+    const secondOperand = formatNumber(
+      nextRow.children[columnIndex].textContent
+    );
+
+    if (firstOperand > secondOperand) {
+      return 1;
+    } else if (firstOperand < secondOperand) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  tableBody.append(...sortedRows);
+});
+
+function formatNumber(string) {
+  return /[$0-9.]/g.test(string[0])
+    ? +string.replace(/[^0-9.]/g, '')
+    : string;
+}
