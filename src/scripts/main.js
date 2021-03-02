d1cfd93a141af760e9fa73170557c0b84164f17e
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-02 18:05:40 +0200

    rewrited using switch construction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,29 +12,41 @@ tableHead.addEventListener('click', e => {
 
   const columnIndex = [...column.parentElement.children]
     .findIndex(child => child === column);
+  const columnName = column.textContent;
+  let sortedRows;
 
-  const sortedRows = [...tableBody.children].sort((currentRow, nextRow) => {
-    const firstOperand = formatNumber(
-      currentRow.children[columnIndex].textContent
-    );
-    const secondOperand = formatNumber(
-      nextRow.children[columnIndex].textContent
-    );
-
-    if (firstOperand > secondOperand) {
-      return 1;
-    } else if (firstOperand < secondOperand) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
+  switch (columnName) {
+    case 'Name':
+    case 'Position':
+      sortedRows = [...tableBody.children].sort((currentRow, nextRow) => {
+        return stringsCompare(
+          currentRow.children[columnIndex].textContent,
+          nextRow.children[columnIndex].textContent,
+        );
+      });
+      break;
+    case 'Age':
+    case 'Salary':
+      sortedRows = [...tableBody.children].sort((currentRow, nextRow) => {
+        return numbersCompare(
+          formatNumber(currentRow.children[columnIndex].textContent),
+          formatNumber(nextRow.children[columnIndex].textContent),
+        );
+      });
+      break;
+  }
 
   tableBody.append(...sortedRows);
 });
 
-function formatNumber(string) {
-  return /[$0-9.]/g.test(string[0])
-    ? +string.replace(/[^0-9.]/g, '')
-    : string;
+function formatNumber(number) {
+  return +number.replace(/[^0-9.]/g, '');
+}
+
+function stringsCompare(first, second) {
+  return first.localeCompare(second);
+}
+
+function numbersCompare(first, second) {
+  return first - second;
 }
