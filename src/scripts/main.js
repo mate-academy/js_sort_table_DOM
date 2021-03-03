33e662f4cd95035202e1fbb6cb0f212d295725f1
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-03 12:10:09 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write code here
+const head = document.querySelector('thead');
+const table = document.querySelector('tbody');
+
+head.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  const sortTableElement = [...table.rows].sort((current, next) => {
+    const currentValue = current.cells[index].innerHTML;
+    const nextValue = next.cells[index].innerHTML;
+
+    const numbersValue = +currentValue.replace(/[^0-9]/g, '');
+    const nextNumbersValue = +nextValue.replace(/[^0-9]/g, '');
+
+    return numbersValue
+      ? numbersValue - nextNumbersValue
+      : currentValue.localeCompare(nextValue);
+  });
+
+  table.append(...sortTableElement);
+});
