e7301f17f597c1d555fbd15f864f002049df536c
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-10 21:41:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = document.querySelectorAll('tbody > tr');
+const copyRows = [...rows];
+
+thead.addEventListener('click', (e) => {
+  const indexOfElement = e.target.cellIndex;
+
+  copyRows.sort((prew, next) => {
+    let firstElement = prew.children[indexOfElement].textContent;
+    let secondElement = next.children[indexOfElement].textContent;
+
+    if (firstElement.includes('$')) {
+      const regexp = new RegExp(/[^0-9]/g);
+
+      firstElement = firstElement.replace(regexp, '');
+      secondElement = secondElement.replace(regexp, '');
+    }
+
+    return isNaN(firstElement)
+      ? firstElement.localeCompare(secondElement)
+      : firstElement - secondElement;
+  });
+
+  tbody.append(...copyRows);
+});
