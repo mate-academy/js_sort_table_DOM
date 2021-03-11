7be9b133711a59f23e80eb78b6e951880926c502
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-11 20:17:51 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const rows = [...tbody.rows];
+
+function getSortedData(column) {
+  const index = column.cellIndex;
+  const contentOfTheFirstCell = rows[0].cells[index].textContent;
+
+  const sortedRows = rows.sort((rowA, rowB) => {
+    const rowFirst = rowA.cells[index].textContent;
+    const rowSecond = rowB.cells[index].textContent;
+
+    if (contentOfTheFirstCell.includes('$')) {
+      const valueFirst = Number(rowFirst
+        .split('$')
+        .join('')
+        .split(',')
+        .join('')
+      );
+      const valueSecond = Number(rowSecond
+        .split('$')
+        .join('')
+        .split(',')
+        .join('')
+      );
+
+      return valueFirst > valueSecond ? 1 : -1;
+    }
+
+    return rowFirst > rowSecond ? 1 : -1;
+  });
+
+  tbody.append(...sortedRows);
+}
+
+table.addEventListener('click', (clickHandler) => {
+  if (table.tHead.contains(clickHandler.target)) {
+    getSortedData(clickHandler.target);
+  }
+});
