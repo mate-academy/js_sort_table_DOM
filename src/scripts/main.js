f033e86bc73651eb62d89964fbd513cb7703b005
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-11 15:26:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+function sortByLetter(index) {
+  const rows = Array.from(tbody.rows).sort((rowA, rowB) => {
+    const firstRow = rowA.cells[index].innerHTML;
+    const secondRow = rowB.cells[index].innerHTML;
+
+    if (rowA.cells[index].innerHTML.includes('$')) {
+      const salaryA = parseFloat(firstRow.replace(/\$/g, ''));
+      const salaryB = parseFloat(secondRow.replace(/\$/g, ''));
+
+      return salaryA - salaryB;
+    } else {
+      return firstRow > secondRow ? 1 : -1;
+    }
+  });
+
+  tbody.append(...rows);
+}
+
+thead.addEventListener('click', e => {
+  const thIndex = e.target.cellIndex;
+
+  sortByLetter(thIndex);
+});
