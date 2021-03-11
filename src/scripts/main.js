7788956234492795ef7a1ad599b0cd978e8a4241
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-11 16:05:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const headings = document.querySelector('thead');
+const rows = [...document.querySelectorAll('tbody tr')];
+const body = document.querySelector('tbody');
+
+headings.addEventListener('click', (e) => {
+  const heading = e.target.closest('th');
+  const columnIndex = heading.cellIndex;
+
+  rows.sort((rowA, rowB) => {
+    if (rowA.children[columnIndex].textContent.includes('$')) {
+      const a = +rowA.cells[columnIndex].textContent
+        .replace('$', '').replace(',', '');
+      const b = +rowB.cells[columnIndex].textContent
+        .replace('$', '').replace(',', '');
+
+      return a - b;
+    } else {
+      return rowA.cells[columnIndex].textContent
+        .localeCompare(rowB.cells[columnIndex].textContent);
+    }
+  });
+
+  body.append(...rows);
+});
