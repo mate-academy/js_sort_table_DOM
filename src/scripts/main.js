e9351d21371223b3b476bf60edadbbac282be43d
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-11 23:35:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headOfTable = table.querySelector('thead');
+const titlesOfTable = headOfTable.querySelectorAll('th');
+const bodyOfTable = table.querySelector('tbody');
+const stringsOfBody = bodyOfTable.querySelectorAll('tr');
+
+const convertToNumber = (string) => {
+  if (string.includes('$')) {
+    return Number(string.replace('$', '').split(',').join(''));
+  }
+
+  return Number(string);
+};
+
+headOfTable.addEventListener('click', (clickEvent) => {
+  const element = clickEvent.target;
+  const indexOfTitle = [...titlesOfTable].findIndex(title => (
+    title.textContent === element.textContent
+  ));
+
+  const sortedElements = [...stringsOfBody].sort((early, next) => {
+    const textOfEarly = early.children[indexOfTitle].textContent;
+    const textOfNext = next.children[indexOfTitle].textContent;
+
+    if (element.textContent === 'Salary' || element.textContent === 'Age') {
+      return convertToNumber(textOfEarly) - convertToNumber(textOfNext);
+    }
+
+    return textOfEarly.localeCompare(textOfNext);
+  });
+
+  bodyOfTable.append(...sortedElements);
+});
