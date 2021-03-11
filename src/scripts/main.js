9699f6f7e3031562e4866c8134d2880445488f19
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-11 14:06:09 +0200

    table sorting implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+function convertStringToNumber(string) {
+  if (string.includes('$')) {
+    return Number(string.replace(/[$,]/g, ''));
+  }
+
+  return Number(string);
+}
+
+function makeSortTablet(tablet) {
+  const thead = tablet.querySelector('thead');
+  const tbody = tablet.querySelector('tbody');
+
+  // eslint-disable-next-line no-shadow
+  thead.addEventListener('click', () => {
+    const th = event.target.closest('th');
+
+    if (!th) {
+      return;
+    }
+
+    const column = th.cellIndex;
+    const rows = [...tbody.children];
+
+    rows.sort((rowA, rowB) => {
+      const valueA = rowA.cells[column].textContent;
+      const valueB = rowB.cells[column].textContent;
+
+      if (convertStringToNumber(valueA)) {
+        return convertStringToNumber(valueA) - convertStringToNumber(valueB);
+      } else {
+        return valueA > valueB ? 1 : -1;
+      }
+    });
+
+    for (const row of rows) {
+      tbody.appendChild(row);
+    }
+  });
+}
+
+makeSortTablet(
+  document.querySelector('table')
+);
