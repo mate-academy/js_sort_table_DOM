7e876b9159cf34dd77aed140a4ba219397559732
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-11 17:09:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const bodyRows = [...tableBody.querySelectorAll('tr')];
+
+tableHead.addEventListener('click', (e) => {
+  const cellIndex = e.target.cellIndex;
+  const rowsAfterSort = sortedTable(cellIndex, bodyRows);
+
+  tableHead.append(...rowsAfterSort);
+});
+
+function sortedTable(index, table) {
+  let sortedList = table.sort((a, b) => a.children[index]
+    .innerText
+    .localeCompare(b.children[index].innerText
+    ));
+
+  if (index === 2) {
+    sortedList = table.sort((a, b) => Number(a.children[index].innerText)
+      - Number(b.children[index].innerText));
+  }
+
+  if (index === 3) {
+    sortedList = table.sort((a, b) => Number(a.children[index]
+      .innerText
+      .replace(/\$|,/g, '')) - Number(b.children[index]
+      .innerText
+      .replace(/\$|,/g, '')));
+  }
+
+  return sortedList;
+}
