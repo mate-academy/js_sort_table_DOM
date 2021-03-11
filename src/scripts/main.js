e7f35d6c0ef7b22747bd8e4c573d97705e498cee
Ping <4ivrik@ukr.net>
2021-03-11 19:47:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
+const tableCells = document.querySelectorAll('td');
+
+const convert = (string) => {
+  return Number(string.replace(/[^0-9]/g, ''));
+};
+
+tableHead.addEventListener('click', (e) => {
+  const column = e.target.closest('th');
+  const columnIndex = column.cellIndex;
+
+  if (!column || !tableHead.contains(column)) {
+    return;
+  }
+
+  if (typeof tableCells[columnIndex].textContent === 'string') {
+    rows.sort((currentCell, nextCell) => {
+      return currentCell.cells[columnIndex].innerText.localeCompare(
+        nextCell.cells[columnIndex].innerText
+      );
+    });
+  }
+
+  rows.sort((currentCell, nextCell) => {
+    return convert(currentCell.cells[columnIndex].innerText) - convert(
+      nextCell.cells[columnIndex].innerText
+    );
+  });
+
+  rows.forEach(row => tableBody.append(row));
+}
+);
