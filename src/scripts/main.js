e55251452df3923bb3f95356aea10f42f2baf20b
Ping <4ivrik@ukr.net>
2021-03-12 12:40:52 +0200

    fixes after the review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,20 @@
 const tableHead = document.querySelector('thead');
 const tableBody = document.querySelector('tbody');
 const rows = [...tableBody.querySelectorAll('tr')];
-const tableCells = document.querySelectorAll('td');
 
-const convert = (string) => {
+const convertToNumber = (string) => {
   return Number(string.replace(/[^0-9]/g, ''));
 };
 
-tableHead.addEventListener('click', (e) => {
-  const column = e.target.closest('th');
+tableHead.addEventListener('click', (clickEvent) => {
+  const column = clickEvent.target.closest('th');
   const columnIndex = column.cellIndex;
 
   if (!column || !tableHead.contains(column)) {
     return;
   }
 
-  if (typeof tableCells[columnIndex].textContent === 'string') {
+  if (typeof column.textContent === 'string') {
     rows.sort((currentCell, nextCell) => {
       return currentCell.cells[columnIndex].innerText.localeCompare(
         nextCell.cells[columnIndex].innerText
@@ -26,11 +25,10 @@ tableHead.addEventListener('click', (e) => {
   }
 
   rows.sort((currentCell, nextCell) => {
-    return convert(currentCell.cells[columnIndex].innerText) - convert(
-      nextCell.cells[columnIndex].innerText
-    );
+    return convertToNumber(currentCell.cells[columnIndex].innerText)
+    - convertToNumber(nextCell.cells[columnIndex].innerText);
   });
 
-  rows.forEach(row => tableBody.append(row));
+  tableBody.append(...rows);
 }
 );
