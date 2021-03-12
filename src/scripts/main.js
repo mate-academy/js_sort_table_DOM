ad91b895435de85e67bfaf495ade2ecee211f008
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-12 19:15:55 +0200

    after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,30 +3,31 @@
 const mainTable = document.querySelector('table');
 
 const tbody = document.querySelector('tbody');
-const trElemnets = [...tbody.rows];
+const thead = document.querySelector('thead');
+const rows = [...tbody.rows];
 
-const thElements = [...mainTable.rows[0].cells];
+const headers = [...mainTable.rows[0].cells];
 
 function formatText(tableValue) {
   return tableValue.replace(/[^a-zA-Z0-9_-]/g, '');
 }
 
-mainTable.addEventListener('click', (clickHandler) => {
-  if (clickHandler.target.tagName === 'TH') {
-    const cellIndex = thElements.indexOf(clickHandler.target);
+thead.parentNode.addEventListener('click', (clickEvent) => {
+  if (clickEvent.target.tagName === 'TH') {
+    const cellIndex = headers.indexOf(clickEvent.target);
 
-    const sortedElements = trElemnets.sort(
+    const sortedElements = rows.sort(
       (currentTbodyElement, nextTbodyElement) => {
-        const Value = formatText(
+        const value = formatText(
           currentTbodyElement.cells[cellIndex].textContent
         );
         const nextValue = formatText(
           nextTbodyElement.cells[cellIndex].textContent
         );
 
-        return isNaN(parseInt(Value))
-          ? Value.localeCompare(nextValue)
-          : parseInt(Value) - parseInt(nextValue);
+        return isNaN(parseInt(value))
+          ? value.localeCompare(nextValue)
+          : parseInt(value) - parseInt(nextValue);
       });
 
     tbody.append(...sortedElements);
