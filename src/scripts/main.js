7305aa096ed10d1142e8e3aabaea634dcb419552
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-12 12:57:58 +0200

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,39 +4,45 @@ const table = document.querySelector('table');
 const tbody = table.querySelector('tbody');
 const rows = [...tbody.rows];
 
+function getSalaryAmount(salary) {
+  return Number(salary
+    .split('$')
+    .join('')
+    .split(',')
+    .join('')
+  );
+}
+
 function getSortedData(column) {
   const index = column.cellIndex;
   const contentOfTheFirstCell = rows[0].cells[index].textContent;
 
-  const sortedRows = rows.sort((rowA, rowB) => {
-    const rowFirst = rowA.cells[index].textContent;
-    const rowSecond = rowB.cells[index].textContent;
+  const sortedRows = rows.sort((cellA, cellB) => {
+    const cellFirst = cellA.cells[index].textContent;
+    const cellSecond = cellB.cells[index].textContent;
 
     if (contentOfTheFirstCell.includes('$')) {
-      const valueFirst = Number(rowFirst
-        .split('$')
-        .join('')
-        .split(',')
-        .join('')
-      );
-      const valueSecond = Number(rowSecond
-        .split('$')
-        .join('')
-        .split(',')
-        .join('')
-      );
+      const valueFirst = getSalaryAmount(cellFirst);
+      const valueSecond = getSalaryAmount(cellSecond);
 
       return valueFirst > valueSecond ? 1 : -1;
     }
 
-    return rowFirst > rowSecond ? 1 : -1;
+    if (Number(contentOfTheFirstCell)) {
+      const valueFirst = Number(cellFirst);
+      const valueSecond = Number(cellSecond);
+
+      return valueFirst > valueSecond ? 1 : -1;
+    }
+
+    return cellFirst > cellSecond ? 1 : -1;
   });
 
   tbody.append(...sortedRows);
 }
 
-table.addEventListener('click', (clickHandler) => {
-  if (table.tHead.contains(clickHandler.target)) {
-    getSortedData(clickHandler.target);
+table.addEventListener('click', (clickEvent) => {
+  if (table.tHead.contains(clickEvent.target)) {
+    getSortedData(clickEvent.target);
   }
 });
