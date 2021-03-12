58497868672838c0131ece1188c3d3b212996b43
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-03-12 15:48:43 +0200

    added sorter for table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tFoot = document.querySelector('tfoot');
+const tBody = document.querySelector('tbody');
+const tableRows = [...tBody.children];
+
+function dollarIntoNumber(value) {
+  return +value.replace('$', '').replace(',', '.');
+};
+
+function sortingTable(targetTitle) {
+  const position = targetTitle.cellIndex;
+
+  switch (targetTitle.innerText.toLowerCase()) {
+    case 'name':
+    case 'position':
+      tableRows.sort((currentRow, nextRow) => {
+        return currentRow.children[position].innerText
+          .localeCompare(nextRow.children[position].innerText);
+      });
+      break;
+
+    case 'age':
+      tableRows.sort((currentRow, nextRow) => {
+        return currentRow.children[position].innerText
+          - nextRow.children[position].innerText;
+      });
+      break;
+
+    case 'salary':
+      tableRows.sort((currentRow, nextRow) => {
+        return dollarIntoNumber(currentRow.children[position].innerText)
+          - dollarIntoNumber(nextRow.children[position].innerText);
+      });
+      break;
+  }
+
+  return tableRows;
+}
+
+function choosingTitle(title) {
+  const item = title.target;
+
+  tBody.append(...sortingTable(item));
+}
+
+tHead.addEventListener('click', choosingTitle);
+
+tFoot.addEventListener('click', choosingTitle);
