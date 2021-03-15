a523f3d8538c55813c241ac7fb8fc9604c37c853
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-15 11:26:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('tbody');
+const header = document.querySelector('thead');
+const rows = [...body.querySelectorAll('tr')];
+
+function toNumber(string) {
+  return +string.replace(/[^\d]/g, '');
+}
+
+header.addEventListener('click', (action) => {
+  const column = action.target.closest('th');
+  const position = column.cellIndex;
+
+  if (!column || !header.contains(column)) {
+    return false;
+  }
+
+  switch (column.innerHTML) {
+    case 'Name':
+    case 'Position':
+      rows.sort((currentRow, nextRow) => {
+        return currentRow.children[position].innerHTML
+          .localeCompare(nextRow.children[position].innerHTML);
+      });
+      break;
+
+    case 'Age':
+    case 'Salary':
+      rows.sort((currentRow, nextRow) => {
+        return toNumber(currentRow.children[position].innerHTML)
+          - toNumber(nextRow.children[position].innerHTML);
+      });
+      break;
+  }
+
+  body.append(...rows);
+});
