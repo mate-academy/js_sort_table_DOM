d17469b523dab3ef195cb1635baf87121ff739a1
Taras Chirkov <chirkovtaras@gmail.com>
2021-03-15 12:53:26 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,30 +4,42 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 
-function sortNumber(number) {
-  return parseFloat(number.replace(/\$/g, ''));
-}
-
-function sortTable(index) {
+function sortByNumber(index) {
   const rows = Array.from(tbody.rows).sort((rowA, rowB) => {
     const firstCell = rowA.cells[index].innerHTML;
     const secondCell = rowB.cells[index].innerHTML;
 
-    if (firstCell.includes('$') || Number(firstCell)) {
-      const firstNumber = sortNumber(firstCell);
-      const secondNumber = sortNumber(secondCell);
+    const firstNumber = parseFloat(firstCell.replace(/\$/g, ''));
+    const secondNumber = parseFloat(secondCell.replace(/\$/g, ''));
 
-      return firstNumber - secondNumber;
-    } else {
-      return firstCell.localeCompare(secondCell);
-    }
+    return firstNumber - secondNumber;
   });
 
   tbody.append(...rows);
 }
 
-thead.addEventListener('click', e => {
-  const header = e.target.cellIndex;
+function sortByLetter(index) {
+  const rows = Array.from(tbody.rows).sort((rowA, rowB) => {
+    const firstCell = rowA.cells[index].innerHTML;
+    const secondCell = rowB.cells[index].innerHTML;
 
-  sortTable(header);
+    return firstCell.localeCompare(secondCell);
+  });
+
+  tbody.append(...rows);
+}
+
+thead.addEventListener('click', clickEvent => {
+  const cellName = clickEvent.target.textContent;
+  const header = clickEvent.target.cellIndex;
+
+  switch (cellName) {
+    case 'Name':
+    case 'Position':
+      sortByLetter(header);
+      break;
+    case 'Age':
+    case 'Salary':
+      sortByNumber(header);
+  }
 });
