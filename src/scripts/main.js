353da1318fed29aec53afece04584da2c31f8d0a
Smihotur Roman <smikhotur@gmail.com>
2021-03-21 11:38:21 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tr = document.querySelectorAll('tbody tr');
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+function takeNumber(str) {
+  return +str.replace(/[$,]/g, '');
+}
+
+thead.addEventListener('click', (e) => {
+  const sorted = [...tr].sort((tdA, tdB) => {
+    const prev = tdA.children[e.target.cellIndex].innerText;
+    const curr = tdB.children[e.target.cellIndex].innerText;
+
+    if (!isNaN(takeNumber(prev))) {
+      return takeNumber(prev) - takeNumber(curr);
+    } else {
+      return prev.localeCompare(curr);
+    }
+  });
+
+  tbody.append(...sorted);
+});
