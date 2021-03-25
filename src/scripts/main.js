a23b41392fbb820d4ad0efa3420cec01b5f8663b
Ihor Drul <druleger@gmail.com>
2021-03-25 15:06:55 +0200

    Solution with toggler

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const categories = document.querySelector('thead');
+const content = document.querySelector('tbody');
+const elements = [...document.querySelectorAll('tbody tr')];
+let toggler = false;
+
+const convert = field => (
+  +field
+    ? +field
+    : +(field.substring(1).split(',').join(''))
+);
+
+categories.addEventListener('click', (e) => {
+  toggler = !toggler;
+
+  const sortedContent = elements.sort((first, second) => {
+    const firstElement = first.children[e.target.cellIndex].textContent;
+    const secondElement = second.children[e.target.cellIndex].textContent;
+
+    if (isNaN(convert(firstElement))) {
+      return toggler
+        ? firstElement.localeCompare(secondElement)
+        : secondElement.localeCompare(firstElement);
+    } else {
+      return toggler
+        ? convert(firstElement) - convert(secondElement)
+        : convert(secondElement) - convert(firstElement);
+    }
+  });
+
+  content.append(...sortedContent);
+});
