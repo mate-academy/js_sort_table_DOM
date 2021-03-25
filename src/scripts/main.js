b9e889d704034debffe6ced5e5253c045498c40e
olena <melolena33@gmail.com>
2021-03-25 18:07:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const tableHeader = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+function indexOfSortColumn(key) {
+  const keys = ['Name', 'Position', 'Age', 'Salary'];
+
+  return keys.indexOf(key);
+}
+
+tableHeader.addEventListener('click', (e) => {
+  const index = indexOfSortColumn(e.target.textContent);
+
+  const bodyChildren = [...tableBody.children];
+
+  bodyChildren.sort((rowA, rowB) => {
+    let textA = rowA.children[index].textContent;
+    let textB = rowB.children[index].textContent;
+
+    if (textA.startsWith('$')) {
+      textA = +textA.slice(1).replace(/,/g, '');
+      textB = +textB.slice(1).replace(/,/g, '');
+    }
+
+    const isNotNumber = isNaN(Number(textA));
+
+    if (isNotNumber) {
+      return textA.localeCompare(textB);
+    } else {
+      return textA - textB;
+    }
+  });
+
+  for (const child of bodyChildren) {
+    tableBody.appendChild(child);
+  }
+});
