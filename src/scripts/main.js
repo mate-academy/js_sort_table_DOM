3393e43ba35fdb6675a4f82b852349b5632613e5
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-25 10:44:01 +0200

    changed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,15 @@ const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const bodyTr = document.querySelectorAll('tbody tr');
 
-thead.addEventListener('click', (e) => {
-  function changeNum(str) {
-    return +str
-      .split('$')
-      .join('')
-      .split(',')
-      .join('');
-  }
+function changeNum(str) {
+  return +str
+    .split('$')
+    .join('')
+    .split(',')
+    .join('');
+}
 
+thead.addEventListener('click', (e) => {
   const sortTable = [...bodyTr].sort((a, b) => {
     const firstTr = a
       .children[e.target.cellIndex]
