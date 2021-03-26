3eb5ed6d9f75d416640c6fec4d254b570302c16d
Inna Sorochynska <innasoro@gmail.com>
2021-03-26 16:20:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('tbody');
+const headers = document.querySelector('thead');
+
+headers.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+  const sorted = [...list.rows].sort((a, b) => {
+    const first = a.cells[index].textContent.replace('$', '').replace(',', '');
+    const second = b.cells[index].textContent.replace('$', '').replace(',', '');
+
+    if (isNaN(+first)) {
+      return first.localeCompare(second);
+    }
+
+    return first - second;
+  });
+
+  list.append(...sorted);
+});
