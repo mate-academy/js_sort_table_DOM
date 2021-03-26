8d905b06c85c07f5a47bab1c2fcdd224508baf2c
Svyat Krit <krit.mgmn@gmail.com>
2021-03-26 15:57:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employeesContainer = document.querySelector('tbody');
+const employeesHeader = document.querySelector('thead');
+const employees = [...employeesContainer.children];
+
+employeesHeader.addEventListener('click', (eventOne) => {
+  const index = eventOne.target.cellIndex;
+
+  const list = [...employees];
+
+  list.sort(
+    (a, b) => compare(
+      a.children[index].textContent, b.children[index].textContent
+    )
+  );
+
+  employeesContainer.append(...list);
+});
+
+function compare(first, second) {
+  const firstElement = parseSalary(first);
+  const secondElement = parseSalary(second);
+
+  if (!isNaN(firstElement)) {
+    return firstElement - secondElement;
+  } else {
+    return first.localeCompare(second);
+  }
+}
+
+function parseSalary(salary) {
+  return parseInt(salary.replace(/[,$]/g, ''));
+}
