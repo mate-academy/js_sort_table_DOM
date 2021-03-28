b31e3d540a39b7793963bf3c757b8a6c222e59ea
Makhmut <grupry@gmail.com>
2021-03-28 21:11:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tr = document.querySelectorAll('tbody tr');
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  const sorted = Array.from(tr).sort((tdA, tdB) => {
+    const prev = tdA.children[e.target.cellIndex].textContent;
+    const curr = tdB.children[e.target.cellIndex].textContent;
+
+    return !isNaN(transformToNum(prev))
+      ? transformToNum(prev) - transformToNum(curr)
+      : prev.localeCompare(curr);
+  });
+
+  tbody.append(...sorted);
+});
+
+function transformToNum(str) {
+  return +str.replace(/[$,]/g, '');
+}
