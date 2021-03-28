e8d7f2d6f73acdfa5f40e53c658fefa4f14d6999
Bohdan Prokopov <b.prokopov96@gmail.com>
2021-03-28 22:15:06 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const bodyRow = document.querySelectorAll('tbody tr');
+
+const salaryConvert = (input) => {
+  return input.replace('$', '').replace(',', '');
+};
+
+thead.addEventListener('click', (e) => {
+  const sort = [...bodyRow].sort((current, next) => {
+    const targetIndex = e.target.cellIndex;
+    const currentConverted = salaryConvert(
+      current.children[targetIndex].textContent
+    );
+    const nextConverted = salaryConvert(
+      next.children[targetIndex].textContent
+    );
+
+    if (isNaN(+currentConverted)) {
+      return currentConverted.localeCompare(nextConverted);
+    }
+
+    return currentConverted - nextConverted;
+  });
+
+  tbody.append(...sort);
+});
