4bc2cf7670972b1f923b7ccef65349e8553c5d44
Taras Chaban <tarekchaban@gmail.com>
2021-03-29 11:24:12 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const body = document.querySelectorAll('tbody tr');
+const tbody = document.querySelector('tbody');
+
+function converter(Salary) {
+  return Salary.replace('$', '').replace(',', '');
+}
+
+head.addEventListener('click', (ev) => {
+  const sortedList = [...body].sort((first, second) => {
+    const index = ev.target.cellIndex;
+    const firstResult = converter(first.children[index].innerText);
+    const secondResult = converter(second.children[index].innerText);
+
+    if (isNaN(+firstResult)) {
+      return firstResult.localeCompare(secondResult);
+    } else {
+      return firstResult - secondResult;
+    }
+  });
+
+  tbody.append(...sortedList);
+});
