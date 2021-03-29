228faa46945bdf7f82b6098b2b3a13951587002f
Oleg Hluhk <glug.oleg@gmail.com>
2021-03-29 18:33:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const tbodyRows = [...document.querySelector('tbody').rows];
+
+const toNumber = function(string) {
+  return +string.replace(/[$,]/g, '');
+};
+
+thead.addEventListener('click', ev => {
+  const sort = tbodyRows.sort((first, second) => {
+    const firtstChildren = first.children[ev.target.cellIndex].innerText;
+    const secondChildren = second.children[ev.target.cellIndex].innerText;
+
+    if (!isNaN(toNumber(firtstChildren))) {
+      return toNumber(firtstChildren) - toNumber(secondChildren);
+    } else {
+      return firtstChildren.localeCompare(secondChildren);
+    }
+  });
+
+  tbody.append(...sort);
+});
