fb97dfa2f14e22973d2aa0717422f1f7d0d640f1
dinexpod <work.toxa@gmail.com>
2021-04-04 14:45:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const theadRows = document.querySelector('thead tr');
+const tbodyElement = document.querySelector('tbody');
+
+theadRows.addEventListener('click', e => {
+  const trElements = [];
+  let columnNumber;
+
+  for (let i = 0; i < theadRows.children.length; i++) {
+    if (theadRows.children.item(i) === e.target) {
+      columnNumber = i;
+    }
+  }
+
+  for (const trChild of tbodyElement.children) {
+    trElements.push(trChild);
+  }
+
+  trElements.sort((a, b) => {
+    let aData = a.children[columnNumber].textContent;
+    let bData = b.children[columnNumber].textContent;
+
+    if (columnNumber === 3) {
+      aData = aData.replace(/[$-,]/g, '');
+      bData = bData.replace(/[$-,]/g, '');
+
+      return aData - bData;
+    }
+
+    return aData.localeCompare(bData);
+  });
+
+  for (const child of trElements) {
+    tbodyElement.removeChild(child);
+  }
+
+  for (const trElement of trElements) {
+    tbodyElement.appendChild(trElement);
+  }
+});
