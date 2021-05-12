bc7f3e35f92b2cd83b7c0f627b393eef1a0f7a10
Kostrica <romiorw@ukr.net>
2021-05-12 19:33:03 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', e => {
+  const onClick = e.target.closest('th');
+
+  sortRows(onClick.cellIndex);
+});
+
+function sortRows(colNum) {
+  const rowsArray = [...tbody.rows].slice(0);
+  let choice;
+
+  switch (colNum) {
+    case 3:
+      choice = (rowA, rowB) =>
+        parseInt(rowA.cells[colNum].innerHTML.slice(1))
+          - parseInt(rowB.cells[colNum].innerHTML.slice(1));
+      break;
+    default:
+      choice = (rowA, rowB) =>
+        rowA.cells[colNum].innerHTML
+          > rowB.cells[colNum].innerHTML
+          ? 1
+          : -1;
+      break;
+  }
+
+  rowsArray.sort(choice);
+
+  tbody.append(...rowsArray);
+}
