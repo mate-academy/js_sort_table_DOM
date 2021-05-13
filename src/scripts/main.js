da5bbf3bd61bb4f94ed2de2030a2ac6762c8c231
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-13 10:00:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const trHead = document.querySelector('thead');
+const thHead = [...[...trHead.rows][0].children];
+
+for (let i = 0; i < thHead.length; i++) {
+  thHead[i].addEventListener('click', (e) => {
+    const sorted = [...tbody.rows]
+      .sort((a, b) => {
+        if (i === 3) {
+          return f(a.cells[i].innerHTML) - f(b.cells[i].innerHTML);
+        }
+
+        return a.cells[i].innerHTML > b.cells[i].innerHTML ? 1 : -1;
+      });
+
+    tbody.append(...sorted);
+  });
+}
+
+function f(str) {
+  return +(str.replace(/\D/g, ''));
+};
