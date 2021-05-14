87c9bc7a164d0a880f2fa6e865912396d13df660
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-14 15:20:53 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
 const tbody = document.querySelector('tbody');
-const trHead = document.querySelector('thead');
-const thHead = [...[...trHead.rows][0].children];
+const tHead = document.querySelector('thead');
+const thHead = [...[...tHead.rows][0].children];
 
-for (let i = 0; i < thHead.length; i++) {
-  thHead[i].addEventListener('click', (e) => {
-    const sorted = [...tbody.rows]
-      .sort((a, b) => {
-        if (i === 3) {
-          return f(a.cells[i].innerHTML) - f(b.cells[i].innerHTML);
-        }
+tHead.addEventListener('click', (e) => {
+  const sorted = [...tbody.rows]
+    .sort((a, b) => {
+      const i = thHead.indexOf(e.target);
 
-        return a.cells[i].innerHTML > b.cells[i].innerHTML ? 1 : -1;
-      });
+      if (e.target.textContent === 'Salary') {
+        return f(a.cells[i].innerHTML) - f(b.cells[i].innerHTML);
+      }
 
-    tbody.append(...sorted);
-  });
-}
+      return a.cells[i].innerHTML > b.cells[i].innerHTML ? 1 : -1;
+    });
+
+  tbody.append(...sorted);
+});
 
 function f(str) {
   return +(str.replace(/\D/g, ''));
