a1b47843b944bd2498efede2080da9e2ad748564
Ivan Major <tarstarcas@gmail.com>
2021-05-15 11:41:20 +0300

    Sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+
+head.addEventListener('click', (push) => {
+  const th = push.target;
+
+  sortHead(th.cellIndex, push.target.textContent);
+});
+
+function sortHead(numCol, nameCol) {
+  const bodyTable = document.querySelector('tbody');
+
+  const sorted = [...bodyTable.rows];
+
+  sorted.sort((a, b) => {
+    if (nameCol === 'Salary') {
+      return conv(a.cells[numCol].innerHTML) - conv(b.cells[numCol].innerHTML);
+    };
+
+    return a.cells[numCol].innerHTML > b.cells[numCol].innerHTML ? 1 : -1;
+  });
+
+  bodyTable.append(...sorted);
+};
+
+function conv(item) {
+  return +(item.replace(/\D/g, ''));
+};
