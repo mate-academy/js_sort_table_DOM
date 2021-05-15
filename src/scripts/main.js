ab64265a4b29a5976d0644133af9fd0666f7bec0
Vlad Trundenko <vlad.trundenko@gmail.com>
2021-05-15 14:51:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const columnName = document.querySelectorAll('thead th');
+const tableBody = document.querySelector('tbody');
+const tableRow = [...tableBody.querySelectorAll('tr')];
+
+const filterStr = (str) => str.replace(/[^a-zA-Z0-9]/g, '');
+
+for (let i = 0; i < columnName.length; i++) {
+  columnName[i].addEventListener('click', () => {
+    tableRow.sort((a, b) => {
+      const valueA = a.children[i].textContent;
+      const valueB = b.children[i].textContent;
+
+      return isFinite(filterStr(valueA))
+        ? filterStr(valueA) - filterStr(valueB)
+        : filterStr(valueA).filterStr(valueB);
+    });
+
+    tableBody.append(...tableRow);
+  });
+}
