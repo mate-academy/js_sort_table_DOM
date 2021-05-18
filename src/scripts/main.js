417d5f67dc7c59832ea83e6bd53535d78da69433
romandubovskyi <rdubowskij@gmail.com>
2021-05-18 20:07:44 +0300

    added table sorting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const tHeader = document.querySelector('thead');
+const tBodyRows = document.querySelector('tbody').rows;
+
+tHeader.addEventListener('click', (e) => {
+  const sortColumnIndex = findColumnIndex(tHeader, e.target.innerText);
+  const columnCells = [...tBodyRows].map((row) => {
+    return row.cells[sortColumnIndex];
+  });
+  const sortedColumn = sortColumn(columnCells);
+
+  [...tBodyRows].forEach((row, index) => {
+    const beforeTargetElement = row.cells[sortColumnIndex - 1] || row.cells[0];
+
+    beforeTargetElement.after(sortedColumn[index]);
+  });
+});
+
+function sortColumn(cells) {
+  const regex = new RegExp('[$,]', 'g');
+  const convertRawInput = (cell) => {
+    return String(cell.innerText).replaceAll(regex, '');
+  };
+
+  return [...cells].sort((a, b) => {
+    const pureAValue = convertRawInput(a);
+    const pureBValue = convertRawInput(b);
+
+    return !isNaN(pureAValue)
+      ? pureAValue - pureBValue
+      : pureAValue.localeCompare(pureBValue);
+  });
+}
+
+function findColumnIndex(header, columnTitle) {
+  return [...header.rows[0].cells].map(element => element.innerText)
+    .indexOf(columnTitle);
+}
