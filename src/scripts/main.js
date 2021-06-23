fe25bd737a9ad0ff5e1c374b030eed04ac76e7ef
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2021-06-23 12:21:28 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,10 +12,24 @@ const strInNumber = (str) => {
 
 const sortTable = (column) => {
   [...tbody].sort((item, item1) => {
+    if (column === 2) {
+      if (+item1.children[column].textContent > +item.children[column].textContent) {
+        return 1
+      }
+      if (+item1.children[column].textContent < +item.children[column].textContent) {
+        return -1
+      }
+      return 0;
+    }
+
     if (column !== 3) {
-      return (
-        item1.children[column].textContent - item.children[column].textContent
-      );
+      if (item1.children[column].textContent > item.children[column].textContent) {
+        return 1
+      }
+      if (item1.children[column].textContent < item.children[column].textContent) {
+        return -1
+      }
+      return 0;
     }
 
     return strInNumber(
