ab392e2e6cc71e0edf0871594181b2380efa0373
John <jozaman96@gmail.com>
2021-07-11 15:44:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headRow = table.tHead;
+const body = table.tBodies[0];
+const makeComparator = (first, second, idx) => {
+  let firstValue = first.children[idx].innerText;
+  let secondValue = second.children[idx].innerText;
+
+  if (firstValue[0] === '$') {
+    firstValue = firstValue.slice(1).split(',').join('');
+    secondValue = secondValue.slice(1).split(',').join('');
+  }
+
+  if (/^\d+$/.test(firstValue)) {
+    return Number(firstValue) - Number(secondValue);
+  }
+
+  return firstValue.localeCompare(secondValue);
+};
+
+headRow.addEventListener('click', e => {
+  const currentCell = e.target;
+  const currentColumn = currentCell.cellIndex;
+  const sortedColumn = [...body.rows];
+
+  sortedColumn.sort((a, b) => makeComparator(a, b, currentColumn));
+  body.append(...sortedColumn);
+});
