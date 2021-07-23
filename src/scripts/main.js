2611196081967c45c05edaf2064715d4caeb7e6d
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-23 13:28:25 +0300

    Changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,87 +1,51 @@
 'use strict';
 
 const table = document.querySelector('table');
-const getList = [...document.querySelector('tbody').querySelectorAll('tr')];
-const tester = {
-  numb: 0,
-  words: 0,
-};
+const list = [...document.querySelectorAll('tbody tr')];
 
-table.addEventListener('click', push => {
-  const cl = push.target.innerText;
+table.addEventListener('click', anEvent => {
+  const clicking = anEvent.target.innerText;
 
-  switch (cl) {
+  switch (clicking) {
     case 'Name':
-      tester.words++;
-      sortList(getList, 0,);
+      sortList(list, 0);
       break;
 
     case 'Position':
-      tester.words++;
-      sortList(getList, 1,);
+      sortList(list, 1);
       break;
 
     case 'Age':
-      tester.numb++;
-      sortList(getList, 2,);
+      sortList(list, 2);
       break;
 
     case 'Salary':
-      tester.numb++;
-      sortList(getList, 3,);
+      sortList(list, 3);
       break;
   };
 });
 
-function sortList(param, column) {
-  const listCopy = param;
-
-  listCopy.sort((a, b) => {
+function sortList(listForSorting, column) {
+  listForSorting.sort((a, b) => {
     const innerA = a.children[column].innerText;
     const innerB = b.children[column].innerText;
 
     if (parseNum(innerA)) {
-      return (tester.numb % 2 === 0)
-        ? parseNum(innerA) - parseNum(innerB)
-        : parseNum(innerB) - parseNum(innerA);
+      return parseNum(innerB) - parseNum(innerA);
     } else {
-      return (tester.words % 2 === 0)
-        ? innerA.localeCompare(innerB)
-        : innerB.localeCompare(innerA);
+      return innerA.localeCompare(innerB);
     };
   });
 
-  document.querySelector('tbody').remove();
-
-  const newTbody = document.createElement('tbody');
-
-  listCopy.forEach(person => {
-    const newTr = document.createElement('tr');
-
-    newTr.innerHTML = `
-    <td>${person.children[0].innerText}</td>
-    <td>${person.children[1].innerText}</td>
-    <td>${person.children[2].innerText}</td>
-    <td>${person.children[3].innerText}</td>
-  `;
-
-    newTbody.append(newTr);
-  });
-
-  table.children[0].after(newTbody);
+  listForSorting.forEach(elem => table.children[1].append(elem));
 };
 
 function parseNum(par) {
   let result = '';
 
   for (const char of par) {
-    if (
-      '1234567890'.includes(char)) {
+    if ('1234567890'.includes(char)) {
       result += char;
-    }
-
-    if (','.includes(char)) {
-      result += '.';
     };
   };
 
