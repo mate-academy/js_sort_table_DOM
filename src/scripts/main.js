db61cc1bce1d5d3cdd7df9e9273061ccebe46094
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-08-06 21:32:51 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // write code here
-const headers = document.querySelectorAll('th');
 const body = document.querySelector('tbody');
 const array = [...body.rows].map(row =>
   [
@@ -12,31 +11,30 @@ const array = [...body.rows].map(row =>
   ]
 );
 
-for (const th of headers) {
-  th.addEventListener('click', (e) => {
-    for (let i = 0; i < body.rows.length; i++) {
-      for (let j = 0; j < body.rows[i].cells.length; j++) {
-        if (e.target.innerText === 'Name') {
-          array.sort((a, b) => a[0].localeCompare(b[0]));
-          body.rows[i].cells[j].innerText = array[i][j];
-        }
-
-        if (e.target.innerText === 'Position') {
-          array.sort((a, b) => a[1].localeCompare(b[1]));
-          body.rows[i].cells[j].innerText = array[i][j];
-        }
-
-        if (e.target.innerText === 'Age') {
-          array.sort((a, b) => a[2] - b[2]);
-          body.rows[i].cells[j].innerText = array[i][j];
-        }
-
-        if (e.target.innerText === 'Salary') {
-          array.sort((a, b) => a[3].slice(1).replace(/,/g, '')
-          - b[3].slice(1).replace(/,/g, ''));
-          body.rows[i].cells[j].innerText = array[i][j];
-        }
-      }
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    switch (e.target.innerText) {
+      case 'Name':
+        array.sort((a, b) => a[0].localeCompare(b[0]));
+        break;
+      case 'Position':
+        array.sort((a, b) => a[1].localeCompare(b[1]));
+        break;
+      case 'Age':
+        array.sort((a, b) => a[2] - b[2]);
+        break;
+      case 'Salary':
+        array.sort((a, b) => {
+          return a[3].slice(1).replace(/,/g, '')
+                 - b[3].slice(1).replace(/,/g, '');
+        });
+        break;
     }
-  });
-}
+  }
+
+  for (let i = 0; i < body.rows.length; i++) {
+    for (let j = 0; j < body.rows[i].cells.length; j++) {
+      body.rows[i].cells[j].innerText = array[i][j];
+    }
+  }
+});
