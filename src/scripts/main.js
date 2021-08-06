d94f15d457790d39df76cbc7b5bea8f33b16bc66
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-06 11:41:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const selectorTop = document.querySelector('tr');
+const tBody = document.querySelector('tbody');
+
+let arrToSort = [...document.querySelectorAll('tr')];
+
+arrToSort = arrToSort.slice(1, -1);
+
+const parseSalary = (string) => {
+  return parseFloat(string.substring(1).replace(',', ''));
+};
+
+for (let i = 0; i <= 3; i++) {
+  selectorTop.children[i].addEventListener('click', (e) => {
+    arrToSort.sort((a, b) => {
+      const firstEl = a.children[i].innerHTML;
+      const secondEl = b.children[i].innerHTML;
+      let compareResult = firstEl - secondEl;
+
+      if (i === 3) {
+        compareResult = parseSalary(firstEl) - parseSalary(secondEl);
+      };
+
+      if (i < 2) {
+        compareResult = firstEl.localeCompare(secondEl);
+      };
+
+      return compareResult;
+    });
+
+    tBody.append(...arrToSort);
+  });
+}
