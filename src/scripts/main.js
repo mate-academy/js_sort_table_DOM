132be61c7a350c1c68a8e8654a13d54908c7920e
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-12 21:40:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const elementTable = document.querySelector('table');
+const rowsArr = [...elementTable.tBodies[0].rows];
+const headCells = [...elementTable.tHead.rows[0].cells];
+const sortCollsColumns = (indexColumn, arr) => {
+  arr.sort((a, b) => {
+    let A = [...a.cells][indexColumn].textContent;
+    let B = [...b.cells][indexColumn].textContent;
+
+    if (headCells[indexColumn].textContent === 'Salary') {
+      A = +A.replace(/\D/g, '');
+      B = +B.replace(/\D/g, '');
+    };
+
+    return (A > B) ? 1 : ((A < B) ? -1 : 0);
+  });
+
+  arr.forEach((el) => {
+    elementTable.appendChild(el);
+  });
+};
+
+elementTable.addEventListener('click', function(e) {
+  if (!this.firstElementChild.contains(e.target) || e.target.tagName !== 'TH') {
+    return;
+  };
+
+  const indexHeadCall = headCells.indexOf(e.target);
+
+  sortCollsColumns(indexHeadCall, rowsArr);
+});
