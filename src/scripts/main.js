0daa8d2aa7cdd3c5fc3b5b56c43455232d2aebc4
Olga Bordiug <obordiug.gmail.com>
2021-08-27 16:27:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = [ ...tbody.rows ];
+
+thead.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  if (!th || !thead.contains(th)) {
+    return;
+  }
+
+  const type = conversationToNumber(
+    rows[0].cells[th.cellIndex].innerHTML);
+
+  const sortTable = (typeof (type) === 'string')
+    ? rows.sort((row1, row2) =>
+      row1.cells[th.cellIndex].innerHTML
+        .localeCompare(row2.cells[th.cellIndex].innerHTML))
+    : rows.sort((row1, row2) =>
+      conversationToNumber(row1.cells[th.cellIndex].innerHTML)
+        - conversationToNumber(row2.cells[th.cellIndex].innerHTML));
+
+  tbody.append(...sortTable);
+});
+
+function conversationToNumber(value) {
+  const result = value.split('')
+    .filter(element => element !== ' ')
+    .filter(element => !isNaN(Number(element)));
+
+  if (result.length === 0) {
+    return value;
+  }
+
+  return Number(result.join(''));
+};
