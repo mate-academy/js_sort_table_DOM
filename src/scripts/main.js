22966271e1d93449aea0ed5d413a1ff3ed7b59c3
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-09-02 11:21:06 +0300

    SOLUTION with "switch/case"

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@
 const table = document.querySelector('table');
 const tableBody = document.querySelector('tbody');
 const tableHead = table.querySelector('thead');
-let rowList = tableBody.rows;
+const rowList = [...tableBody.rows];
 
 function getNumber(string) {
   let str = '';
@@ -24,34 +24,36 @@ tableHead.addEventListener('click', e => {
     return;
   }
 
-  if (headCol.innerText === 'Name') {
-    rowList = [...rowList].sort((a, b) => {
-      const aElem = a.cells[0].innerText;
-      const bElem = b.cells[0].innerText;
+  switch (headCol.innerText) {
+    case 'Name':
+      rowList.sort((a, b) => {
+        const aElem = a.cells[0].innerText;
+        const bElem = b.cells[0].innerText;
 
-      return aElem.localeCompare(bElem);
-    });
-  }
+        return aElem.localeCompare(bElem);
+      });
+      break;
 
-  if (headCol.innerText === 'Position') {
-    rowList = [...rowList].sort((a, b) => {
-      return a.cells[1].innerText.localeCompare(b.cells[1].innerText);
-    });
-  }
+    case 'Position':
+      rowList.sort((a, b) => {
+        return a.cells[1].innerText.localeCompare(b.cells[1].innerText);
+      });
+      break;
 
-  if (headCol.innerText === 'Age') {
-    rowList = [...rowList].sort((a, b) => {
-      return a.cells[2].innerText - b.cells[2].innerText;
-    });
-  }
+    case 'Age':
+      rowList.sort((a, b) => {
+        return a.cells[2].innerText - b.cells[2].innerText;
+      });
+      break;
 
-  if (headCol.innerText === 'Salary') {
-    rowList = [...rowList].sort((a, b) => {
-      const aElem = getNumber(a.cells[3].innerText);
-      const bElem = getNumber(b.cells[3].innerText);
+    case 'Salary':
+      rowList.sort((a, b) => {
+        const aElem = getNumber(a.cells[3].innerText);
+        const bElem = getNumber(b.cells[3].innerText);
 
-      return aElem - bElem;
-    });
+        return aElem - bElem;
+      });
+      break;
   }
 
   for (const row of rowList) {
