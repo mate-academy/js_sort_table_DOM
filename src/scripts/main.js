435c9c33ba27be1c2abc6702ec0e2747478f701a
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-02 18:43:51 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,42 +1,31 @@
 'use strict';
 
-const counter = document.querySelectorAll('tr');
-const listName = document.querySelector('thead');
-const list = document.querySelector('tbody');
+const tabletHeader = document.querySelector('thead');
+const tabletBody = document.querySelector('tbody');
 
-const changeToNum = (value) => value.replace('$', '').replace(',', '');
+const changeToNum = (value) => +value.replace('$', '').replace(',', '');
 
-for (let i = 0; i < [...counter].length; i++) {
-  for (let y = 0; y < [...counter[i].children].length; y++) {
-    counter[i].children[y].count = y;
-  }
-}
-
-listName.addEventListener('click', (e) => {
+tabletHeader.addEventListener('click', (e) => {
   const item = e.target;
+  const contents = [...tabletHeader.children[0].children];
+  const indexContents = contents.findIndex(elem => elem === item);
 
-  sortedList(item.count, list);
+  sortList(indexContents, tabletBody);
 });
 
-function sortedList(count, sortList) {
-  const arr = [];
+function sortList(index, list) {
+  const children = [...list.children];
 
-  for (let i = 0; i < sortList.children.length; i++) {
-    arr.push(sortList.children[i]);
-  }
+  const sorted = children.sort((a, b) => {
+    const prev = a.children[index].textContent;
+    const next = b.children[index].textContent;
 
-  const sorted = arr.sort((a, b) => {
-    const prev = [...a.children].find(item => item.count === count);
-    const next = [...b.children].find(item => item.count === count);
-
-    if (isNaN(changeToNum(prev.textContent))) {
-      return prev.textContent.localeCompare(next.textContent);
+    if (isNaN(changeToNum(prev))) {
+      return prev.localeCompare(next);
     }
 
-    return changeToNum(prev.textContent) - changeToNum(next.textContent);
+    return changeToNum(prev) - changeToNum(next);
   });
 
-  for (let y = 0; y < sorted.length; y++) {
-    list.append(sorted[y]);
-  }
+  tabletBody.append(...sorted);
 };
