59906022bfd0bed442e7cc22854e4e9f33f06332
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2021-09-17 14:01:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tableHeader = document.querySelector('thead');
+
+tableHeader.addEventListener('click', e => {
+  const column = e.target.closest('th').cellIndex;
+  const tableBody = document.querySelector('tbody');
+  const tableRows = [...tableBody.rows];
+
+  tableRows.sort((a, b) => {
+    if (!isNaN(+a.cells[column].innerText)) {
+      return +a.cells[column].innerText - +b.cells[column].innerText;
+    }
+
+    if (a.cells[column].innerText.includes('$')) {
+      return a.cells[column].innerText.slice(1).split(',').join('')
+        - b.cells[column].innerText.slice(1).split(',').join('');
+    }
+
+    return a.cells[column].innerText.localeCompare(b.cells[column].innerText);
+  });
+
+  tableBody.append(...tableRows);
+});
