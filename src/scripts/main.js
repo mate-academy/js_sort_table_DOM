7b4deb5e85f539dff8dca8221c78f92dc10eb241
Anton Forzun <anton_forzun@ukr.net>
2021-09-28 13:08:23 +0300

    sort_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const sortList = document.querySelector('thead');
+const bodyList = document.querySelector('tbody');
+const rows = [...bodyList.rows];
+
+sortList.addEventListener('click', (e) => {
+  const cellInd = e.target.cellIndex;
+
+  if (e.target.innerText === 'Salary') {
+    rows.sort((a, b) => {
+      return pars(a.cells[cellInd].innerHTML)
+         - pars(b.cells[cellInd].innerHTML);
+    });
+  } else {
+    rows.sort((a, b) => {
+      return a.cells[cellInd].innerHTML > b.cells[cellInd].innerHTML ? 1 : -1;
+    });
+  }
+
+  bodyList.append(...rows);
+
+  function pars(item) {
+    return +(item.replace('$', '').split(',').join(''));
+  }
+});
