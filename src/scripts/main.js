11129b10280bcea7c3e29bcf6b83a8a9c8953e97
Aleksandr Maltsev <0po4ta@gmail.com>
2021-10-23 17:43:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableRows = tableBody.querySelectorAll('tr');
+
+tableHead.addEventListener('click', e => {
+  const header = e.target.closest('th');
+
+  if (!header || !tableHead.contains(header)) {
+    return;
+  }
+
+  const values = [...tableRows].map(row => {
+    return row.cells[e.target.cellIndex].textContent;
+  });
+
+  switch (header.textContent) {
+    case 'Age':
+      values.sort((a, b) => +a - +b);
+      break;
+    case 'Salary':
+      values.sort((a, b) => +a.replace(/\D/g, '') - +b.replace(/\D/g, ''));
+      break;
+    case 'Name':
+    case 'Position':
+    default:
+      values.sort();
+      break;
+  }
+
+  tableRows.forEach((row, index) => {
+    row.cells[e.target.cellIndex].textContent = values[index];
+  });
+});
