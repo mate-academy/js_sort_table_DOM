46a4e97fa8b90eba83f8dff24f2d0da267a19666
Mariia Kovach <maria_kovach@ukr.net>
2021-11-02 13:11:42 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead th');
+const tableBody = document.querySelector('tbody');
+const tableRows = [...tableBody.rows];
+
+function toNumber(str) {
+  return +str.replace('$', '').split(',').join('');
+}
+
+headers.forEach(header => {
+  header.addEventListener('click', (e) => {
+    const headerIndex = [...headers].indexOf(e.target);
+
+    tableRows.sort((a, b) => {
+      const x = a.cells[headerIndex].innerText;
+      const y = b.cells[headerIndex].innerText;
+
+      if (typeof x === 'string') {
+        return x.localeCompare(y);
+      }
+
+      return toNumber(x) - toNumber(y);
+    });
+
+    tableBody.append(...tableRows);
+  });
+});
