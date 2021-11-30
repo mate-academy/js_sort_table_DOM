2b93e1b4291c07855237e75d854261757acdd39f
FocusHuK <kirill919191kirill@gmail.com>
2021-11-30 15:55:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelectorAll('table');
+
+table.forEach(tableTH =>
+  tableTH.addEventListener('click', () => getSort(event)));
+
+const getSort = (events) => {
+  const target = events.target;
+  const order = (target.dataset.order = -(target.dataset.order || -1));
+  const index = [...target.parentNode.cells].indexOf(target);
+  const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+  const comparator = (column, valueOrder) =>
+    (a, b) => valueOrder * collator.compare(
+      a.children[column].innerHTML,
+      b.children[column].innerHTML
+    );
+
+  for (const tBody of target.closest('table').tBodies) {
+    tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+  }
+
+  for (const cell of target.parentNode.cells) {
+    cell.classList.toggle('sorted', cell === target);
+  };
+};
