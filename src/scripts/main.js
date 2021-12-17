9f2c7d61934ab5d8e2ef1dc57e3c5696f4566fd2
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-12-17 17:29:54 +0200

    Combined cases by data type, wrapped actions in functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,47 +2,23 @@
 
 const tBody = document.querySelector('tbody');
 const tr = tBody.querySelectorAll('tr');
-const thead = document.querySelector('thead');
-const th = thead.querySelectorAll('th');
-const arrayOfEmployees = [];
-const personsAttributes = [];
+const tHead = document.querySelector('thead');
+const th = tHead.querySelectorAll('th');
 
-for (const value of th) {
-  personsAttributes.push(value.textContent);
-}
-
-for (const employeeData of tr) {
-  const person = {};
-
-  for (let i = 0; i < employeeData.children.length; i++) {
-    person[personsAttributes[i]] = employeeData.children[i].textContent;
-  }
-
-  arrayOfEmployees.push(person);
-}
-
-thead.addEventListener('click', (e) => {
+tHead.addEventListener('click', (e) => {
   const item = e.target.textContent;
 
   isSorted(item);
 });
 
 function isSorted(columnName) {
-  if (columnName === 'Name') {
-    arrayOfEmployees.sort((a, b) => a.Name.localeCompare(b.Name));
-  }
+  const arrayOfEmployees = createArrayOfEmployees(tr);
 
-  if (columnName === 'Position') {
-    arrayOfEmployees.sort((a, b) => a.Position.localeCompare(b.Position));
-  }
-
-  if (columnName === 'Age') {
-    arrayOfEmployees.sort((a, b) => +a.Age - +b.Age);
-  }
-
-  if (columnName === 'Salary') {
-    arrayOfEmployees.sort((a, b) => stringToNumber(a.Salary)
-    - stringToNumber(b.Salary));
+  if (columnName === 'Name' || columnName === 'Position') {
+    arrayOfEmployees.sort((a, b) => a[columnName].localeCompare(b[columnName]));
+  } else {
+    arrayOfEmployees.sort((a, b) => stringToNumber(a[columnName])
+      - stringToNumber(b[columnName]));
   }
 
   for (let i = 0; i < arrayOfEmployees.length; i++) {
@@ -53,6 +29,33 @@ function isSorted(columnName) {
       counter++;
     }
   }
+}
+
+function getPersonsAttributes(element) {
+  const personsAttributes = [];
+
+  for (const value of element) {
+    personsAttributes.push(value.textContent);
+  }
+
+  return personsAttributes;
+}
+
+function createArrayOfEmployees(element) {
+  const arrayOfEmployees = [];
+  const personsAttributes = getPersonsAttributes(th);
+
+  for (const employeeData of element) {
+    const person = {};
+
+    for (let i = 0; i < employeeData.children.length; i++) {
+      person[personsAttributes[i]] = employeeData.children[i].textContent;
+    }
+
+    arrayOfEmployees.push(person);
+  }
+
+  return arrayOfEmployees;
 }
 
 function stringToNumber(string) {
