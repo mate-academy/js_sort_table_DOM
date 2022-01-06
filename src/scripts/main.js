c187bd2fdf2abc2cb1d7ccc3c836399abdcaf150
Виталий Пасечник <47535101+VitalyPasechnik@users.noreply.github.com>
2022-01-06 20:27:16 +0200

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+'use strict';
+
+const head = document.querySelector('thead');
+
+head.addEventListener('click', (e) => {
+  const list = document.querySelectorAll('tr');
+  const sortList = document.querySelector('tbody');
+  const row = e.target.cellIndex;
+  const sorted = [];
+
+  for (let i = 1; i < list.length - 1; i++) {
+    sorted.push(list[i]);
+    list[i].remove();
+  }
+
+  sorted.sort((a, b) => {
+    const aE = a.children[row].textContent;
+    const bE = b.children[row].textContent;
+
+    if (row === 0 || row === 1) {
+      return aE >= bE ? 1 : -1;
+    }
+
+    if (row === 3) {
+      return +bE.toString().replace(/\$|,/g, '')
+            - +aE.toString().replace(/\$|,/g, '');
+    }
+
+    return +bE - +aE;
+  });
+
+  sorted.forEach(item => sortList.appendChild(item));
+});
