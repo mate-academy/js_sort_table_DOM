71d2b2c536894e55b7b9ca5cdd5ca40b9330e880
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2022-01-11 20:01:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBodyChild = document.querySelector('tBody').children;
+const tBody = document.querySelector('tBody');
+const data = [];
+
+for (const man of tBodyChild) {
+  data.push({
+    name: man.children[0].textContent,
+    position: man.children[1].textContent,
+    age: man.children[2].textContent,
+    salary: man.children[3].textContent,
+  });
+}
+
+table.addEventListener('click', e => {
+  const item = e.target.closest('th');
+
+  if (!item) {
+    return;
+  }
+
+  switch (e.target.textContent) {
+    case 'Name':
+      data.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+
+    case 'Position':
+      data.sort((a, b) => a.position.localeCompare(b.position));
+      break;
+
+    case 'Age':
+      data.sort((a, b) => a.age - b.age);
+      break;
+
+    case 'Salary':
+      data.sort((a, b) => a.salary.replace(/[$,]/g, '')
+        - b.salary.replace(/[$,]/g, ''));
+      break;
+  }
+
+  tBody.innerHTML = '';
+
+  for (const i in data) {
+    tBody.insertAdjacentHTML('beforeend', `
+      <tr>
+        <td>${data[i].name}</td>
+        <td>${data[i].position}</td>
+        <td>${data[i].age}</td>
+        <td>${data[i].salary}</td>
+      </tr>
+    `);
+  }
+});
