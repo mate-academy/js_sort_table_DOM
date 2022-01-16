4a0fc7deedd03aed953d91d1628c58d8de48e44a
Anna Tovkailo <babylike24@gmail.com>
2022-01-16 08:23:01 +0200

    solution3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,20 @@
 'use strict';
 
-const getEmployees = function(list) {
-  const arrayList = [...list].map(item => {
-    const itemEmployee = {
-      Name: item.children[0].innerHTML,
-      Position: item.children[1].innerHTML,
-      Age: Number(item.children[2].innerHTML),
-      Salary: Number(
-        item.children[3].innerHTML.slice(1, 10).split(',').join('')),
-    };
-
-    return itemEmployee;
-  });
-
-  return arrayList;
-};
-
-const sortTable = function(table, column) {
+const sortTable1 = function(table, column) {
   table.sort(function(a, b) {
-    if (a[column] > b[column]) {
+    let aItem = a.children[column].innerHTML;
+    let bItem = b.children[column].innerHTML;
+
+    if (column === 3) {
+      aItem = Number(a.children[3].innerHTML.slice(1, 10).split(',').join(''));
+      bItem = Number(b.children[3].innerHTML.slice(1, 10).split(',').join(''));
+    }
+
+    if (aItem > bItem) {
       return 1;
     }
 
-    if (a[column] < b[column]) {
+    if (aItem < bItem) {
       return -1;
     }
 
@@ -32,50 +24,38 @@ const sortTable = function(table, column) {
   return table;
 };
 
-const rebuildTable = (table) => {
-  const tableBody = document.querySelector('tbody');
-
-  for (let i = 0; i < listForSort.length; i++) {
-    const row = tableBody.querySelector('tbody tr');
-
-    row.remove();
-  }
-
-  for (let i = 0; i < listForSort.length; i++) {
-    const newRow = document.createElement('tr');
-    const newCell1 = document.createElement('td');
-
-    newCell1.innerHTML = listForSort[i].Name;
-
-    const newCell2 = document.createElement('td');
-
-    newCell2.innerHTML = listForSort[i].Position;
-
-    const newCell3 = document.createElement('td');
-
-    newCell3.innerHTML = listForSort[i].Age;
-
-    const newCell4 = document.createElement('td');
-
-    newCell4.innerHTML = listForSort[i].Salary;
-
-    newRow.append(newCell1);
-    newRow.append(newCell2);
-    newRow.append(newCell3);
-    newRow.append(newCell4);
-
-    tableBody.append(newRow);
-  }
-};
-
-const rows = document.querySelectorAll('tbody tr');
-const listForSort = getEmployees(rows);
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.rows];
 
 document.addEventListener('click', (e) => {
   if (e.target.tagName !== 'TH') {
     return;
   }
 
-  sortTable(listForSort, e.target.innerHTML);
-  rebuildTable(listForSort);
+  let column;
+
+  switch (e.target.innerHTML) {
+    case 'Name': {
+      column = 0;
+      break;
+    }
+
+    case 'Position': {
+      column = 1;
+      break;
+    }
+
+    case 'Age': {
+      column = 2;
+      break;
+    }
+
+    case 'Salary': {
+      column = 3;
+      break;
+    }
+  }
+
+  sortTable1(rows, column);
+  tbody.append(...rows);
 });
