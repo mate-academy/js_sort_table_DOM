f445dc94340755286196cc237846aff7917c6c89
Dima Cherednyk <dicherednyk@gmail.com>
2022-03-09 22:45:01 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('tbody');
+const person = body.querySelectorAll('tr');
+const head = document.querySelector('thead');
+
+head.addEventListener('click', (e) => {
+  const order = e.target;
+
+  if (order.textContent === 'Name') {
+    const y = [...person].map(x => x);
+    const res = y.sort((a, b) =>
+      ((a.children[0].textContent).localeCompare(b.children[0].textContent)));
+
+    for (const key of res) {
+      body.append(key);
+    };
+  };
+
+  if (order.textContent === 'Position') {
+    const y = [...person].map(x => x);
+    const res = y.sort((a, b) =>
+      ((a.children[1].textContent).localeCompare(b.children[1].textContent)));
+
+    for (const key of res) {
+      body.append(key);
+    };
+  };
+
+  if (order.textContent === 'Age') {
+    const y = [...person].map(x => x);
+    const res = y.sort((a, b) =>
+      (parseInt(a.children[2].textContent)
+  - parseInt(b.children[2].textContent)));
+
+    for (const key of res) {
+      body.append(key);
+    };
+  };
+
+  if (order.textContent === 'Salary') {
+    const y = [...person].map(x => x);
+    const res = y.sort((a, b) =>
+      (parseFloat(a.children[3].textContent.slice(1))
+  - parseFloat(b.children[3].textContent.slice(1))));
+
+    for (const key of res) {
+      body.append(key);
+    };
+  };
+});
