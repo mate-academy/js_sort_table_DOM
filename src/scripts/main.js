e661523f38b352fcc4e983e4e4ec3c9f6b2caa17
Dima Cherednyk <dicherednyk@gmail.com>
2022-03-10 19:19:08 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,43 +8,47 @@ head.addEventListener('click', (e) => {
   const order = e.target;
 
   if (order.textContent === 'Name') {
-    const y = [...person].map(x => x);
-    const res = y.sort((a, b) =>
+    const persons = [...person];
+
+    persons.sort((a, b) =>
       ((a.children[0].textContent).localeCompare(b.children[0].textContent)));
 
-    for (const key of res) {
+    for (const key of persons) {
       body.append(key);
     };
   };
 
   if (order.textContent === 'Position') {
-    const y = [...person].map(x => x);
-    const res = y.sort((a, b) =>
+    const persons = [...person];
+
+    persons.sort((a, b) =>
       ((a.children[1].textContent).localeCompare(b.children[1].textContent)));
 
-    for (const key of res) {
+    for (const key of persons) {
       body.append(key);
     };
   };
 
   if (order.textContent === 'Age') {
-    const y = [...person].map(x => x);
-    const res = y.sort((a, b) =>
+    const persons = [...person];
+
+    persons.sort((a, b) =>
       (parseInt(a.children[2].textContent)
   - parseInt(b.children[2].textContent)));
 
-    for (const key of res) {
+    for (const key of persons) {
       body.append(key);
     };
   };
 
   if (order.textContent === 'Salary') {
-    const y = [...person].map(x => x);
-    const res = y.sort((a, b) =>
+    const persons = [...person];
+
+    persons.sort((a, b) =>
       (parseFloat(a.children[3].textContent.slice(1))
   - parseFloat(b.children[3].textContent.slice(1))));
 
-    for (const key of res) {
+    for (const key of persons) {
       body.append(key);
     };
   };
