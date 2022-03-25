e5b359b996fa352de741dde5f8f7b43d0ce640b9
Dmytro Bychkivskyi <dbychkivskyi@cccis.com>
2022-03-25 14:10:52 +0200

    Finish hw.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableHeaders = table.querySelector('thead');
+
+tableHeaders.addEventListener('click', (e) => {
+  sortColumn(e.target.cellIndex);
+});
+
+function sortColumn(columnIndex) {
+  const rows = [];
+
+  for (let i = 1; i < table.rows.length - 1; i++) {
+    rows.push(table.rows[i]);
+  }
+
+  rows.sort((a, b) => {
+    if (columnIndex === 3) {
+      return parseSalary(a.children[columnIndex].innerText)
+        - parseSalary(b.children[columnIndex].innerText);
+    }
+
+    return a.children[columnIndex].innerText
+      .localeCompare(b.children[columnIndex].innerText);
+  });
+
+  for (const row of rows) {
+    table.append(row);
+  }
+}
+
+function parseSalary(salaryString) {
+  return +salaryString.replaceAll('$', '').replaceAll(',', '');
+}
