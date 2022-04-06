0eb4041b5e98def2bb7386302e402c94a70c5075
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-04-06 23:16:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const rows = tBody.querySelectorAll('tr');
+
+tHead.addEventListener('click', (events) => {
+  const index = events.target.cellIndex;
+  const sorted = [...rows];
+
+  if (index === 0 || index === 1) {
+    sorted.sort((a, b) => {
+      return a.cells[index].textContent.localeCompare(
+        b.cells[index].textContent);
+    });
+  }
+
+  if (index === 2 || index === 3) {
+    sorted.sort((a, b) => {
+      const elementA = a.cells[index].textContent.split(',').join('').slice(1);
+      const elementB = b.cells[index].textContent.split(',').join('').slice(1);
+
+      return elementA - elementB;
+    });
+  }
+
+  tBody.append(...sorted);
+});
