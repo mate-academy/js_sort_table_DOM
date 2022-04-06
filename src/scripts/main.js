30f5a52ed01309edc41d5b0166b97e1b7b84371c
Vladyslav317 <you@example.com>
2022-04-06 09:23:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const rows = document.querySelector('tbody').rows;
+const table = document.querySelector('table');
+
+function getToNumber(str) {
+  return str.split('$').join('').split(',').join('');
+}
+
+table.addEventListener('click', e => {
+  const targeted = e.target.closest('th');
+
+  if (!targeted) {
+    return;
+  }
+
+  const cellIndex = targeted.cellIndex;
+
+  const sorted = [...rows].sort((a, b) => {
+    const first = getToNumber(a.cells[cellIndex].textContent);
+    const second = getToNumber(b.cells[cellIndex].textContent);
+
+    if (isNaN(first)) {
+      return first.localeCompare(second);
+    } else {
+      return first - second;
+    }
+  });
+
+  tbody.append(...sorted);
+});
