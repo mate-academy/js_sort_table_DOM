8287a35986861bc7e3bf4c8a8c97587da6ea6fac
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-04-06 16:44:54 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const headers = document.querySelector('thead');
+const body = document.querySelector('tbody');
+const rows = body.querySelectorAll('tr');
+const arrRows = [...rows];
+
+headers.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const index = e.target.cellIndex;
+  const headerName = e.target.innerHTML;
+  var sorted;
+
+  if (headerName === 'Name' || headerName === 'Position'){
+    sorted = arrRows.sort((a, b) => {
+      const elementA = a.cells[index].innerHTML;
+      const elementB = b.cells[index].innerHTML;
+      return elementA.localeCompare(elementB);
+    }) 
+  }
+  if (headerName === 'Age' || headerName === 'Salary'){
+    sorted = arrRows.sort((a, b) => {
+      const elementA = a.cells[index].innerHTML.replace(/\$|,/g, '');
+      const elementB = b.cells[index].innerHTML.replace(/\$|,/g, '');
+      return elementA - elementB;
+    })
+  }
+
+  body.append(...sorted);
+});
