980c5c6c28c2a26d2bbb03444b09efe0c8897989
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-18 11:57:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+const sortByABC = (a, b) => {
+  const nameA = a.textContent.toLowerCase();
+  const nameB = b.textContent.toLowerCase();
+
+  if (nameA < nameB) {
+    return -1;
+  }
+
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  return 0;
+};
+
+table.addEventListener('click', e => {
+  const item = e.target;
+  const column = [];
+
+  if (!item.closest('thead')) {
+    return;
+  }
+
+  table.querySelectorAll('tbody tr').forEach(tr => {
+    column.push(tr.querySelectorAll('td')[item.cellIndex]);
+  });
+
+  const result = [...column].sort(sortByABC);
+  let count = 0;
+
+  table.querySelectorAll('tbody tr').forEach(tr => {
+    const td = tr.children[item.cellIndex];
+
+    td.outerHTML = `<td>${result[count].textContent}</td>`;
+    count++;
+  });
+});
