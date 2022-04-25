492f7ed650cc4b94619126de4e46fe719bc247de
Max Shylenko <maxshylenko@gmail.com>
2022-04-25 23:47:31 +0200

    corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,22 @@
 const table = document.querySelector('table');
 const tBody = document.querySelector('tbody');
 const rows = [...tBody.rows];
+let numberOfCell;
 
 const convertToNumber = number =>
   +(number.split('').filter(num => !isNaN(num)).join(''));
+
+const sortNumbers = (a, b) => {
+  return convertToNumber(a.cells[numberOfCell].innerText)
+    - convertToNumber(b.cells[numberOfCell].innerText);
+};
+
+const sortString = (a, b) => {
+  const strA = a.cells[numberOfCell].innerText;
+  const strB = b.cells[numberOfCell].innerText;
+
+  return strA.localeCompare(strB);
+};
 
 table.addEventListener('click', (e) => {
   const sortButton = e.target.closest('thead');
@@ -14,18 +27,16 @@ table.addEventListener('click', (e) => {
     return;
   }
 
-  const numberOfCell = e.target.cellIndex;
+  numberOfCell = e.target.cellIndex;
 
   rows.sort((a, b) => {
-    if (e.target.innerText === 'Salary') {
-      return convertToNumber(a.cells[numberOfCell].innerText)
-        - convertToNumber(b.cells[numberOfCell].innerText);
+    switch (e.target.innerText) {
+      case 'Salary':
+      case 'Age':
+        return sortNumbers(a, b);
+      default:
+        return sortString(a, b);
     }
-
-    const strA = a.cells[numberOfCell].innerText.toString();
-    const strB = b.cells[numberOfCell].innerText.toString();
-
-    return strA.localeCompare(strB);
   });
 
   for (const row of rows) {
