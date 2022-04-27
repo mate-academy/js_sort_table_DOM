fdf19da27047fd3fd6f7cc485a57a0abf954b389
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-27 23:29:43 +0300

    move function from eventlistener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,33 +4,33 @@ const tableHead = document.querySelector('thead');
 const tableBody = document.querySelector('tbody');
 const tableRow = [...tableBody.rows];
 
-tableHead.addEventListener('click', e => {
-  const sortData = (index, tr) => {
-    const words = string => string.children[index].innerText;
-    const numbers = number => {
-      return number
-        .children[index]
-        .innerText
-        .slice(1)
-        .split(',')
-        .join('');
-    };
-
-    switch (tr) {
-      case 'Salary':
-        tableRow.sort((first, last) => {
-          return numbers(first) - numbers(last);
-        });
-        break;
-
-      default:
-        tableRow.sort((first, last) => {
-          return words(first).localeCompare(words(last));
-        });
-    };
-
-    tableBody.append(...tableRow);
+const sortData = (index, tr) => {
+  const words = string => string.children[index].innerText;
+  const numbers = number => {
+    return number
+      .children[index]
+      .innerText
+      .slice(1)
+      .split(',')
+      .join('');
   };
 
+  switch (tr) {
+    case 'Salary':
+      tableRow.sort((first, last) => {
+        return numbers(first) - numbers(last);
+      });
+      break;
+
+    default:
+      tableRow.sort((first, last) => {
+        return words(first).localeCompare(words(last));
+      });
+  };
+
+  tableBody.append(...tableRow);
+};
+
+tableHead.addEventListener('click', e => {
   sortData(e.target.cellIndex, e.target.innerText);
 });
