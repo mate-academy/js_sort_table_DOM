962d0264b0d8186f925c4eeed89364c237d56e7a
Denys Lipin <lipin.transstream@gmail.com>
2022-04-29 18:36:25 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableRows = [...tableBody.rows];
+
+tableHead.addEventListener('click', e => {
+  const i = e.target.cellIndex;
+
+  tableRows.sort((a, b) => {
+    let first = a.cells[i].innerText;
+    let second = b.cells[i].innerText;
+
+    if (i === 3) {
+      first = +first.slice(1).replace(',', '');
+      second = +second.slice(1).replace(',', '');
+
+      return first - second;
+    }
+
+    return first.localeCompare(second);
+  });
+
+  tableBody.append(...tableRows);
+});
