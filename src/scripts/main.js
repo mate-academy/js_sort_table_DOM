d9f206f57c04a072c35adb35c21ebb4ee5bfa292
Lilia Mazur <liliamazur2508@gmail.com>
2022-05-02 19:03:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+
+function sortList(n) {
+  return [...tbody.rows].sort((start, end) => {
+    const a = start.cells[n].innerText.replace(/[$,]/g, '');
+    const b = end.cells[n].innerText.replace(/[$,]/g, '');
+
+    if (isNaN(a)) {
+      return a.localeCompare(b);
+    } else {
+      return a - b;
+    }
+  });
+};
+
+table.addEventListener('click', e => {
+  const item = e.target.closest('th');
+
+  if (!item) {
+    return;
+  }
+
+  const number = item.cellIndex;
+
+  const newList = sortList(number);
+
+  tbody.append(...newList);
+});
