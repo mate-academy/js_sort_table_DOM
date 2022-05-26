49adb50702dccb886985a9670208c336b6c3d6bc
Anastasiia Sydorchuk <nastia.sydorchuk@gmail.com>
2022-05-26 21:06:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rowsArray = [...tbody.rows];
+
+function sortTable(n) {
+  if (n === 3) {
+    rowsArray.sort((a, b) => {
+      let x = a;
+      let y = b;
+
+      x = x.children[n].textContent;
+      y = y.children[n].textContent;
+
+      return +x.split(',').join('').slice(1)
+        - (+y.split(',').join('').slice(1));
+    });
+  } else {
+    rowsArray.sort((a, b) => {
+      let x = a;
+      let y = b;
+
+      x = x.children[n].textContent;
+      y = y.children[n].textContent;
+
+      return x.localeCompare(y);
+    });
+  }
+
+  tbody.append(...rowsArray);
+}
+
+thead.addEventListener('click', (browserEvent) => {
+  const index = browserEvent.target.cellIndex;
+
+  sortTable(index);
+});
