a849e43794e052a2661f2224e8dd23d1ac9efef0
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-26 22:29:56 +0300

    sort-table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const headers = document.querySelectorAll('thead th');
+const body = document.querySelector('tbody');
+const rows = [...body.rows];
+
+function toNumber(str) {
+  return +str.replace('$', '').split(',').join('');
+}
+
+headers.forEach(header => {
+  header.addEventListener('click', (e) => {
+    const headerIndex = [...headers].indexOf(e.target);
+
+    rows.sort((a, b) => {
+      const x = a.cells[headerIndex].innerText;
+      const y = b.cells[headerIndex].innerText;
+
+      if (e.target.innerText === 'Salary') {
+        return toNumber(x) - toNumber(y);
+      }
+
+      return x - y || x.localeCompare(y);
+    });
+
+    body.append(...rows);
+  });
+});
