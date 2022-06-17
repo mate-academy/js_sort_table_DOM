5bbf7af09246ac6145e901953e3500f43126e40a
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-17 11:12:09 +0300

    second try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,40 @@
 'use strict';
 
-const table = document.querySelector('table');
-const tabBody = table.querySelector('tbody');
+const sortProps = [...document.querySelector('tr').children]
+  .map(item => item.innerText);
+const tabBody = document.querySelector('tbody');
 const tabRows = tabBody.querySelectorAll('tr');
-const tR = document.querySelector('tr');
 const rowsArr = [...tabRows];
 
 function SalaryToNumber(salary) {
   return parseInt(salary.substring(1).split(',').join(''));
 }
 
-tR.addEventListener('click', (e) => {
-  const header = [...tR.children].indexOf(e.target);
+const sort = (index, type) => {
+  return rowsArr.sort((current, next) => {
+    const item = current.children[index].innerText;
+    const nextItem = next.children[index].innerText;
 
-  if (e.target.innerText === 'Salary') {
-    const rows = rowsArr.sort((x, y) =>
-      SalaryToNumber(x.children[header].innerText)
-        - SalaryToNumber(y.children[header].innerText));
+    switch (type) {
+      case 'Salary' :
+        return SalaryToNumber(item) - SalaryToNumber(nextItem);
+      case 'Age' :
+        return +item - +nextItem;
+      default:
+        return item.localeCompare(nextItem);
+    }
+  });
+};
 
-    table.appendChild(tabBody);
-
-    rows.forEach(element => {
-      tabBody.appendChild(element);
-    });
-  } else if (e.target.innerText === 'Age') {
-    const rows = rowsArr.sort((x, y) =>
-      +x.children[header].innerText - +y.children[header].innerText);
-
-    table.appendChild(tabBody);
-
-    rows.forEach(element => {
-      tabBody.appendChild(element);
-    });
-  } else {
-    const rows = rowsArr.sort((x, y) =>
-      x.children[header].innerText.localeCompare(y.children[header].innerText));
-
-    table.appendChild(tabBody);
-
-    rows.forEach(element => {
-      tabBody.appendChild(element);
-    });
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('th')) {
+    return;
   }
+
+  const type = e.target.closest('th').innerText;
+  const index = sortProps.indexOf(type);
+
+  const sorted = sort(index, type);
+
+  tabBody.append(...sorted);
 });
