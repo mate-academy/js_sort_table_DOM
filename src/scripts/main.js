fdac8c071a66e22c8d1edd592ee18cfb83e14388
Artem Furdela <furdela.artem@gmail.com>
2022-06-19 23:49:33 +0300

    Sort first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const headers = [...document.querySelector('tr').children];
+const tBody = document.querySelector('tbody');
+const rows = tBody.rows;
+
+const sortingTable = (ind) => {
+  const sortedTable = [...rows].sort((row1, row2) => {
+    if (ind === 3) {
+      return (row1.children[ind].textContent.replace(/[$,]/g, ''))
+        - (row2.children[ind].textContent.replace(/[$,]/g, ''));
+    }
+
+    return row1.children[ind].textContent
+      .localeCompare(row2.children[ind].textContent);
+  });
+
+  tBody.replaceChildren(...sortedTable);
+};
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortingTable(index);
+  });
+});
