ecd58e0da4cb5eb5c9fe649397750e3bcdad8856
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-23 13:06:13 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,42 @@
 'use strict';
 
-const clickable = document.getElementsByTagName('thead')[0]
-  .children[0].children;
+const tableName = document.getElementsByTagName('thead')[0]
+  .children[0].children[0];
+let nameBool = true;
 
-for (let i = 0; i < clickable.length; i++) {
-  clickable[i].addEventListener('click', () => {
-    sort();
-  });
-}
+const tablePosition = document.getElementsByTagName('thead')[0]
+  .children[0].children[1];
+let positionBool = true;
 
-function sort() {
-  // const trs1 = document.getElementsByTagName('tr');
+const tableAge = document.getElementsByTagName('thead')[0]
+  .children[0].children[2];
+let ageBool = true;
+
+const tableSalary = document.getElementsByTagName('thead')[0]
+  .children[0].children[3];
+let salaryBool = true;
+
+tableName.addEventListener('click', () => {
+  sortByAplabet('name', nameBool);
+  nameBool = !nameBool;
+});
+
+tablePosition.addEventListener('click', () => {
+  sortByAplabet('position', positionBool);
+  positionBool = !positionBool;
+});
+
+tableAge.addEventListener('click', () => {
+  sortByNum('age', ageBool);
+  ageBool = !ageBool;
+});
+
+tableSalary.addEventListener('click', () => {
+  sortByNum('salary', salaryBool);
+  salaryBool = !salaryBool;
+});
+
+function sortByNum(arg, bool) {
   const trs1 = document.getElementsByTagName('tbody')[0]
     .children;
   const tbody = document.getElementsByTagName('tbody')[0];
@@ -32,8 +58,15 @@ function sort() {
     people.push(obj);
   }
 
-  const sorted = people.sort((a, b) => parseInt(a.salary)
-    - parseInt(b.salary));
+  let sorted = [];
+
+  if (bool) {
+    sorted = people.sort((a, b) => parseInt(a[arg])
+    - parseInt(b[arg]));
+  } else {
+    sorted = people.sort((a, b) => parseInt(b[arg])
+    - parseInt(a[arg]));
+  }
 
   for (let i = 0; i < trs.length; i++) {
     trs[i].remove();
@@ -46,6 +79,52 @@ function sort() {
         <td>${sorted[i].position}</td>
         <td>${sorted[i].age}</td>
         <td>${sorted[i].salary.toLocaleString('en-US')}</td>
+      </tr>
+  `);
+  }
+}
+
+function sortByAplabet(arg, bool) {
+  const trs1 = document.getElementsByTagName('tbody')[0]
+    .children;
+  const tbody = document.getElementsByTagName('tbody')[0];
+
+  const trs = [...trs1].filter(el => el.parentElement === tbody);
+
+  const people = [];
+
+  for (let i = 0; i < trs.length; i++) {
+    const obj = {};
+
+    obj.name = trs[i].children[0].textContent;
+    obj.position = trs[i].children[1].textContent;
+    obj.age = trs[i].children[2].textContent;
+    obj.salary = trs[i].children[3].textContent;
+
+    people.push(obj);
+  }
+
+  let sorted = [];
+
+  if (bool) {
+    sorted = people
+      .sort((a, b) => a[arg].localeCompare(b[arg]));
+  } else {
+    sorted = people
+      .sort((a, b) => b[arg].localeCompare(a[arg]));
+  }
+
+  for (let i = 0; i < trs.length; i++) {
+    trs[i].remove();
+  }
+
+  for (let i = 0; i < sorted.length; i++) {
+    tbody.insertAdjacentHTML(`beforeend`, `
+    <tr>
+        <td>${sorted[i].name}</td>
+        <td>${sorted[i].position}</td>
+        <td>${sorted[i].age}</td>
+        <td>${sorted[i].salary}</td>
       </tr>
   `);
   }
