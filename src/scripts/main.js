4564ab2832ae28d32fb61458e5fc9612072057da
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-27 22:09:33 +0300

    sort table DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tr = [...tbody.children];
+
+thead.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  const sortedList = tr.sort((a, b) => {
+    // const salaryToNumber = (value) => Number(value.replace(/[$,]/g, ''));
+    // const max = salaryToNumber(a.cells[index].textContent);
+    // const min = salaryToNumber(b.cells[index].textContent);
+
+    const max = a.cells[index].textContent.replace(/[$,]/g, '');
+    const min = b.cells[index].textContent.replace(/[$,]/g, '');
+
+    if (isNaN(max)) {
+      return max.localeCompare(min);
+    } else {
+      return min - max;
+    }
+  });
+
+  tbody.append(...sortedList);
+});
