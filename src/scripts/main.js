dcfb3cbacdcf2181e00d8d9c31a61f6ad075c2c9
Olexandr Istomin <gn.53@ukr.net>
2022-06-28 19:11:57 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', e => {
+  const arrayRows = [...table.tBodies[0].rows];
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return false;
+  }
+
+  function sorting(a, b) {
+    const firstElement = a.cells[th.cellIndex].textContent;
+    const secondtElement = b.cells[th.cellIndex].textContent;
+
+    switch (th.cellIndex) {
+      case 0:
+      case 1:
+        return firstElement.localeCompare(secondtElement);
+
+      case 2:
+        return +firstElement - +secondtElement;
+
+      case 3:
+        const newfirseElement = firstElement.slice(1);
+        const newSecondElement = secondtElement.slice(1);
+
+        return parseFloat(newfirseElement) - parseFloat(newSecondElement);
+    }
+  }
+
+  arrayRows.sort(sorting);
+  table.tBodies[0].append(...arrayRows);
+});
