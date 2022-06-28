469143e30474c441a15da022467ca85a4776ca73
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-28 14:05:54 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write code here
+const th = document.querySelectorAll('th');
+
+for (const head of th) {
+  head.addEventListener('click', sort);
+}
+
+function sort() {
+  const arr = [...document.querySelector('table').rows].slice(1);
+
+  arr.sort((a, b) => parseFloat(a.cells[3].innerHTML
+    .replaceAll('$', '')
+    .replaceAll(',', '')) - parseFloat(b.cells[3].innerHTML
+    .replaceAll('$', '').replaceAll(',', '')));
+
+  const tbody = document.querySelector('tbody');
+
+  tbody.append(...arr);
+}
