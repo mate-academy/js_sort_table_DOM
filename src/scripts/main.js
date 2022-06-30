cc2210e4612012015c143e4c7264eec1c717b875
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-06-30 21:58:01 +0300

    Sort Table DOM try 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,17 +10,13 @@ thead.addEventListener('click', (e) => {
   const index = e.target.cellIndex;
 
   const sortedList = tr.sort((a, b) => {
-    // const salaryToNumber = (value) => Number(value.replace(/[$,]/g, ''));
-    // const max = salaryToNumber(a.cells[index].textContent);
-    // const min = salaryToNumber(b.cells[index].textContent);
-
     const max = a.cells[index].textContent.replace(/[$,]/g, '');
     const min = b.cells[index].textContent.replace(/[$,]/g, '');
 
     if (isNaN(max)) {
       return max.localeCompare(min);
     } else {
-      return min - max;
+      return max - min;
     }
   });
 
