44f2986896569e2f24c37ae97c8611be0a8a2352
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-07-01 14:24:33 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,11 @@ const th = document.querySelectorAll('th');
 
 th[3].addEventListener('click', sortSalary);
 th[2].addEventListener('click', sortAge);
+th[1].addEventListener('click', sortStatus);
+th[0].addEventListener('click', sortName);
 
 function sortSalary() {
-  const arr = [...document.querySelector('table').rows].slice(1);
+  const arr = [...document.querySelectorAll('tbody tr')];
 
   arr.sort((a, b) => parseFloat(a.cells[3].innerHTML
     .replaceAll('$', '')
@@ -20,10 +22,34 @@ function sortSalary() {
 }
 
 function sortAge() {
-  const arr = [...document.querySelector('table').rows].slice(1);
+  const arr = [...document.querySelectorAll('tbody tr')];
 
   arr.sort((a, b) => parseFloat(a.cells[2].innerHTML)
   - parseFloat(b.cells[2].innerHTML));
+
+  const tbody = document.querySelector('tbody');
+
+  tbody.append(...arr);
+}
+
+function sortName() {
+  const arr = [...document.querySelectorAll('tbody tr')];
+
+  arr.sort((a, b) => a.cells[0].innerHTML.toLowerCase().replaceAll(' ', '')
+    .localeCompare(b.cells[0].innerHTML.toLowerCase().replaceAll(' ', ''))
+  );
+
+  const tbody = document.querySelector('tbody');
+
+  tbody.append(...arr);
+}
+
+function sortStatus() {
+  const arr = [...document.querySelectorAll('tbody tr')];
+
+  arr.sort((a, b) => a.cells[1].innerHTML.toLowerCase()
+    .localeCompare(b.cells[1].innerHTML.toLowerCase())
+  );
 
   const tbody = document.querySelector('tbody');
 
