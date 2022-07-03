cb12e7db5c8dda461dda52a8afa68e88a73d4dfa
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-07-03 10:32:35 +0200

    revised

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,57 +1,36 @@
 'use strict';
 
 // write code here
-const th = document.querySelectorAll('th');
 
-th[3].addEventListener('click', sortSalary);
-th[2].addEventListener('click', sortAge);
-th[1].addEventListener('click', sortStatus);
-th[0].addEventListener('click', sortName);
+const tbody = document.querySelector('tbody');
+const th = document.querySelectorAll('thead th');
+const rows = document.querySelectorAll('tbody tr');
 
-function sortSalary() {
-  const arr = [...document.querySelectorAll('tbody tr')];
+[].forEach.call(th, (title, index) => {
+  title.addEventListener('click', () => {
+    sortColumn(index);
+  });
+});
 
-  arr.sort((a, b) => parseFloat(a.cells[3].innerHTML
-    .replaceAll('$', '')
-    .replaceAll(',', '')) - parseFloat(b.cells[3].innerHTML
-    .replaceAll('$', '').replaceAll(',', '')));
+const sortColumn = (index) => {
+  const newRows = [...rows];
 
-  const tbody = document.querySelector('tbody');
+  newRows.sort((a, b) => {
+    const cellA = a.querySelectorAll('td')[index].innerHTML;
+    const cellB = b.querySelectorAll('td')[index].innerHTML;
 
-  tbody.append(...arr);
-}
+    switch (true) {
+      case cellA > cellB: return 1;
+      case cellA < cellB: return -1;
+      case cellA === cellB: return 0;
+    }
+  });
 
-function sortAge() {
-  const arr = [...document.querySelectorAll('tbody tr')];
+  [].forEach.call(rows, (row) => {
+    tbody.removeChild(row);
+  });
 
-  arr.sort((a, b) => parseFloat(a.cells[2].innerHTML)
-  - parseFloat(b.cells[2].innerHTML));
-
-  const tbody = document.querySelector('tbody');
-
-  tbody.append(...arr);
-}
-
-function sortName() {
-  const arr = [...document.querySelectorAll('tbody tr')];
-
-  arr.sort((a, b) => a.cells[0].innerHTML.toLowerCase().replaceAll(' ', '')
-    .localeCompare(b.cells[0].innerHTML.toLowerCase().replaceAll(' ', ''))
-  );
-
-  const tbody = document.querySelector('tbody');
-
-  tbody.append(...arr);
-}
-
-function sortStatus() {
-  const arr = [...document.querySelectorAll('tbody tr')];
-
-  arr.sort((a, b) => a.cells[1].innerHTML.toLowerCase()
-    .localeCompare(b.cells[1].innerHTML.toLowerCase())
-  );
-
-  const tbody = document.querySelector('tbody');
-
-  tbody.append(...arr);
-}
+  newRows.forEach((newRow) => {
+    tbody.appendChild(newRow);
+  });
+};
