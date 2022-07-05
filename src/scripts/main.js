f9d49407a319b219dca083b235f45f7408c1a838
Konstantin <ivanchenkomypost@gmail.com>
2022-07-05 23:10:58 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+function parseSalary(sum) {
+  return +sum.slice(1).split(',').join('');
+}
+
+const head = document.querySelector('thead');
+const rows = [...document.querySelectorAll('tbody>tr')];
+const body = document.querySelector('tbody');
+
+head.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const index = e.target.cellIndex;
+
+  if (index < 2) {
+    rows.sort((a, b) => {
+      const aText = a.children[index].textContent;
+      const bText = b.children[index].textContent;
+
+      return aText.localeCompare(bText);
+    });
+  }
+
+  if (index === 2) {
+    rows.sort((a, b) => {
+      const ageA = +a.children[index].textContent;
+      const ageB = +b.children[index].textContent;
+
+      return ageA - ageB;
+    });
+  }
+
+  if (index === 3) {
+    rows.sort((a, b) => {
+      const aSalary = parseSalary(a.children[index].textContent);
+      const bSalary = parseSalary(b.children[index].textContent);
+
+      return aSalary - bSalary;
+    });
+  }
+
+  body.innerHTML = '';
+  body.append(...rows);
+});
