99fe2c47ed0b461b331f9066df4ce68e736cef4c
Tanya Hryn <tanyagryn1984@gmail.com>
2022-07-11 22:07:43 +0300

    sorted-table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write code here
+
+const header = document.querySelector('thead');
+const bodyTable = document.querySelector('tbody');
+
+header.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+  const rows = bodyTable.rows;
+
+  const sortedList = [...rows].sort((a, b) => {
+    const row1 = a.cells[index].innerText.replace(/[$,]/g, '');
+    const row2 = b.cells[index].innerText.replace(/[$,]/g, '');
+
+    if (e.target.innerText === 'Name' || e.target.innerText === 'Position') {
+      return row1.localeCompare(row2);
+    } else {
+      return row1 - row2;
+    }
+  });
+
+  bodyTable.append(...sortedList);
+});
