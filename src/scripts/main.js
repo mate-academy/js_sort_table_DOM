ce4f28297bc186926a38dc75c248ab2c038feee7
Anton Ustavshchykov <unton84@gmail.com>
2022-07-14 16:59:58 +0300

    variables fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,6 @@
 
 const main = document.querySelector('table');
 let count;
-let x;
-let y;
 
 main.addEventListener('click', (e) => {
   do {
@@ -13,22 +11,24 @@ main.addEventListener('click', (e) => {
 
     if (e.target.tagName === 'TH') {
       for (let i = 2; i < (rows.length - 1); i++) {
-        x = rows[i - 1].getElementsByTagName('TD')[e.target.cellIndex]
-          .innerHTML.toLowerCase().split(',').join('').split('$').join('');
+        const prev = rows[i - 1].getElementsByTagName('TD')[e.target.cellIndex];
+        const prevStr = prev.innerHTML.toLowerCase()
+          .split(',').join('').split('$').join('');
 
-        y = rows[i].getElementsByTagName('TD')[e.target.cellIndex]
-          .innerHTML.toLowerCase().split(',').join('').split('$').join('');
+        const current = rows[i].getElementsByTagName('TD')[e.target.cellIndex];
+        const currentStr = current.innerHTML.toLowerCase()
+          .split(',').join('').split('$').join('');
 
         switch (e.target.innerHTML) {
           case 'Age':
           case 'Salary':
-            if (Number(x) > Number(y)) {
+            if (Number(prevStr) > Number(currentStr)) {
               rows[i].parentNode.insertBefore(rows[i], rows[i - 1]);
               count++;
             }
             break;
           default:
-            if (x.localeCompare(y) === 1) {
+            if (prevStr.localeCompare(currentStr) === 1) {
               rows[i].parentNode.insertBefore(rows[i], rows[i - 1]);
               count++;
             }
