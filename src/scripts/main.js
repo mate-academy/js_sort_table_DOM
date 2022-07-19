4c43847f43c45ed171485d8119987bea8fbd0507
Sergii Maniurov <maniurov_sv@ukr.net>
2022-07-19 16:50:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+tHead.addEventListener('click', e => {
+  const th = [...tHead.rows[0].children].indexOf(e.target);
+
+  [...tBody.rows].sort((a, b) =>
+    th > 1
+      ? a.children[th].textContent.replace(/\$|,/g, '')
+        - b.children[th].textContent.replace(/\$|,/g, '')
+      : a.children[th].textContent.localeCompare(b.children[th].textContent))
+    .forEach(row => tBody.append(row));
+});
