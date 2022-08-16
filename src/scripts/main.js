0281760da1b55ccab226854d1f8552c1b40ed3fc
Andrii Melnyk <andriimelnyq@gmail.com>
2022-08-16 22:09:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tableRows = [...tBody.children];
+const tableHeads = [...document.querySelectorAll('th')];
+
+tHead.addEventListener('click', e => {
+  const indexOfColumn = tableHeads.indexOf(e.target);
+
+  tableRows.sort((a, b) =>
+
+    (['Age', 'Salary'].includes(e.target.innerText))
+
+      ? (a.children[indexOfColumn].innerText.replace(/[^+\d]/g, '')
+        - b.children[indexOfColumn].innerText.replace(/[^+\d]/g, ''))
+
+      : (a.children[indexOfColumn].innerText
+        .localeCompare(b.children[indexOfColumn].innerText))
+  );
+
+  tableRows.forEach(row => tBody.append(row));
+});
