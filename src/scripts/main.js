7b735392afaaeccd44e63621b1dbdf9c82901f39
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-17 15:03:45 +0300

    code was optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,12 +14,12 @@ titles.forEach((element, i) => (
 
     if (i === 0 || i === 1) {
       titleItems
-        .sort((a, b) => a.textContent.localeCompare(b.textContent))
-        .forEach(item => table.appendChild(item.parentElement));
+        .sort((a, b) => a.textContent.localeCompare(b.textContent));
+    } else {
+      titleItems
+        .sort((a, b) => getNumber(a.textContent) - getNumber(b.textContent));
     }
 
-    titleItems
-      .sort((a, b) => getNumber(a.textContent) - getNumber(b.textContent))
-      .forEach(item => table.appendChild(item.parentElement));
+    titleItems.forEach(item => table.appendChild(item.parentElement));
   })
 ));
