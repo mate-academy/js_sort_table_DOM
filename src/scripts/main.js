90fb2405c8ac077e78c974ee4fd110818a319f77
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-09-02 19:14:29 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tBodyTr = document.querySelectorAll('tbody tr');
+
+tHead.addEventListener('click', e => {
+  const item = e.target;
+
+  switch (item.innerText) {
+    case ('Name'):
+      const sortedName = [...tBodyTr].sort((a, b) =>
+        a.children[0].innerText.localeCompare(b.children[0].innerText));
+
+      tBody.innerHTML = '';
+      sortedName.forEach(tr => tBody.appendChild(tr));
+      break;
+
+    case ('Position'):
+      const sortedPos = [...tBodyTr].sort((a, b) =>
+        a.children[1].innerText.localeCompare(b.children[1].innerText));
+
+      tBody.innerHTML = '';
+      sortedPos.forEach(tr => tBody.appendChild(tr));
+
+      break;
+    case ('Age'):
+
+      const sortedAge = [...tBodyTr].sort((a, b) =>
+        a.children[2].innerText.localeCompare(b.children[2].innerText));
+
+      tBody.innerHTML = '';
+      sortedAge.forEach(tr => tBody.appendChild(tr));
+      break;
+    case ('Salary'):
+
+      const sortedSalary = [...tBodyTr].sort((a, b) =>
+        a.children[3].innerHTML.replace(/[$,]/g, '')
+        - b.children[3].innerHTML.replace(/[$,]/g, ''));
+
+      tBody.innerHTML = '';
+      sortedSalary.forEach(tr => tBody.appendChild(tr));
+      break;
+  }
+});
