f924b57832e3988433c00cb771886ad0e47c8446
Hovera-Liubomyr <ljusuk@gmail.com>
2022-09-04 12:27:25 +0300

    sort_table_dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const body = document.querySelector('tbody');
+
+head.addEventListener('click', e => {
+  const index = e.target.closest('th').cellIndex;
+  const rows = [...body.rows];
+
+  const sortTable = rows.sort((a, b) => {
+    const valueA = a.cells[index].innerText.replace(/[$,]/g, '');
+    const valueB = b.cells[index].innerText.replace(/[$,]/g, '');
+
+    if (isNaN(valueA)) {
+      return valueA.localeCompare(valueB);
+    } else {
+      return valueA - valueB;
+    };
+  });
+
+  body.append(...sortTable);
+});
