999b22d837fa14406175bb5b930945513090805a
Misha Kostov <kostov.mihail321@gmail.com>
2022-09-13 22:45:26 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const body = document.querySelector('tbody');
+
+head.addEventListener('click', (event) => {
+  const position = event.target.cellIndex;
+
+  const sort = [...body.children].sort((a, b) => {
+    const sortA = a.cells[position].textContent.replace(',', '')
+      .replace('$', '');
+    const sortB = b.cells[position].textContent.replace(',', '')
+      .replace('$', '');
+
+    if (isNaN(sortA)) {
+      return sortA.localeCompare(sortB);
+    }
+
+    return sortA - sortB;
+  });
+
+  body.append(...sort);
+});
