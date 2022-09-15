c60fcf2e79dcab21bd5935e3f02c026aab8aeddf
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-15 16:12:01 +0300

    sort table with delegetion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,59 +1,44 @@
 'use strict';
 
-const tableBody = document.querySelector('tbody');
-const headlines = document.querySelector('thead')
-  .getElementsByTagName('th');
-const lowerHeadlines = document.querySelector('tfoot')
-  .getElementsByTagName('th');
+const table = document.getElementsByTagName('table')[0];
 
-for (let i = 0; i < headlines.length; i++) {
-  headlines[i].addEventListener('click', () => {
-    Array.from(tableBody.children)
-      .sort((prevRow, currentRow) =>
-        sortedTable(prevRow, currentRow, i))
+table.addEventListener('click', (evt) => {
+  const targetEl = evt.target.closest('th');
+
+  if (targetEl) {
+    const arrTBody = Array.from(table.rows).slice(1, -1);
+
+    arrTBody.sort((prevRow, currentRow) => {
+      const index = [].indexOf.call(targetEl.parentNode.children, targetEl);
+      const prev = prevRow.cells[index].textContent;
+      const current = currentRow.cells[index].textContent;
+
+      switch (index) {
+        case 2:
+          return Number(prev) - Number(current);
+        case 3:
+
+          return getNumber(prev) - getNumber(current);
+
+        default:
+          if (prev === current) {
+            return 0;
+          }
+
+          if (prev > current) {
+            return 1;
+          }
+
+          return -1;
+      }
+    })
       .forEach(row => {
         row.remove();
-        tableBody.append(row);
+        table.children[1].append(row);
       });
-  });
-
-  lowerHeadlines[i].addEventListener('click', () => {
-    Array.from(tableBody.children)
-      .sort((prevRow, currentRow) =>
-        sortedTable(prevRow, currentRow, i))
-      .forEach(row => {
-        row.remove();
-        tableBody.append(row);
-      });
-  });
-}
-
-function sortedTable(prevRow, currentRow, i) {
-  switch (i) {
-    case 2:
-      return prevRow.cells[i].textContent
-        - currentRow.cells[i].textContent;
-    case 3:
-      return prevRow.cells[i].textContent
-        .split('$')[1]
-        .split(',')
-        .join('')
-        - currentRow.cells[i].textContent
-          .split('$')[1]
-          .split(',')
-          .join('');
-
-    default:
-      if (prevRow.cells[i].textContent
-        === currentRow.cells[i].textContent) {
-        return 0;
-      }
-
-      if (prevRow.cells[i].textContent
-        > currentRow.cells[i].textContent) {
-        return 1;
-      } else {
-        return -1;
-      }
   }
-}
+});
+
+function getNumber(str) {
+  return Number(str.split('$')[1].split(',').join(''));
+};
