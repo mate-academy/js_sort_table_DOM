b4a008a1dfda42840659727ec9e728f8f674317f
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-09-26 12:04:33 +0300

    Solution Sort of employees table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tableHeader = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const rows = [...document.body.querySelector('tbody').rows];
+
+function sortTable(e) {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  if (th.textContent === 'Salary') {
+    rows.sort((trA, trB) => formatingSalary(trA.cells[th.cellIndex].textContent)
+      - (formatingSalary(trB.cells[th.cellIndex].textContent)));
+  } else {
+    rows.sort((trA, trB) => trA.cells[th.cellIndex].textContent
+      .localeCompare(trB.cells[th.cellIndex].textContent));
+  }
+
+  rows.forEach(tr => tableBody.append(tr));
+}
+
+function formatingSalary(salary) {
+  return +salary.split('').filter(item =>
+    '0123456789'.includes(item)).join('');
+}
+
+tableHeader.addEventListener('click', sortTable);
