425200a6fdbb4a64bc04bcef1612468f41fee13f
Serhii Yunnikov <yunnikov@icloud.com>
2022-10-08 12:54:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const heading = document.querySelector('thead').firstElementChild;
+const headingItems = [...heading.querySelectorAll('th')];
+const tableBody = document.querySelector('tbody');
+const tableRows = [...tableBody.children];
+
+headingItems.forEach(item => item.setAttribute('class', 'column'));
+
+function tableSort(column) {
+  const index = headingItems.indexOf(column);
+
+  const callbackSort = (rowA, rowB) => {
+    const first = rowA.children[index].innerText;
+    const second = rowB.children[index].innerText;
+
+    if (index <= 1) {
+      return first.localeCompare(second);
+    } else if (index === 3) {
+      return first.slice(1).split(',').join('')
+      - second.slice(1).split(',').join('');
+    }
+
+    return first - second;
+  };
+
+  const sorted = tableRows.sort(callbackSort);
+
+  tableRows.forEach(item => item.remove());
+  sorted.forEach(item => tableBody.append(item));
+}
+
+heading.addEventListener('click', (e) => {
+  const item = e.target.closest('.column');
+
+  if (!item || !heading.contains(item)) {
+    return;
+  }
+
+  tableSort(item);
+});
