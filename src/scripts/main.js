352329f7bc96c31706630b256973dcaa4a74683a
Oleksiy Uchaev <corob.dir@gmail.com>
2022-10-16 10:40:09 +0300

    sorting solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const headTopic = [...document.querySelectorAll('thead th')]
+  .map(i => i.innerText);
+
+const tableBody = document.querySelector('tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
+
+tableHead.addEventListener('click', (e) => {
+  let sorted;
+  const target = e.target.closest('th').innerText;
+  const cellNum = headTopic.indexOf(target);
+
+  if (cellNum >= 2) {
+    sorted = rows.sort((a, b) =>
+      cell(a, cellNum) - (cell(b, cellNum)));
+  } else {
+    sorted = rows.sort((a, b) =>
+      cell(a, cellNum).localeCompare(cell(b, cellNum)));
+  };
+  sorted.map(i => tableBody.appendChild(i));
+});
+
+const cell = (row, n) => row.children[n]
+  .textContent.replace(/[$,]/g, '');
