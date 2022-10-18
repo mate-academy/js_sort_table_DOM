c8b78e598af066e87e5a29b7f26ebc9879e366d6
Mykhailo Oblat <moblat890@gmail.com>
2022-10-18 14:57:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const theadList = [...document.querySelector('thead tr').children];
+const rows = [...tbody.rows];
+
+document.body.addEventListener('click', e => {
+  const index = theadList.indexOf(e.target);
+
+  if (e.target.tagName !== 'TH') {
+    return;
+  };
+
+  rows.sort((a, b) => {
+    const elementA = a.cells[index].textContent;
+    const elementB = b.cells[index].textContent;
+
+    if (e.target.textContent === 'Salary') {
+      return salaryToNumber(elementA) - salaryToNumber(elementB);
+    }
+
+    return elementA.localeCompare(elementB);
+  });
+
+  function salaryToNumber(sal) {
+    return +sal.slice(1).split(',').join('');
+  };
+
+  tbody.append(...rows);
+});
