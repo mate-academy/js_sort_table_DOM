f7eebc024f67dff1c2dae496b53033d06c47e43d
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-10-19 20:07:06 +0300

    sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const rows = Array.from(table.rows).slice(1, -1);
+
+document.addEventListener('click', function(ev) {
+  const indexCell = ev.target.cellIndex;
+  const sortedRows = rows.sort((a, b) => {
+    if (ev.target.textContent === 'Name'
+      || ev.target.textContent === 'Position') {
+      return a.cells[indexCell].textContent
+        .localeCompare(b.cells[indexCell].textContent);
+    }
+
+    if (ev.target.textContent === 'Age') {
+      return a.cells[indexCell].textContent - b.cells[indexCell].textContent;
+    }
+
+    if (ev.target.textContent === 'Salary') {
+      return a.cells[indexCell].textContent.replace('$', '').replace(',', '')
+      - b.cells[indexCell].textContent.replace('$', '').replace(',', '');
+    }
+  });
+
+  table.tBodies[0].append(...sortedRows);
+});
