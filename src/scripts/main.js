d20267e25cdb4268f7e56d90c27b11206a1cee35
Mykhailo Oblat <moblat890@gmail.com>
2022-10-19 21:28:45 +0300

    fixed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,14 @@
 const tbody = document.querySelector('tbody');
 const theadList = [...document.querySelector('thead tr').children];
 const rows = [...tbody.rows];
+const table = document.querySelector('table');
 
-document.body.addEventListener('click', e => {
+function salaryToNumber(sal) {
+  return +sal.slice(1).split(',').join('');
+};
+
+table.addEventListener('click', e => {
   const index = theadList.indexOf(e.target);
-
-  function salaryToNumber(sal) {
-    return +sal.slice(1).split(',').join('');
-  };
 
   if (e.target.tagName !== 'TH') {
     return;
