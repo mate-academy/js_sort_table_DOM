37d704031a776aec84638d79f061f4501f18cd9b
Oleksandr Naumchak <naumchak@ukr.net>
2022-10-20 21:29:54 +0300

    Bugfix (first solution)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,44 @@
 'use strict';
 
-const table = document.querySelector('table');
-let rows = document.querySelectorAll('tbody > tr');
-const headers = document.querySelectorAll('thead > tr > th');
+const headers = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
 
-const sortTable = (elem) => {
-  for (let i = 0; i < 4; i++) {
-    if (elem.target === headers[i]) {
-      rows = [...rows].sort((a, b) => {
-        let stringBegin = a.querySelectorAll('td')[i].innerText;
-        let stringEnd = b.querySelectorAll('td')[i].innerText;
-
-        if (elem.target.innerText === 'Age') {
-          return +stringBegin - +stringEnd;
-        }
-
-        if (elem.target.innerText === 'Salary') {
-          stringBegin = parseFloat(stringBegin.slice(1));
-          stringEnd = parseFloat(stringEnd.slice(1));
-
-          return +stringBegin - +stringEnd;
-        }
-
-        return stringBegin.localeCompare(stringEnd);
-      });
-    }
-  }
-
-  for (const row of rows) {
-    table.append(row);
-  }
+const convertToNumber = (str) => {
+  return +parseFloat(str.slice(1));
 };
 
-table.addEventListener('click', sortTable);
+const sortTable = (elem) => {
+  const item = elem.target;
+
+  if (!item.matches('th')) {
+    return;
+  };
+
+  if (item.innerText === 'Name') {
+    rows.sort((a, b) =>
+      a.children[0].innerText.localeCompare(b.children[0].innerText));
+  };
+
+  if (item.innerText === 'Position') {
+    rows.sort((a, b) =>
+      a.children[1].innerText.localeCompare(b.children[1].innerText));
+  };
+
+  if (item.innerText === 'Age') {
+    rows.sort((a, b) =>
+      +a.children[2].innerText - +b.children[2].innerText);
+  };
+
+  if (item.innerText === 'Salary') {
+    rows.sort((a, b) =>
+      convertToNumber(a.children[3].innerText)
+      - convertToNumber(b.children[3].innerText));
+  };
+
+  for (let i = 0; i < (rows.length - 1); i++) {
+    rows[i].after(rows[i + 1]);
+  };
+};
+
+headers.addEventListener('click', sortTable);
