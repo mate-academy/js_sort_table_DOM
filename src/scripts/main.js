5bdd5d8ef23ca2229b0a12901561d9892dc8726f
Zykhor Andrii <zuxoran@gmail.com>
2022-10-21 14:05:43 +0300

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,42 @@
 'use strict';
 
-// write code here
-const tr = [...document.querySelectorAll('tr')].slice(1, -1);
-const first = document.querySelector('tr');
-const tb = document.querySelector('tbody');
+const listTable = [...document.querySelectorAll('tr')].slice(1, -1);
+const headerTable = document.querySelector('tr');
+const table = document.querySelector('tbody');
 
 const convertNum = salary => +salary.slice(1).split(',').join('');
 
-first.addEventListener('click', e => {
+headerTable.addEventListener('click', e => {
   if (e.target.innerText === 'Name') {
-    const sort = tr.sort((a, b) => {
+    const sort = listTable.sort((a, b) => {
       return a.children[0].innerText.localeCompare(b.children[0].innerText);
     });
 
-    tb.append(...sort);
+    table.append(...sort);
   }
 
   if (e.target.innerText === 'Position') {
-    const sort = tr.sort((a, b) => {
+    const sort = listTable.sort((a, b) => {
       return a.children[1].innerText.localeCompare(b.children[1].innerText);
     });
 
-    tb.append(...sort);
+    table.append(...sort);
   }
 
   if (e.target.innerText === 'Age') {
-    const sort = tr.sort((a, b) => {
+    const sort = listTable.sort((a, b) => {
       return a.children[2].innerText - b.children[2].innerText;
     });
 
-    tb.append(...sort);
+    table.append(...sort);
   }
 
   if (e.target.innerText === 'Salary') {
-    const sort = tr.sort((a, b) => {
+    const sort = listTable.sort((a, b) => {
       return convertNum(a.children[3].innerText)
     - convertNum(b.children[3].innerText);
     });
 
-    tb.append(...sort);
+    table.append(...sort);
   }
 });
