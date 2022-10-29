7a84092500ecaf807a3c6e0ea9fb1a6b88862d79
Sergii Iatsenko <sergii.iatsenko@gmail.com>
2022-10-29 23:33:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+const employeeList = [...document.querySelectorAll('tr')].slice(1, -1);
+const mainList = document.querySelector('tbody');
+const menu = document.querySelector('thead').firstElementChild.children;
+
+function salarySort(list) {
+  list.sort((a, b) => (+a.children[3].innerText.slice(1).split(',').join(''))
+   - (+b.children[3].innerText.slice(1).split(',').join('')));
+
+  mainList.append(...list);
+}
+
+function ageSort(list) {
+  list.sort((a, b) => (+a.children[2].innerText) - (+b.children[2].innerText));
+
+  mainList.append(...list);
+}
+
+function positionSort(list) {
+  list.sort((a, b) => (a.children[1].innerText
+    .localeCompare(b.children[1].innerText)));
+
+  mainList.append(...list);
+}
+
+function nameSort(list) {
+  list.sort((a, b) => (a.children[0].innerText
+    .localeCompare(b.children[0].innerText)));
+
+  mainList.append(...list);
+}
+
+menu[3].addEventListener('click', () => {
+  salarySort(employeeList);
+});
+
+menu[2].addEventListener('click', () => {
+  ageSort(employeeList);
+});
+
+menu[1].addEventListener('click', () => {
+  positionSort(employeeList);
+});
+
+menu[0].addEventListener('click', () => {
+  nameSort(employeeList);
+});
