cc1db52e23d03c16ddc34704f82ecdbaab89c523
Zykhor Andrii <zuxoran@gmail.com>
2022-10-30 17:56:46 +0200

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,36 +7,40 @@ const table = document.querySelector('tbody');
 const convertNum = salary => +salary.slice(1).split(',').join('');
 
 headerTable.addEventListener('click', e => {
-  if (e.target.innerText === 'Name') {
-    const sort = listTable.sort((a, b) => {
-      return a.children[0].innerText.localeCompare(b.children[0].innerText);
-    });
+  let sort;
 
-    table.append(...sort);
-  }
+  switch (e.target.innerText) {
 
-  if (e.target.innerText === 'Position') {
-    const sort = listTable.sort((a, b) => {
-      return a.children[1].innerText.localeCompare(b.children[1].innerText);
-    });
+    case 'Name':
+      sort = listTable.sort((a, b) => {
+        return a.children[0].innerText.localeCompare(b.children[0].innerText);
+      });
 
-    table.append(...sort);
-  }
+      table.append(...sort);
+      break;
 
-  if (e.target.innerText === 'Age') {
-    const sort = listTable.sort((a, b) => {
-      return a.children[2].innerText - b.children[2].innerText;
-    });
+    case 'Position':
+      sort = listTable.sort((a, b) => {
+        return a.children[1].innerText.localeCompare(b.children[1].innerText);
+      });
 
-    table.append(...sort);
-  }
+      table.append(...sort);
+      break;
 
-  if (e.target.innerText === 'Salary') {
-    const sort = listTable.sort((a, b) => {
-      return convertNum(a.children[3].innerText)
-    - convertNum(b.children[3].innerText);
-    });
+    case 'Age':
+      sort = listTable.sort((a, b) => {
+        return a.children[2].innerText - b.children[2].innerText;
+      });
 
-    table.append(...sort);
+      table.append(...sort);
+      break;
+
+    case 'Salary':
+      sort = listTable.sort((a, b) => {
+        return convertNum(a.children[3].innerText)
+          - convertNum(b.children[3].innerText);
+      });
+
+      table.append(...sort);
   }
 });
