83fce80d543fd9bfe36cc9ec20a35051fbab8b27
Oleksandr Lytvynenko <olek.lytvynenko@gmail.com>
2022-11-04 17:18:32 +0200

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('tbody').children;
+const objList = [...list].map(item => ({
+  Name: item.children[0].textContent,
+  Position: item.children[1].textContent,
+  Age: +item.children[2].textContent,
+  Salary: +item.children[3].textContent.replace(/[^0-9]/g, ''),
+}));
+
+const sortTable = (array, data) => {
+  array.sort((a, b) => {
+    return isNaN(a[data])
+      ? a[data].localeCompare(b[data])
+      : a[data] - b[data];
+  });
+};
+
+document.addEventListener('click', (e) => {
+  sortTable(objList, e.target.innerText);
+
+  for (let i = 0; i < objList.length; i++) {
+    list[i].children[0].innerText = objList[i].Name;
+    list[i].children[1].innerText = objList[i].Position;
+    list[i].children[2].innerText = objList[i].Age;
+
+    list[i].children[3].textContent = `$${
+      objList[i].Salary.toLocaleString('en-US')
+    }`;
+  }
+});
