707ba6c5a9224664bb4ff402882079f1f610ae3a
July Terekhova <sky206380@gmail.com>
2022-11-14 05:08:22 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.tHead.addEventListener('click', (ev) => {
+  const item = ev.target;
+  const cell = item.cellIndex;
+  const row = [];
+
+  for (let i = 1; i < table.rows.length - 1; i++) {
+    const values = [];
+
+    for (let j = 0; j < table.rows[0].cells.length; j++) {
+      const value = table.rows[i].cells[j].innerText;
+
+      values.push(value);
+    }
+
+    row.push(values);
+  }
+
+  row.sort((a, b) => {
+    const ifIncludeNumber = convertToNumber(a[cell]);
+
+    return ifIncludeNumber
+      ? convertToNumber(a[cell]) - convertToNumber(b[cell])
+      : a[cell].localeCompare(b[cell]);
+  });
+
+  for (let i = 1; i < table.rows.length - 1; i++) {
+    const x = table.rows[i].cells;
+
+    [...x].forEach((el, a) => {
+      el.innerText = row[i - 1][a];
+    });
+  }
+});
+
+function convertToNumber(string) {
+  let result = '';
+
+  for (const char of string) {
+    if (isFinite(char)) {
+      result += char;
+    }
+  }
+
+  return +result;
+};
