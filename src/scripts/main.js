1c2a166b8b5d3514af893b64d2e80a498dfb861d
Anastasiia Horidieieva <gordeevaisgreat@gmail.com>
2022-11-20 18:18:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const getSalaryNumber = (str) =>
+  Number(str.trim().split(',').join('').slice(1));
+
+document.querySelector('thead').addEventListener('click', function(e) {
+  const clicked = e.target;
+  const type = clicked.textContent;
+  const index = [...clicked.closest('tr').children].indexOf(clicked);
+
+  const tableBody = document.querySelector('tbody');
+  const tableData = [...tableBody.children];
+
+  tableData.sort((a, b) => {
+    const firstText = a.children[index].textContent;
+    const secondText = b.children[index].textContent;
+
+    switch (type) {
+      case 'Position':
+      case 'Name':
+        return (firstText.localeCompare(secondText));
+      case 'Age':
+        return firstText - secondText;
+      case 'Salary':
+        return getSalaryNumber(firstText) - getSalaryNumber(secondText);
+    };
+  });
+  tableBody.innerHTML = '';
+  tableData.forEach(el => tableBody.append(el));
+});
