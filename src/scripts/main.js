dce45d88d3c56d102f68b5c6155e818c5cf15a86
Yurii Teslia <teslya.yuriy@gmail.com>
2022-12-02 03:04:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+const headTable = document.querySelector('thead');
+const bodyTable = document.querySelector('tbody');
+
+const headers = getFieldsFromRow(headTable.firstElementChild);
+
+const arr = [];
+
+function getFieldsFromRow(element) {
+  const result = [];
+
+  for (const item of element.children) {
+    result.push(item.innerText);
+  }
+
+  return result;
+}
+
+for (const row of bodyTable.children) {
+  arr.push(getFieldsFromRow(row));
+}
+
+const sortItems = (items, index) => {
+  items.sort((a, b) => {
+    if (+a[index].replace(/\D/g, '')) {
+      return a[index].replace(/\D/g, '') - b[index].replace(/\D/g, '');
+    } else {
+      return a[index].localeCompare(b[index]);
+    }
+  });
+
+  for (let i = 0; i < bodyTable.children.length; i++) {
+    for (let j = 0; j < bodyTable.children[i].children.length; j++) {
+      bodyTable.children[i].children[j].innerText = items[i][j];
+    }
+  }
+};
+
+for (const item of headTable.firstElementChild.children) {
+  item.addEventListener('click', e => {
+    sortItems(arr, headers.indexOf(e.target.innerText));
+  });
+}
