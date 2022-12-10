3296bcbfe3d5db7f7f61855c1eaf0800d2265d64
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-10 13:40:05 +0200

    Some excess code has been removed, swith case has been added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,46 +3,46 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const tr = tbody.rows;
+let sort;
 
 thead.addEventListener('click', e => {
   if (e.target.tagName !== 'TH') {
     return;
   }
 
-  if (e.target.textContent === 'Name') {
-    const sortNames = function(arr) {
-      return [...arr].sort((a, b) =>
-        a.children[0].textContent < b.children[0].textContent ? -1 : 1);
-    };
+  switch (e.target.textContent) {
+    case 'Name':
+      sort = function(arr) {
+        return [...arr].sort((a, b) =>
+          a.children[0].textContent < b.children[0].textContent ? -1 : 1);
+      };
+      break;
 
-    tbody.append(...sortNames(tr));
+    case 'Position':
+      sort = function(arr) {
+        return [...arr].sort((a, b) =>
+          a.children[1].textContent < b.children[1].textContent ? -1 : 1);
+      };
+      break;
+
+    case 'Age':
+      sort = function(arr) {
+        return [...arr].sort((a, b) =>
+          +a.children[2].textContent - +b.children[2].textContent);
+      };
+      break;
+
+    case 'Salary':
+      sort = function(arr) {
+        return [...arr].sort((a, b) =>
+          +a.children[3].textContent.slice(1).split(',').join('')
+            - +b.children[3].textContent.slice(1).split(',').join(''));
+      };
+      break;
+
+    default:
+      return;
   }
 
-  if (e.target.textContent === 'Position') {
-    const sortPosition = function(arr) {
-      return [...arr].sort((a, b) =>
-        a.children[1].textContent < b.children[1].textContent ? -1 : 1);
-    };
-
-    tbody.append(...sortPosition(tr));
-  }
-
-  if (e.target.textContent === 'Age') {
-    const sortAges = function(arr) {
-      return [...arr].sort((a, b) =>
-        +a.children[2].textContent - +b.children[2].textContent);
-    };
-
-    tbody.append(...sortAges(tr));
-  }
-
-  if (e.target.textContent === 'Salary') {
-    const sortSalaries = function(arr) {
-      return [...arr].sort((a, b) =>
-        +a.children[3].textContent.slice(1).split(',').join('')
-          - +b.children[3].textContent.slice(1).split(',').join(''));
-    };
-
-    tbody.append(...sortSalaries(tr));
-  }
+  tbody.append(...sort(tr));
 });
