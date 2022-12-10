84cacaa23717f15e06d92a54a4281d154ad868ab
Artem Hirzhev <a.hirzhev@gmail.com>
2022-12-10 18:26:53 +0200

    "switch case" has been simplified

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,46 +3,38 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const tr = tbody.rows;
-let sort;
 
 thead.addEventListener('click', e => {
+  let sort;
+  const index = e.target.cellIndex;
+
   if (e.target.tagName !== 'TH') {
     return;
   }
 
   switch (e.target.textContent) {
-    case 'Name':
-      sort = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[0].textContent < b.children[0].textContent ? -1 : 1);
-      };
-      break;
-
-    case 'Position':
-      sort = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[1].textContent < b.children[1].textContent ? -1 : 1);
-      };
-      break;
-
     case 'Age':
-      sort = function(arr) {
+      sort = function(arr, i) {
         return [...arr].sort((a, b) =>
-          +a.children[2].textContent - +b.children[2].textContent);
+          +a.children[i].textContent - +b.children[i].textContent);
       };
       break;
 
     case 'Salary':
-      sort = function(arr) {
+      sort = function(arr, i) {
         return [...arr].sort((a, b) =>
-          +a.children[3].textContent.slice(1).split(',').join('')
-            - +b.children[3].textContent.slice(1).split(',').join(''));
+          +a.children[i].textContent.slice(1).split(',').join('')
+            - +b.children[i].textContent.slice(1).split(',').join('')
+        );
       };
       break;
 
     default:
-      return;
+      sort = function(arr, i) {
+        return [...arr].sort((a, b) =>
+          a.children[i].textContent < b.children[i].textContent ? -1 : 1);
+      };
   }
 
-  tbody.append(...sort(tr));
+  tbody.append(...sort(tr, index));
 });
