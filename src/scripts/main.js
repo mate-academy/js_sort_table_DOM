14b94066e30cf106121ee10d11f523311f8b0dc1
Yurii Teslia <teslya.yuriy@gmail.com>
2022-12-11 05:33:30 +0200

    improvements implemented according to suggestions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,42 +3,43 @@
 const headTable = document.querySelector('thead');
 const bodyTable = document.querySelector('tbody');
 
-const headers = getFieldsFromRow(headTable.firstElementChild);
+const headers = Array.from(headTable.firstElementChild.children);
+const arrayOfRows = [...bodyTable.rows];
 
-const arr = [];
+headTable.addEventListener('click', e => {
+  const targetElement = e.target;
+  const orderByColumnIndex = headers.indexOf(targetElement);
+  const orderByColumnName = targetElement.innerText.toLowerCase();
 
-function getFieldsFromRow(element) {
-  const result = [];
+  bodyTable.append(
+    ...sortItems(arrayOfRows, orderByColumnName, orderByColumnIndex));
+});
 
-  for (const item of element.children) {
-    result.push(item.innerText);
-  }
-
-  return result;
-}
-
-for (const row of bodyTable.children) {
-  arr.push(getFieldsFromRow(row));
-}
-
-const sortItems = (items, index) => {
-  items.sort((a, b) => {
-    if (+a[index].replace(/\D/g, '')) {
-      return a[index].replace(/\D/g, '') - b[index].replace(/\D/g, '');
-    } else {
-      return a[index].localeCompare(b[index]);
-    }
-  });
-
-  for (let i = 0; i < bodyTable.children.length; i++) {
-    for (let j = 0; j < bodyTable.children[i].children.length; j++) {
-      bodyTable.children[i].children[j].innerText = items[i][j];
-    }
+const sortItems = (items, orderBy, index) => {
+  switch (orderBy) {
+    case 'name':
+    case 'position':
+      return items.sort((a, b) =>
+        sortString(a, b, index)
+      );
+    case 'age':
+    case 'salary':
+      return items.sort((a, b) =>
+        sortNumber(a, b, index)
+      );
   }
 };
 
-for (const item of headTable.firstElementChild.children) {
-  item.addEventListener('click', e => {
-    sortItems(arr, headers.indexOf(e.target.innerText));
-  });
+function sortString(prevRow, actualRow, index) {
+  const prevText = prevRow.children[index].innerText;
+  const actText = actualRow.children[index].innerText;
+
+  return prevText.localeCompare(actText);
+}
+
+function sortNumber(prevRow, actualRow, index) {
+  const prevNumber = +prevRow.children[index].innerText.replace(/\D/g, '');
+  const actNumber = +actualRow.children[index].innerText.replace(/\D/g, '');
+
+  return prevNumber - actNumber;
 }
