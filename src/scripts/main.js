9d3167f50ab10d5bd622a9450f6134d07b101fe8
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-12-12 11:37:41 +0200

    Update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,86 +5,72 @@ const tBody = document.querySelector('tbody');
 const rows = [...tBody.children];
 let reverse = false;
 
-function handleEvent(e) {
+function sortedBy(a, b, string, boolean) {
+  switch (string) {
+    case 'Name':
+    case 'Position':
+      return boolean
+        ? b.textContent.localeCompare(a.textContent)
+        : a.textContent.localeCompare(b.textContent);
+
+    case 'Age':
+    case 'Salary':
+      return !+a.textContent
+        ? boolean
+          ? +b.textContent.slice(1).split(',').join('')
+            - +a.textContent.slice(1).split(',').join('')
+          : +a.textContent.slice(1).split(',').join('')
+            - +b.textContent.slice(1).split(',').join('')
+        : boolean
+          ? +b.textContent - +a.textContent
+          : +a.textContent - +b.textContent;
+
+    default:
+      break;
+  }
+}
+
+function handleEvent(evnt) {
   let newList = [...rows];
 
-  if (reverse) {
-    reverse = false;
+  switch (evnt.target.textContent) {
+    case 'Name':
+      newList = [...rows].sort(
+        (a, b) =>
+          sortedBy(a.children[0], b.children[0], 'Name', reverse)
+      );
+      break;
 
-    switch (e.target.textContent) {
-      case 'Name':
-        newList = [...rows].sort(
-          (a, b) =>
-            b.children[0].textContent.localeCompare(a.children[0].textContent)
-        );
-        break;
+    case 'Position':
+      newList = [...rows].sort(
+        (a, b) =>
+          sortedBy(a.children[1], b.children[1], 'Position', reverse)
+      );
+      break;
 
-      case 'Position':
-        newList = [...rows].sort(
-          (a, b) =>
-            b.children[1].textContent.localeCompare(a.children[1].textContent)
-        );
-        break;
+    case 'Age':
+      newList = [...rows].sort(
+        (a, b) =>
+          sortedBy(a.children[2], b.children[2], 'Age', reverse)
+      );
+      break;
 
-      case 'Age':
-        newList = [...rows].sort(
-          (a, b) =>
-            +b.children[2].textContent - +a.children[2].textContent
-        );
-        break;
+    case 'Salary':
+      newList = [...rows].sort(
+        (a, b) =>
+          sortedBy(a.children[3], b.children[3], 'Salary', reverse)
+      );
+      break;
 
-      case 'Salary':
-        newList = [...rows].sort(
-          (a, b) =>
-            +b.children[3].textContent.slice(1).split(',').join('')
-              - +a.children[3].textContent.slice(1).split(',').join('')
-        );
-        break;
-
-      default:
-        break;
-    }
-  } else {
-    reverse = true;
-
-    switch (e.target.textContent) {
-      case 'Name':
-        newList = [...rows].sort(
-          (a, b) =>
-            a.children[0].textContent.localeCompare(b.children[0].textContent)
-        );
-        break;
-
-      case 'Position':
-        newList = [...rows].sort(
-          (a, b) =>
-            a.children[1].textContent.localeCompare(b.children[1].textContent)
-        );
-        break;
-
-      case 'Age':
-        newList = [...rows].sort(
-          (a, b) =>
-            +a.children[2].textContent - +b.children[2].textContent
-        );
-        break;
-
-      case 'Salary':
-        newList = [...rows].sort(
-          (a, b) =>
-            +a.children[3].textContent.slice(1).split(',').join('')
-              - +b.children[3].textContent.slice(1).split(',').join('')
-        );
-        break;
-
-      default:
-        break;
-    }
+    default:
+      break;
   }
 
   for (const item of newList) {
     tBody.append(item);
   }
+
+  reverse = !reverse;
 }
 
 tHead.addEventListener('click', handleEvent);
