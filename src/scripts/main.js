bebdd4acccf1d93a3f8a35e2beb63e61152f476a
Sergey Mironenko <serg1prime@gmail.com>
2022-12-13 13:47:59 +0200

    Sort table second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,11 @@ const tableHead = document.querySelector('thead');
 const headers = tableHead.querySelectorAll('th');
 const rows = document.querySelector('tbody').querySelectorAll('tr');
 const headerText = {};
-let count;
+let count = 0;
+
+function toNumber(text) {
+  return Number(text.split(',').join('').split('$').join(''));
+}
 
 for (let i = 0; i < [...headers].length; i++) {
   headerText[headers[i].textContent] = i;
@@ -17,12 +21,13 @@ tableHead.addEventListener('click', action => {
   function replace(prev, current, compareCondition) {
     if (compareCondition) {
       count++;
+
       const prevContent = prev.innerHTML;
       const currentContent = current.innerHTML;
 
       prev.innerHTML = currentContent;
       current.innerHTML = prevContent;
-    }  
+    }
   }
 
   if (!targetElement.matches('th')) {
@@ -39,10 +44,8 @@ tableHead.addEventListener('click', action => {
       if (prevText[0].match(/[A-Z]/i)) {
         replace(prevRow, currentRow, prevText.localeCompare(currentText) === 1);
       } else {
-        const prevNumber
-          = Number(prevText.split(',').join('').split('$').join(''));
-        const currentNumber
-          = Number(currentText.split(',').join('').split('$').join(''));
+        const prevNumber = toNumber(prevText);
+        const currentNumber = toNumber(currentText);
 
         replace(prevRow, currentRow, prevNumber > currentNumber);
       }
