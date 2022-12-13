8585583db422c79d440565b0f49e1cce43039830
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2022-12-13 23:54:47 +0200

    Sort table solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', e => {
+  const th = e.target.closest('th');
+
+  if (!th || !table.contains(th)) {
+    return;
+  }
+
+  sortTable(th.cellIndex, th.innerHTML);
+});
+
+function sortTable(index, text) {
+  const tbody = document.querySelector('tbody');
+  const row = [...tbody.children];
+
+  switch (text) {
+    case 'Name':
+    case 'Position':
+      row.sort((a, b) =>
+        a.cells[index].innerHTML.localeCompare(b.cells[index].innerHTML));
+      break;
+
+    case 'Age':
+      row.sort((a, b) =>
+        a.cells[index].innerHTML - b.cells[index].innerHTML);
+      break;
+
+    case 'Salary':
+      row.sort((a, b) =>
+        normalNumber(a.cells[index].innerHTML)
+          - normalNumber(b.cells[index].innerHTML));
+      break;
+  }
+
+  tbody.append(...row);
+}
+
+function normalNumber(number) {
+  return +number.slice(1).split(',').join('');
+}
