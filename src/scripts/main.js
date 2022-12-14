91080aeba6b4ec61d11c7aab59e1f35f2ed136ab
Sergey Vorobey <sergey88nik@gmail.com>
2022-12-14 15:51:42 +0200

    Sort table solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+const title = document.querySelector('thead');
+const table = document.querySelector('tbody');
+let asc = true;
+let active;
+
+function sortTable(collectionRows, idx) {
+  asc = true;
+
+  return Array.from(collectionRows).sort((a, b) => {
+    const v1 = a.cells[idx].innerText;
+    const v2 = b.cells[idx].innerText;
+
+    if (isNaN(num(v1))) {
+      return v1.localeCompare(v2);
+    } else {
+      return num(v1) - num(v2);
+    }
+  });
+}
+
+function reSortTable(value, idx) {
+  asc = false;
+
+  return Array.from(value).sort((a, b) => {
+    const v1 = a.cells[idx].innerText;
+    const v2 = b.cells[idx].innerText;
+
+    if (isNaN(num(v1))) {
+      return v2.localeCompare(v1);
+    } else {
+      return num(v2) - num(v1);
+    }
+  });
+}
+
+function num(str) {
+  return +str.replace(/[,$]/g, '');
+}
+
+title.addEventListener('click', (ev) => {
+  const targ = ev.target;
+  const idx = targ.cellIndex;
+
+  if (active === idx && asc) {
+    return reSortTable(table.rows, idx).forEach(row => {
+      table.append(row);
+    });
+  }
+
+  sortTable(table.rows, idx).forEach(row => {
+    table.append(row);
+  });
+  active = idx;
+});
