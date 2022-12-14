ab85c7901b369710dc0c20c73972879bd8774f69
Uliana Kudriavtseva <uliana.kudriavtseva@gmail.com>
2022-12-14 13:11:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tableHeader = document.querySelector('thead');
+const table = document.querySelector('tbody');
+
+const getSalaryNumber = (str) =>
+  Number(str.split(',').join('').slice(1));
+
+tableHeader.addEventListener('click', (e) => {
+  const clicked = e.target;
+  const type = clicked.textContent;
+  const index = [...clicked.closest('tr').children].indexOf(clicked);
+
+  const tableData = [...table.children];
+
+  tableData.sort((a, b) => {
+    const firstText = a.children[index].textContent;
+    const secondText = b.children[index].textContent;
+
+    switch (type) {
+      case 'Position':
+      case 'Name':
+        return (firstText.localeCompare(secondText));
+      case 'Age':
+        return firstText - secondText;
+      case 'Salary':
+        return getSalaryNumber(firstText) - getSalaryNumber(secondText);
+    };
+  });
+
+  table.innerHTML = '';
+  tableData.forEach(el => table.append(el));
+});
