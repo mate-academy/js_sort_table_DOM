a8e4ea022a1efa93a22587c0498868cefe092435
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-12-15 21:23:54 +0300

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,39 +10,36 @@ for (const element of navListElements) {
       return item.textContent;
     };
 
-    if (action.target === navListElements[0]) {
-      const sortedNames = tbodyChildrens.sort(
-        (a, b) => getTextContent(a.children[0]).localeCompare(
-          getTextContent(b.children[0])));
+    let sortedRows = null;
 
-      tbody.append(...sortedNames);
+    switch (action.target) {
+      case navListElements[0]:
+        sortedRows = tbodyChildrens.sort(
+          (a, b) => getTextContent(a.children[0]).localeCompare(
+            getTextContent(b.children[0])));
+        break;
+
+      case navListElements[1]:
+        sortedRows = tbodyChildrens.sort(
+          (a, b) => getTextContent(a.children[1]).localeCompare(
+            getTextContent(b.children[1])));
+        break;
+
+      case navListElements[2]:
+        sortedRows = tbodyChildrens.sort(
+          (a, b) => +a.children[2].textContent - +b.children[2].textContent);
+        break;
+
+      default:
+        const makeNumber = (string) => {
+          return +string.slice(1).split(',').join('');
+        };
+
+        sortedRows = tbodyChildrens.sort(
+          (a, b) => (makeNumber(a.children[3].textContent))
+              - (makeNumber(b.children[3].textContent)));
     }
 
-    if (action.target === navListElements[1]) {
-      const sortedPosition = tbodyChildrens.sort(
-        (a, b) => getTextContent(a.children[1]).localeCompare(
-          getTextContent(b.children[1])));
-
-      tbody.append(...sortedPosition);
-    }
-
-    if (action.target === navListElements[2]) {
-      const sortedAge = tbodyChildrens.sort(
-        (a, b) => +a.children[2].textContent - +b.children[2].textContent);
-
-      tbody.append(...sortedAge);
-    }
-
-    if (action.target === navListElements[3]) {
-      const makeNumber = (string) => {
-        return +string.slice(1).split(',').join('');
-      };
-
-      const sortedSalary = tbodyChildrens.sort(
-        (a, b) => (makeNumber(a.children[3].textContent))
-            - (makeNumber(b.children[3].textContent)));
-
-      tbody.append(...sortedSalary);
-    }
+    tbody.append(...sortedRows);
   });
 }
