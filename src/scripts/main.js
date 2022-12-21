97f8f1cf0467efa6d5b756f2ae186e4082759081
Deniskakaka <krollik1000000@gmail.com>
2022-12-21 18:09:27 +0200

    soolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('tbody');
+const people = [];
+
+const getNumber = (string) => {
+  return parseFloat(string.replace('$', ''));
+};
+
+for (let i = 0; i < body.children.length; i++) {
+  people.push({
+    name: body.children[i].children[0].innerText,
+    position: body.children[i].children[1].innerText,
+    age: body.children[i].children[2].innerText,
+    salary: body.children[i].children[3].innerText,
+  });
+}
+
+const sortBy = document.querySelectorAll('th');
+
+sortBy.forEach(el => {
+  el.addEventListener('click', (e) => {
+    body.innerHTML = '';
+
+    if (e.target.innerText === 'Name') {
+      people.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    if (e.target.innerText === 'Age') {
+      people.sort((a, b) => a.age - b.age);
+    }
+
+    if (e.target.innerText === 'Position') {
+      people.sort((a, b) => b.position.localeCompare(a.position));
+    }
+
+    if (e.target.innerText === 'Salary') {
+      people.sort((a, b) => getNumber(a.salary) - getNumber(b.salary));
+    }
+
+    people.forEach(q => {
+      body.insertAdjacentHTML('beforeend',
+        `<tr>
+            <td>${q.name}</td>
+            <td>${q.position}</td>
+            <td>${q.age}</td>
+            <td>${q.salary}</td>
+          </tr>`);
+    });
+  });
+});
