b6f5a0b53f1bb1a744671c311da95ae8ac89b39b
Denis Bokov <denisbokov1703@gmail.com>
2022-12-27 22:10:51 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
 // write code here
+
+// const head = document.querySelector('thead');
+// const body = document.querySelector('tbody');
+
+// const copyBodyEl = [...body.children];
+
+// head.addEventListener('click', (event) => {
+//     const itemId = event.target.cellIndex;
+
+//     const sort = copyBodyEl.sort((a, b) => {
+//         const first = a.cells[itemId].textContent;
+//         const second = b.cells[itemId].textContent;
+
+//         // if (first.slice(0, 1) === '$') {
+//         //     first = first.replace(/[^+\d]/g, '');
+//         //     second = second.replace(/[^+\d]/g, '')
+
+//         //     return first - second
+
+//         // }
+
+//         return first.localeCompare(second);
+//     });
+
+//     body.append(...sort);
+// });
+
+const head = document.querySelector('thead');
+const body = document.querySelector('tbody');
+
+const bodyChildren = [...body.children];
+
+head.addEventListener('click', (event) => {
+  const currentItem = event.target.cellIndex;
+
+  console.log(currentItem);
+
+  const sort = bodyChildren.sort((a, b) => {
+    const first = a.cells[currentItem].textContent;
+    const second = b.cells[currentItem].textContent;
+
+    return first.localeCompare(second);
+  });
+
+  body.append(...sort);
+});
