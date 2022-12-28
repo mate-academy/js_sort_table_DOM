66124babeccd34231d66ca2ec3792aab045e16ac
Vlad Boikov <vladboikov95@hotmail.com>
2022-12-28 15:44:35 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('tbody');
+const pers = [...body.querySelectorAll('tr')];
+
+document.querySelector('thead').addEventListener('click', e => {
+  const coords = e.target;
+
+  pers.sort((a, b) => {
+    switch (coords.innerText) {
+      case 'Name':
+        return a.children[coords.cellIndex].innerText
+          .localeCompare(b.children[coords.cellIndex].innerText);
+
+      case 'Position':
+        return a.children[coords.cellIndex].innerText
+          .localeCompare(b.children[coords.cellIndex].innerText);
+
+      case 'Age':
+        return +a.children[coords.cellIndex].innerText
+        - +b.children[coords.cellIndex].innerText;
+
+      case 'Salary':
+        return trimmer(a.children[coords.cellIndex].innerText)
+        - trimmer(b.children[coords.cellIndex].innerText);
+    }
+  });
+
+  body.append(...pers);
+});
+
+function trimmer(salary) {
+  return salary.replace(/\D/g, '');
+};
