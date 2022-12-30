7e62e7fb9dd8f92f0c6c8ec26989cfbb29d5f5a7
Denis Bokov <denisbokov1703@gmail.com>
2022-12-30 01:34:00 +0100

    Solution (2)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,23 @@
 'use strict';
 
-// write code here
-
-// const head = document.querySelector('thead');
-// const body = document.querySelector('tbody');
-
-// const copyBodyEl = [...body.children];
-
-// head.addEventListener('click', (event) => {
-//     const itemId = event.target.cellIndex;
-
-//     const sort = copyBodyEl.sort((a, b) => {
-//         const first = a.cells[itemId].textContent;
-//         const second = b.cells[itemId].textContent;
-
-//         // if (first.slice(0, 1) === '$') {
-//         //     first = first.replace(/[^+\d]/g, '');
-//         //     second = second.replace(/[^+\d]/g, '')
-
-//         //     return first - second
-
-//         // }
-
-//         return first.localeCompare(second);
-//     });
-
-//     body.append(...sort);
-// });
-
 const head = document.querySelector('thead');
 const body = document.querySelector('tbody');
 
-const bodyChildren = [...body.children];
+const copyBodyEl = [...body.children];
 
 head.addEventListener('click', (event) => {
-  const currentItem = event.target.cellIndex;
+  const itemId = event.target.cellIndex;
 
-  console.log(currentItem);
+  const sort = copyBodyEl.sort((a, b) => {
+    let first = a.cells[itemId].textContent;
+    let second = b.cells[itemId].textContent;
 
-  const sort = bodyChildren.sort((a, b) => {
-    const first = a.cells[currentItem].textContent;
-    const second = b.cells[currentItem].textContent;
+    if (first.slice(0, 1) === '$') {
+      first = first.replace(/[^+\d]/g, '');
+      second = second.replace(/[^+\d]/g, '');
+
+      return first - second;
+    }
 
     return first.localeCompare(second);
   });
