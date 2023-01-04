5a98e0aff516f0fe8123f5c73625b657a34d9506
Dmitriy Sporysh <dopmoneyd@gmail.com>
2023-01-04 22:41:30 +0200

    some solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableHeader = table.rows[0];
+
+tableHeader.addEventListener('click', e => {
+  const item = e.target.closest('th');
+
+  if (!item || !tableHeader.contains(item)) {
+    return null;
+  }
+
+  const index = e.target.cellIndex;
+
+  tableSort(index);
+});
+
+function tableSort(index) {
+  const arr = Array.from(table.querySelectorAll('tbody tr'));
+
+  arr.sort((a, b) => {
+    let aValue = a.children[index].innerText;
+    let bValue = b.children[index].innerText;
+
+    if (aValue.includes('$')) {
+      aValue = aValue.slice(1).replace(',', '');
+      bValue = bValue.slice(1).replace(',', '');
+
+      return aValue - bValue;
+    }
+
+    return aValue.localeCompare(bValue);
+  });
+
+  arr.forEach(elem => {
+    table.querySelector('tbody').appendChild(elem);
+  });
+}
