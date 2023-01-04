b88b681d221a90180ba8377c4d3efc13e17cffe6
Vasiliy22 <hitretsukrnet@gmail.com>
2023-01-04 16:05:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = table.querySelector('tbody');
+const headers = table.querySelectorAll('th');
+const rows = tableBody.querySelectorAll('tr');
+
+[].forEach.call(headers, (header, index) => {
+  header.addEventListener('click', () => {
+    sortColumn(index);
+  });
+});
+
+const sortColumn = (index) => {
+  const newRows = Array.from(rows);
+
+  newRows.sort((rowA, rowB) => {
+    const cellA = rowA.querySelectorAll('td')[index].innerHTML;
+    const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+
+    switch (true) {
+      case cellA > cellB:
+        return 1;
+      case cellA < cellB:
+        return -1;
+      case cellA === cellB:
+        return 0;
+    }
+  });
+
+  [].forEach.call(rows, (row) => {
+    tableBody.removeChild(row);
+  });
+
+  newRows.forEach((newRow) => {
+    tableBody.appendChild(newRow);
+  });
+};
