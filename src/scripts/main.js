f44f083776dad5048d2321876b45352a5e1bb768
Ivan Vyhinnyi <ivan665303@gmail.com>
2023-01-06 16:48:36 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (ev) => {
+  const element = ev.target.innerHTML;
+  const indx = ev.target.cellIndex;
+  const copy = [...tbody.children];
+  const toNum = el => el.slice(1).split(',').join('');
+
+  switch (element) {
+    case 'Name':
+    case 'Position':
+      copy.sort((a, b) => {
+        return a.cells[indx].innerHTML.localeCompare(b.cells[indx].innerHTML);
+      });
+      break;
+
+    case 'Age':
+      copy.sort((a, b) => {
+        return b.cells[indx].innerHTML - a.cells[indx].innerHTML;
+      });
+      break;
+
+    case 'Salary':
+      copy.sort((a, b) => {
+        return toNum(b.cells[indx].innerHTML) - toNum(a.cells[indx].innerHTML);
+      });
+      break;
+
+    default:
+      return;
+  }
+
+  tbody.append(...copy);
+});
