c85fa886d3d85a5488d886b17f2f75f61d680691
Yaroslav <yaroslavfoorgala@gmail.com>
2023-01-09 16:55:47 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const headers = document.querySelectorAll('th');
+const rows = table.querySelectorAll('tr');
+
+function convertInNum(string) {
+  return Number(string.replace(/[$,]/g, ''));
+}
+
+[...headers].forEach((header, i) => {
+  header.addEventListener('click', () => {
+    const sorted = [...rows].sort((a, b) => {
+      const el1 = a.children[i].textContent;
+      const el2 = b.children[i].textContent;
+
+      if (!isNaN(convertInNum(el1))) {
+        return convertInNum(el1) - convertInNum(el2);
+      } else {
+        return el1.localeCompare(el2);
+      }
+    });
+
+    table.append(...sorted);
+  });
+});
