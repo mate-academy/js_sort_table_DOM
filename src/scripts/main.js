135989065eb823018191be113235794f4b7e36f4
Avichez <starkavichi@gmail.com>
2023-01-13 09:49:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+function removeSymbols(text) {
+  return text.replace(/\W/g, '');
+}
+
+// eslint-disable-next-line
+tableHead.addEventListener('click', (event) => {
+  const index = event.target.cellIndex;
+
+  const sortedList = [...tableBody.rows].sort((elementA, elementB) => {
+    const textA = removeSymbols(elementA.cells[index].innerText);
+    const textB = removeSymbols(elementB.cells[index].innerText);
+
+    if (isNaN(textA)) {
+      return textA.localeCompare(textB);
+    }
+
+    return textA - textB;
+  });
+
+  tableBody.append(...sortedList);
+});
