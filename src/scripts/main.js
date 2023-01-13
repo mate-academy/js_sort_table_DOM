a265b066ae95c32321b3e3c56911113550ad6f97
yana-kotsulym <yanak7170@gmail.com>
2023-01-13 22:53:39 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+tableHead.addEventListener('click', (e) => {
+  const item = e.target.cellIndex;
+
+  const sorted = [...tableBody.children].sort((a, b) => {
+    let prev = a.cells[item].innerText;
+    let curr = b.cells[item].innerText;
+
+    if (prev.includes('$')) {
+      prev = prev.replace(/[$,]/g, '');
+      curr = curr.replace(/[$,]/g, '');
+
+      return prev - curr;
+    }
+
+    if (isFinite(prev)) {
+      return prev - curr;
+    }
+
+    return prev.localeCompare(curr);
+  });
+
+  tableBody.append(...sorted);
+});
