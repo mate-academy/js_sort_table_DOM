97b373908e4f828f8e48d483df88d76ebe9e5433
Taras Baran <baranprivat22@gmail.com>
2023-01-22 23:52:42 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+window.addEventListener('DOMContentLoaded', () => {
+  const tableTitle = document.querySelector('table thead');
+  const tablePeople = document.querySelector('table tbody');
+
+  function sortElement(list, index) {
+    return list.sort((x, y) => {
+      const parsedX = x.children[index].textContent.replace(/\W/g, '');
+      const parsedY = y.children[index].textContent.replace(/\W/g, '');
+
+      if (!isNaN(parsedX)) {
+        return parsedX - parsedY;
+      }
+
+      return parsedX.localeCompare(parsedY);
+    });
+  }
+
+  tableTitle.addEventListener('click', e => {
+    const { target } = e;
+
+    if (target.tagName === 'TH') {
+      const newArr = sortElement([...tablePeople.children], target.cellIndex);
+
+      tablePeople.innerHTML = '';
+
+      newArr.forEach(el => {
+        tablePeople.append(el);
+      });
+    }
+  });
+});
