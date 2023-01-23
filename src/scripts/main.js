b56f7587da2a41559c0e088f84fc783700878d42
valntyn <romanenko.valentyn1@gmail.com>
2023-01-23 11:44:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const menu = document.querySelector('thead');
+const body = document.querySelector('tbody');
+const list = body.querySelectorAll('tr');
+
+menu.addEventListener('click', e => {
+  const item = e.target.closest('th');
+  const index = e.target.cellIndex;
+
+  if (!item || !menu.contains(item)) {
+    return;
+  }
+
+  const sortedCells = [...list]
+    .sort((a, b) => {
+      const aText = a.children[index].innerText;
+      const bText = b.children[index].innerText;
+
+      switch (index) {
+        case 0:
+        case 1:
+          return aText.localeCompare(bText);
+
+        case 2:
+          return +aText - +bText;
+
+        case 3:
+          const aNum = +(aText.replace(/\W/g, ''));
+          const bNum = +(bText.replace(/\W/g, ''));
+
+          return aNum - bNum;
+
+        default:
+          return 0;
+      }
+    });
+
+  sortedCells.forEach(el => {
+    return body.append(el);
+  });
+});
