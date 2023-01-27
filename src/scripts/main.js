2ae7cf265b7774c730ad741b52e690e4f04ab214
Olena Shevchenko <shevochkas@gmail.com>
2023-01-27 18:57:19 +0200

    function sortTableString was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,12 @@ function convertSalary(salary) {
   return +salary.slice(1).split(',').join('');
 };
 
+function sortTableString(columnNumber) {
+  rows.sort((a, b) =>
+    a.children[columnNumber].textContent
+      .localeCompare(b.children[columnNumber].textContent));
+};
+
 const table = document.querySelector('table');
 const tableBody = document.querySelector('tbody');
 const rows = [...tableBody.querySelectorAll('tr')];
@@ -14,15 +20,11 @@ table.addEventListener('click', (e) => {
   if (header) {
     switch (e.target.textContent) {
       case 'Name':
-        rows.sort((a, b) =>
-          a.children[0].textContent
-            .localeCompare(b.children[0].textContent));
+        sortTableString(0);
         break;
 
       case 'Position':
-        rows.sort((a, b) =>
-          a.children[1].textContent
-            .localeCompare(b.children[1].textContent));
+        sortTableString(1);
         break;
 
       case 'Age':
