b49128249389929013337a8b679d5bfa7accb221
Vladyslav Krainyk <vladkraynyk2701@gmail.com>
2023-02-02 16:34:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBody = table.tBodies[0];
+
+const rows = Array.from(tBody.querySelectorAll('tr'));
+
+table.addEventListener('click', e => {
+  const sortParam = e.target.closest('th').cellIndex;
+
+  const sort = rows.sort((a, b) => {
+    const aItem = a.children[sortParam].textContent.trim()
+      .replace('$', '').replace(',', '');
+    const bItem = b.children[sortParam].textContent.trim()
+      .replace('$', '').replace(',', '');
+
+    return +aItem - +bItem || aItem.localeCompare(bItem);
+  });
+
+  tBody.append(...sort);
+});
