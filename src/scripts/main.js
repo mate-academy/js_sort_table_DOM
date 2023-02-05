b5ea8dcb24152acec12d6d34b1f2302858cac662
Gabriella135 <gabriella.ivanjuk@gmail.com>
2023-02-05 19:10:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tableHeader = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+tableHeader.addEventListener('click', e => {
+  const index = e.target.cellIndex;
+
+  const sorted = [...tableBody.rows].sort((a, b) => {
+    const aText = a.cells[index].innerText;
+    const bText = b.cells[index].innerText;
+
+    switch (index) {
+      case 0:
+      case 1:
+        return aText.localeCompare(bText);
+      case 2:
+        return +aText - +bText;
+      case 3:
+        return +aText.replace(/\W/g, '')
+        - +bText.replace(/\W/g, '');
+      default:
+        throw new Error('unexpected cell index');
+    }
+  });
+
+  tableBody.append(...sorted);
+});
