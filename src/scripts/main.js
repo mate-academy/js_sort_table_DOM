ea216bf2d0114ebfd7ab30ba83ba68a992cae134
Maksym Tsarenko <makstsar94@gmail.com>
2023-02-07 14:26:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const headerRow = document.querySelector('table > thead > tr');
+
+const tbody = document.querySelector('tbody');
+const rows = [...document.querySelectorAll('tbody > tr')];
+const salaryToNumber = salary => +salary.slice(1).split(',').join('');
+
+headerRow.addEventListener('click', e => {
+  const cellIndex = e.target.cellIndex;
+
+  rows.sort((a, b) => {
+    const contentA = a.cells[cellIndex].innerText;
+    const contentB = b.cells[cellIndex].innerText;
+
+    if (contentA.toUpperCase() !== contentA.toLowerCase()) {
+      return contentA.localeCompare(contentB);
+    }
+
+    return contentA[0] !== '$' || contentB[0] !== '$'
+      ? contentA - contentB
+      : salaryToNumber(contentA) - salaryToNumber(contentB);
+  });
+
+  tbody.append(...rows);
+});
