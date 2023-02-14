383f3b307a89d58754799989e45576e9e1d6760d
Vasyl Pavlenko <genlus@ukr.net>
2023-02-14 12:12:36 +0200

    Table was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tableHeaders = document.querySelectorAll('th');
+
+tableHeaders.forEach((header) => {
+  header.addEventListener('click', () => {
+    const tableBody = document.querySelector('tbody');
+    const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+    const column = header.cellIndex;
+    const sortDirection = header.dataset.sort === 'asc' ? -1 : 1;
+
+    const sortedRows = rows.sort((a, b) => {
+      const aCell = a.querySelectorAll('td')[column].textContent;
+      const bCell = b.querySelectorAll('td')[column].textContent;
+
+      return sortDirection * aCell.localeCompare(bCell, undefined,
+        { numeric: true });
+    });
+
+    tableBody.innerHTML = '';
+
+    sortedRows.forEach((row) => {
+      tableBody.append(row);
+    });
+  });
+});
