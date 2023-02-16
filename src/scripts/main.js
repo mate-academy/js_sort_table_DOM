3b520b15b9e36da93a969e56ca1f6de875db851f
Nigga <yomzfk@gmail.com>
2023-02-16 17:25:24 +0200

    Sort Table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const trArray = [...tBody.children];
+
+tHead.addEventListener('click', e => {
+  if (!e.target.tagName === 'TH') {
+    return;
+  }
+
+  const targetIndex = e.target.closest('th').cellIndex;
+
+  const sortedArr = trArray.sort((a, b) => {
+    const first = a.children[targetIndex].innerText.replace(/[$,]/g, '');
+    const second = b.children[targetIndex].innerText.replace(/[$,]/g, '');
+
+    if (!isNaN(first)) {
+      return first - second;
+    }
+
+    return first.localeCompare(second);
+  });
+
+  tBody.append(...sortedArr);
+});
