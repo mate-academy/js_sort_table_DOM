496482f7a5afae59b1ab57d90b0e83f12457043a
Volodymyr <vovannty@ukr.net>
2023-02-19 22:39:20 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,42 +3,46 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const tr = tbody.rows;
+let sortNames;
 
 thead.addEventListener('click', (e) => {
-  if (e.target.textContent === 'Name') {
-    const sortNames = function(arr) {
-      return [...arr].sort((a, b) =>
-        a.children[0].textContent < b.children[0].textContent ? -1 : 1);
-    };
+  switch (e.target.textContent) {
+    case 'Name':
+      sortNames = function(arr) {
+        return [...arr].sort((a, b) =>
+          a.children[0].textContent < b.children[0].textContent ? -1 : 1);
+      };
 
-    tbody.append(...sortNames(tr));
-  }
+      tbody.append(...sortNames(tr));
+      break;
 
-  if (e.target.textContent === 'Position') {
-    const sortNames = function(arr) {
-      return [...arr].sort((a, b) =>
-        a.children[1].textContent < b.children[1].textContent ? -1 : 1);
-    };
+    case 'Position':
+      sortNames = function(arr) {
+        return [...arr].sort((a, b) =>
+          a.children[1].textContent < b.children[1].textContent ? -1 : 1);
+      };
 
-    tbody.append(...sortNames(tr));
-  }
+      tbody.append(...sortNames(tr));
+      break;
 
-  if (e.target.textContent === 'Age') {
-    const sortNames = function(arr) {
-      return [...arr].sort((a, b) =>
-        a.children[2].textContent < b.children[2].textContent ? -1 : 1);
-    };
+    case 'Age':
+      sortNames = function(arr) {
+        return [...arr].sort((a, b) =>
+          a.children[2].textContent < b.children[2].textContent ? -1 : 1);
+      };
 
-    tbody.append(...sortNames(tr));
-  }
+      tbody.append(...sortNames(tr));
+      break;
 
-  if (e.target.textContent === 'Salary') {
-    const sortNames = function(arr) {
-      return [...arr].sort((a, b) =>
-        +a.children[3].textContent.slice(1).split(',').join('')
-          - +b.children[3].textContent.slice(1).split(',').join(''));
-    };
+    case 'Salary':
+      sortNames = function(arr) {
+        return [...arr].sort((a, b) =>
+          +a.children[3].textContent.slice(1).split(',').join('')
+            - +b.children[3].textContent.slice(1).split(',').join(''));
+      };
 
-    tbody.append(...sortNames(tr));
+      tbody.append(...sortNames(tr));
+
+      break;
   }
 });
