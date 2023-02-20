c3313688c3247673016ea289d95d52b15ac838cd
Volodymyr <vovannty@ukr.net>
2023-02-20 17:36:55 +0200

    added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,45 +3,29 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const tr = tbody.rows;
-let sortNames;
+const sortByIndex = (items, index) => [...items].sort((a, b) =>
+  a.children[index].textContent < b.children[index].textContent ? -1 : 1);
+
+const sortByNumber = (items, index) => [...items].sort((a, b) =>
+  +a.children[index].textContent.slice(1).split(',').join('')
+    - +b.children[index].textContent.slice(1).split(',').join(''));
 
 thead.addEventListener('click', (e) => {
   switch (e.target.textContent) {
     case 'Name':
-      sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[0].textContent < b.children[0].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
+      tbody.append(...sortByIndex(tr, 0));
       break;
 
     case 'Position':
-      sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[1].textContent < b.children[1].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
+      tbody.append(...sortByIndex(tr, 1));
       break;
 
     case 'Age':
-      sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          a.children[2].textContent < b.children[2].textContent ? -1 : 1);
-      };
-
-      tbody.append(...sortNames(tr));
+      tbody.append(...sortByIndex(tr, 2));
       break;
 
     case 'Salary':
-      sortNames = function(arr) {
-        return [...arr].sort((a, b) =>
-          +a.children[3].textContent.slice(1).split(',').join('')
-            - +b.children[3].textContent.slice(1).split(',').join(''));
-      };
-
-      tbody.append(...sortNames(tr));
+      tbody.append(...sortByNumber(tr, 3));
 
       break;
   }
