e9b34495f7f57bfe0d61e509935029b8f0e69931
Yevhenii Hura <yevheniihura@gmail.com>
2023-02-22 15:04:18 +0200

    the task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', (e) => {
+  const heading = e.target.closest('th');
+
+  if (!heading) {
+    return;
+  }
+
+  const tableBody = table.querySelector('tbody');
+  const headingIndex = e.target.cellIndex;
+
+  const rowsArray = [...tableBody.rows];
+
+  rowsArray.sort((firstElement, secondElement) => {
+    const firstContent = firstElement.cells[headingIndex].textContent;
+    const secondContent = secondElement.cells[headingIndex].textContent;
+
+    if (parseInt(firstContent) === +firstContent) {
+      return firstContent - secondContent;
+    };
+
+    if (isNaN(parseInt(firstContent.slice(1)))) {
+      return firstContent.localeCompare(secondContent);
+    } else {
+      return parseInt(firstContent.slice(1)) - parseInt(secondContent.slice(1));
+    }
+  });
+
+  for (const element of rowsArray) {
+    tableBody.append(element);
+  }
+});
