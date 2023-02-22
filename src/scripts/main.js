8f2659a399b32c1e84fb4f3318a080da6b7aa031
mykolachupyak <chupyak@gmail.com>
2023-02-22 18:28:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const headElements = document
+  .querySelector('thead')
+  .querySelectorAll('th');
+
+for (const item of headElements) {
+  item.addEventListener('click', () => {
+    const table
+      = [...document.querySelector('tbody')
+        .querySelectorAll('tr')]
+        .sort(compareValues);
+
+    function compareValues(aInput, bInput) {
+      let a = aInput.children[item.cellIndex].textContent;
+      let b = bInput.children[item.cellIndex].textContent;
+
+      if (convert(a) > 0) {
+        a = (convert(a));
+        b = (convert(b));
+      }
+
+      return a <= b ? -1 : 1;
+    }
+
+    function convert(stringNumber) {
+      return Number(stringNumber.toLocaleString()
+        .replace(/\D/g, ''));
+    }
+
+    document.querySelector('tbody').append(...table);
+  });
+}
