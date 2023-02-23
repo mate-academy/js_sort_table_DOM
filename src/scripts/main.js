1d2b78308d762bdfc07a3321820b58c68ce5b381
mykolachupyak <chupyak@gmail.com>
2023-02-23 10:29:19 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,19 +12,18 @@ for (const item of headElements) {
   item.addEventListener('click', () => {
     const table
       = [...document.querySelector('tbody').querySelectorAll('tr')]
-        .sort(compareValues);
+        .sort((aInput, bInput) => {
+          let a = aInput.children[item.cellIndex].textContent;
+          let b = bInput.children[item.cellIndex].textContent;
 
-    function compareValues(aInput, bInput) {
-      let a = aInput.children[item.cellIndex].textContent;
-      let b = bInput.children[item.cellIndex].textContent;
+          if (convert(a) > 0) {
+            a = (convert(a));
+            b = (convert(b));
+          }
 
-      if (convert(a) > 0) {
-        a = (convert(a));
-        b = (convert(b));
-      }
+          return a < b ? -1 : 1;
+        });
 
-      return a < b ? -1 : 1;
-    }
     document.querySelector('tbody').append(...table);
   });
 }
