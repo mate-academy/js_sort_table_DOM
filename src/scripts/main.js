3f912f3efa6b76eeef20c6c6cb6fecb5ca735324
mykolachupyak <chupyak@gmail.com>
2023-02-23 11:29:36 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,22 @@ function convert(stringNumber) {
   return Number(stringNumber.toLocaleString().replace(/\D/g, ''));
 }
 
+function compareVariables(aa, bb, eventFunc) {
+  let a = aa.children[eventFunc.target.cellIndex].textContent;
+  let b = bb.children[eventFunc.target.cellIndex].textContent;
+
+  if (convert(a) > 0) {
+    a = (convert(a));
+    b = (convert(b));
+  }
+
+  return a < b ? -1 : 1;
+}
+
 document.querySelector('thead').addEventListener('click', (eventFunc) => {
   const bodyRows = document.querySelector('tbody');
-  const table = [...bodyRows.children].sort((aa, bb) => {
-    let a = aa.children[eventFunc.target.cellIndex].textContent;
-    let b = bb.children[eventFunc.target.cellIndex].textContent;
+  const arrayElements = [...bodyRows.children]
+    .sort((aa, bb) => compareVariables(aa, bb, eventFunc));
 
-    if (convert(a) > 0) {
-      a = (convert(a));
-      b = (convert(b));
-    }
-
-    return a < b ? -1 : 1;
-  });
-
-  bodyRows.append(...table);
+  bodyRows.append(...arrayElements);
 });
