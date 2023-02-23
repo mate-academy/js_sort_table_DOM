55b427b68d1b3de5634e64d11a53d7219eed3bda
mykolachupyak <chupyak@gmail.com>
2023-02-23 11:05:51 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,22 @@
 'use strict';
 
-const headElements = document
-  .querySelector('thead')
-  .querySelectorAll('th');
-
 function convert(stringNumber) {
   return Number(stringNumber.toLocaleString().replace(/\D/g, ''));
 }
 
-for (const item of headElements) {
-  item.addEventListener('click', (ev) => {
-    const index = ev.target.cellIndex;
-    const table
-      = [...document.querySelector('tbody')
-        .querySelectorAll('tr')]
-        .sort((aInput, bInput) => {
-          let a = aInput.children[index].textContent;
-          let b = bInput.children[index].textContent;
+document.querySelector('thead').addEventListener('click', (eventFunc) => {
+  const table = [...document.querySelector('tbody').querySelectorAll('tr')]
+    .sort((aa, bb) => {
+      let a = aa.children[eventFunc.target.cellIndex].textContent;
+      let b = bb.children[eventFunc.target.cellIndex].textContent;
 
-          if (convert(a) > 0) {
-            a = (convert(a));
-            b = (convert(b));
-          }
+      if (convert(a) > 0) {
+        a = (convert(a));
+        b = (convert(b));
+      }
 
-          return a < b ? -1 : 1;
-        });
+      return a < b ? -1 : 1;
+    });
 
-    document.querySelector('tbody').append(...table);
-  });
-}
+  document.querySelector('tbody').append(...table);
+});
