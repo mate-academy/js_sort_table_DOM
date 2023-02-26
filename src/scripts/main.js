c8db2bc2af3617553a0d7b2a8ff353d039fd10d8
Vlad <bojko.vladislav16@gmail.com>
2023-02-26 16:34:14 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = document.querySelectorAll('th');
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.querySelectorAll('tr')];
+
+table.addEventListener('click', e => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  let indexOfCurrentHeader = [...headers].indexOf(e.target);
+
+  rows.sort((a, b) => {
+    const firstData = a.children[indexOfCurrentHeader].innerText;
+    const secondData = b.children[indexOfCurrentHeader].innerText;
+
+    return firstData.localeCompare(secondData, undefined, {
+      numeric: true, sensitivity: 'base',
+    });
+  });
+
+  rows.forEach(row => tbody.append(row));
+});
