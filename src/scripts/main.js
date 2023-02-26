af7e98742112f004c31e3953c3983e16a6b2333f
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-02-26 10:19:03 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,60 +6,29 @@ const data = [...tBody.children];
 
 const titles = document.querySelectorAll('th');
 
-for (const title of Array.from(titles)) {
-  if (title.innerHTML === 'Name') {
-    title.addEventListener('click', (e) => {
-      data.sort((a, b) => {
-        const contentA = a.cells[0].innerHTML;
-        const contentB = b.cells[0].innerHTML;
+function sortFn(cellIndex) {
+  data.sort((a, b) => {
+    const contentA = a.cells[cellIndex].innerHTML;
+    const contentB = b.cells[cellIndex].innerHTML;
 
-        return contentA.localeCompare(contentB);
-      });
+    if (cellIndex === 2) {
+      return contentA - contentB;
+    }
 
-      return tBody.append(...data);
-    });
-  }
+    if (cellIndex === 3) {
+      return getNumber(contentA) - getNumber(contentB);
+    }
 
-  if (title.innerHTML === 'Position') {
-    title.addEventListener('click', (e) => {
-      data.sort((a, b) => {
-        const contentA = a.cells[1].innerHTML;
-        const contentB = b.cells[1].innerHTML;
+    return contentA.localeCompare(contentB);
+  });
+};
 
-        return contentA.localeCompare(contentB);
-      });
-
-      return tBody.append(...data);
-    });
-  }
-
-  if (title.innerHTML === 'Age') {
-    title.addEventListener('click', (e) => {
-      data.sort((a, b) => {
-        const contentA = a.cells[2].innerHTML;
-        const contentB = b.cells[2].innerHTML;
-
-        return contentA - contentB;
-      });
-
-      return tBody.append(...data);
-    });
-  }
-
-  if (title.innerHTML === 'Salary') {
-    title.addEventListener('click', (e) => {
-      data.sort((a, b) => {
-        const contentA = a.cells[3].innerHTML;
-        const contentB = b.cells[3].innerHTML;
-
-        return getNumber(contentA) - getNumber(contentB);
-      });
-
-      function getNumber(arg) {
-        return Number(arg.replace(',', '').replace('$', ''));
-      }
-
-      return tBody.append(...data);
-    });
-  }
+function getNumber(arg) {
+  return Number(arg.replace(',', '').replace('$', ''));
 }
+
+titles.forEach(th => th.addEventListener('click', (e) => {
+  sortFn(e.target.cellIndex);
+
+  tBody.append(...data);
+}));
