c16afd26236b05de8b5d3af243eec6816f7890ea
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-03-01 16:48:00 +0200

    new solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,32 @@
 'use strict';
 
-const headers = document.querySelector('tr');
+const theadRef = document.querySelector('thead');
 const tbodyRef = document.querySelector('tbody');
-const rows = [...(tbodyRef.querySelectorAll('tr'))];
+const tableRows = [...tbodyRef.querySelectorAll('tr')];
 
-function sortNames() {
-  return rows.sort((str1, str2) =>
-    str1.firstElementChild.textContent.localeCompare(
-      str2.firstElementChild.textContent));
-}
+theadRef.addEventListener('click', (e) => {
+  const headerIndex = e.target.cellIndex;
 
-function sortPosition() {
-  return rows.sort((str1, str2) =>
-    str1.firstElementChild.nextElementSibling.textContent.localeCompare(
-      str2.firstElementChild.nextElementSibling.textContent));
-}
+  tableRows.sort((a, b) => {
+    const valueType = e.target.textContent;
+    const aText = a.cells[headerIndex].textContent;
+    const bText = b.cells[headerIndex].textContent;
 
-function sortAge() {
-  return rows.sort((str1, str2) =>
-    parseInt(str1.lastElementChild.previousElementSibling.textContent)
-      - parseInt(str2.lastElementChild.previousElementSibling.textContent));
-}
+    switch (valueType) {
+      case 'Name':
+      case 'Position':
+        return aText.localeCompare(bText);
 
-function sortSalary() {
-  return rows.sort((str1, str2) =>
-    parseInt(str1.lastElementChild.textContent.slice(1).split(',').join(''))
-      - parseInt(str2.lastElementChild.textContent
-        .slice(1).split(',').join('')));
-}
+      case 'Age':
+        return aText - bText;
 
-function addEvent(element, sortFunction) {
-  element.addEventListener('click', () => {
-    const sorted = sortFunction();
+      case 'Salary':
+        const normalizeSalary = (item) => item.slice(1).split(',').join('');
 
-    tbodyRef.innerHTML = `
-    ${sorted.map(person => `
-    <tr>${person.innerHTML}</tr>
-    `).join('')}`;
+        return normalizeSalary(aText) - normalizeSalary(bText);
+      default:
+    }
   });
-};
 
-addEvent(headers.firstElementChild, sortNames);
-addEvent(headers.firstElementChild.nextElementSibling, sortPosition);
-addEvent(headers.lastElementChild.previousElementSibling, sortAge);
-addEvent(headers.lastElementChild, sortSalary);
+  tableRows.forEach(item => tbodyRef.append(item));
+});
