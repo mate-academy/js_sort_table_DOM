1c89ba8bbe7698f18617ea39aa291c8fcb974b78
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-03-05 13:27:56 +0200

    Solution 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,12 @@ const staff = Array.from(document.querySelector('tbody').children);
 const list = document.querySelector('tbody');
 let arraySort = [];
 
+for (let i = 0; i < headers.length; i++) {
+  headers[i].addEventListener('click', ev => {
+    sortStaff(i);
+  });
+}
+
 function formatItem(item) {
   return Number(item.match(/\d+/g).join(''));
 }
@@ -14,35 +20,35 @@ function sortStaff(i) {
 
   const isDollars = staff[0].children[i].textContent.includes('$');
 
-  staff.forEach(person => {
-    if (isDollars) {
-      arraySort.push(formatItem(person.children[i].textContent));
-    } else {
-      arraySort.push(person.children[i].textContent);
-    }
-  });
-
   if (isDollars) {
-    arraySort.sort((a, b) => a - b).forEach(element => {
-      staff.forEach(person => {
+    staff.forEach(person => {
+      arraySort.push(formatItem(person.children[i].textContent));
+    });
+
+    arraySort.sort((a, b) => a - b);
+  } else {
+    staff.forEach(person => {
+      arraySort.push(person.children[i].textContent);
+    });
+
+    arraySort.sort();
+  }
+
+  rowRender(i, isDollars);
+}
+
+function rowRender(i, isDollars) {
+  arraySort.forEach(element => {
+    staff.forEach(person => {
+      if (isDollars) {
         if (formatItem(person.children[i].textContent) === element) {
           list.append(person);
         };
-      });
-    });
-  } else {
-    arraySort.sort().forEach(element => {
-      staff.forEach(person => {
+      } else {
         if (person.children[i].textContent === element) {
           list.append(person);
         };
-      });
+      }
     });
-  }
-}
-
-for (let i = 0; i < headers.length; i++) {
-  headers[i].addEventListener('click', ev => {
-    sortStaff(i);
   });
 }
