67be4292967718843960b79409ada164dd41aa1c
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-03-05 20:08:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,54 @@
 'use strict';
 
-// write code here
+const columnName = document.querySelectorAll('th')[0];
+const columnPosition = document.querySelectorAll('th')[1];
+const columnAge = document.querySelectorAll('th')[2];
+const columnSalary = document.querySelectorAll('th')[3];
+const list = document.querySelector(' tbody');
+const person = [...list.querySelectorAll('tr')];
+
+columnName.addEventListener('click', () => {
+  sortString(person, 0);
+});
+
+columnPosition.addEventListener('click', () => {
+  sortString(person, 1);
+});
+
+columnAge.addEventListener('click', () => {
+  sortNumber(person, 2);
+});
+
+columnSalary.addEventListener('click', () => {
+  sortNumber(person, 3);
+});
+
+function sortString(arr, i) {
+  arr.sort((a, b) => {
+    const stringA = a.children[i].textContent;
+    const stringB = b.children[i].textContent;
+
+    return stringA.localeCompare(stringB);
+  });
+
+  list.append(...arr);
+}
+
+function sortNumber(arr, i) {
+  arr.sort((a, b) => {
+    const numberA = getNumbers(a.children[i].textContent);
+    const numberB = getNumbers(b.children[i].textContent);
+
+    return numberA - numberB;
+  });
+
+  list.append(...arr);
+}
+
+function getNumbers(string) {
+  return +string.split('').map(num => {
+    if ('0123456789'.includes(num)) {
+      return num;
+    }
+  }).join('');
+}
