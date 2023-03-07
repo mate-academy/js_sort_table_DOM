b18a2b8c3141cb087a28130478340ab55b538189
tarasnakonechnyi <nakonechnyi.taras@gmail.com>
2023-03-07 10:06:37 +0200

    Solution 3.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const headers = Array.from(document.querySelector('thead').children[0].cells);
-const staff = Array.from(document.querySelector('tbody').children);
 const list = document.querySelector('tbody');
 let arraySort = [];
+let staff = [];
 
 for (let i = 0; i < headers.length; i++) {
   headers[i].addEventListener('click', ev => {
@@ -16,39 +16,50 @@ function formatItem(item) {
 }
 
 function sortStaff(i) {
+  staff = Array.from(document.querySelector('tbody').children);
   arraySort = [];
 
   const isDollars = staff[0].children[i].textContent.includes('$');
 
-  if (isDollars) {
-    staff.forEach(person => {
-      arraySort.push(formatItem(person.children[i].textContent));
-    });
+  staff.forEach(person => {
+    arraySort.push(
+      isDollars
+        ? formatItem(person.children[i].textContent)
+        : person.children[i].textContent
+    );
+  });
 
-    arraySort.sort((a, b) => a - b);
-  } else {
-    staff.forEach(person => {
-      arraySort.push(person.children[i].textContent);
-    });
+  isDollars
+    ? arraySort.sort((a, b) => a - b)
+    : arraySort.sort();
 
-    arraySort.sort();
-  }
+  // if (isDollars) {
+  //   staff.forEach(person => {
+  //     arraySort.push(formatItem(person.children[i].textContent));
+  //   });
+
+  //   arraySort.sort((a, b) => a - b);
+  // } else {
+  //   staff.forEach(person => {
+  //     arraySort.push(person.children[i].textContent);
+  //   });
+
+  //   arraySort.sort();
+  // }
 
   rowRender(i, isDollars);
 }
 
 function rowRender(i, isDollars) {
   arraySort.forEach(element => {
-    staff.forEach(person => {
-      if (isDollars) {
-        if (formatItem(person.children[i].textContent) === element) {
-          list.append(person);
-        };
-      } else {
-        if (person.children[i].textContent === element) {
-          list.append(person);
-        };
-      }
+    const row = staff.find(person => {
+      return isDollars
+        ? formatItem(person.children[i].textContent) === element
+        : person.children[i].textContent === element;
     });
+
+    list.append(row);
+
+    staff.splice(staff.indexOf(row), 1);
   });
 }
