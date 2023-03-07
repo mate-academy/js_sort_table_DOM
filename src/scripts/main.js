07ffa50222461015c7c7adfea593cd05e5c29bd8
Alex Nastychenko <nastychenko1993@gmail.com>
2023-03-07 14:19:30 +0200

    add compare functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,34 +3,40 @@
 const tableData = document.querySelector('table');
 const tableBody = document.querySelector('tbody');
 
+const compareCellText = (i) => {
+  return (a, b) => {
+    return a.cells[i].textContent.localeCompare(b.cells[i].textContent);
+  };
+};
+
+const compareCellNumber = (i) => {
+  return (a, b) => {
+    return +a.cells[i].textContent.replace(/[$,]/g, '')
+    - +b.cells[i].textContent.replace(/[$,]/g, '');
+  };
+};
+
 const sortPeople = (targetName) => {
   if (targetName === 'name') {
-    const sortedRows = [...tableBody.rows]
-      .sort((a, b) => a.cells[0].textContent
-        .localeCompare(b.cells[0].textContent));
+    const sortedRows = [...tableBody.rows].sort(compareCellText(0));
 
     tableData.tBodies[0].append(...sortedRows);
   };
 
   if (targetName === 'position') {
-    const sortedRows = [...tableBody.rows]
-      .sort((a, b) => a.cells[1].textContent
-        .localeCompare(b.cells[1].textContent));
+    const sortedRows = [...tableBody.rows].sort(compareCellText(1));
 
     tableData.tBodies[0].append(...sortedRows);
   };
 
   if (targetName === 'age') {
-    const sortedRows = [...tableBody.rows]
-      .sort((a, b) => a.cells[2].textContent - b.cells[2].textContent);
+    const sortedRows = [...tableBody.rows].sort(compareCellNumber(2));
 
     tableData.tBodies[0].append(...sortedRows);
   };
 
   if (targetName === 'salary') {
-    const sortedRows = [...tableBody.rows]
-      .sort((a, b) => +a.cells[3].textContent.replace(/[$,]/g, '')
-        - b.cells[3].textContent.replace(/[$,]/g, ''));
+    const sortedRows = [...tableBody.rows].sort(compareCellNumber(3));
 
     tableData.tBodies[0].append(...sortedRows);
   };
