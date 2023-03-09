e3268205ea33acc579875cae8fda4204d3461189
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-03-09 13:53:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,25 +4,27 @@ const titles = document.querySelector('tr');
 const tbody = document.querySelector('tbody');
 const rows = tbody.querySelectorAll('tr');
 
-const namesSorted = [...rows].sort((name1, name2) =>
+const namesSorted = (name1, name2) =>
   name1.firstElementChild.textContent.localeCompare(
-    name2.firstElementChild.textContent));
+    name2.firstElementChild.textContent);
 
-const positionSorted = [...rows].sort((position1, position2) =>
+const positionSorted = (position1, position2) =>
   position1.firstElementChild.nextElementSibling.textContent.localeCompare(
-    position2.firstElementChild.nextElementSibling.textContent));
+    position2.firstElementChild.nextElementSibling.textContent);
 
-const ageSorted = [...rows].sort((value1, value2) =>
+const ageSorted = (value1, value2) =>
   value1.lastElementChild.previousElementSibling.textContent
-  - value2.lastElementChild.previousElementSibling.textContent);
+  - value2.lastElementChild.previousElementSibling.textContent;
 
-const salarySorted = [...rows].sort((value1, value2) =>
+const salarySorted = (value1, value2) =>
   value1.lastElementChild.textContent.replaceAll(',', '').slice(1)
-  - value2.lastElementChild.textContent.replaceAll(',', '').slice(1));
+  - value2.lastElementChild.textContent.replaceAll(',', '').slice(1);
 
-function addSortedList(sorted) {
+function addSortedList(callback) {
+  const sorted = [...rows].sort(callback);
+
   tbody.innerHTML = `
-    ${[...sorted].map(person => `
+    ${sorted.map(person => `
     <tr>${person.innerHTML}</tr>
     `).join('')}`;
 };
