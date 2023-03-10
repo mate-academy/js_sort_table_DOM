2622a95198de746313d148cb2e2aa3b73b2fc744
Vlad Kurachenko <x0674016244@gmail.com>
2023-03-10 13:23:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tH = thead.querySelectorAll('th');
+
+const tbody = document.querySelector('tbody');
+const tBodyRows = [...tbody.rows];
+
+function usdNum(str) {
+  return Number(str.replace(/\D/g, ''));
+}
+
+thead.addEventListener('click', (ev) => {
+  const sortByIndex = [...tH].indexOf(ev.target);
+
+  tBodyRows.sort((a, b) => {
+    const aIn = a.cells[sortByIndex].innerText;
+    const bIn = b.cells[sortByIndex].innerText;
+
+    return usdNum(aIn) - usdNum(bIn) || aIn.localeCompare(bIn);
+  });
+
+  tbody.append(...tBodyRows);
+});
