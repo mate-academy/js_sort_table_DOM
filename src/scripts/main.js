79c4a59572ff4a07927ddd14f5ba5dd31ded33a3
Tolik Petras <toliketras4@gmail.com>
2023-03-13 14:14:53 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const sortBy = document.querySelector('tr');
+
+function toNum(element) {
+  return +element.innerText.slice(1).replaceAll(',', '');
+}
+
+sortBy.addEventListener('click', el => {
+  const columns = el.target.parentNode.children;
+
+  for (let i = 0; i < columns.length; i++) {
+    if (el.target.parentNode.children[i].innerText === el.target.innerText) {
+      const parentEl = document.querySelector('tbody');
+      const sortsEl = parentEl.querySelectorAll('tr');
+
+      let sorted;
+
+      if (el.target.parentNode.children[i].innerText === 'Salary') {
+        sorted = [...sortsEl].sort((el1, el2) =>
+          toNum(el1.children[i]) - toNum(el2.children[i]));
+      } else {
+        sorted = [...sortsEl].sort((el1, el2) =>
+          el1.children[i].innerText.localeCompare(el2.children[i].innerText));
+      }
+
+      parentEl.append(...sorted);
+    }
+  }
+});
