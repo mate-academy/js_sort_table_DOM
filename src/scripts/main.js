900eb49fe9bd5d5868e182a52a99fdf7b0cb5e6c
Roman Kulchytskyi-Dashynych <kdrs0812@gmail.com>
2023-03-14 11:49:17 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const headRow = document.querySelector('thead tr');
+const tr = [...tbody.children];
+
+const normalize = (number) => {
+  return +number.slice(1).split(',').join('');
+};
+
+headRow.addEventListener('click', e => {
+  const sorted = tr.sort((a, b) => {
+    const aValue = a.children[e.target.cellIndex].innerText;
+    const bValue = b.children[e.target.cellIndex].innerText;
+
+    switch (e.target.cellIndex) {
+      case 0:
+      case 1:
+        return aValue.localeCompare(bValue);
+      case 2:
+        return aValue - bValue;
+      case 3:
+        return normalize(aValue) - normalize(bValue);
+    }
+  });
+
+  sorted.forEach(item => tbody.append(item));
+});
