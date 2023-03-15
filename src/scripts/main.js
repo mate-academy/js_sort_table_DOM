bfde5e874fcef1d8c193d1931a9070b8b91c7f07
Ilona Romaniuk <ilona09041983@gmail.com>
2023-03-15 17:05:30 +0200

    Table sorting by clicking on the title

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const headlines = document.querySelector('thead').rows[0];
+const list = document.querySelector('tbody');
+const listRows = list.rows;
+
+headlines.addEventListener('click', (e) => {
+  const headline = e.target.closest('th');
+
+  const index = headline.cellIndex;
+
+  [...listRows].sort((a, b) => {
+    const row = a.cells[index].textContent;
+    const nextRow = b.cells[index].textContent;
+
+    if (/\d/.test(row)) {
+      const checkedRow = convertToNumber(row);
+      const checkedNextRow = convertToNumber(nextRow);
+
+      return +checkedRow - +checkedNextRow;
+    }
+
+    return row.localeCompare(nextRow);
+  }).forEach(row => list.appendChild(row));
+});
+
+function convertToNumber(string) {
+  const number = Number(string.replace(/[$,]/g, ''));
+
+  return `${number}`;
+}
