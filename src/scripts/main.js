7bd734ad6dbc9044ceed334faf75021472f697aa
Nazar <nazar378378379379@gmail.com>
2023-03-16 16:18:58 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+const tBody = document.querySelector('tbody');
+const arraySpecialists = [...tBody.children];
+const elementsTotal = document.querySelectorAll('th');
+
+for (const element of elementsTotal) {
+  const innerText = element.innerText;
+
+  if (innerText === 'Name') {
+    element.addEventListener('click', (e) => {
+      arraySpecialists.sort((a, b) => {
+        const itemA = a.cells[0].innerText;
+        const itemB = b.cells[0].innerText;
+
+        return itemA.localeCompare(itemB);
+      });
+
+      return tBody.append(...arraySpecialists);
+    });
+  } else if (innerText === 'Position') {
+    element.addEventListener('click', (e) => {
+      arraySpecialists.sort((a, b) => {
+        const itemA = a.cells[1].innerText;
+        const itemB = b.cells[1].innerText;
+
+        return itemA.localeCompare(itemB);
+      });
+
+      return tBody.append(...arraySpecialists);
+    });
+  } else if (innerText === 'Age') {
+    element.addEventListener('click', (e) => {
+      arraySpecialists.sort((a, b) => {
+        const itemA = a.cells[2].innerText;
+        const itemB = b.cells[2].innerText;
+
+        return itemA - itemB;
+      });
+
+      return tBody.append(...arraySpecialists);
+    });
+  } else if (innerText === 'Salary') {
+    element.addEventListener('click', (e) => {
+      arraySpecialists.sort((a, b) => {
+        const itemA = a.cells[3].innerText;
+        const itemB = b.cells[3].innerText;
+
+        return itemA.slice(1).split(',').join('')
+          - itemB.slice(1).split(',').join('');
+      });
+
+      return tBody.append(...arraySpecialists);
+    });
+  }
+}
