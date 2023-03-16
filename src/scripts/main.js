82f2aa3447bbf1bd7cc4bd386b71cab3b2706ccd
Tolik Petras <toliketras4@gmail.com>
2023-03-16 18:15:39 +0200

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,13 +16,11 @@ sortBy.addEventListener('click', el => {
 
       let sorted;
 
-      if (el.target.parentNode.children[i].innerText === 'Salary') {
-        sorted = [...sortsEl].sort((el1, el2) =>
-          toNum(el1.children[i]) - toNum(el2.children[i]));
-      } else {
-        sorted = [...sortsEl].sort((el1, el2) =>
+      el.target.parentNode.children[i].innerText === 'Salary'
+        ? sorted = [...sortsEl].sort((el1, el2) =>
+          toNum(el1.children[i]) - toNum(el2.children[i]))
+        : sorted = [...sortsEl].sort((el1, el2) =>
           el1.children[i].innerText.localeCompare(el2.children[i].innerText));
-      }
 
       parentEl.append(...sorted);
     }
