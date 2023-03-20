be16151d77e68c4a71ef20f52b44d107c81bc4d8
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-20 12:09:12 +0100

    complete test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tRow = [...tBody.querySelectorAll('tr')];
+
+tHead.addEventListener('click', (e) => {
+  const cellIndex = e.target.cellIndex;
+
+  tRow.sort((a, b) => {
+    const type = e.target.textContent;
+    const aText = a.cells[cellIndex].textContent;
+    const bText = b.cells[cellIndex].textContent;
+
+    switch (type) {
+      case 'Name':
+      case 'Position':
+        return aText.localeCompare(bText);
+
+      case 'Age':
+        return aText - bText;
+
+      case 'Salary':
+        const normalize = (item) => item.slice(1).split(',').join('');
+
+        return normalize(aText) - normalize(bText);
+      default:
+    }
+  });
+
+  tRow.forEach(item => tBody.append(item));
+});
