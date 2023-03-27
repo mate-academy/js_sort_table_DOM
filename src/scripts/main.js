3c8b278aed7bba1a662f1ffbde90da4c2db598de
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-27 18:41:30 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,28 @@
 'use strict';
 
-// Находим таблицу и ее заголовок и тело
 const table = document.querySelector('table');
 const tableHead = table.querySelector('thead');
 const tableBody = table.querySelector('tbody');
 
-// Назначаем обработчик события "click" на заголовок таблицы
 tableHead.addEventListener('click', e => {
-  // Получаем индекс ячейки, на которую было нажато
   const index = e.target.cellIndex;
 
-  // Сортируем строки в теле таблицы
   const sorted = [...tableBody.rows].sort((a, b) => {
-    // Получаем текст ячеек a и b для сравнения
     const aText = a.cells[index].innerText;
     const bText = b.cells[index].innerText;
 
-    // В зависимости от индекса ячейки, сравниваем ячейки a и b
     switch (index) {
       case 0:
-      case 1: // Для первых двух колонок - сортировка по алфавиту
+      case 1:
         return aText.localeCompare(bText);
-      case 2: // Для третьей колонки - сортировка по числовому значению
+      case 2:
         return +aText - +bText;
-      case 3: // Для четвертой колонки - сортировка по числовому значению
-      //  без символов, не являющихся буквами и цифрами
+      case 3:
         return +aText.replace(/\W/g, '') - +bText.replace(/\W/g, '');
+      default:
+        return 0;
     }
   });
 
-  // Добавляем отсортированные строки обратно в тело таблицы
   tableBody.append(...sorted);
 });
