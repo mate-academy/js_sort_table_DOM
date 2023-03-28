0f3e0081ebd813c423dd6b9438cf016aaba9cef6
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-03-28 23:54:03 +0100

    the task was solved;
    hi]

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+const tableRows = [...tbody.rows];
+
+thead.addEventListener('click', () => {
+  const onlyNumbers = string => {
+    const result = string.replace(/[$,]/g, '');
+
+    return result.length ? +result : NaN;
+  };
+
+  tableRows.sort((a, b) => {
+    const first = a.children[event.target.cellIndex].innerText;
+    const second = b.children[event.target.cellIndex].innerText;
+
+    return isNaN(onlyNumbers(first)) ? first.localeCompare(second)
+      : onlyNumbers(first) - onlyNumbers(second);
+  }
+  );
+  tbody.append(...tableRows);
+}
+);
