d16df3dc0988534603028b7ce757a5e80caddb57
Anastasiia Em <ema199625@gmail.com>
2023-03-29 13:54:02 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.tHead.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+  const tableBody = [...table.tBodies][0];
+
+  const sorted = [ ...tableBody.rows ].sort((a, b) => {
+    const aText = a.cells[index].innerText.replace(/\W/g, '');
+    const bText = b.cells[index].innerText.replace(/\W/g, '');
+
+    return !isNaN(parseFloat(aText)) ? +aText - +bText
+      : aText.localeCompare(bText);
+  });
+
+  tableBody.append(...sorted);
+});
