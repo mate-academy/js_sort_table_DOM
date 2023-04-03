94db3fff222e5751d715806ccd945e65982ef593
Nadiya Ovsiyenko <nadia.ovsienko1009@gmail.com>
2023-04-03 18:43:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', e => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  sortColumn(e.target.cellIndex, e.target.innerText);
+});
+
+function sortColumn(columnNumber, columnTitle) {
+  const tbody = document.querySelector('tbody');
+
+  const employeeInfo = Array.from(tbody.rows);
+
+  const compare = function(firstPerson, secondPerson) {
+    const firstPersonData = firstPerson.cells[columnNumber].innerHTML;
+    const secondPersonData = secondPerson.cells[columnNumber].innerHTML;
+
+    switch (columnTitle) {
+      case 'Name':
+      case 'Position':
+        return firstPersonData.localeCompare(secondPersonData);
+
+      case 'Age':
+        return firstPersonData - secondPersonData;
+
+      case 'Salary':
+        return convertToNumber(firstPersonData)
+          - convertToNumber(secondPersonData);
+    }
+  };
+
+  employeeInfo.sort(compare);
+
+  tbody.append(...employeeInfo);
+}
+
+function convertToNumber(salaryString) {
+  return salaryString.slice(1).split(',').join('');
+}
