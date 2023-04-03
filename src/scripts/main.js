c4f5e8f056e08e5e5c775e4ef5ed541c5ccea0e9
Nazar Kharkevych <nkharkevych@gmail.com>
2023-04-03 17:39:10 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBodyRows = document.querySelectorAll('tbody > tr');
+
+table.addEventListener('click', function(e) {
+  const header = e.target.closest('th');
+
+  if (!header) {
+    return;
+  }
+
+  const cellIndex = header.cellIndex;
+
+  const sortedColumn = sortColumnData(tBodyRows, cellIndex);
+
+  sortedColumn.map((data, rowIndex) => {
+    const td = document.createElement('td');
+
+    td.innerText = data;
+
+    tBodyRows[rowIndex].cells[cellIndex].replaceWith(td);
+  });
+});
+
+function sortColumnData(rows, index) {
+  const result = [];
+  const bySalary = index === 3;
+
+  for (const row of rows) {
+    result.push(row.cells[index].innerText);
+  }
+
+  result.sort((a, b) => {
+    if (bySalary) {
+      const numA = a.slice(1).replace(',', '');
+      const numB = b.slice(1).replace(',', '');
+
+      return numA - numB;
+    }
+
+    return a.localeCompare(b);
+  });
+
+  return result;
+}
