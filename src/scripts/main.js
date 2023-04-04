f6f9b88d4732c6c452abd98860d2681ff9071e14
metja <mikolapenzanik@gmail.com>
2023-04-04 23:00:22 +0300

    Completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableRows = tableBody.rows;
+
+function getSalaryValue(str) {
+  return +str.replace('$', '').replace(',', '');
+};
+
+tableHead.addEventListener('click', e => {
+  const index = e.target.cellIndex;
+
+  const sortedTable = [...tableRows].sort((a, b) => {
+    const cellA = a.cells[index].innerHTML;
+    const cellB = b.cells[index].innerHTML;
+
+    if (cellA.includes('$')) {
+      return (getSalaryValue(cellA) - getSalaryValue(cellB));
+    }
+
+    if (isFinite(cellA)) {
+      return cellA - cellB;
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  tableBody.append(...sortedTable);
+});
