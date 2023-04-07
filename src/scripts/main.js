6d6652197365f4e3ee2a8ac3556754df6ba918b5
Viktor <vasha2020@gmail.com>
2023-04-07 13:47:15 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+
+function sortingColumn(e) {
+  const sortByColumn = e.target.innerText;
+  const tableRows = [...document.querySelectorAll('tr')];
+  const tableDataToSort = [];
+
+  for (let i = 1; i < tableRows.length - 1; i++) {
+    const cells = tableRows[i].cells;
+    const person = {};
+
+    person.Name = cells[0].innerText;
+    person.Position = cells[1].innerText;
+    person.Age = cells[2].innerText;
+    person.Salary = cells[3].innerText;
+
+    tableDataToSort.push(person);
+  };
+
+  if (sortByColumn === 'Salary') {
+    tableDataToSort.sort((a, b) => {
+      const toNumberA = a.Salary.match(/\d+/g).join('');
+      const toNumberB = b.Salary.match(/\d+/g).join('');
+
+      return toNumberA - toNumberB;
+    });
+  } else {
+    tableDataToSort.sort((a, b) =>
+      a[sortByColumn].localeCompare(b[sortByColumn]));
+  }
+
+  for (let i = 1; i < tableRows.length - 1; i++) {
+    const tableRow = tableRows[i];
+
+    tableRow.innerHTML = `
+      <td>${tableDataToSort[i - 1].Name}</td>
+      <td>${tableDataToSort[i - 1].Position}</td>
+      <td>${tableDataToSort[i - 1].Age}</td>
+      <td>${tableDataToSort[i - 1].Salary}</td>
+    `;
+  }
+};
+
+tableHead.addEventListener('click', sortingColumn);
