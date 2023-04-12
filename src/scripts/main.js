600a4b5760c4cb8c9bb6dc4a144c79850b9c7443
Daryna Hnidash <melissa998877@gmail.com>
2023-04-12 20:32:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  rows.sort((a, b) => {
+    const cell1 = a.cells[e.target.cellIndex].textContent;
+    const cell2 = b.cells[e.target.cellIndex].textContent;
+
+    if (e.target.textContent === 'Salary') {
+      const num1 = cell1.replace('$', '').replace(',', '');
+      const num2 = cell2.replace('$', '').replace(',', '');
+
+      return num1 - num2;
+    }
+
+    return cell1.localeCompare(cell2);
+  });
+
+  tbody.append(...rows);
+});
