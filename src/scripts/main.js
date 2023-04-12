141450c3e8fd08cebc4842cad753cdd4297609f3
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-12 16:35:01 +0300

    the table was sorted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write code here
+// const tr = document.querySelectorAll('tr');
+// const list = [];
+
+// tr.forEach(row => {
+//   list.push(row.children[0]);
+//   // console.log(row.children[0].innerHTML);
+// });
+
+// console.log(list);
+
+// function sortList(list) {
+//   const sorted = list.sort((a, b) => {
+//     a.innerText.localeCompare(b.innerText);
+//   });
+
+//   console.log(sorted);
+//   return sorted;
+// }
+
+// console.log(sortList(list));
+
+const table = document.querySelector('table');
+const headers = table.tHead.querySelectorAll('th');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    sortTable(header);
+  });
+});
+
+function sortTable(column) {
+  const rows = Array.from(table.tBodies[0].querySelectorAll('tr'));
+
+  rows.sort((a, b) => {
+    const aValue = a.cells[column.cellIndex].textContent;
+    const bValue = b.cells[column.cellIndex].textContent;
+
+    if (!convertToNumber(aValue)) {
+      return aValue.localeCompare(bValue);
+    }
+
+    return convertToNumber(aValue) - convertToNumber(bValue);
+  });
+
+  table.tBodies[0].append(...rows);
+}
+
+function convertToNumber(number) {
+  return parseFloat(number.replace(/[$,]/g, ''));
+}
