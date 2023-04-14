021294ac38e9e2d4c74905612085dfaeb0ae41bc
Natalia Maryshko <n.maryshko@gmail.com>
2023-04-14 07:15:15 -0500

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const people = [...document.querySelectorAll('tbody tr')];
+
+function toNumber(salary) {
+  return +salary.slice(1).split(',').join('');
+}
+
+thead.addEventListener('click', e => {
+  const i = e.target.cellIndex;
+
+  if (e.target.textContent === 'Salary') {
+    people.sort((a, b) =>
+      toNumber(a.children[i].textContent)
+      - toNumber(b.children[i].textContent));
+  } else {
+    people.sort((a, b) =>
+      a.children[i].textContent.localeCompare(b.children[i].textContent));
+  }
+
+  tbody.append(...people);
+});
