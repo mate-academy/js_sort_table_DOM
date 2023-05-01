6918776fd5b7774d22d8068f55d9fc355f2c46ef
Max Vovk <vovkmax1@gmail.com>
2023-05-01 11:22:26 +0300

    Solution_v01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const bodyRows = [...tbody.rows];
+
+thead.addEventListener('click', e => {
+  if (!e.target.tagName === 'TH') {
+    return;
+  };
+
+  const targetIndex = e.target.closest('th').cellIndex;
+
+  const number = string => string.replace(/[$,]/g, '');
+
+  const sortTable = bodyRows.sort((a, b) => {
+    const valueA = number(a.children[targetIndex].textContent);
+    const valueB = number(b.children[targetIndex].textContent);
+
+    if (!isNaN(valueA)) {
+      return valueA - valueB;
+    };
+
+    return valueA.localeCompare(valueB);
+  });
+
+  tbody.append(...sortTable);
+});
