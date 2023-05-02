1100b8d8608430cde77fc3cae0afa14299f03dfd
Oksana Tytanych <oksanatytanych@gmail.com>
2023-05-02 14:39:03 +0300

    js_sort_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const header = document.querySelectorAll('thead tr th');
+
+[...header].map((element, index) => {
+  element.addEventListener('click', (e) => {
+    const rows = document.querySelectorAll('tbody tr');
+
+    const sortedRows = [...rows].sort((a, b) => {
+      const compareA = a.querySelectorAll('td')[index].innerText;
+      const compareB = b.querySelectorAll('td')[index].innerText;
+
+      if (e.target.innerText === 'Salary') {
+        return compareA.slice(1).split(',').join('.')
+          - compareB.slice(1).split(',').join('.');
+      }
+
+      if (typeof (compareA) === 'string') {
+        return compareA.localeCompare(compareB);
+      }
+
+      return compareA - compareB;
+    });
+
+    document.querySelector('tbody').replaceChildren(...rows, ...sortedRows);
+  });
+});
