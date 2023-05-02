82417a7f03f9eb255d65b854be36a112089690e7
Nick <tortickk8475@gmail.com>
2023-05-02 13:51:08 +0300

    chore: arrange list in asc order on header click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const tBody = document.querySelector('tbody');
+
+function toSort(a, b) {
+  if (a.lastElementChild.innerHTML < b.lastElementChild.innerHTML) {
+    return -1;
+  } else if (a.lastElementChild.innerHTML > b.lastElementChild.innerHTML) {
+    return 1;
+  }
+
+  return 0;
+}
+
+document.querySelector('thead').addEventListener(
+  'click', e => {
+    const item = e.target;
+
+    if (item.tagName === 'TH') {
+      const trList = [...tBody.children];
+      const sortedTables = trList.sort(toSort);
+
+      sortedTables.forEach(elem => {
+        document.querySelector('tbody').appendChild(elem);
+      });
+    }
+  },
+);
