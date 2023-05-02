7a08b7c41b5193c65b3a2a5e86c6e4b38fe67e68
Nick <tortickk8475@gmail.com>
2023-05-02 18:14:27 +0300

    fix: make all items sortable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,50 @@
 'use strict';
 
 const tBody = document.querySelector('tbody');
+const trList = [...tBody.children];
 
-function toSort(a, b) {
-  if (a.lastElementChild.innerHTML < b.lastElementChild.innerHTML) {
-    return -1;
-  } else if (a.lastElementChild.innerHTML > b.lastElementChild.innerHTML) {
-    return 1;
-  }
+const sortArray = (list, num) => {
+  return list.sort((a, b) => {
+    if (a.children[num].innerHTML < b.children[num].innerHTML) {
+      return -1;
+    } else if (a.children[num].innerHTML > b.children[num].innerHTML) {
+      return 1;
+    }
 
-  return 0;
-}
+    return 0;
+  });
+};
 
 document.querySelector('thead').addEventListener(
   'click', e => {
     const item = e.target;
 
-    if (item.tagName === 'TH') {
-      const trList = [...tBody.children];
-      const sortedTables = trList.sort(toSort);
+    switch (item.innerHTML) {
+      case 'Salary':
+        const sortedSalary = sortArray(trList, 3);
 
-      sortedTables.forEach(elem => {
-        document.querySelector('tbody').appendChild(elem);
-      });
+        sortedSalary.forEach(sal => {
+          tBody.appendChild(sal);
+        });
 
-      tBody.prepend(trList[trList.length - 1]);
+        tBody.prepend(trList[trList.length - 1]);
+        break;
+
+      case 'Age':
+        const sortedAge = sortArray(trList, 2);
+
+        sortedAge.forEach(age => tBody.appendChild(age));
+        break;
+
+      case 'Position':
+        const sortedPosition = sortArray(trList, 1);
+
+        sortedPosition.forEach(pos => tBody.appendChild(pos));
+        break;
+
+      default:
+        const sortedName = sortArray(trList, 0);
+
+        sortedName.forEach(n => tBody.appendChild(n));
     }
-  },
-);
+  });
