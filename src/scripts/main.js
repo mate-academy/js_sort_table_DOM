0cfe2d28ed9f54baf5be542247bba0902f1be4de
Nick <tortickk8475@gmail.com>
2023-05-04 09:46:52 +0300

    fix: make code readable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,45 @@
 'use strict';
 
 const tBody = document.querySelector('tbody');
+const trList = [...tBody.children];
 
-function toSort(a, b) {
-  if (a.lastElementChild.innerHTML < b.lastElementChild.innerHTML) {
-    return -1;
-  } else if (a.lastElementChild.innerHTML > b.lastElementChild.innerHTML) {
-    return 1;
-  }
-
-  return 0;
-}
-
-document.querySelector('thead').addEventListener(
-  'click', e => {
-    const item = e.target;
-
-    if (item.tagName === 'TH') {
-      const trList = [...tBody.children];
-      const sortedTables = trList.sort(toSort);
-
-      sortedTables.forEach(elem => {
-        document.querySelector('tbody').appendChild(elem);
-      });
-
-      tBody.prepend(trList[trList.length - 1]);
+const sortArray = (list, num) => {
+  return list.sort((a, b) => {
+    if (a.children[num].innerHTML < b.children[num].innerHTML) {
+      return -1;
+    } else if (a.children[num].innerHTML > b.children[num].innerHTML) {
+      return 1;
     }
-  },
-);
+
+    return 0;
+  });
+};
+
+document.querySelector('thead')
+  .firstElementChild.children[0].addEventListener('click', () => {
+    const sortedList = sortArray(trList, 0);
+
+    sortedList.forEach(i => tBody.appendChild(i));
+  });
+
+document.querySelector('thead')
+  .firstElementChild.children[1].addEventListener('click', () => {
+    const sortedList = sortArray(trList, 1);
+
+    sortedList.forEach(i => tBody.appendChild(i));
+  });
+
+document.querySelector('thead')
+  .firstElementChild.children[2].addEventListener('click', () => {
+    const sortedList = sortArray(trList, 2);
+
+    sortedList.forEach(i => tBody.appendChild(i));
+  });
+
+document.querySelector('thead')
+  .firstElementChild.children[3].addEventListener('click', () => {
+    const sortedList = sortArray(trList, 3);
+
+    sortedList.forEach(i => tBody.appendChild(i));
+    tBody.prepend(trList[trList.length - 1]);
+  });
