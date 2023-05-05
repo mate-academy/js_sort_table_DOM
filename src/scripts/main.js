f08fd088ab862918b54a40fe605e9209a4d1646a
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-05-05 22:38:43 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+const tableBody = document.querySelector('tbody');
+const allEmployees = tableBody.rows;
+
+const sortList = (ulNode, index) => {
+  [...allEmployees].sort((prev, next) => {
+    if (prev.children[index].textContent.charAt(0) === '$') {
+      return convertToNumber(prev.children[index].textContent)
+        - convertToNumber(next.children[index].textContent);
+    } else {
+      return (prev.children[index].textContent)
+        .localeCompare(next.children[index].textContent);
+    }
+  }).forEach(item => ulNode.append(item));
+};
+
+for (let i = 0; i <= headers.length; i++) {
+  headers[i].addEventListener('click', () => {
+    sortList(tableBody, i);
+  });
+}
+
+function convertToNumber(number) {
+  return +number.replace('$', '').replace(',', '.');
+}
