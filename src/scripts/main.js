e02ef9eb6729cd71710cd424a4196d56b0206cca
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-05-08 12:17:44 +0200

    after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,23 +4,26 @@ const headers = document.querySelectorAll('th');
 const tableBody = document.querySelector('tbody');
 const allEmployees = tableBody.rows;
 
-const sortList = (ulNode, index) => {
-  [...allEmployees].sort((prev, next) => {
-    if (prev.children[index].textContent.charAt(0) === '$') {
-      return convertToNumber(prev.children[index].textContent)
-        - convertToNumber(next.children[index].textContent);
-    } else {
-      return (prev.children[index].textContent)
-        .localeCompare(next.children[index].textContent);
-    }
-  }).forEach(item => ulNode.append(item));
+const sortList = (index) => {
+  [...allEmployees]
+    .sort((prev, next) => {
+      const prevContent = prev.children[index].textContent;
+      const nextContent = next.children[index].textContent;
+
+      if (prevContent.charAt(0) === '$') {
+        return convertToNumber(prevContent) - convertToNumber(nextContent);
+      };
+
+      return (prevContent).localeCompare(nextContent);
+    })
+    .forEach(item => tableBody.append(item));
 };
 
-for (let i = 0; i <= headers.length; i++) {
-  headers[i].addEventListener('click', () => {
-    sortList(tableBody, i);
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortList(index);
   });
-}
+});
 
 function convertToNumber(number) {
   return +number.replace('$', '').replace(',', '.');
