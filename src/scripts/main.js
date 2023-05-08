5eb78223cf9e86351c15a9af593065fd0ba86f7e
Alla Gutsul <allagutsul1990@gmail.com>
2023-05-08 21:18:01 +0300

    sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const thead = table.tHead;
+
+thead.addEventListener('click', function(e) {
+  if (!e.target.tagName === 'TH') {
+    return;
+  }
+
+  const targetIndex = e.target.cellIndex;
+  const tBody = table.tBodies[0];
+
+  const bodyRowsArray = [...tBody.rows];
+
+  bodyRowsArray.sort((rowA, rowB) => rowA.cells[targetIndex].innerHTML
+    .localeCompare(rowB.cells[targetIndex].innerHTML));
+
+  if (e.target.textContent === 'Salary') {
+    bodyRowsArray.sort((rowA, rowB) => normalizeNum(
+      rowA.cells[targetIndex].innerHTML) - normalizeNum(
+      rowB.cells[targetIndex].innerHTML));
+  }
+
+  tBody.append(...bodyRowsArray);
+});
+
+function normalizeNum(number) {
+  return number.split(',').join('').slice(1);
+}
