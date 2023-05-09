896848468f26e20c39bdcbadb8730d0074bd1b2d
Roman <65686226+morozit@users.noreply.github.com>
2023-05-09 13:18:54 +0300

    add task MOROZ v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead tr');
+const tbody = table.querySelector('tbody');
+const rows = tbody.querySelectorAll('tr');
+const number = string => string.replace(/[$,]/g, '');
+
+thead.addEventListener('click', even => {
+  const headerIndex = Array.from(thead.children).indexOf(even.target);
+
+  if (even.target.innerHTML === 'Salary') {
+    Array.from(rows)
+      .sort((a, b) => {
+        const aValue = number(a.children[headerIndex].textContent);
+        const bValue = number(b.children[headerIndex].textContent);
+
+        return aValue - bValue;
+      }).forEach(row => {
+        tbody.appendChild(row);
+      });
+  }
+
+  Array.from(rows)
+    .sort((a, b) => {
+      const aValue = a.children[headerIndex].textContent;
+      const bValue = b.children[headerIndex].textContent;
+
+      return aValue.localeCompare(bValue);
+    }).forEach(row => {
+      tbody.appendChild(row);
+    });
+});
