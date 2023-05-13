e47bc161acfdbae91d3f6d5d2c412714f8e6790e
Maria Shmakova <masha.shmakova@gmail.com>
2023-05-13 20:33:46 +0300

    sort table were created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const header = document.querySelector('tr');
+const tbody = document.querySelector('tbody');
+const rowsArray = tbody.children;
+
+header.addEventListener('click', (e) => {
+  let sorted;
+
+  if (e.target.innerText === 'Name') {
+    sorted = [...rowsArray].sort((a, b) =>
+      a.children[0].innerText.localeCompare(b.children[0].innerText));
+  }
+
+  if (e.target.innerText === 'Age') {
+    sorted = [...rowsArray].sort((a, b) =>
+      a.children[2].innerText - b.children[2].innerText);
+  }
+
+  if (e.target.innerText === 'Salary') {
+    sorted = [...rowsArray].sort((a, b) =>
+      toNumber(a.children[3]) - toNumber(b.children[3]));
+  }
+
+  tbody.prepend(...sorted);
+});
+
+function toNumber(element) {
+  return +element.innerText.slice(1).replaceAll(',', '');
+};
