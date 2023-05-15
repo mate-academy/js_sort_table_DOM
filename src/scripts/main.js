6e588386afcde42b8d0f0990870f8166769fa2b7
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-15 14:15:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const headTable = document.querySelector('thead');
+const bodyTable = document.querySelector('tbody');
+const rows = [...bodyTable.rows];
+
+headTable.addEventListener('click', e => {
+  if (!e.target.tagName === 'th') {
+    return;
+  };
+
+  const targetIndex = e.target.cellIndex;
+
+  function numberOptimizer(str) {
+    return str.replace(/[$,]/g, '');
+  };
+
+  const sortList = rows.sort((a, b) => {
+    const rowOne = numberOptimizer(a.cells[targetIndex].textContent);
+    const rowTwo = numberOptimizer(b.cells[targetIndex].textContent);
+
+    if (typeof elementOne === 'number') {
+      return rowOne - rowTwo;
+    };
+
+    return rowOne.localeCompare(rowTwo);
+  });
+
+  bodyTable.append(...sortList);
+});
