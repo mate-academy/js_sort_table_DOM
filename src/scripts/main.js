d4993dd96f702dd6059882090531ee9a7771f5bd
volodymyr.tkachenko <vladimirscloud@icloud.com>
2023-05-21 02:49:29 +0300

    sort_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const theadList = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tbodyRows = [...tbody.querySelectorAll('tr')];
+
+theadList.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  tbodyRows.sort((a,b) => {
+    const aContent = a.cells[index].textContent;
+    const bContent = b.cells[index].textContent;
+
+    switch (index) {
+      case 0:
+      case 1:
+        return aContent.localeCompare(bContent);
+      case 2:
+        return aContent - bContent;
+      case 3:
+        const toNumber = (item) =>
+item.slice(1).split(',').join('');
+
+        return toNumber(aContent) - toNumber(bContent);
+      default:
+    }
+  });
+  tbodyRows.forEach(item => tbody.append(item));
+});
