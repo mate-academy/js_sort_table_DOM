aebebf808631d4129fa823d753ea393a3f86340d
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-05-24 17:55:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const header = document.querySelector('tr');
+const rows = document.querySelector('tbody');
+
+header.addEventListener('click', e => {
+  const indexOfColumn = [...header.children].findIndex(index =>
+    index === e.target
+  );
+  const sortedRows = [...rows.children].sort((a, b) => {
+    const first = a.children[indexOfColumn].innerText;
+    const second = b.children[indexOfColumn].innerText;
+
+    if (first.includes('$')) {
+      return first.replace(',', '').slice(1) - second.replace(',', '').slice(1);
+    }
+
+    return first.localeCompare(second);
+  });
+
+  rows.append(...sortedRows);
+});
