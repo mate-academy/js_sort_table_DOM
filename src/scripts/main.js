46d5fa59d992e9e25b21fade3a18d3e26af00867
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-05-25 17:18:57 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+function conversion(value) {
+  return +value
+    .split('').slice(1).join('').split(',').join('');
+}
+
+addEventListener('click', e => {
+  const sort = document.querySelector('thead').children[0].children;
+  const cheak = document.querySelector('tfoot');
+  const i = [...sort].findIndex(
+    element => element.innerText === e.target.innerText
+    && !cheak.contains(e.target));
+
+  if (i === -1) {
+    return;
+  }
+
+  const listValue = document.querySelector('tbody');
+
+  function sortList(list) {
+    const result = [...list.children].sort((a, b) => {
+      if (i === 3) {
+        const valueA = conversion(a.children[i].innerText);
+        const valueB = conversion(b.children[i].innerText);
+
+        return valueA - valueB;
+      }
+
+      const value1 = a.children[i].innerText;
+      const value2 = b.children[i].innerText;
+
+      return value1.localeCompare(value2);
+    });
+
+    listValue.append(...result);
+  }
+
+  sortList(listValue);
+});
