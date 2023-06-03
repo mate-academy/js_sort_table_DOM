5f3e3c2a65b36746d9a472af3a4d604d733d9b23
Sasha Green <aleksa146311@gmail.com>
2023-06-03 16:57:15 +0300

    add task sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+
+thead.addEventListener('click', e => {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const tbody = table.querySelector('tbody');
+  const rows = [...tbody.querySelectorAll('tr')];
+  const cellIndex = th.cellIndex;
+  const field = th.innerText;
+
+  rows.sort((a, b) => {
+    const current = a.cells[cellIndex].innerText;
+    const next = b.cells[cellIndex].innerText;
+
+    if (field === 'Age') {
+      return current - next;
+    }
+
+    if (field === 'Salary') {
+      return convStrToNum(current) - convStrToNum(next);
+    }
+
+    return current.localeCompare(next);
+  });
+
+  tbody.append(...rows);
+});
+
+function convStrToNum(str) {
+  return +str.replace(/[$,]/g, '');
+}
