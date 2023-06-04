778f9f93f5e6d591f4c1faa9eab40080cd14ad9a
AndriiYelieva <andrii.yelieva@gmail.com>
2023-06-04 10:25:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+
+thead.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+  const tbody = table.querySelector('tbody');
+  const rows = [...tbody.querySelectorAll('tr')];
+  const cellIndex = th.cellIndex;
+  const field = th.innerText;
+
+  rows.sort((a, b) => {
+    const current = a.cells[cellIndex].innerHTML;
+    const next = b.cells[cellIndex].innerHTML;
+
+    if (field === 'Salary') {
+      return getSalary(current) - getSalary(next);
+    }
+
+    if (field === 'Age') {
+      return current - next;
+    }
+
+    return current.localeCompare(next);
+  });
+
+  tbody.append(...rows);
+});
+
+function getSalary(salary) {
+  return +salary.replace('$', '').replace(',', '');
+}
