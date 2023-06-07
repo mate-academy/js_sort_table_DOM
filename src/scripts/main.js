75d7dbc707dd9265294e42ff0c2970c46d11db3d
sheva <vladislavshevchenko10@gmail.com>
2023-06-07 14:26:18 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const rows = [...tbody.rows];
+
+thead.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const targetIndex = e.target.closest('th').cellIndex;
+
+  const result = rows.sort((a, b) => {
+    const firstCell = a
+      .querySelectorAll('td')[targetIndex]
+      .innerText.replace(/[$,]/g, '');
+    const secondCell = b
+      .querySelectorAll('td')[targetIndex]
+      .innerText.replace(/[$,]/g, '');
+
+    if (targetIndex === 2 || targetIndex === 3) {
+      return firstCell - secondCell;
+    }
+
+    return firstCell.localeCompare(secondCell);
+  });
+
+  tbody.prepend(...result);
+});
