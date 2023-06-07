b30913cf912535cb49ca4aab89493f5abaf61989
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-06-07 14:50:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
+
+function sortTable(columnIndex) {
+  const tbody = table.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  rows.sort((a, b) => {
+    const cellA = a.querySelector(`td:nth-child(${columnIndex + 1})`);
+    const cellB = b.querySelector(`td:nth-child(${columnIndex + 1})`);
+
+    const valueA = cellA.textContent.trim();
+    const valueB = cellB.textContent.trim();
+
+    return valueA.localeCompare(valueB, undefined, { numeric: true });
+  });
+
+  // Очищаем tbody от существующих строк
+  tbody.innerHTML = '';
+
+  // Добавляем отсортированные строки в tbody
+  rows.forEach(row => {
+    tbody.appendChild(row);
+  });
+}
