093f28061aea8532cc4c847d5cd56ee78a9f328a
Vasyl Kozak <sasdaiv@gmail.com>
2023-06-08 16:22:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const rows = table.querySelectorAll('tbody tr');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    const columnIndex = [...headers].indexOf(header);
+    const sortedRows = [...rows].sort((rowA, rowB) => {
+      const cellA = rowA.querySelectorAll('td')[columnIndex].textContent.trim();
+      const cellB = rowB.querySelectorAll('td')[columnIndex].textContent.trim();
+
+      return cellA < cellB ? -1 : cellA > cellB ? 1 : 0;
+    });
+
+    rows.forEach(row => row.remove());
+
+    sortedRows.forEach(sortedRow =>
+      table.querySelector('tbody').appendChild(sortedRow));
+  });
+});
