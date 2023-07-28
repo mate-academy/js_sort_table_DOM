6d809d6f0903e12b05cfa95e8afccf1f9ba40fb5
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-29 02:25:50 +0300

    sort_table_DOM - first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('tbody');
+const rowsArray = Array.from(document.querySelectorAll('tbody tr'));
+const headings = document.querySelector('thead');
+const headingsArray = [];
+
+Array.from(headings.firstElementChild.children).forEach(heading => {
+  headingsArray.push(heading.textContent);
+});
+
+function convertToNumber(string) {
+  return parseInt(string.slice(1).replaceAll(',', ''));
+}
+
+headings.addEventListener('click', e => {
+  const index = headingsArray.indexOf(e.target.textContent);
+
+  rowsArray.sort((a, b) => {
+    const aTextContent = a.cells[index].textContent;
+    const bTextContent = b.cells[index].textContent;
+
+    if (+aTextContent) {
+      return +aTextContent - (+bTextContent);
+    } else if (+convertToNumber(aTextContent)) {
+      return convertToNumber(aTextContent)
+      - convertToNumber(bTextContent);
+    } else if (typeof aTextContent === 'string'
+    && !convertToNumber(aTextContent)) {
+      return aTextContent.localeCompare(bTextContent);
+    }
+  });
+
+  rowsArray.forEach(row => {
+    tableBody.appendChild(row);
+  });
+});
