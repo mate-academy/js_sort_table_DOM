5312035ea87b18e70ad21d5934f8696c71c1b923
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-31 00:48:53 +0300

    requested changes were made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,35 +3,30 @@
 const tableBody = document.querySelector('tbody');
 const rowsArray = Array.from(document.querySelectorAll('tbody tr'));
 const headings = document.querySelector('thead');
-const headingsArray = [];
-
-Array.from(headings.firstElementChild.children).forEach(heading => {
-  headingsArray.push(heading.textContent);
-});
 
 function convertToNumber(string) {
   return parseInt(string.slice(1).replaceAll(',', ''));
 }
 
 headings.addEventListener('click', e => {
-  const index = headingsArray.indexOf(e.target.textContent);
+  const index = e.target.cellIndex;
 
   rowsArray.sort((a, b) => {
-    const aTextContent = a.cells[index].textContent;
-    const bTextContent = b.cells[index].textContent;
+    const aText = a.cells[index].textContent;
+    const bText = b.cells[index].textContent;
+    const callWithA = convertToNumber(aText);
+    const callWithB = convertToNumber(bText);
 
-    if (+aTextContent) {
-      return +aTextContent - (+bTextContent);
-    } else if (+convertToNumber(aTextContent)) {
-      return convertToNumber(aTextContent)
-      - convertToNumber(bTextContent);
-    } else if (typeof aTextContent === 'string'
-    && !convertToNumber(aTextContent)) {
-      return aTextContent.localeCompare(bTextContent);
+    if (+aText) {
+      return +aText - (+bText);
+    } else if (+callWithA) {
+      return callWithA
+      - callWithB;
+    } else if (typeof aText === 'string'
+    && !callWithA) {
+      return aText.localeCompare(bText);
     }
   });
 
-  rowsArray.forEach(row => {
-    tableBody.appendChild(row);
-  });
+  tableBody.append(...rowsArray);
 });
