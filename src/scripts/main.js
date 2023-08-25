65e0de3fd8e17e7fa653c3720b963f88f6b81fd0
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-25 10:14:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,31 +10,29 @@ function toNumbers(string) {
 }
 
 function sortBy(filter) {
-  const t = rows;
-
   switch (filter) {
     case 'Name':
-      t.sort(
+      rows.sort(
         (a, b) => a.children[0].innerHTML.localeCompare(b.children[0].innerHTML)
       );
       break;
     case 'Position':
-      t.sort(
+      rows.sort(
         (a, b) => a.children[1].innerHTML.localeCompare(b.children[1].innerHTML)
       );
       break;
     case 'Age':
-      t.sort((a, b) => +a.children[2].innerHTML - +b.children[2].innerHTML);
+      rows.sort((a, b) => +a.children[2].innerHTML - +b.children[2].innerHTML);
       break;
     case 'Salary':
-      t.sort(
+      rows.sort(
         (a, b) => toNumbers(a.children[3].innerHTML)
           - toNumbers(b.children[3].innerHTML)
       );
       break;
   }
 
-  t.forEach(row => {
+  rows.forEach(row => {
     tableBody.append(row);
   });
 }
