4d852951bb98e381fc01ba74c115486e85e1dbd2
Artem Velychko <avelychko@icloud.com>
2023-08-26 15:10:17 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,87 +2,51 @@
 
 const thead = document.querySelector('thead');
 const mainColumn = thead.querySelector('tr');
-const nameRow = mainColumn.children[0];
-const positionRow = mainColumn.children[1];
-const ageRow = mainColumn.children[2];
-const salaryRow = mainColumn.children[3];
 const tBody = document.querySelector('tbody');
 const tBodyRows = tBody.rows;
+const rowsArray = Array.from(tBodyRows);
 
 function convertToNumber(stringWithSymbols) {
   return parseFloat(stringWithSymbols.replace(/[$,]/g, ''));
 }
 
-const rowsArray = Array.from(tBodyRows);
-
-salaryRow.addEventListener('click', e => {
-  const sortedRows = rowsArray.sort((a, b) => {
-    const salaryA = convertToNumber(a.cells[3].innerText);
-    const salaryB = convertToNumber(b.cells[3].innerText);
-
-    return salaryA - salaryB;
-  });
-
+function sortRow(rowToSort) {
   while (tBody.firstChild) {
     tBody.removeChild(tBody.firstChild);
   }
 
-  sortedRows.forEach(row => {
+  rowToSort.forEach(row => {
     tBody.appendChild(row);
   });
 }
-);
 
-ageRow.addEventListener('click', e => {
-  const sortedRows = rowsArray.sort((a, b) => {
-    const ageA = convertToNumber(a.cells[2].innerText);
-    const ageB = convertToNumber(b.cells[2].innerText);
+thead.addEventListener('click', e => {
+  const target = e.target;
 
-    return ageA - ageB;
-  });
+  for (let i = 0; i < mainColumn.children.length; i++) {
+    let sortedRows;
 
-  while (tBody.firstChild) {
-    tBody.removeChild(tBody.firstChild);
+    if (target === mainColumn.children[i] && i >= 2) {
+      sortedRows = rowsArray.sort((a, b) => {
+        const salaryA = convertToNumber(a.cells[i].innerText);
+        const salaryB = convertToNumber(b.cells[i].innerText);
+
+        return salaryA - salaryB;
+      });
+
+      sortRow(sortedRows);
+    }
+
+    if (target === mainColumn.children[i] && i < 2) {
+      sortedRows = rowsArray.sort((a, b) => {
+        const nameA = a.cells[i].innerText.toLowerCase();
+        const nameB = b.cells[i].innerText.toLowerCase();
+
+        return nameA.localeCompare(nameB);
+      });
+
+      sortRow(sortedRows);
+    }
   }
-
-  sortedRows.forEach(row => {
-    tBody.appendChild(row);
-  });
-}
-);
-
-nameRow.addEventListener('click', e => {
-  const sortedRows = rowsArray.sort((a, b) => {
-    const nameA = a.cells[0].innerText.toLowerCase();
-    const nameB = b.cells[0].innerText.toLowerCase();
-
-    return nameA.localeCompare(nameB);
-  });
-
-  while (tBody.firstChild) {
-    tBody.removeChild(tBody.firstChild);
-  }
-
-  sortedRows.forEach(row => {
-    tBody.appendChild(row);
-  });
-}
-);
-
-positionRow.addEventListener('click', e => {
-  const sortedRows = rowsArray.sort((a, b) => {
-    const positionA = a.cells[1].innerText.toLowerCase();
-    const positionB = b.cells[1].innerText.toLowerCase();
-
-    return positionA.localeCompare(positionB);
-  });
-
-  while (tBody.firstChild) {
-    tBody.removeChild(tBody.firstChild);
-  }
-
-  sortedRows.forEach(row => {
-    tBody.appendChild(row);
-  });
 }
 );
