e918464665559b5b00ed45c1dba78cca4c0459c0
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-09-23 13:22:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('tbody');
+const headers = document.querySelectorAll('thead th');
+
+const stringToInt = (str) => {
+  return parseInt(str.replace(/\$|,/g, ''), 10);
+};
+
+const sortList = (indexOfChild, isDataNumeric = false) => {
+  const rows = [...body.rows];
+
+  rows.sort((row1, row2) => {
+    const data1 = row1.cells[indexOfChild].textContent;
+    const data2 = row2.cells[indexOfChild].textContent;
+
+    return isDataNumeric
+      ? stringToInt(data1) - stringToInt(data2)
+      : data1.localeCompare(data2);
+  });
+
+  rows.forEach((row) => body.appendChild(row));
+};
+
+const addClickListener = (header, index, isDataNumeric) => {
+  return header.addEventListener('click', () => sortList(index, isDataNumeric));
+};
+
+headers.forEach((header, index) => {
+  if (index < 2) {
+    addClickListener(header, index);
+  } else {
+    addClickListener(header, index, true);
+  }
+});
