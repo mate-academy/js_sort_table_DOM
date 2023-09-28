c852fcc4a5702d386725443a33bfda3fc712b4ff
spencerspe <spencerspe@gmail.com>
2023-09-28 10:50:17 +0100

    minor changes made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,13 +8,13 @@ const tablerows = document.querySelectorAll('tbody tr');
 const tablerowsArray = Array.from(tablerows);
 
 table.addEventListener('click', function(evt) {
-  evt.preventDefault();
-
-  const clickCol = evt.target.cellIndex + 1;
+  const clickedColumnIndex = evt.target.cellIndex + 1;
 
   const sorted = tablerowsArray.sort((a, b) => {
-    const itemOne = a.querySelector(`td:nth-child(${clickCol}`).innerHTML;
-    const itemTwo = b.querySelector(`td:nth-child(${clickCol}`).innerHTML;
+    const itemOne = a.querySelector(`td:nth-child(${clickedColumnIndex}`)
+      .innerHTML;
+    const itemTwo = b.querySelector(`td:nth-child(${clickedColumnIndex}`)
+      .innerHTML;
 
     return itemOne.localeCompare(itemTwo, 'en', { numeric: 'true' });
   });
