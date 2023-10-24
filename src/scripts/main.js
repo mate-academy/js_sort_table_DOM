96f10f9fc5bf51ec0d18a85bd6ff36909cbdca1a
Andrii Rykal <andreuka79@gmail.com>
2023-10-24 12:27:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', e => {
+  if (e.target.nodeName !== 'TH') {
+    return;
+  };
+
+  const theadElements = [...document
+    .querySelector('thead').firstElementChild.children];
+    
+  const tbody = document.querySelector('tbody');
+  const tbodyElements = [...tbody.children];
+  const value = e.target.textContent;
+
+  const tbodyElemSort = sortBy(tbodyElements, theadElements, value);
+  const tbodySort = document.createElement('tbody');
+
+  for (const el of tbodyElemSort) {
+    tbodySort.append(el);
+  }
+
+  tbody.replaceWith(tbodySort);
+});
+
+function salaryToNumber(salary) {
+  return +salary.slice(1).split(',').join('');
+}
+
+function sortBy(tbEl, thEl, value) {
+  for (let i = 0; i < thEl.length; i++) {
+    if (thEl[i].textContent !== value) {
+      continue;
+    }
+
+    tbEl.sort((a, b) => {
+      switch (value) {
+        case 'Name':
+        case 'Position':
+          return a.children[i].textContent
+            .localeCompare(b.children[i].textContent);
+
+        case 'Age':
+          return +a.children[i].textContent - +b.children[i].textContent;
+
+        case 'Salary':
+          return salaryToNumber(a.children[i].textContent)
+            - salaryToNumber(b.children[i].textContent);
+      }
+    });
+  }
+
+  return tbEl;
+}
