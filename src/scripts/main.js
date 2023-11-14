c7c06b31082c7fc4bd4194e737540a24f2d661d4
Damian <damian.adamczewski92@gmail.com>
2023-11-14 17:05:02 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tableHeaders = document.querySelectorAll('thead th');
+const tableRows = document.querySelectorAll('tbody tr');
+const table = document.querySelector('tbody');
+
+tableHeaders.forEach(header => {
+  header.addEventListener('click', sort);
+});
+
+function sort(header) {
+  const headerIndex = [...tableHeaders].indexOf(header.target);
+
+  const sortedTable = [...tableRows].sort((a, b) => {
+    const textA = a.children[headerIndex].innerText;
+    const textB = b.children[headerIndex].innerText;
+
+    if (textA[0] === '$' || !isNaN(+textA)) {
+      return parseInt(textA.replace(/\D/g, ''))
+      - parseInt(textB.replace(/\D/g, ''));
+    }
+
+    return textA.localeCompare(textB);
+  });
+
+  table.append(...sortedTable);
+};
