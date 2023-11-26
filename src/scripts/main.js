57e6bb143e2525987fb020ce2f5e343d55b1a30e
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-26 16:52:03 +0100

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tBody = document.querySelector('tbody');
+const thElemArr = document.querySelectorAll('th');
+const filterArea = document.querySelector('thead');
+const trElem = tBody.querySelectorAll('tr');
+
+filterArea.addEventListener('click', e => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const index = [...thElemArr].indexOf(e.target);
+
+  const sorted = [...trElem].sort((a, b) => {
+    const elemA = a.cells[index].innerText.replace('$', '').replace(',', '');
+    const elemB = b.cells[index].innerText.replace('$', '').replace(',', '');
+
+    if (!isNaN(elemA) || elemA[0] === '$') {
+      return elemA - elemB;
+    }
+
+    return elemA.localeCompare(elemB);
+  });
+
+  tBody.append(...sorted);
+});
