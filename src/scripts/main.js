aa8fedd75e581d29068a20b9f6688ef97c16753a
donizer <bogdan.ua45@gmail.com>
2023-12-05 15:25:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', function() {
+  const table = document.querySelector('table');
+  const headers = table.querySelectorAll('thead th');
+
+  headers.forEach(function(header, index) {
+    header.addEventListener('click', function() {
+      sortTable(index);
+    });
+  });
+
+  function sortTable(columnIndex) {
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    const clickedHeader = headers[columnIndex];
+
+    rows.sort(function(rowA, rowB) {
+      let cellA = rowA.querySelectorAll('td')[columnIndex].textContent.trim();
+      let cellB = rowB.querySelectorAll('td')[columnIndex].textContent.trim();
+
+      if (clickedHeader.textContent.trim() === 'Salary') {
+        cellA = parseFloat(cellA.replace(/\$/g, '').replace(/,/g, ''));
+        cellB = parseFloat(cellB.replace(/\$/g, '').replace(/,/g, ''));
+      }
+
+      return columnIndex === 3 ? cellA - cellB : cellA.localeCompare(cellB);
+    });
+
+    rows.forEach(function(row) {
+      table.querySelector('tbody').removeChild(row);
+    });
+
+    rows.forEach(function(row) {
+      table.querySelector('tbody').appendChild(row);
+    });
+  }
+});
