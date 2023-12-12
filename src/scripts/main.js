c98569cc0aa7fdab79d693968e61949960e4af19
Andrey Bondar <andrey_bondar@outlook.com>
2023-12-12 22:09:24 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
-// write code here
+const tbody = document.getElementsByTagName('tbody')[0];
+const rows = tbody.children;
+
+document.body.addEventListener('click', (e) => {
+  const { target } = e;
+  let arr;
+
+  if (target.localName === 'th') {
+    switch (target.innerText) {
+      case 'Name':
+        arr = [...rows].sort((elem1, elem2) => {
+          const first = elem1.children[0].innerText;
+          const second = elem2.children[0].innerText;
+
+          return first.localeCompare(second);
+        });
+
+        break;
+
+      case 'Position':
+        arr = [...rows].sort((elem1, elem2) => {
+          const first = elem1.children[1].innerText;
+          const second = elem2.children[1].innerText;
+
+          return first.localeCompare(second);
+        });
+
+        break;
+
+      case 'Age':
+        arr = [...rows].sort((elem1, elem2) => {
+          const first = getNumber(elem1.children[2].innerText);
+          const second = getNumber(elem2.children[2].innerText);
+
+          return first - second;
+        });
+
+        break;
+
+      case 'Salary':
+        arr = [...rows].sort((elem1, elem2) => {
+          const first = getNumber(elem1.children[3].innerText);
+          const second = getNumber(elem2.children[3].innerText);
+
+          return first - second;
+        });
+
+        break;
+
+      default:
+        break;
+    }
+
+    arr.forEach(li => {
+      tbody.append(li);
+    });
+  }
+});
+
+function getNumber(string) {
+  if (!string.includes('$')) {
+    return +string;
+  }
+
+  return +string.replace(/[$,]/ig, '');
+}
