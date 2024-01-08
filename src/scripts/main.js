59c6e97c709c69a86063bcc06703e638508fb015
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-08 17:30:25 +0200

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const mainRow = table.rows[0];
+const dataRows = Array.from(table.rows).slice(1, 13);
+
+function compareValues(a, b) {
+  const aNum = a.slice(1).replace(',', '');
+  const bNum = b.slice(1).replace(',', '');
+
+  if (!isNaN(aNum) && !isNaN(bNum)) {
+    return aNum - bNum;
+  } else {
+    return a.localeCompare(b);
+  }
+}
+
+mainRow.addEventListener('click', (e) => {
+  const clickedCell = e.target.closest('th');
+
+  if (clickedCell) {
+    const columnIndex = clickedCell.cellIndex;
+
+    dataRows.sort((a, b) => {
+      const aValue = a.cells[columnIndex].textContent.trim();
+      const bValue = b.cells[columnIndex].textContent.trim();
+
+      return compareValues(aValue, bValue);
+    });
+
+    dataRows.forEach((row) => table.tBodies[0].appendChild(row));
+  }
+});
