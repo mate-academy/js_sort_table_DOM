5e1268871e5ca4258d9080770bde5859b53f9df5
Sergey Arnautov <sirius9312@ukr.net>
2024-01-16 12:23:05 +0200

    Sort table solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const header = document.querySelector('thead tr');
+const columns = document.querySelectorAll('tbody tr');
+
+header.addEventListener('click', e => {
+  e.preventDefault();
+
+  const headerIndex = [...e.target.parentNode.children]
+    .findIndex(item => item === e.target);
+
+  sortTableByColumn(headerIndex);
+});
+
+function sortTableByColumn(headerIndex) {
+  const sortArr = (headerIndex === 3)
+    ? sortBySalaryColumn(headerIndex)
+    : sortByRestColumns(headerIndex);
+
+  for (let y = 0; y < [...columns].length; y++) {
+    for (let x = 0; x < [...columns][y].children.length; x++) {
+      [...columns][y].children[x].textContent = sortArr[y][x];
+    }
+  }
+}
+
+function sortBySalaryColumn(index) {
+  return [...columns]
+    .map(item => [...item.children]
+      .map(iteM => iteM.textContent))
+    .sort((item1, item2) => +item1[index].slice(1).replace(/,/g, '.')
+      - (+item2[index].slice(1).replace(/,/g, '.')));
+}
+
+function sortByRestColumns(index) {
+  return [...columns]
+    .map(item => [...item.children]
+      .map(iteM => iteM.textContent))
+    .sort((item1, item2) => item1[index]
+      .localeCompare(item2[index]));
+}
