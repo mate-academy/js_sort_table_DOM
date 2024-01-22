9621f8a52ec9ec84ecd95536d080e03009940041
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-22 20:50:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,14 +15,38 @@ const employees = [...allRows].splice(1);
 employees.pop();
 
 tableHead.addEventListener('click', e => {
-  if (e.target.innerText === 'Salary') {
-    const sorted = employees.sort((employee1, employee2) => {
-      return convertToNumber(employee1.children[3].innerText)
-        > convertToNumber(employee2.children[3].innerText) ? 1 : -1;
-    });
+  switch (e.target.innerText) {
+    case 'Salary':
+      employees.sort((employee1, employee2) => {
+        return convertToNumber(employee1.children[3].innerText)
+          > convertToNumber(employee2.children[3].innerText) ? 1 : -1;
+      });
+      break;
 
-    tableBody.innerHTML = `
-      ${sorted.map(element => `
+    case 'Age':
+      employees.sort((employee1, employee2) => {
+        return +employee1.children[2].innerText
+          > +employee2.children[2].innerText ? 1 : -1;
+      });
+      break;
+
+    case 'Position':
+      employees.sort((employee1, employee2) => {
+        return employee1.children[1].innerText
+          .localeCompare(employee2.children[1].innerText);
+      });
+      break;
+
+    case 'Name':
+      employees.sort((employee1, employee2) => {
+        return employee1.children[0].innerText
+          .localeCompare(employee2.children[0].innerText);
+      });
+      break;
+  }
+
+  tableBody.innerHTML = `
+      ${employees.map(element => `
         <tr>
           <td>${element.children[0].innerText}</td>
           <td>${element.children[1].innerText}</td>
@@ -31,59 +55,4 @@ tableHead.addEventListener('click', e => {
         </tr>
       `).join('')}
     `;
-  }
-
-  if (e.target.innerText === 'Age') {
-    const sorted = employees.sort((employee1, employee2) => {
-      return +employee1.children[2].innerText
-        > +employee2.children[2].innerText ? 1 : -1;
-    });
-
-    tableBody.innerHTML = `
-      ${sorted.map(element => `
-        <tr>
-          <td>${element.children[0].innerText}</td>
-          <td>${element.children[1].innerText}</td>
-          <td>${element.children[2].innerText}</td>
-          <td>${element.children[3].innerText}</td>
-        </tr>
-      `).join('')}
-    `;
-  }
-
-  if (e.target.innerText === 'Name') {
-    const sorted = employees.sort((employee1, employee2) => {
-      return employee1.children[0].innerText
-        .localeCompare(employee2.children[0].innerText);
-    });
-
-    tableBody.innerHTML = `
-      ${sorted.map(element => `
-        <tr>
-          <td>${element.children[0].innerText}</td>
-          <td>${element.children[1].innerText}</td>
-          <td>${element.children[2].innerText}</td>
-          <td>${element.children[3].innerText}</td>
-        </tr>
-      `).join('')}
-    `;
-  }
-
-  if (e.target.innerText === 'Position') {
-    const sorted = employees.sort((employee1, employee2) => {
-      return employee1.children[1].innerText
-        .localeCompare(employee2.children[1].innerText);
-    });
-
-    tableBody.innerHTML = `
-      ${sorted.map(element => `
-        <tr>
-          <td>${element.children[0].innerText}</td>
-          <td>${element.children[1].innerText}</td>
-          <td>${element.children[2].innerText}</td>
-          <td>${element.children[3].innerText}</td>
-        </tr>
-      `).join('')}
-    `;
-  }
 });
