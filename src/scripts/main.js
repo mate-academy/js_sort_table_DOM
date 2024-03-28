eee48dceab9c5eae0152b1e033c7d73cc6888193
AnoriFrell <anori.fjord@gmail.com>
2024-03-28 13:27:09 +0200

    sorted data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const data = document.querySelectorAll('tbody tr');
+const header = document.querySelectorAll('thead th');
+
+header.forEach((title) => {
+  title.addEventListener('click', sorting);
+});
+
+function sorting(title) {
+  const targetIndex = [...header].indexOf(title.target);
+
+  const sortedData = [...data].sort((a, b) => {
+    const dataA = a.children[targetIndex].textContent;
+    const dataB = b.children[targetIndex].textContent;
+
+    if (dataA[0] === '$' || !isNaN(+dataA)) {
+      return +dataA.replace(/\D/g, '') - +dataB.replace(/\D/g, '');
+    }
+
+    return dataA.localeCompare(dataB);
+  });
+
+  table.append(...sortedData);
+}
