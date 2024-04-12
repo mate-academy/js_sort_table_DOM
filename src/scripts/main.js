670bcfe0996570bc6a565780dbaf7a0baca6b4b7
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-12 19:27:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('tbody');
+const data = document.querySelectorAll('tbody tr');
+const thead = document.querySelectorAll('thead th');
+
+thead.forEach((title) => {
+  title.addEventListener('click', sorting);
+});
+
+function sorting(e) {
+  const targetIndex = [...thead].indexOf(e.target);
+
+  const sortedData = [...data].sort((a, b) => {
+    const dataA = a.children[targetIndex].textContent;
+    const dataB = b.children[targetIndex].textContent;
+
+    if (dataA[0] === '$' || !isNaN(+dataA)) {
+      return (
+        parseFloat(dataA.replace(/\D/g, '')) -
+        parseFloat(dataB.replace(/\D/g, ''))
+      );
+    }
+
+    return dataA.localeCompare(dataB);
+  });
+
+  table.append(...sortedData);
+}
