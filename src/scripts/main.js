9f7e7780631dfc34ed178c046055ba7b16616c12
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-29 12:12:25 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const headItems = document.querySelectorAll('thead th');
+const bodyRows = document.querySelectorAll('tbody tr');
+
+function sortItems(index) {
+  const rows = Array.from(bodyRows);
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent;
+    const cellB = rowB.cells[index].textContent;
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+
+  rows.forEach((row) => row.parentNode.append(row));
+}
+
+headItems.forEach((item, index) => {
+  item.addEventListener('click', () => sortItems(index));
+});
