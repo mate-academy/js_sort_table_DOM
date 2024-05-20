7922bf7b31d2f375436023589332058eae460b14
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-05-20 15:28:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = table.querySelector('tbody');
+const tableBodyRows = Array.from(tableBody.querySelectorAll('tr'));
+
+table.addEventListener('click', (e) => {
+  const header = e.target.closest('th');
+
+  if (!header) {
+    return;
+  }
+
+  const headers = Array.from(table.querySelectorAll('th'));
+  const currentHeaderIndex = headers.indexOf(header);
+  const currentHeaderName = header.textContent.toLowerCase();
+
+  function sortRows(rowA, rowB) {
+    const cellA = rowA.cells[currentHeaderIndex].textContent;
+    const cellB = rowB.cells[currentHeaderIndex].textContent;
+
+    switch (currentHeaderName) {
+      case 'name':
+      case 'position':
+        return cellA.localeCompare(cellB);
+
+      case 'age':
+        return +cellA - +cellB;
+
+      case 'salary':
+        return (
+          +cellA.replaceAll('$', '').replaceAll(',', '') -
+          +cellB.replaceAll('$', '').replaceAll(',', '')
+        );
+    }
+  }
+
+  tableBodyRows.sort(sortRows).forEach((row) => {
+    tableBody.appendChild(row);
+  });
+});
