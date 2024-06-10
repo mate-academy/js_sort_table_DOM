1ea0160eecef44ca2a5161d174f5e4aeacfca671
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-06-10 14:23:07 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const theadCells = [...thead.rows[0].cells];
+const tbodyRows = [...tbody.rows];
+
+const sortTable = (columnIndex) => {
+  tbodyRows
+    .sort((a, b) => {
+      a.cells[columnIndex].textContent
+        .trim()
+        .localeCompare(b.cells[columnIndex].textContent.trim(), 'en', {
+          numeric: true,
+        });
+    })
+    .forEach((row) => tbody.appendChild(row));
+};
+
+theadCells.forEach((cell, index) => {
+  cell.onclick = () => sortTable(index);
+});
