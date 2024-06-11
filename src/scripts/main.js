12b8ef72ef1a1355f4cb02ac64a5dfbbf3781eb8
Krzysztof <krzysiek.gugala@gmail.com>
2024-06-11 14:39:37 +0200

    solutionV3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const titles = Array.from(document.querySelectorAll('th'));
+const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+
+titles.forEach((title, index) => {
+  title.addEventListener('click', () => {
+    tableRows.sort((a, b) => {
+      const aValue = a.cells[index].textContent.trim();
+      const bValue = b.cells[index].textContent.trim();
+
+      const aNum = parseFloat(aValue.replace(/[^0-9.-]+/g, ''));
+      const bNum = parseFloat(bValue.replace(/[^0-9.-]+/g, ''));
+
+      if (!isNaN(aNum) && !isNaN(bNum)) {
+        return aNum - bNum;
+      } else {
+        return aValue.localeCompare(bValue);
+      }
+    });
+
+    const tbody = document.querySelector('tbody');
+
+    tableRows.forEach((row) => tbody.appendChild(row));
+  });
+});
