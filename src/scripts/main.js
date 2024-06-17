0fced3048ce9239320bca77b492e2d5bcb97bf9a
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-06-17 17:31:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+window.addEventListener('DOMContentLoaded', app);
+
+function app() {
+  const table = document.querySelector('table');
+  const tHead = table.tHead;
+
+  [...tHead.rows[0].cells].forEach((cell, index) =>
+    cell.addEventListener('click', (e) =>
+      sortHandler(e, cell.innerText.toLowerCase().trim(), index),
+    ),
+  );
+
+  function sortHandler(e, sortBy, index) {
+    const tBody = table.tBodies[0];
+
+    const sorted = [...tBody.rows].sort((a, b) => {
+      const first = a.cells[index].innerText;
+      const second = b.cells[index].innerText;
+
+      switch (sortBy) {
+        case 'age':
+          return +first - +second;
+
+        case 'salary':
+          return extractNumber(first) - extractNumber(second);
+
+        default:
+          return first.localeCompare(second);
+      }
+    });
+    const newTbody = document.createElement('tbody');
+
+    sorted.forEach((tr) => newTbody.appendChild(tr));
+
+    tBody.remove();
+    table.appendChild(newTbody);
+  }
+}
+
+function extractNumber(str) {
+  return parseFloat(str.replace(/[$,]/g, ''));
+}
