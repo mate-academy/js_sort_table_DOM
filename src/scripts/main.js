80f57d9b6ea1ba75449c9e2aa51969760815bae8
Hanna Svirina <26anna07@ukr.net>
2024-06-18 19:35:00 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,5 @@
 'use strict';
 
-// const headerItems = document.querySelectorAll('thead th');
-// const tableRows = document.querySelectorAll('tbody tr');
-// const table = document.querySelector('tbody');
-
-// function sort(item) {
-//   const itemIndex = [...headerItems].indexOf(item.target);
-
-//   const sortedTable = [...tableRows].sort((row1, row2) => {
-//     const textRowA = row1.children[itemIndex].innerText;
-//     const textRowB = row2.children[itemIndex].innerText;
-
-//     if (textRowA.includes('$') || !isNaN(+textRowA)) {
-//       return (
-//         parseInt(textRowA.replace(/\D/g, '')) -
-//         parseInt(textRowB.replace(/\D/g, ''))
-//       );
-//     }
-
-//     return textRowA.localeCompare(textRowB);
-//   });
-
-//   table.append(...sortedTable);
-// }
-
-// headerItems.forEach((headerItem) => {
-//   headerItem.addEventListener('click', sort);
-// });
-
 const headerItems = document.querySelectorAll('thead th');
 const tableRows = document.querySelectorAll('tbody tr');
 const table = document.querySelector('tbody');
