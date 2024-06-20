10a64754d52da39cf198db860f9c99b65ad9a09f
Viktor Talashko <viktortalashko@gmail.com>
2024-06-20 23:51:29 +0300

    my solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+const titlesList = document.querySelectorAll('table th');
+const tbody = document.querySelector('tbody');
+const rows = Array.from(tbody.querySelectorAll('tr'));
+
+titlesList.forEach((title, index) => {
+  title.addEventListener('click', (e) => {
+    const titleText = e.target.textContent.trim();
+
+    switch (titleText) {
+      case 'Name':
+        sortTable(index, 'text');
+        break;
+      case 'Position':
+        sortTable(index, 'text');
+        break;
+      case 'Age':
+        sortTable(index, 'number');
+        break;
+      case 'Salary':
+        sortTable(index, 'number');
+        break;
+      default:
+        break;
+    }
+
+    // tbody.innerHTML = '';
+    rows.forEach((row) => tbody.append(row));
+  });
+});
+
+function sortTable(columnIndex, dataType) {
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.querySelectorAll('td')[columnIndex].textContent.trim();
+    const cellB = rowB.querySelectorAll('td')[columnIndex].textContent.trim();
+
+    if (dataType === 'number') {
+      return helper(cellA) - helper(cellB);
+    } else {
+      return cellA.localeCompare(cellB);
+    }
+  });
+}
+
+function helper(string) {
+  return Number(string.replace(/[^0-9.-]+/g, ''));
+}
