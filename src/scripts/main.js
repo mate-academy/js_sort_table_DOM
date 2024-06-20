3ef84cc5f717bb30d98769a486a7c368cc9095e5
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-20 19:10:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+function transformToNumber(str) {
+  return +str.replace(',', '').slice(1);
+}
+
+const tbody = document.querySelector('tbody');
+const title = document.querySelector('thead');
+const rows = [...tbody.querySelectorAll('tr')];
+
+title.addEventListener('click', (e) => {
+  // получаю заголовк хедер по якому клікнув
+  const value = e.target.closest('th').textContent.toLowerCase();
+  // получаю індекс заголовка cellIndex
+  const titleIndex = e.target.cellIndex;
+  // сортуємо і треба мати на увазі що ми сортуємо
+  // дом елементи по значеннях, ми лише переставляємо елементи
+  const newRows = rows.sort((a, b) => {
+    const cellsA = a.cells[titleIndex].textContent;
+    const cellsB = b.cells[titleIndex].textContent;
+
+    switch (value) {
+      case 'name':
+      case 'position':
+        return cellsA.localeCompare(cellsB);
+
+      case 'age':
+        return cellsA - cellsB;
+
+      case 'salary':
+        return transformToNumber(cellsA) - transformToNumber(cellsB);
+
+      default:
+        return null;
+    }
+  });
+
+  tbody.innerHTML = '';
+
+  newRows.forEach((item) => {
+    tbody.appendChild(item);
+  });
+});
