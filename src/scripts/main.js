60f43d68796e6ee14641e8cd8ded1c50e9e98890
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-24 21:43:30 +0300

    Sorting of the table by clicking on the title has been implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', function (e) {
+  const headerCell = e.target.closest('thead th');
+
+  if (headerCell) {
+    const { cellIndex, textContent } = headerCell;
+    const tbodyRows = [...table.querySelector('tbody').rows];
+
+    tbodyRows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[cellIndex].textContent;
+      const cellB = rowB.cells[cellIndex].textContent;
+      const regExp = /[^0-9]/g;
+
+      if (['Name', 'Position'].includes(textContent)) {
+        return cellA.localeCompare(cellB);
+      } else if (['Age', 'Salary'].includes(textContent)) {
+        return +cellA.replace(regExp, '') - +cellB.replace(regExp, '');
+      }
+    });
+
+    const tableBody = table.querySelector('tbody');
+
+    tableBody.innerHTML = '';
+    tbodyRows.forEach((row) => tableBody.appendChild(row));
+  }
+});
