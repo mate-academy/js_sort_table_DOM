1b53b60893f959f7e74ada40531438ef1df519ad
Marina Avramenko <marina.avramenko123@gmail.com>
2024-06-24 11:33:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const thead = document.querySelectorAll('th');
+
+for (let i = 0; i < thead.length; i++) {
+  thead[i].addEventListener('click', () => {
+    sortByColumnIndex(i);
+  });
+}
+
+const tbody = document.querySelector('table tbody');
+const rows = [...tbody.querySelectorAll('tr')];
+
+function sortByColumnIndex(index) {
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent;
+    const cellB = rowB.cells[index].textContent;
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+
+  tbody.innerHTML = '';
+  rows.forEach((row) => tbody.append(row));
+}
