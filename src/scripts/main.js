44a8ef7a69c0a69f6ace34561ee1e6dd19d1d7c3
Serhii Prikhodko <venom99@ukr.net>
2024-06-27 16:27:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+
+const tableRows = [...tableBody.rows];
+const onlyNumbers = (string) => +string.replace(/[$,]/g, '');
+
+tableHead.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  tableRows.sort((a, b) => {
+    const el1 = a.children[index].innerText;
+    const el2 = b.children[index].innerText;
+
+    return isNaN(onlyNumbers(el1))
+      ? el1.localeCompare(el2)
+      : onlyNumbers(el1) - onlyNumbers(el2);
+  });
+
+  tableBody.append(...tableRows);
+});
