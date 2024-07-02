d6185b4cb1073620df3db718847094613de39c81
Denis Reznik <reznik.d.o12@gmail.com>
2024-07-02 13:33:25 +0300

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,19 +9,10 @@ const tBodyRows = [...tBody.rows];
 for (let i = 0; i < tHeadCells.length; i++) {
   tHeadCells[i].addEventListener('click', function () {
     tBodyRows.sort((a, b) => {
-      const cellA = a.cells[i].innerText.toLowerCase();
-      const cellB = b.cells[i].innerText.toUpperCase();
+      const cellA = a.cells[i].textContent;
+      const cellB = b.cells[i].textContent;
 
-      if (!isNaN(cellA) && isNaN(cellB)) {
-        return parseFloat(cellA) - parseFloat(cellB);
-      } else if (cellA.includes('$')) {
-        const newA = cellA.replace(/[$,]/g, '');
-        const newB = cellB.replace(/[$,]/g, '');
-
-        return parseFloat(newA) - parseFloat(newB);
-      } else {
-        return cellA.localeCompare(cellB);
-      }
+      return cellA.localeCompare(cellB, undefined, { numeric: true });
     });
 
     tBodyRows.forEach((row) => {
