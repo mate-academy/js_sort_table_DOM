31c2cb51de1c5868ecf3ce1e866abd9ea41ef632
4xmplme <andrii_kondratenko@icloud.com>
2024-07-05 14:25:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tableColumns = [...document.querySelectorAll('table thead th')];
+const tableBody = document.querySelector('table tbody');
+const tableRows = [...tableBody.children];
+
+document.addEventListener('click', (e) => {
+  const { target } = e;
+
+  if (tableColumns.includes(target)) {
+    const columnIndex = tableColumns.indexOf(target);
+
+    tableRows.sort((rowA, rowB) => {
+      const { textContent: cellA } = rowA.children[columnIndex];
+      const { textContent: cellB } = rowB.children[columnIndex];
+
+      switch (target.textContent) {
+        case 'Name':
+        case 'Position':
+        default:
+          return cellA.localeCompare(cellB);
+
+        case 'Age':
+        case 'Salary':
+          return +cellA.replace(/\D/g, '') - +cellB.replace(/\D/g, '');
+      }
+    });
+  }
+
+  tableBody.append(...tableRows);
+});
