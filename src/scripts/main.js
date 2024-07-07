4bbc176d071610c9639998d6a799050a7d8524a9
luter <newgglutt@gmail.com>
2024-07-07 16:06:29 +0300

    sdsdsdsd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,7 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const aText = a.children[columnIndex].textContent.trim();
         const bText = b.children[columnIndex].textContent.trim();
 
-        if (!isNaN(aText) && !isNaN(bText)) {
+        if (header.textContent === 'Salary') {
+          const aValue = parseFloat(aText.replace(/[^0-9.-]+/g, ''));
+
+          const bValue = parseFloat(bText.replace(/[^0-9.-]+/g, ''));
+
+          return aValue - bValue;
+        } else if (!isNaN(aText) && !isNaN(bText)) {
           return Number(aText) - Number(bText);
         } else {
           return aText.localeCompare(bText);
