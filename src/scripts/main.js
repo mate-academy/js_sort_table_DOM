91523060160d30582cee3ab0509595e2cedf9d01
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-15 23:21:37 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead').firstElementChild;
+
+tHead.addEventListener('click', (e) => {
+  const column = [...tHead.children].indexOf(e.target);
+  const tbody = document.querySelector('tbody');
+  const rows = [...tbody.children];
+
+  rows
+    .sort((row1, row2) => {
+      let sortCell1 = row1.children[column].innerText;
+      let sortCell2 = row2.children[column].innerText;
+
+      if (column === 2) {
+        sortCell1 = +sortCell1;
+        sortCell2 = +sortCell2;
+
+        return sortCell1 - sortCell2;
+      }
+
+      if (column === 3) {
+        sortCell1 = +sortCell1.slice(1).replace(',', '');
+        sortCell2 = +sortCell2.slice(1).replace(',', '');
+
+        return sortCell1 - sortCell2;
+      }
+
+      return sortCell1.localeCompare(sortCell2);
+    })
+    .forEach((row) => {
+      tbody.append(row);
+    });
+});
