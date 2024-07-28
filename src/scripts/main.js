bd9c2d51b424483f3307c9bf2932129b4e06c988
Vasyl Chukhrai <wasik1995@mail.com>
2024-07-28 23:17:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const getNumber = (tr) => +tr.textContent.replace(/\D/g, '');
+
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+
+thead.querySelectorAll('th').forEach((th, index) => {
+  th.dataset.index = index;
+});
+
+thead.onclick = (e) => {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const trs = [...tbody.querySelectorAll('tr')];
+
+  const index = th.dataset.index;
+
+  const sortedTrs = trs.sort((trOne, trTwo) => {
+    const tdsOne = trOne.querySelectorAll('td');
+    const tdsTwo = trTwo.querySelectorAll('td');
+    const valueOne = tdsOne[index].textContent;
+    const valueTwo = tdsTwo[index].textContent;
+
+    if (getNumber(tdsTwo[index])) {
+      return getNumber(tdsOne[index]) - getNumber(tdsTwo[index]);
+    }
+
+    return valueOne.localeCompare(valueTwo);
+  });
+
+  sortedTrs.forEach((tr) => tbody.appendChild(tr));
+};
