5a5f3ea3b21f11d19ca517c9e51e9165e866681a
Korolko Anastasiia <korolkonasta@gmail.com>
2024-08-01 21:45:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+const tableBody = document.querySelector('table tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
+
+function sortTable(columnIndex) {
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[columnIndex].textContent;
+    const cellB = rowB.cells[columnIndex].textContent;
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+  tableBody.innerHTML = '';
+  rows.forEach((row) => tableBody.appendChild(row));
+}
