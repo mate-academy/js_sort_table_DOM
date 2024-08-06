fd39a262efc871e0c16366dacb437b3640a3aadd
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-06 22:28:30 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const allTh = table.querySelectorAll('th');
+const tBody = table.querySelector('tbody');
+
+allTh.forEach((headerItem, index) => {
+  headerItem.addEventListener('click', function () {
+    const rows = [...tBody.rows];
+    const sortedRows = rows.sort((a, b) => {
+      const aText = a.cells[index].textContent.trim();
+      const bText = b.cells[index].textContent.trim();
+
+      if (aText.includes('$')) {
+        const aValue = parseFloat(aText.replace(/[$,]/g, ''));
+        const bValue = parseFloat(bText.replace(/[$,]/g, ''));
+
+        return aValue - bValue;
+      } else {
+        return aText > bText ? 1 : aText < bText ? -1 : 0;
+      }
+    });
+
+    tBody.append(...sortedRows);
+  });
+});
