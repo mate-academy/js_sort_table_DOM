ae6bb17f997d2ebc5a14a5a76838e2f3d5b1c621
mrsvolodya <vovamursky@gmail.com>
2024-08-07 11:39:11 +0300

    reload-repos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+
+const sortedList = (i) => {
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  if (i < 2) {
+    rows.sort((a, b) =>
+      a.children[i].textContent.localeCompare(b.children[i].textContent)
+    );
+  } else {
+    rows.sort(
+      (a, b) =>
+        +a.children[i].textContent.replaceAll(/[$,]/g, '') -
+        +b.children[i].textContent.replaceAll(/[$,]/g, ''),
+    );
+  }
+
+  tbody.innerHTML = '';
+  tbody.append(...rows);
+};
+
+thead.addEventListener('click', (e) => {
+  const title = e.target.textContent;
+
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  switch (title) {
+    case 'Name':
+      sortedList(0);
+      break;
+    case 'Position':
+      sortedList(1);
+      break;
+    case 'Age':
+      sortedList(2);
+      break;
+    case 'Salary':
+      sortedList(3);
+      break;
+  }
+});
