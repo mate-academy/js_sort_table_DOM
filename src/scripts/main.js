a233163dcb904404218384e4790456360710217d
Ivan Mondok <ivanmondok8@gmail.com>
2024-08-08 17:43:18 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const theads = document.querySelectorAll('th');
+const tbody = document.querySelector('table tbody');
+const trArr = [...tbody.querySelectorAll('tr')];
+
+function sort(i) {
+  trArr.sort((a, b) => {
+    const A = a.cells[i].textContent.trim();
+    const B = b.cells[i].textContent.trim();
+
+    return A.localeCompare(B, undefined, { numeric: true });
+  });
+
+  tbody.append(...trArr);
+}
+
+theads.forEach((thead, index) => {
+  thead.addEventListener('click', () => {
+    sort(index);
+  });
+});
