036d1a6515a46c91f92e578fb2b94d6c17b9f68b
Polina <polinabondar4@gmail.com>
2024-08-09 23:32:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const tableHeaders = document.querySelectorAll('th');
+const tbody = document.querySelector('tbody');
+const rowsForSort = tbody.querySelectorAll('tr');
+
+tableHeaders.forEach((header) => {
+  const onHeaderClick = (e) => {
+    const headerEl = e.target;
+    const index = [...headerEl.parentNode.children].indexOf(headerEl);
+
+    const sortedRows = [...rowsForSort].sort((a, b) => {
+      const rowA = a.children[index].innerHTML;
+      const rowB = b.children[index].innerHTML;
+
+      if (index === 0 || index === 1) {
+        return rowA.localeCompare(rowB);
+      } else if (index === 2) {
+        return +rowA - +rowB;
+      } else {
+        const formatedRowA = +rowA.split(',').join('').slice(1);
+        const formatedRowB = +rowB.split(',').join('').slice(1);
+
+        return formatedRowA - formatedRowB;
+      }
+    });
+
+    tbody.innerHTML = '';
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  };
+
+  header.addEventListener('click', onHeaderClick);
+});
