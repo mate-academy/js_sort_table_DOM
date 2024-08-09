0c9802b379856bf5a35ea843e193812353716b0b
Maria <maria.pozniak2013@gmail.com>
2024-08-09 15:02:16 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write code here
+let tBody = document.querySelector('tbody')
+let rows = [...tBody.querySelectorAll('tr')]
+
+function sortTable(cellIndex, type = 'text'){
+    rows.sort((a, b) => {
+        let characteristicA = a.querySelectorAll('td')[cellIndex].textContent
+        let characteristicB = b.querySelectorAll('td')[cellIndex].textContent
+        
+        switch(type) {
+            case 'text':
+                return characteristicA.localeCompare(characteristicB)
+            case 'number':
+                return characteristicA - characteristicB
+            case 'money':
+                characteristicA = parseFloat(characteristicA.replace(/[^0-9.]/g, ''));
+                characteristicB = parseFloat(characteristicB.replace(/[^0-9.]/g, ''));
+                return characteristicA - characteristicB
+            default:
+                return
+        }
+    })
+
+    rows.forEach(row => {
+        tBody.appendChild(row)
+    });
+}
+
+document.querySelector('#nameSort').addEventListener('click', (e) => {
+    sortTable(0, 'text')
+})
+
+document.querySelector('#positionSort').addEventListener('click', (e) => {
+    sortTable(1, 'text')
+})
+
+document.querySelector('#ageSort').addEventListener('click', (e) => {
+    sortTable(2, 'number')
+})
+
+document.querySelector('#salarySort').addEventListener('click', (e) => {
+    sortTable(3, 'money')
+})
