9e73196b7808aba9b24661471c8e2eda216efe2d
Victor Komara <vitioklss@gmail.com>
2024-08-14 14:04:49 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
-// write code here
+const tableBodie = document.querySelector('table').tBodies[0];
+const colectionRow = tableBodie.children;
+const arrRow = Array.from(colectionRow);
+
+document.addEventListener('click', function (e) {
+  const id = e.target.closest('thead');
+
+  if (!id) {
+    return;
+  }
+
+  if (e.target.innerText === 'Name') {
+    const sorted = arrRow.sort((row1, row2) =>
+      row1.cells[0].innerText.localeCompare(row2.cells[0].innerText),
+    );
+
+    sorted.forEach((row) => tableBodie.appendChild(row));
+  }
+
+  if (e.target.innerText === 'Position') {
+    const sorted = arrRow.sort((row1, row2) =>
+      row1.cells[1].innerText.localeCompare(row2.cells[1].innerText),
+    );
+
+    sorted.forEach((row) => tableBodie.appendChild(row));
+  }
+
+  if (e.target.innerText === 'Age') {
+    const sorted = arrRow.sort(
+      (row1, row2) => +row1.cells[2].innerText - +row2.cells[2].innerText,
+    );
+
+    sorted.forEach((row) => tableBodie.appendChild(row));
+  }
+
+  if (e.target.innerText === 'Salary') {
+    const sorted = arrRow.sort(
+      (row1, row2) =>
+        +row1.cells[3].innerText.slice(1).split(',').join('') -
+        +row2.cells[3].innerText.slice(1).split(',').join(''),
+    );
+
+    sorted.forEach((row) => tableBodie.appendChild(row));
+  }
+});
