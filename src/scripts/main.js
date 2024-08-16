370f21c27e5d81f99789bf6ebde30cb35b686338
Tatiana <tanya_potapenko@bigmir.net>
2024-08-16 10:30:03 +0200

    js_sort-table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tbody = table.querySelector('tbody');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+    const sortedRows = rows.sort((a, b) => {
+      const aText = a.querySelectorAll('td')[index].textContent.trim();
+      const bText = b.querySelectorAll('td')[index].textContent.trim();
+      const aValue = aText.replace(/[$,]/g, '');
+      const bValue = bText.replace(/[$,]/g, '');
+
+      if (!isNaN(parseFloat(aValue)) && !isNaN(parseFloat(bValue))) {
+        return parseFloat(aValue) - parseFloat(bValue);
+        // parseFloat()- convert text values into num;
+      } else {
+        return aValue.localeCompare(bValue);
+      }
+    });
+
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  });
+});
