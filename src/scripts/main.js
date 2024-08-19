e76137210b90d0191292883d7ff28726920eff96
Daryna Kukharets <darinakuharec61@gmail.com>
2024-08-19 21:33:29 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+const rows = [...document.querySelectorAll('tbody tr')];
+const body = document.querySelector('tbody');
+
+function sort(index) {
+  rows.sort((a, b) => {
+    const cellA = a.cells[index].textContent;
+    const cellB = b.cells[index].textContent;
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+
+  body.innerHTML = '';
+
+  rows.forEach((row) => body.append(row));
+}
+
+headers.forEach((head, index) => {
+  head.addEventListener('click', () => {
+    sort(index);
+  });
+});
