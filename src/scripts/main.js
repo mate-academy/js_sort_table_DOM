d336ebd408c22bd4b24980c184e6879d463cbbde
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-20 22:18:57 +0200

    added proper salary column sorting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,21 @@
 document.querySelector('thead').addEventListener('click', (e) => {
   if (e.target.tagName === 'TH') {
     const index = e.target.cellIndex;
-    const rows = Array.from(document.querySelectorAll('tbody tr'));
-    const sortedRows = rows.sort((a, b) => {
-      const aVal = a.children[index].textContent;
-      const bVal = b.children[index].textContent;
+    const rows = [...document.querySelectorAll('tbody tr')];
+    const salaryColumb = index === 3;
 
-      return aVal.localeCompare(bVal);
+    const sortedRows = rows.sort((a, b) => {
+      let aVal = a.children[index].textContent;
+      let bVal = b.children[index].textContent;
+
+      if (salaryColumb) {
+        aVal = Number(aVal.replace(/[^0-9]/g, ''));
+        bVal = Number(bVal.replace(/[^0-9]/g, ''));
+
+        return aVal - bVal;
+      } else {
+        return aVal.localeCompare(bVal);
+      }
     });
     const tbody = document.querySelector('tbody');
 
