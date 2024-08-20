019317ae5ea4e2e0b35e582286aaf218deb1d79c
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-20 19:26:31 +0300

    code with problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tbody = table.querySelector('tbody');
+
+function sortTable(index) {
+  const rowsArray = Array.from(tbody.rows);
+
+  const sortedRows = rowsArray.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent;
+    const cellB = rowB.cells[index].textContent;
+
+    if (!isNaN(cellA) && !isNaN(cellB)) {
+      return cellA - cellB;
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  tbody.innerHTML = '';
+  tbody.append(...sortedRows);
+}
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', sortTable(index));
+});
