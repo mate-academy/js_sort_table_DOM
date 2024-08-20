653f6628697bb91a0ae9c4b539acb744a5edd3ec
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-20 18:34:09 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+document.querySelector('thead').addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const index = e.target.cellIndex;
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    const sortedRows = rows.sort((a, b) => {
+      const aVal = a.children[index].textContent;
+      const bVal = b.children[index].textContent;
+
+      return aVal.localeCompare(bVal);
+    });
+    const tbody = document.querySelector('tbody');
+
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  }
+});
