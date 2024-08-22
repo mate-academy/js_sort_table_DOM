14e1042645d5be807e4e87cdf64b9f74a57a6b5a
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-08-22 18:05:06 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tbody = table.querySelector('tbody');
+const rows = [...tbody.querySelectorAll('tr')];
+
+headers.forEach((header, index) => {
+  const isIntegerColumn = index === 2 || index === 3;
+
+  header.addEventListener('click', () => {
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[index].textContent.trim();
+      const cellB = rowB.cells[index].textContent.trim();
+
+      if (isIntegerColumn) {
+        const valueA = parseFloat(cellA.replace('$', ''));
+        const valueB = parseFloat(cellB.replace('$', ''));
+
+        return valueA - valueB;
+      } else {
+        return cellA.localeCompare(cellB);
+      }
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
