1cf9c873135cd91eb95c4e7029405354636a6c30
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-08-22 23:47:31 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,69 @@
 'use strict';
 
-// write code here
+const tr = document.querySelector('tr');
+
+tr.addEventListener('click', (e) => {
+  const link = e.target.closest('th');
+
+  let index;
+
+  tr.querySelectorAll('th').forEach((item, i) => {
+    if (item === link) {
+      index = i;
+    }
+  });
+
+  if (!link) {
+    return;
+  }
+
+  sortTable(index);
+
+  if (link.classList.contains('asc')) {
+    link.classList.remove('asc');
+    link.classList.add('desc');
+  } else if (link.classList.contains('desc')) {
+    link.classList.remove('desc');
+    link.classList.add('asc');
+  } else {
+    link.classList.add('asc');
+  }
+});
+
+function sortTable(sortIndex) {
+  const tbody = document.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  const sortOrder = tr
+    .querySelectorAll('th')
+    [sortIndex].classList.contains('asc');
+
+  tr.querySelectorAll('th').forEach((item, index) => {
+    if (index !== sortIndex) {
+      item.classList.remove('asc', 'desc');
+    }
+  });
+
+  rows.sort((a, b) => {
+    const aText = a.querySelectorAll('td')[sortIndex].innerText;
+    const bText = b.querySelectorAll('td')[sortIndex].innerText;
+
+    const aNumber = parseFloat(aText.replace(/[$,]/g, ''));
+    const bNumber = parseFloat(bText.replace(/[$,]/g, ''));
+
+    if (
+      aText.toString() === aText &&
+      bText.toString() === bText &&
+      isNaN(aNumber) &&
+      isNaN(bNumber)
+    ) {
+      return sortOrder
+        ? bText.localeCompare(aText)
+        : aText.localeCompare(bText);
+    } else {
+      return sortOrder ? bNumber - aNumber : aNumber - bNumber;
+    }
+  });
+
+  rows.forEach((row) => tbody.appendChild(row));
+}
