9a0d5ab2841bc54de0a0d3376b808e799cae2433
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-23 16:07:10 +0300

    add a new condition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,10 @@ const cellAge = rows[0].cells[2];
 document.addEventListener('click', (e) => {
   const switchColumn = e.target.cellIndex;
 
+  if (e.target.tagName === 'TD') {
+    return;
+  }
+
   const sortedRows = [...rows].slice(1, -1).sort((row1, row2) => {
     const item1 = row1.cells[switchColumn].innerText;
     const item2 = row2.cells[switchColumn].innerText;
