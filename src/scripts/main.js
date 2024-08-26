5e0da7698795cf0e69ad45c57078dc562dfad5de
Yurii Kramar <y.kramar15@gmail.com>
2024-08-26 21:21:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const listSalary = document.querySelector('tbody').children;
+const SALARY = 'Salary';
+
+thead.addEventListener('click', (e) => {
+  if (e.target.textContent === SALARY) {
+    const sorted = sortListForSaalry(listSalary);
+
+    tbody.innerHTML = '';
+
+    sorted.forEach((elm) => {
+      tbody.appendChild(elm);
+    });
+  }
+});
+
+function sortListForSaalry(colect) {
+  const out = Array.from(colect).sort((a, b) => {
+    return (
+      +cleanCurrency(b.children[b.children.length - 1].textContent) -
+      +cleanCurrency(a.children[a.children.length - 1].textContent)
+    );
+  });
+
+  return out;
+}
+
+function cleanCurrency(input) {
+  return input.replace(/[$,]/g, '');
+}
