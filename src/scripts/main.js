f34abb6bd06743eb05dad6a35b78a7f785a16493
Yurii Kramar <y.kramar15@gmail.com>
2024-08-27 19:47:53 +0200

    add other sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,56 @@
 const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const listSalary = document.querySelector('tbody').children;
-const SALARY = 'Salary';
+
+const TRIGERNAME_SALARY = { name: 'Salary', index: 3 };
+const TRIGERNAME_AGE = { name: 'Age', index: 2 };
+const TRIGERNAME_POSITION = { name: 'Position', index: 1 };
+const TRIGERNAME_NAME = { name: 'Name', index: 0 };
 
 thead.addEventListener('click', (e) => {
-  if (e.target.textContent === SALARY) {
-    const sorted = sortListForSaalry(listSalary);
+  const targetName = e.target.textContent;
 
-    tbody.innerHTML = '';
+  switch (targetName) {
+    case TRIGERNAME_SALARY.name:
+      refreshList(TRIGERNAME_SALARY.index);
 
-    sorted.forEach((elm) => {
-      tbody.appendChild(elm);
-    });
+      break;
+    case TRIGERNAME_AGE.name:
+      refreshList(TRIGERNAME_AGE.index);
+
+      break;
+    case TRIGERNAME_POSITION.name:
+      refreshList(TRIGERNAME_POSITION.index);
+
+      break;
+    case TRIGERNAME_NAME.name:
+      refreshList(TRIGERNAME_NAME.index);
+
+      break;
   }
 });
 
-function sortListForSaalry(colect) {
+function refreshList(indx) {
+  const sortedAgeList = sortList(listSalary, indx);
+
+  tbody.innerHTML = '';
+
+  sortedAgeList.forEach((elm) => {
+    tbody.appendChild(elm);
+  });
+}
+
+function sortList(colect, indx) {
   const out = Array.from(colect).sort((a, b) => {
-    return (
-      +cleanCurrency(b.children[b.children.length - 1].textContent) -
-      +cleanCurrency(a.children[a.children.length - 1].textContent)
+    if (!isNaN(cleanCurrency(a.children[indx].textContent))) {
+      return (
+        cleanCurrency(b.children[indx].textContent) -
+        cleanCurrency(a.children[indx].textContent)
+      );
+    }
+
+    return a.children[indx].textContent.localeCompare(
+      b.children[indx].textContent,
     );
   });
 
@@ -29,5 +60,5 @@ function sortListForSaalry(colect) {
 }
 
 function cleanCurrency(input) {
-  return input.replace(/[$,]/g, '');
+  return +input.replace(/[$,]/g, '');
 }
