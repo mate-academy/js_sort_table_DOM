dd2cdbe8de123f43a7e2cb7953fd2ce9ae77b2d8
Yurii Kramar <y.kramar15@gmail.com>
2024-08-27 20:13:35 +0200

    refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,36 +4,21 @@ const thead = document.querySelector('thead');
 const tbody = document.querySelector('tbody');
 const listSalary = document.querySelector('tbody').children;
 
-const TRIGERNAME_SALARY = { name: 'Salary', index: 3 };
-const TRIGERNAME_AGE = { name: 'Age', index: 2 };
-const TRIGERNAME_POSITION = { name: 'Position', index: 1 };
-const TRIGERNAME_NAME = { name: 'Name', index: 0 };
+const TITLES = {
+  Salary: 3,
+  Age: 2,
+  Position: 1,
+  Name: 0,
+};
 
 thead.addEventListener('click', (e) => {
   const targetName = e.target.textContent;
 
-  switch (targetName) {
-    case TRIGERNAME_SALARY.name:
-      refreshList(TRIGERNAME_SALARY.index);
-
-      break;
-    case TRIGERNAME_AGE.name:
-      refreshList(TRIGERNAME_AGE.index);
-
-      break;
-    case TRIGERNAME_POSITION.name:
-      refreshList(TRIGERNAME_POSITION.index);
-
-      break;
-    case TRIGERNAME_NAME.name:
-      refreshList(TRIGERNAME_NAME.index);
-
-      break;
-  }
+  refreshList(targetName);
 });
 
-function refreshList(indx) {
-  const sortedAgeList = sortList(listSalary, indx);
+function refreshList(triger) {
+  const sortedAgeList = sortList(listSalary, TITLES[triger]);
 
   tbody.innerHTML = '';
 
