8d03952ea2991c08477131962cbc9a5fcddd9be4
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-05 13:26:33 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const salaryCell = table.tHead.rows[0].cells[3];
+const tbody = table.tBodies[0].rows;
+const tbodyArr = [...tbody];
+
+for (let i = 0; i < tbodyArr.length; i++) {
+  tbodyArr[i].cells[3].innerHTML = Number(
+    tbodyArr[i].cells[3].innerHTML.replace(/[$,]/g, ''),
+  );
+}
+
+salaryCell.addEventListener('click', () => {
+  tbodyArr.sort(
+    (personA, personB) =>
+      personA.cells[3].innerHTML - personB.cells[3].innerHTML,
+  );
+
+  for (let i = 0; i < tbodyArr.length; i++) {
+    tbody[i].outerHTML = tbodyArr[i].outerHTML;
+  }
+});
