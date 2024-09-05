261f06d97b6cffd38ca6e4818adf326351793ea7
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-05 13:54:06 +0300

    refinement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,44 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
 const table = document.querySelector('table');
-const salaryCell = table.tHead.rows[0].cells[3];
+const thead = table.tHead.rows[0];
+
+const nameCell = thead.cells[0];
+const positionCell = thead.cells[1];
+const ageCell = thead.cells[2];
+const salaryCell = thead.cells[3];
+
 const tbody = table.tBodies[0].rows;
 const tbodyArr = [...tbody];
 
-for (let i = 0; i < tbodyArr.length; i++) {
-  tbodyArr[i].cells[3].innerHTML = Number(
-    tbodyArr[i].cells[3].innerHTML.replace(/[$,]/g, ''),
-  );
-}
+thead.addEventListener('click', (_event) => {
+  if (_event.target === nameCell) {
+    tbodyArr.sort((personA, personB) =>
+      personA.cells[0].innerHTML.localeCompare(personB.cells[0].innerHTML),
+    );
+  }
 
-salaryCell.addEventListener('click', () => {
-  tbodyArr.sort(
-    (personA, personB) =>
-      personA.cells[3].innerHTML - personB.cells[3].innerHTML,
-  );
+  if (_event.target === positionCell) {
+    tbodyArr.sort((personA, personB) =>
+      personA.cells[1].innerHTML.localeCompare(personB.cells[1].innerHTML),
+    );
+  }
+
+  if (_event.target === ageCell) {
+    tbodyArr.sort(
+      (personA, personB) =>
+        personA.cells[2].innerHTML - personB.cells[2].innerHTML,
+    );
+  }
+
+  if (_event.target === salaryCell) {
+    tbodyArr.sort(
+      (personA, personB) =>
+        Number(personA.cells[3].innerHTML.replace(/[$,]/g, '')) -
+        Number(personB.cells[3].innerHTML.replace(/[$,]/g, '')),
+    );
+  }
 
   for (let i = 0; i < tbodyArr.length; i++) {
     tbody[i].outerHTML = tbodyArr[i].outerHTML;
