345308853e4c6b3629fb11f17b62aa75a157953d
Yehor Gordiienko <egordienko319@gmail.com>
2024-09-06 18:08:56 +0300

    test commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+
+thead.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'th') {
+    const index = target.cellIndex;
+
+    sortTable(index);
+  }
+});
+
+function sortTable(indexColumn) {
+  const column = indexColumn; // [3]
+  const tbody = document.querySelector('tbody');
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  rows.sort((a, b) => {
+    if (column === 3) {
+      const aValue = a.cells[column].innerText.replace(/[$,]/g, '');
+      const bValue = b.cells[column].innerText.replace(/[$,]/g, '');
+
+      return aValue - bValue;
+    }
+
+    if (column === 2) {
+      return a - b;
+    }
+
+    a.cells[column].toLowerCase();
+    b.cells[column].toLowerCase();
+
+    return a.localeCompare(b);
+  });
+
+  rows.forEach((row) => tbody.appendChild(row));
+}
