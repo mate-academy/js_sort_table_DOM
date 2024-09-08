33fd03a298b78dacb16ea0b5c4c7f5c12c06b753
MTS13 <mtstaras@gmail.com>
2024-09-08 18:35:10 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tBodyRows = [...tBody.rows];
+
+const asc = (e) => {
+  const index = e.target.cellIndex;
+
+  const sort = tBodyRows.sort((a, b) => {
+    let cellA = a.cells[index].innerHTML;
+    let cellB = b.cells[index].innerHTML;
+
+    if (cellA[0] === '$') {
+      cellA = +cellA.slice(1).split(',').join('');
+      cellB = +cellB.slice(1).split(',').join('');
+
+      return cellA - cellB;
+    }
+
+    if (+cellA) {
+      return cellA - cellB;
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  table.tBodies[0].append(...sort);
+};
+
+tHead.addEventListener('click', asc);
