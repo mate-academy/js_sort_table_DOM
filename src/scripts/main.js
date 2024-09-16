d48a294be4a602ae5cb440de52dd9e42c1a342ae
olshum <olshum8@gmail.com>
2024-09-16 21:37:37 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
+
+function sortTable(index) {
+  const table = document.querySelector('table');
+  const rows = Array.from(table.rows).slice(1);
+  let isNumeric = !isNaN(rows[0].cells[index].textContent);
+  const isSalary = index === 3;
+
+  rows.sort((rowA, rowB) => {
+    let cellA = rowA.cells[index].textContent;
+    let cellB = rowB.cells[index].textContent;
+
+    if (isSalary) {
+      cellA = parseFloat(cellA.replace(/[$,]/g, ''));
+      cellB = parseFloat(cellB.replace(/[$,]/g, ''));
+      isNumeric = true;
+    }
+
+    if (isNumeric) {
+      return cellA - cellB;
+    } else {
+      return cellA.localeCompare(cellB);
+    }
+  });
+
+  rows.forEach((row) => table.appendChild(row));
+}
