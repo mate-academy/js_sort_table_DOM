279f3983dc15a6fbd968d9a41b5a7101921d213d
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-09-17 19:48:27 +0200

    sort_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const tableBody = table.querySelector('tbody');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortColumn(index);
+  });
+});
+
+const sortColumn = (index) => {
+  const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.querySelectorAll('td')[index].innerHTML;
+    const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+
+    const numA = parseInt(cellA.replace(/\D/g,''));
+    const numB = parseInt(cellB.replace(/\D/g,''));
+
+    if (!isNaN(numA) && !isNaN(numB)) {
+      return numA - numB;
+    }
+
+    if (cellA > cellB) return 1;
+    if (cellA < cellB) return -1;
+    return 0;
+  });
+
+  rows.forEach((newRow) => {
+    tableBody.appendChild(newRow);
+  });
+};
