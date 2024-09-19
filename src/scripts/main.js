df372e6b84fab1e82bbf98c54ed362f09c5a7c3a
Ivan <fanmetalheavy@gmail.com>
2024-09-19 16:27:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
 // write code here
+function getSortTable(sortBy, table) {
+  const copyOfTable = [...table];
+
+  switch (sortBy) {
+    case 'Name':
+      copyOfTable.sort((a, b) => {
+        return a.cells[0].textContent.localeCompare(b.cells[0].textContent);
+      });
+
+      break;
+
+    case 'Position':
+      copyOfTable.sort((a, b) => {
+        return a.cells[1].textContent.localeCompare(b.cells[1].textContent);
+      });
+
+      break;
+
+    case 'Age':
+      copyOfTable.sort((a, b) => {
+        return a.cells[2].textContent.localeCompare(b.cells[2].textContent);
+      });
+
+      break;
+
+    case 'Salary':
+      copyOfTable.sort((a, b) => {
+        const numberA = a.cells[3].textContent.slice(1).replace(',', '');
+        const numberB = b.cells[3].textContent.slice(1).replace(',', '');
+
+        return numberA - numberB;
+      });
+
+      break;
+  }
+
+  return copyOfTable;
+}
+
+const tableHead = [...document.querySelector('thead').querySelectorAll('th')];
+const tableBodyRows = [...document.querySelector('tbody').rows];
+
+addEventListener('click', (e) => {
+  if (tableHead.includes(e.target)) {
+    const sortedRows = getSortTable(e.target.textContent, tableBodyRows);
+    const tableBody = document.querySelector('tbody');
+    const newTableBody = document.createElement('tbody');
+
+    sortedRows.forEach((row) => newTableBody.append(row));
+    tableBody.replaceWith(newTableBody);
+  }
+});
