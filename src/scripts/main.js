f38eed8129a6da88ecd6ecc795fd98cbf796d522
Serhii-Khobotov <hobbitura@gmail.com>
2024-09-19 14:07:58 +0300

    My implemention the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+
+headers.forEach((header, index) => {
+    header.addEventListener('click', () => {
+        setSortTable(index);
+    });
+});
+
+const setSortTable = (columnIndex) => {
+    const tBody = table.querySelector('tbody');
+    const rows = tBody.querySelectorAll('tr');
+
+    const sortedRows = rows.sort((a, b) => {
+        const aValue = a.cells[columnIndex].textContent.trim();
+        const bValue = b.cells[columnIndex].textContent.trim();
+
+        if (+aValue) {
+            return +aValue - bValue;
+        }
+
+        if (columnIndex === 3) {
+            const aSalary = parseFloat(aValue.replace(/[$,]/g, ''));
+            const bSalary = parseFloat(bValue.replace(/[$,]/g, ''));
+      
+            return aSalary - bSalary;
+          }
+
+        return aValue.localeCompare(bValue);
+    })
+
+    tBody.innerHTML = '';
+    tBody.append(...sortedRows);
+};
