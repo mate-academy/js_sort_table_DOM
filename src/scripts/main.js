a0aa8a96a01107115a1fa582403cdea588d1f24b
Yevhen Rudnytskyi <yevhen.dodev@gmail.com>
2024-09-24 08:59:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('#employeeTable th').forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
+
+function sortTable(columnIndex) {
+  const table = document.getElementById('employeeTable').querySelector('tbody');
+  const rowsArray = Array.from(table.rows);
+
+  rowsArray.sort((rowA, rowB) => {
+    const cellA = rowA.cells[columnIndex].innerText;
+    const cellB = rowB.cells[columnIndex].innerText;
+
+    if (columnIndex === 3) {
+      return (
+        parseFloat(cellA.replace(/[$,]/g, '')) -
+        parseFloat(cellB.replace(/[$,]/g, ''))
+      );
+    }
+
+    if (columnIndex === 2) {
+      return parseInt(cellA) - parseInt(cellB);
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  rowsArray.forEach((row) => table.appendChild(row));
+}
