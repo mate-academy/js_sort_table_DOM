1a7fc4ed7c28e5423d070baed6f6add5aeb3cd67
Heorhii Lytvynenko <104064704+Grifion@users.noreply.github.com>
2024-09-25 04:04:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const tbody = [...document.getElementsByTagName('tr')];
+const thead = tbody.shift();
+const tfood = tbody.pop();
+
+thead.addEventListener('click', (e) => {
+  const sorted = sort([...tbody], e.target.closest('th'));
+
+  tbody.forEach((column, index) => {
+    column.replaceWith(sorted[index].cloneNode(true));
+  });
+});
+
+tfood.addEventListener('click', (e) => {
+  const sorted = sort([...tbody], e.target.closest('th'));
+
+  tbody.forEach((column, index) => {
+    column.replaceWith(sorted[index].cloneNode(true));
+  });
+});
+
+function sort(table, selector) {
+  const i = [...thead.children] // get array from all th elements
+    .map((v) => v.textContent) // get text from th element
+    .findIndex((v) => v === selector.textContent); // find index of th element
+
+  return table.sort((a, b) => {
+    // geting text content from a and b elements
+    let cola = [...a.children].map((v) => v.textContent)[i];
+    let colb = [...b.children].map((v) => v.textContent)[i];
+
+    if (cola[0] === '$') {
+      // parsing elements if it's sallary
+      cola = parseInt(cola.replace(/[$,]/g, ''));
+      colb = parseInt(colb.replace(/[$,]/g, ''));
+    }
+
+    return cola > colb;
+  });
+}
