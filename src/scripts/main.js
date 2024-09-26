2b64748349810175b7f7301cf5005c4b352b749a
Korshun <dmitriy.kor91@gmail.com>
2024-09-26 15:46:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write code here
+const headerTitle = document.querySelectorAll('th');
+const sortedRows = document.querySelectorAll('tbody tr');
+
+headerTitle.forEach((head) => {
+  head.addEventListener('click', (e) => {
+    const indexRow = e.target.cellIndex;
+
+    sortedSort(sortedRowsToArr, indexRow);
+  });
+});
+
+const sortedRowsToArr = Array.from(sortedRows);
+
+function sortedSort(titles, indexRow) {
+  titles.sort((a, b) => {
+    const cellTextA = a.querySelectorAll('td')[indexRow].textContent;
+    const cellTextB = b.querySelectorAll('td')[indexRow].textContent;
+
+    if (indexRow === 0 || indexRow === 1) {
+      return cellTextA.localeCompare(cellTextB);
+    } else {
+      const numA = parseFloat(cellTextA.replace(/[^0-9.-]+/g, ''));
+      const numB = parseFloat(cellTextB.replace(/[^0-9]+/g, ''));
+
+      return numA - numB;
+    }
+  });
+
+  const bodyT = document.querySelector('tbody');
+
+  bodyT.innerHTML = '';
+
+  titles.forEach((row) => {
+    bodyT.appendChild(row);
+  });
+}
