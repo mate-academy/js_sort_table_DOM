601f40094949f7315476fea469a696c8ed58d168
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-10-02 17:19:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const columnIndex = Array.from(headers).indexOf(header);
+    const rows = Array.from(document.querySelectorAll('tr')).slice(1, -1);
+
+    const values = rows.map((row) => {
+      const cell = row.cells[columnIndex].textContent;
+
+      return { row, cell };
+    });
+
+    const sortedValues = values.sort((a, b) => {
+      // Обробка значень, що починаються зі знака долара
+      if (a.cell[0] === '$' && b.cell[0] === '$') {
+        const first = parseFloat(a.cell.replace(/[$,]/g, ''));
+        const second = parseFloat(b.cell.replace(/[$,]/g, ''));
+
+        return first - second;
+      } else if (!isNaN(a.cell) && !isNaN(b.cell)) {
+        return parseFloat(a.cell) - parseFloat(b.cell);
+      } else {
+        return a.cell.localeCompare(b.cell);
+      }
+    });
+
+    const tableBody = document.querySelector('tbody');
+
+    tableBody.innerHTML = '';
+
+    sortedValues.forEach(({ row }) => {
+      tableBody.appendChild(row); // Додаємо рядки назад до тіла таблиці
+    });
+  });
+});
