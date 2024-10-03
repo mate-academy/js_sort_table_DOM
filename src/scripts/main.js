efc8656b58253402f7e386236cbb2ff73673a638
Anastasiia-Tatarchenko <neveykina.nastya@gmail.com>
2024-10-03 13:34:33 +0200

    task_done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+const tableBody = document.querySelector('table tbody');
+const rows = [...tableBody.querySelectorAll('tr')];
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
+
+function sortTable(columIndex) {
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[columIndex].textContent;
+    const cellB = rowB.cells[columIndex].textContent;
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+
+  tableBody.innerHTML = '';
+  rows.forEach((row) => tableBody.appendChild(row));
+}
