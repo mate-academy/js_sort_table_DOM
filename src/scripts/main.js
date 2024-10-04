d7480460af0b77826b0bafa4c8dcdd03520cc287
missk <rostock.vasiljev@gmail.com>
2024-10-04 20:56:47 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+
+headers.forEach((header, i) => {
+  header.addEventListener('click', () => {
+    const rows = Array.from(table.querySelectorAll('tbody tr'));
+    const isNumericColumn = i === 2 || i === 3;
+
+    rows.sort((rowA, rowB) => {
+      let cellA = rowA.children[i].textContent.trim();
+      let cellB = rowB.children[i].textContent.trim();
+
+      if (isNumericColumn) {
+        if (i === 3) {
+          cellA = cellA.replace(/[$,]/g, '');
+          cellB = cellB.replace(/[$,]/g, '');
+        }
+
+        return +cellA - +cellB;
+      } else {
+        return cellA.localeCompare(cellB);
+      }
+    });
+    rows.forEach((row) => table.querySelector('tbody').appendChild(row));
+  });
+});
