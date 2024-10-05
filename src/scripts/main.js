9ebdb1ef6c10a21e58c78108c18a24d46d673563
Zyrak Nur <noorzirak7@gmail.com>
2024-10-05 18:18:42 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+function convertToNumber(string) {
+  let result = string.replace('$', '');
+  result = result.replace(',', '');
+
+  return +result;
+}
+
+const tableBody = document.querySelector('tbody');
+const rows = [...tableBody.rows];
+
+document.querySelectorAll('th').forEach((rowHead, index) => {
+  const sortBy = rowHead.textContent.toLowerCase();
+
+  rowHead.addEventListener('click', () => {
+    rows.sort((row1, row2) => {
+      const value1 = row1.children[index].textContent;
+      const value2 = row2.children[index].textContent;
+
+      if (value1.startsWith('$')) {
+        return convertToNumber(value1) - convertToNumber(value2);
+      }
+
+      return value1.localeCompare(value2);
+    });
+
+    tableBody.innerHTML = '';
+    rows.forEach(row => tableBody.append(row));
+  });
+});
