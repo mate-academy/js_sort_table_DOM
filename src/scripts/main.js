6ef66839d57d2cc3da96e9752ee1c772b93ccb00
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-10-05 09:51:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+
+document.querySelectorAll('th').forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const rows = [...tbody.rows];
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[index].innerText;
+      const cellB = rowB.cells[index].innerText;
+
+      return isNaN(cellA) || isNaN(cellB)
+        ? cellA.localeCompare(cellB)
+        : cellA - cellB;
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
