7ff1becd996a83b5d08a9f7d198f645cbf6c4b12
Oleksii Karpov <alneon1@gmail.com>
2024-10-06 20:43:06 +0300

    implemented solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,61 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = document.querySelectorAll('th');
+let sortDirection = 1;
+let lastSortedColumn = -1;
+
+table.querySelector('thead').addEventListener('click', (e) => {
+  const header = e.target.closest('th');
+
+  if (!header) {
+    return;
+  }
+
+  const index = [...headers].indexOf(header);
+
+  updateSortDirection(index);
+
+  sortTable(index);
+});
+
+function updateSortDirection(columnIndex) {
+  if (columnIndex === lastSortedColumn) {
+    sortDirection = -sortDirection;
+  } else {
+    sortDirection = 1;
+  }
+
+  lastSortedColumn = columnIndex;
+}
+
+function getCellValue(row, columnIndex) {
+  return currencyToNumber(row.cells[columnIndex].innerText);
+}
+
+function currencyToNumber(value) {
+  if (value[0] === '$') {
+    return +value.slice(1).split(',').join('');
+  }
+
+  return isNaN(value) ? value : +value;
+}
+
+function sortTable(columnIndex) {
+  const tbody = table.querySelector('tbody');
+  const rows = [...tbody.querySelectorAll('tr')];
+
+  rows.sort((row1, row2) => {
+    const value1 = getCellValue(row1, columnIndex);
+    const value2 = getCellValue(row2, columnIndex);
+
+    if (typeof value1 === 'number') {
+      return (value1 - value2) * sortDirection;
+    }
+
+    return value1.toString().localeCompare(value2.toString()) * sortDirection;
+  });
+
+  tbody.innerHTML = '';
+  rows.forEach((row) => tbody.appendChild(row));
+}
