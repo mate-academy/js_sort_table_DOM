890d5d872fe14f94a924cc0431e42fd159b585b1
Blervin <blervin121212@gmail.com>
2025-01-06 15:54:50 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const headers = table.querySelectorAll('th');
+  const tbody = table.querySelector('tbody');
 
-// write code here
+  headers.forEach((header, index) => {
+    header.addEventListener('click', () => {
+      const rows = Array.from(tbody.querySelectorAll('tr'));
+
+      rows.sort((rowA, rowB) => {
+        const cellA = cleanValue(rowA.children[index].textContent);
+        const cellB = cleanValue(rowB.children[index].textContent);
+
+        return cellA - cellB || String(cellA).localeCompare(String(cellB));
+      });
+
+      rows.forEach((row) => tbody.appendChild(row));
+    });
+  });
+
+  function cleanValue(value) {
+    return parseFloat(value.replace(/[^0-9.]/g, '')) || value.trim();
+  }
+});
