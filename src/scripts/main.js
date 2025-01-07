4d90996f6b5394f69644ef8d3d1e153b15c0fd67
Oleh Chernov <nineuito@mail.com>
2025-01-07 16:53:54 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+
+const sortTable = (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'TH') {
+    const columnName = target.textContent;
+    let columnIndex = 0;
+
+    if (columnName === 'Name') {
+      columnIndex = 0;
+    }
+
+    if (columnName === 'Position') {
+      columnIndex = 1;
+    }
+
+    if (columnName === 'Age') {
+      columnIndex = 2;
+    }
+
+    if (columnName === 'Salary') {
+      columnIndex = 3;
+    }
+
+    const rows = [...tbody.querySelectorAll('tr')];
+
+    const sortedRows = rows.sort((a, b) => {
+      const aText = a.cells[columnIndex].textContent;
+      const bText = b.cells[columnIndex].textContent;
+
+      if (columnName === 'Salary') {
+        const aNum = Number(aText.replace(/[$,]/g, ''));
+        const bNum = Number(bText.replace(/[$,]/g, ''));
+
+        return aNum - bNum;
+      }
+
+      if (columnName === 'Age') {
+        return Number(aText) - Number(bText);
+      }
+
+      return aText.localeCompare(bText);
+    });
+
+    tbody.append(...sortedRows);
+  }
+};
+
+table.addEventListener('click', sortTable);
