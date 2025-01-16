725638a7e7bb6dded1a3e3f5b19e2c671e2057e3
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2025-01-16 20:33:10 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+const rows = Array.from(table.querySelector('tbody').rows);
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const value = rows[0].cells[index].textContent
+      .trim()
+      .replace(/[^0-9.]/g, '');
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[index].textContent.trim();
+      const cellB = rowB.cells[index].textContent.trim();
+
+      if (value) {
+        return (
+          parseFloat(cellA.replace(/[^0-9.]/g, '')) -
+          parseFloat(cellB.replace(/[^0-9.]/g, ''))
+        );
+      } else {
+        return cellA.localeCompare(cellB);
+      }
+    });
+
+    const tbody = table.querySelector('tbody');
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
