2fc3aa817451f4e4675fc34173aa250bca7af2c6
Volodymyr Makhonin <zavozi@gmail.com>
2025-01-19 11:37:33 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('thead th');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.rows);
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[index].textContent.trim();
+      const cellB = rowB.cells[index].textContent.trim();
+
+      if (index === 2 || index === 3) {
+        const valueA = parseFloat(cellA.replace(/[\\$,]/g, ''));
+        const valueB = parseFloat(cellB.replace(/[\\$,]/g, ''));
+
+        return valueA - valueB;
+      } else {
+        return cellA.localeCompare(cellB);
+      }
+    });
+
+    tbody.innerHTML = '';
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
