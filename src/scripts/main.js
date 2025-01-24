9791912aa38372e202267a56c3bc1f7231c83b49
maksimka <maks.markevych@gmail.com>
2025-01-24 17:51:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const headers = [...document.querySelectorAll('table thead th')];
+const rows = document.querySelectorAll('table tbody tr');
+const arrOfRows = [...rows];
+
+headers.forEach((header) => {
+  header.addEventListener('click', (e) => {
+    const element = e.target;
+    const index = headers.indexOf(element);
+
+    sortRows(index);
+  });
+});
+
+function sortRows(index) {
+  const sortedRows = arrOfRows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent.trim().replace(/[$,]/g, '');
+    const cellB = rowB.cells[index].textContent.trim().replace(/[$,]/g, '');
+    const isNumber = !isNaN(cellA) && !isNaN(cellB);
+
+    if (isNumber) {
+      return parseFloat(cellA) - parseFloat(cellB);
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  const tbody = document.querySelector('table tbody');
+
+  tbody.innerHTML = '';
+  sortedRows.forEach((row) => tbody.appendChild(row));
+}
