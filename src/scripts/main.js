324fa2924056e530abb4ef51aea7229fc97030b6
Oleh Mysko <olegmysko1996@gmail.com>
2025-01-27 18:03:01 +0200

    task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,73 @@
 'use strict';
 
-// write code here
+const title = document.querySelectorAll('th');
+const tbody = document.querySelector('tbody');
+const initialRows = Array.from(tbody.querySelectorAll('tr'));
+
+function sortRowsByColumn(dataRows, columnIndex) {
+  return dataRows.sort((a, b) => {
+    const cellA = a.cells[columnIndex].textContent.trim();
+    const cellB = b.cells[columnIndex].textContent.trim();
+
+    if (cellA < cellB) {
+      return -1;
+    }
+
+    if (cellA > cellB) {
+      return 1;
+    }
+
+    return 0;
+  });
+}
+
+function sortRowsByNumber(dataRows, columnIndex) {
+  return dataRows.sort((a, b) => {
+    const cellA = parseFloat(
+      a.cells[columnIndex].textContent.replace('$', '').replace(/,/g, ''),
+    );
+    const cellB = parseFloat(
+      b.cells[columnIndex].textContent.replace('$', '').replace(/,/g, ''),
+    );
+
+    return cellA - cellB;
+  });
+}
+
+title.forEach((element, index) => {
+  element.addEventListener('click', (eve) => {
+    const link = eve.target.textContent;
+
+    if (link === 'Age') {
+      const sortedRows = sortRowsByNumber(initialRows, 2);
+
+      sortedRows.forEach((row) => {
+        tbody.appendChild(row);
+      });
+    }
+
+    if (link === 'Salary') {
+      const sortedRows = sortRowsByNumber(initialRows, 3);
+
+      sortedRows.forEach((row) => {
+        tbody.appendChild(row);
+      });
+    }
+
+    if (link === 'Name') {
+      const sortedRows = sortRowsByColumn(initialRows, 0);
+
+      sortedRows.forEach((row) => {
+        tbody.appendChild(row);
+      });
+    }
+
+    if (link === 'Position') {
+      const sortedRows = sortRowsByColumn(initialRows, 1);
+
+      sortedRows.forEach((row) => {
+        tbody.appendChild(row);
+      });
+    }
+  });
+});
