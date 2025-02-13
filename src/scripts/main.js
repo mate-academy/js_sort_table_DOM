840cf568dc60e5aa544cd1e7c4712fbe4f8a75f7
mmromanova <emy2004.bo@gmail.com>
2025-02-13 18:47:17 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const headers = table.querySelectorAll('th');
+  const tbody = table.querySelector('tbody');
 
-// write code here
+  headers.forEach((header, index) => {
+    header.addEventListener('click', () => {
+      const rows = Array.from(tbody.querySelectorAll('tr'));
+
+      const isNumeric = !isNaN(
+        rows[0].cells[index].textContent.replace(/[$,]/g, ''),
+      );
+
+      rows.sort((rowA, rowB) => {
+        let a = rowA.cells[index].textContent.trim();
+        let b = rowB.cells[index].textContent.trim();
+
+        if (isNumeric) {
+          a = parseFloat(a.replace(/[$,]/g, ''));
+          b = parseFloat(b.replace(/[$,]/g, ''));
+        }
+
+        return a > b ? 1 : -1;
+      });
+
+      tbody.innerHTML = '';
+      rows.forEach((row) => tbody.appendChild(row));
+    });
+  });
+});
