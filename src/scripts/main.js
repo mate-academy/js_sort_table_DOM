6707126344e2aedf09c0cda7024393c13cf37ad1
Artem <stilon670@gmail.com>
2025-02-18 14:58:22 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  const nameClick = e.target.innerText;
+  const fields = [...tbody.querySelectorAll('tr')];
+  const names = ['Name', 'Position', 'Age', 'Salary'];
+  const indexName = names.indexOf(nameClick);
+
+  fields.sort((elem1, elem2) => {
+    let td1 = elem1.querySelectorAll('td')[indexName].innerHTML;
+    let td2 = elem2.querySelectorAll('td')[indexName].innerHTML;
+
+    if (nameClick === 'Name' || nameClick === 'Position') {
+      return td1.localeCompare(td2);
+    }
+
+    if (nameClick === 'Age') {
+      return parseInt(td1) - parseInt(td2);
+    }
+
+    if (nameClick === 'Salary') {
+      td1 = parseInt(td1.replace('$', '').replace(',', ''));
+      td2 = parseInt(td2.replace('$', '').replace(',', ''));
+
+      return td1 - td2;
+    }
+  });
+
+  fields.forEach((field) => tbody.appendChild(field));
+});
