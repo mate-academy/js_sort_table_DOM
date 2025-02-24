82c4d333d06e6410341c64e8a298d3e78933b944
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-24 14:40:34 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableRows = [...document.querySelectorAll('tbody tr')];
+
+tableHead.addEventListener('click', (e) => {
+  if (e.target.cellIndex <= 1) {
+    tableRows.sort((a, b) => {
+      return a.children[e.target.cellIndex].textContent.localeCompare(
+        b.children[e.target.cellIndex].textContent,
+      );
+    });
+  } else {
+    tableRows.sort(
+      (a, b) =>
+        a.children[e.target.cellIndex].textContent.replace(/\D/g, '') -
+        b.children[e.target.cellIndex].textContent.replace(/\D/g, ''),
+    );
+  }
+
+  tableBody.append(...tableRows);
+});
