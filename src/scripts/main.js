73ae37109cfef09db462c600b03e9dcbfdc287b5
Alex Marcovych <alexmarcovych@gmail.com>
2025-03-06 22:02:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const columnIndex = e.target.cellIndex;
+    const table = e.target.closest('table');
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.rows);
+
+    rows.sort((rowA, rowB) => {
+      let cellA = rowA.cells[columnIndex].textContent.trim();
+      let cellB = rowB.cells[columnIndex].textContent.trim();
+
+      if (columnIndex === 3) {
+        cellA = parseFloat(cellA.replace(/[$,]/g, ''));
+        cellB = parseFloat(cellB.replace(/[$,]/g, ''));
+      }
+
+      if (isNaN(cellA) || isNaN(cellB)) {
+        return cellA.localeCompare(cellB);
+      } else {
+        return Number(cellA) - Number(cellB);
+      }
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  }
+});
