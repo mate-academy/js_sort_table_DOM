1a150f6c46ac25d59050886ba4c15ca0f6e8409e
Andriian <an.hrytsan@gmail.com>
2025-03-11 20:20:05 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableRowsArray = Array.from(document.querySelectorAll('tbody tr'));
+
+tableHead.addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const columnIndex = e.target.cellIndex;
+
+    sortTable(columnIndex);
+  }
+});
+
+function sortTable(columnIndex) {
+  tableRowsArray.sort((firstRow, secondRow) => {
+    const firstValue = getCellValue(firstRow, columnIndex);
+    const secondValue = getCellValue(secondRow, columnIndex);
+
+    return typeof firstValue === 'string'
+      ? firstValue.localeCompare(secondValue)
+      : firstValue - secondValue;
+  });
+
+  tableBody.append(...tableRowsArray);
+}
+
+function getCellValue(row, columnIndex) {
+  const cell = row.cells[columnIndex];
+  const content = cell.textContent;
+  const isAgeColumn = columnIndex === 2;
+  const isSalaryColumn = columnIndex === 3;
+
+  if (isAgeColumn) {
+    return Number(content);
+  } else if (isSalaryColumn) {
+    return parseFloat(content.replace(/[$,]/g, ''));
+  } else {
+    return content.toLowerCase();
+  }
+}
