948ec208262d8dfebc527978686e164dd4e9c649
Yaroslav <yarmoshyk96@gmail.com>
2025-03-12 09:32:07 +0200

    Add sort table DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+document.querySelector('thead').addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const table = e.target.closest('table');
+    const tBody = table.querySelector('tbody');
+    const index = e.target.cellIndex;
+    const rows = Array.from(tBody.rows);
+
+    rows.sort((a, b) => {
+      const cellA = a.cells[index].textContent.trim().replace(/[$,]/g, '');
+      const cellB = b.cells[index].textContent.trim().replace(/[$,]/g, '');
+
+      const valueA = isNaN(cellA) ? cellA : Number(cellA);
+      const valueB = isNaN(cellB) ? cellB : Number(cellB);
+
+      return valueA > valueB ? 1 : -1; // Ascending order
+    });
+
+    rows.forEach((row) => tBody.appendChild(row));
+  }
+});
