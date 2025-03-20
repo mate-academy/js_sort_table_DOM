13630d780fe8ace4055f9645de767b0a434effd5
Ostap Danyliv <danyliv.ostap@gmail.com>
2025-03-20 10:22:38 -0500

    sorting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (ev) => {
+  if (ev.target.tagName === 'TH') {
+    const columnIndex = ev.target.cellIndex;
+    const table = ev.target.closest('table');
+    const tBody = table.querySelector('tbody');
+    const rows = Array.from(tBody.rows);
+
+    rows.sort((a, b) => {
+      let cellA = a.cells[columnIndex].textContent.trim();
+      let cellB = b.cells[columnIndex].textContent.trim();
+
+      if (columnIndex === 3) {
+        cellA = parseFloat(cellA.replace(/[$,]/g, ''));
+        cellB = parseFloat(cellB.replace(/[$,]/g, ''));
+      }
+
+      if (isNaN(cellA) || isNaN(cellB)) {
+        return cellA.localeCompare(cellB);
+      } else {
+        return Number(cellA) - Number(cellB);
+      }
+    });
+
+    rows.forEach((row) => tBody.appendChild(row));
+  }
+});
