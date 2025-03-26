1981a51782ad2018e78a8f15aa48d92c24e3eb94
Andrii <andreyfil1337@gmail.com>
2025-03-26 23:41:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
+/* eslint-disable no-shadow */
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const rowHead = head.querySelector('tr');
+const body = document.querySelector('tbody');
+const rowsBody = [...body.querySelectorAll('tr')];
+
+function compare(element1, element2, regime) {
+  switch (regime) {
+    case 'Age':
+      return +element1 - +element2;
+    case 'Salary':
+      const firstFormated = +element1.replaceAll(/[^0-9]+/g, '');
+      const secondFormated = +element2.replaceAll(/[^0-9]+/g, '');
+
+      return firstFormated - secondFormated;
+    default:
+      return element1.localeCompare(element2);
+  }
+}
+
+function tableSort(event) {
+  const target = event.target;
+
+  if (!target.matches('th')) {
+    return;
+  }
+
+  const index = target.cellIndex;
+
+  rowsBody.sort((row1, row2) => {
+    const firstValue = [...row1.children][index].innerText;
+    const secondValue = [...row2.children][index].innerText;
+    const regime = target.innerText;
+
+    return compare(firstValue, secondValue, regime);
+  });
+
+  rowsBody.forEach((row) => body.append(row));
+}
+
+rowHead.addEventListener('click', tableSort);
