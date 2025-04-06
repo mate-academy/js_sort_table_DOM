492e55d483dd2701e14dfcf6513eab58a30de0f8
Ihor <i-melnyk@github.io>
2025-04-06 16:08:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const th = document.querySelector('thead');
+
+th.addEventListener('click', (e) => {
+  sortTBody(e.target.cellIndex);
+});
+
+function sortTBody(indexCell) {
+  const tbody = document.querySelector('tbody');
+  const tbodyCopyContent = [...tbody.rows];
+
+  tbodyCopyContent.sort((rowA, rowB) => {
+    if (indexCell === 3) {
+      const cellA = rowA.cells[indexCell].textContent
+        .trim()
+        .replace(',', '')
+        .replace('$', '');
+      const cellB = rowB.cells[indexCell].textContent
+        .trim()
+        .replace(',', '')
+        .replace('$', '');
+
+      return cellA - cellB;
+    } else {
+      const cellA = rowA.cells[indexCell].textContent.trim();
+      const cellB = rowB.cells[indexCell].textContent.trim();
+
+      return cellA.localeCompare(cellB);
+    }
+  });
+
+  tbody.innerHTML = '';
+  tbodyCopyContent.forEach((x) => tbody.appendChild(x));
+}
