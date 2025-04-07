a870244ade6b8995ff13149b3f213e906f0df4a4
dimon <80713na@gmail.com>
2025-04-07 23:44:18 +0300

    rowSort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const theah = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+theah.addEventListener('click', (elem) => {
+  if (elem.target.tagName === 'TH') {
+    const columnIndex = elem.target.cellIndex;
+    
+    const rows = Array.from(tbody. rows);
+
+    const sorted = rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[columnIndex].textContent.trim();
+      const cellB = rowB.cells[columnIndex].textContent.trim();
+    
+      return cellA.localeCompare(cellB, 'uk', { numeric: true });
+    })
+
+    tbody.innerHTML = '';
+    sorted.forEach((row) => tbody.appendChild(row));
+  }
+} )
+
