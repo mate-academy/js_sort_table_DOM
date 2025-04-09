266cb4092749baf27643a3db325d4763976bd255
gabai62 <gabai62it@gmail.com>
2025-04-09 18:30:17 +0300

    table sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (ev) => {
+  if (ev.target.tagName === 'TH') {
+    const columnIndex = ev.target.cellIndex;
+    const table = document.querySelector('tbody');
+    const tableRows = Array.from(table.rows);
+
+    tableRows.sort((a, b) => {
+      let firstRow = a.cells[columnIndex].textContent.trim();
+      let secondRow = b.cells[columnIndex].textContent.trim();
+
+      if (columnIndex === 3) {
+        firstRow = +firstRow.replace(/[$,]/g, '');
+        secondRow = +secondRow.replace(/[$,]/g, '');
+      }
+
+      if (isNaN(firstRow) && isNaN(secondRow)) {
+        return firstRow.localeCompare(secondRow);
+      } else {
+        return Number(firstRow) - Number(secondRow);
+      }
+    });
+
+    tableRows.forEach((row) => table.appendChild(row));
+  }
+});
