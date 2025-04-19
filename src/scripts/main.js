8f2270dba3061aeb07f31a3572910f3ba611b1df
Roman <romak8162@gmail.com>
2025-04-20 01:01:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('tbody');
+const titles = document.querySelectorAll('th');
+const rows = Array.from(document.querySelectorAll('tr'));
+
+titles.forEach((title, index) => {
+  title.addEventListener('click', () => {
+    rows.sort((a, b) => {
+      const textA = a.children[index].textContent.trim();
+      const textB = b.children[index].textContent.trim();
+
+      const valueA = parseFloat(textA.replace(/[$,]/g, ''));
+      const valueB = parseFloat(textB.replace(/[$,]/g, ''));
+
+      const isANumber = !isNaN(valueA);
+      const isBNumber = !isNaN(valueB);
+
+      if (isANumber && isBNumber) {
+        return valueA - valueB;
+      } else {
+        return textA.localeCompare(textB);
+      }
+    });
+    tableBody.innerHTML = '';
+
+    rows.forEach((row) => {
+      tableBody.appendChild(row);
+    });
+  });
+});
