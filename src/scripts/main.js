d3fa2cfa941ca7e09b1900f93baabcdbec460704
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-23 19:08:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-// write code here
+const sortCategorys = document.querySelectorAll('thead th');
+const employees = [];
+
+document.querySelectorAll('tbody tr').forEach((employeeRow) => {
+  const cells = employeeRow.querySelectorAll('td');
+  const employee = {};
+
+  sortCategorys.forEach((category, index) => {
+    const key = category.innerText;
+    const value = cells[index].innerText;
+
+    employee[key] = value;
+  });
+
+  employees.push(employee);
+});
+
+sortCategorys.forEach((category) => {
+  category.addEventListener('click', () => {
+    const columnName = category.innerText;
+
+    employees.sort((a, b) => {
+      const aValue = a[columnName].trim();
+      const bValue = b[columnName].trim();
+
+      const aNum = Number(aValue.replace(/[$,]/g, ''));
+      const bNum = Number(bValue.replace(/[$,]/g, ''));
+
+      const isNumeric = !isNaN(aNum) && !isNaN(bNum);
+
+      if (isNumeric) {
+        return aNum - bNum;
+      } else {
+        return aValue.localeCompare(bValue);
+      }
+    });
+
+    const tbody = document.querySelector('tbody');
+
+    tbody.innerHTML = '';
+
+    employees.forEach((employee) => {
+      const row = document.createElement('tr');
+
+      Object.values(employee).forEach((value) => {
+        const cell = document.createElement('td');
+
+        cell.textContent = value;
+        row.appendChild(cell);
+      });
+
+      tbody.appendChild(row);
+    });
+  });
+});
