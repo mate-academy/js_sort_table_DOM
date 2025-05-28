38bdd4d3ea1d9389f69c47094ef2e20eeb2f7c7b
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-28 13:28:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const th = table.querySelectorAll('th');
+
+th.forEach((column, index) => {
+  column.addEventListener('click', () => {
+    sortColumn(index);
+  });
+});
+
+function sortColumn(index) {
+  const rows = [...table.querySelectorAll('tbody>tr')];
+
+  rows.sort((a, b) => {
+    const cellA = a.cells[index].textContent.replace('$', '').replace(',', '');
+    const cellB = b.cells[index].textContent.replace('$', '').replace(',', '');
+
+    if (isNaN(+cellA)) {
+      return cellA.localeCompare(cellB);
+    }
+
+    return parseFloat(cellA) - parseFloat(cellB);
+  });
+
+  table.querySelector('tbody').append(...rows);
+}
