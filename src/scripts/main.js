4f8c9178a8be76170cff9be32fdf58995e45905e
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-06-05 14:40:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,21 @@ const tableHaders = document.querySelectorAll('th');
 
 const tbody = document.querySelector('tbody');
 
-tableHaders.forEach((header, columIndex) => {
+tableHaders.forEach((header, columnIndex) => {
   header.addEventListener('click', (e) => {
     const rows = [...tbody.querySelectorAll('tr')];
 
     rows.sort((rowA, rowB) => {
-      const columA = rowA.children[columIndex].textContent.trim();
-      const columB = rowB.children[columIndex].textContent.trim();
+      const columnA = rowA.children[columnIndex].textContent.trim();
+      const columnB = rowB.children[columnIndex].textContent.trim();
 
-      const value1 = parseFloat(columA.replace(/[$,]/g, ''));
-      const value2 = parseFloat(columB.replace(/[$,]/g, ''));
+      const value1 = parseFloat(columnA.replace(/[$,]/g, ''));
+      const value2 = parseFloat(columnB.replace(/[$,]/g, ''));
 
       if (!isNaN(value1) && !isNaN(value2)) {
         return value1 - value2;
       } else {
-        return columA.localeCompare(columB);
+        return columnA.localeCompare(columnB);
       }
     });
 
