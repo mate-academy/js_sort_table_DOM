4a687b352f109bb6602f0562cb65fef1366a75d3
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-06-05 14:34:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const tableHaders = document.querySelectorAll('th');
+const tbody = document.querySelector('tbody');
+
+tableHaders.forEach((header, columIndex) => {
+  header.addEventListener('click', (e) => {
+    const rows = [...tbody.querySelectorAll('tr')];
+
+    rows.sort((rowA, rowB) => {
+      const columA = rowA.children[columIndex].textContent.trim();
+      const columB = rowB.children[columIndex].textContent.trim();
+
+      const value1 = parseFloat(columA.replace(/[$,]/g, ''));
+      const value2 = parseFloat(columB.replace(/[$,]/g, ''));
+
+      if (!isNaN(value1) && !isNaN(value2)) {
+        return value1 - value2;
+      } else {
+        return columA.localeCompare(columB);
+      }
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
