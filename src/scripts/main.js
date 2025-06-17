c858735191a4c0a57bb4b90b74dcc468c58170de
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-06-17 13:28:33 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+// write code herе
+document.querySelectorAll('th').forEach((th) => {
+  th.addEventListener('click', () => {
+    const columnIndex = Array.from(th.parentNode.children).indexOf(th);
+    const tbody = document.querySelector('tbody');
+    const rows = Array.from(tbody.rows);
+
+    rows.sort((a, b) => {
+      let valA = a.cells[columnIndex].textContent.trim();
+      let valB = b.cells[columnIndex].textContent.trim();
+
+      if (
+        !isNaN(valA.replace(/[$,]/g, '')) &&
+        !isNaN(valB.replace(/[$,]/g, ''))
+      ) {
+        valA = parseFloat(valA.replace(/[$,]/g, ""));
+        valB = parseFloat(valB.replace(/[$,]/g, ""));
+      }
+
+      return valA > valB ? 1 : -1;
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
