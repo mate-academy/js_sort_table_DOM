9df11d9a50437882f640d2cf5b7cfdc907f20464
Alexey Kravets <kravecoleksiy@gmail.com>
2025-06-18 18:24:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead th');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const tbody = document.querySelector('tbody');
+    const rowsArray = Array.from(tbody.querySelectorAll('tr'));
+
+    rowsArray.sort((a, b) => {
+      const aText = a.children[index].textContent;
+      const bText = b.children[index].textContent;
+
+      const aNum = parseFloat(aText.replace(/[^0-9.-]+/g, ''));
+      const bNum = parseFloat(bText.replace(/[^0-9.-]+/g, ''));
+
+      const bothAreNumbers = !isNaN(aNum) && !isNaN(bNum);
+
+      return bothAreNumbers ? aNum - bNum : aText.localeCompare(bText);
+    });
+
+    tbody.innerHTML = '';
+    rowsArray.forEach((row) => tbody.appendChild(row));
+  });
+});
