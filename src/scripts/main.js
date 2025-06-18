673d7284e7eb4fc6f1065bba081395a3a91c5a98
AzartiksMan <azartofgod@gmail.com>
2025-06-18 21:37:57 +0300

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('thead th');
+const tableBody = table.querySelector('tbody');
+
+headers.forEach((header, idx) => {
+  header.addEventListener('click', () => {
+    const rows = Array.from(tableBody.querySelectorAll('tr'));
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.children[idx].textContent;
+      const cellB = rowB.children[idx].textContent;
+
+      const a = parseFloat(cellA.replace(/[$,]/g, '')) || cellA;
+      const b = parseFloat(cellB.replace(/[$,]/g, '')) || cellB;
+
+      if (typeof a === 'number' && typeof b === 'number') {
+        return a - b;
+      }
+
+      return a.localeCompare(b);
+    });
+
+    tableBody.innerHTML = '';
+    rows.forEach((row) => tableBody.appendChild(row));
+  });
+});
