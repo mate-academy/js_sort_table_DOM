c2f39648e94275c5507f4575ddd97160c9c2ec5a
Savtenko Volodymyr <volodimer.sovtenko2018@gmail.com>
2025-06-23 18:06:43 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const table = document.body.querySelector('table');
+
+table.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const tbody = document.body.querySelector('tbody');
+  const rows = tbody.querySelectorAll('tr');
+  const rowsArray = [...rows];
+
+  const index = th.cellIndex;
+
+  rowsArray.sort((a, b) => {
+    const cellA = a.cells[index].textContent.trim();
+    const cellB = b.cells[index].textContent.trim();
+
+    const aNumber = Number(cellA);
+    const bNumber = Number(cellB);
+
+    if (!isNaN(aNumber) && !isNaN(bNumber)) {
+      return aNumber - bNumber;
+    } else {
+      return cellA.localeCompare(cellB);
+    }
+  });
+
+  tbody.innerHTML = '';
+
+  rowsArray.forEach((row) => {
+    tbody.appendChild(row);
+  });
+});
