5ed34e1bbed374a552e6b5fd4515566725500f23
Eksonurit <fizervitalij897@gmail.com>
2025-07-29 22:46:26 +0300

    add solutin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,39 +3,35 @@
 const employers = document.querySelectorAll('tbody > tr');
 const tableBody = document.querySelector('tbody');
 
-console.log(Array.from(employers))
-
 const sortByName = () => {
   const sorted = Array.from(employers).sort((employer1, employer2) => {
     const name1 = employer1.children[0].textContent.trim();
     const name2 = employer2.children[0].textContent.trim();
 
-    return name1.localeCompare(name2)
-  })
-  tableBody.innerHTML = '';
+    return name1.localeCompare(name2);
+  });
 
-  console.log(sorted);
+  tableBody.innerHTML = '';
 
   for (const n of sorted) {
     tableBody.append(n);
   }
-}
+};
 
 const sortByPosition = () => {
   const sorted = Array.from(employers).sort((employer1, employer2) => {
     const position1 = employer1.children[1].textContent.trim();
     const position2 = employer2.children[1].textContent.trim();
 
-    return position1.localeCompare(position2)
-  })
-  tableBody.innerHTML = '';
+    return position1.localeCompare(position2);
+  });
 
-  console.log(sorted);
+  tableBody.innerHTML = '';
 
   for (const n of sorted) {
     tableBody.append(n);
   }
-}
+};
 
 const sortByAge = () => {
   const sorted = Array.from(employers).sort((employer1, employer2) => {
@@ -43,42 +39,42 @@ const sortByAge = () => {
     const position2 = Number(employer2.children[2].textContent);
 
     return position1 - position2;
-  })
-  tableBody.innerHTML = '';
+  });
 
-  console.log(sorted);
+  tableBody.innerHTML = '';
 
   for (const n of sorted) {
     tableBody.append(n);
   }
-}
+};
 
 const sortBySalary = () => {
   const sorted = Array.from(employers).sort((employer1, employer2) => {
-    const position1 = employer1.children[3].textContent.replace('$', '').replace(/,/g, '');
-    const position2 = employer2.children[3].textContent.replace('$', '').replace(/,/g, '');
+    const position1 = employer1.children[3].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
+    const position2 = employer2.children[3].textContent
+      .replace('$', '')
+      .replace(/,/g, '');
 
     return Number(position1) - Number(position2);
-  })
-  tableBody.innerHTML = '';
+  });
 
-  console.log(sorted);
+  tableBody.innerHTML = '';
 
   for (const n of sorted) {
     tableBody.append(n);
   }
-}
+};
 
-document.addEventListener('click', (event) => {
-  console.log(event.target)
-
-  if (event.target.textContent === 'Name' && event.target.closest('thead')) {
-    sortByName()
-  } else if (event.target.textContent === 'Position' && event.target.closest('thead')) {
-    sortByPosition()
-  } else if (event.target.textContent === 'Age' && event.target.closest('thead')) {
-    sortByAge().textContent
-  } else if (event.target.textContent === 'Salary' && event.target.closest('thead')) {
-    sortBySalary()
+document.addEventListener('click', (e) => {
+  if (e.target.textContent === 'Name' && e.target.closest('thead')) {
+    sortByName();
+  } else if (e.target.textContent === 'Position' && e.target.closest('thead')) {
+    sortByPosition();
+  } else if (e.target.textContent === 'Age' && e.target.closest('thead')) {
+    sortByAge();
+  } else if (e.target.textContent === 'Salary' && e.target.closest('thead')) {
+    sortBySalary();
   }
 });
