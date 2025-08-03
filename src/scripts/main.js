da0b7d2d8d15c6e23777e5751ed04c19ccba8918
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-08-03 22:55:52 +0300

    table sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const title = document.querySelector('table');
+const allTitle = title.querySelectorAll('thead th');
+
+allTitle.forEach((e, index) => {
+  e.addEventListener('click', () => {
+    const tbody = title.querySelector('tbody');
+    const rows = Array.from(title.querySelectorAll('tr'));
+    const sortRows = rows.sort((aRow, bRow) => {
+      const aCel = aRow.cells[index].textContent.trim();
+      const bCel = bRow.cells[index].textContent.trim();
+      const clearA = aCel.replace(/[$,\s]/g, '');
+      const clearB = bCel.replace(/[$,\s]/g, '');
+
+      const thisNumber = !isNaN(clearA) && !isNaN(clearB);
+
+      if (thisNumber) {
+        return clearA - clearB;
+      } else {
+        return aCel.localeCompare(bCel);
+      }
+    });
+
+    tbody.innerHTML = '';
+    sortRows.forEach((row) => tbody.appendChild(row));
+  });
+});
