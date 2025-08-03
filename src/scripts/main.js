39dcf51e736dea27fe9c135d69c714e88de1a351
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-08-03 23:19:49 +0300

    fixed sort without title

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,12 @@ const allTitle = title.querySelectorAll('thead th');
 allTitle.forEach((e, index) => {
   e.addEventListener('click', () => {
     const tbody = title.querySelector('tbody');
-    const rows = Array.from(title.querySelectorAll('tr'));
+    const rows = Array.from(tbody.rows);
     const sortRows = rows.sort((aRow, bRow) => {
       const aCel = aRow.cells[index].textContent.trim();
       const bCel = bRow.cells[index].textContent.trim();
-      const clearA = aCel.replace(/[$,\s]/g, '');
-      const clearB = bCel.replace(/[$,\s]/g, '');
+      const clearA = parseFloat(aCel.replace(/[$,\s]/g, ''));
+      const clearB = parseFloat(bCel.replace(/[$,\s]/g, ''));
 
       const thisNumber = !isNaN(clearA) && !isNaN(clearB);
 
