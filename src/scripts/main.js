420a75dc4df5a7f2c33f619024e19f093e2bb90f
Nazariy <nazariypanochko@gmail.com>
2025-08-11 14:55:20 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,6 @@ const table = document.querySelector('table');
 const headers = table.querySelectorAll('th');
 const tBody = table.querySelector('tbody');
 
-let asc = true;
-
 headers.forEach((header, index) => {
   header.addEventListener('click', () => {
     const rows = Array.from(tBody.querySelectorAll('tr'));
@@ -15,28 +13,25 @@ headers.forEach((header, index) => {
       let valA = cellA;
       let valB = cellB;
 
-      // Якщо колонка Salary — парсимо як число
       if (header.textContent.trim() === 'Salary') {
         valA = parseFloat(cellA.replace(/\$/g, '').replace(/,/g, ''));
         valB = parseFloat(cellB.replace(/\$/g, '').replace(/,/g, ''));
       } else if (!isNaN(cellA) && !isNaN(cellB)) {
-        // Якщо це числа (наприклад, Age) - парсимо як числа
         valA = Number(cellA);
         valB = Number(cellB);
       }
 
       if (valA < valB) {
-        return asc ? -1 : 1;
+        return -1;
       }
 
       if (valA > valB) {
-        return asc ? 1 : -1;
+        return 1;
       }
 
       return 0;
     });
 
     rows.forEach((row) => tBody.appendChild(row));
-    asc = !asc;
   });
 });
