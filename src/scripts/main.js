2d800c6cd5f85006d81683415abac3108a8081a7
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-21 11:34:06 +0300

    added tasl solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('thead th');
+const tbody = table.querySelector('tbody');
+
+function sortTable(index) {
+  const rows = Array.from(tbody.children);
+
+  const sortedRows = rows.sort((a, b) => {
+    let firstRow = a.cells[index].textContent;
+    let secondRow = b.cells[index].textContent;
+
+    if (index === 2) {
+      return parseInt(firstRow) - parseInt(secondRow);
+    }
+
+    if (index === 3) {
+      firstRow = parseInt(firstRow.replace(/[$,]/g, ''));
+      secondRow = parseInt(secondRow.replace(/[$,]/g, ''));
+
+      return firstRow - secondRow;
+    }
+
+    return firstRow.localeCompare(secondRow);
+  });
+
+  tbody.innerHTML = '';
+
+  sortedRows.forEach((row) => tbody.appendChild(row));
+}
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    sortTable(index);
+  });
+});
