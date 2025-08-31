ad52b3ddbfb9186bc1d536277a12cd5a2100d060
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-31 14:43:13 +0300

    sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+tHead.addEventListener('click', (e) => {
+  const el = e.target;
+  const allTh = tHead.querySelectorAll('th');
+
+  if (el.tagName === 'TH') {
+    const thPosition = [...allTh].indexOf(el);
+    // 0-Name,1-Position,2-Age,3-Salary;
+
+    const allTrBody = tBody.querySelectorAll('tr');
+    const allTrArray = [...allTrBody];
+
+    allTrArray.sort((a, b) => {
+      const elA = a.querySelectorAll('td')[thPosition].textContent;
+      const elB = b.querySelectorAll('td')[thPosition].textContent;
+      const clearA = elA.replace(/[$,]/g, '');
+      const clearB = elB.replace(/[$,]/g, '');
+
+      if (!isNaN(Number(clearA)) && !isNaN(Number(clearB))) {
+        return Number(clearA) - Number(clearB);
+      } else {
+        return elA.localeCompare(elB);
+      }
+    });
+
+    tBody.append(...allTrArray);
+  }
+});
