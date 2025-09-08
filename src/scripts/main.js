6cb1c3d0547bb87fa3d003998a953de312762a07
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-09-08 03:22:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const title = table.querySelectorAll('th');
+
+title.forEach((th, index) => {
+  th.addEventListener('click', (e) => {
+    const rows = [...table.querySelectorAll('tbody tr')];
+    const tbody = document.querySelector('tbody');
+
+    rows.sort((first, second) => {
+      const firstCell = first.querySelectorAll('td')[index].textContent;
+      const secondCell = second.querySelectorAll('td')[index].textContent;
+      const cleanFirst = firstCell.replace(/[$,]/g, '');
+      const cleanSecond = secondCell.replace(/[$,]/g, '');
+
+      if (isNaN(Number(cleanFirst)) && isNaN(Number(cleanSecond))) {
+        return firstCell.localeCompare(secondCell);
+      } else {
+        return Number(cleanFirst) - Number(cleanSecond);
+      }
+    });
+
+    tbody.innerHTML = '';
+
+    rows.forEach((item) => tbody.appendChild(item));
+  });
+});
