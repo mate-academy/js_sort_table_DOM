19e33f77aed6b50569bfe5e1b04f858ccc8178a7
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-12 02:04:07 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,33 +3,31 @@
 const table = document.querySelector('table');
 const thead = table.tHead;
 const tbody = table.tBodies[0];
+const headers = [...thead.querySelectorAll('tr th')];
 
 thead.addEventListener('click', (e) => {
-  const headers = [...thead.querySelectorAll('tr th')];
+  const th = e.target.closest('th');
+  const index = headers.indexOf(th);
 
-  headers.forEach((header, index) => {
-    if (e.target === header) {
-      const sortedList = [...tbody.rows].sort((item1, item2) => {
-        if (
-          item1.cells[index].innerText.toUpperCase() ===
-          item1.cells[index].innerText.toLowerCase()
-        ) {
-          return (
-            normalizeStr(item1.cells[index].innerText) -
-            normalizeStr(item2.cells[index].innerText)
-          );
-        } else {
-          return item1.cells[index].innerText.localeCompare(
-            item2.cells[index].innerText,
-          );
-        }
-      });
-
-      sortedList.forEach((item) => tbody.append(item));
+  const sortedList = [...tbody.rows].sort((item1, item2) => {
+    if (
+      !isNaN(normalizeStr(item1.cells[index].innerText)) &&
+      !isNaN(normalizeStr(item2.cells[index].innerText))
+    ) {
+      return (
+        Number(normalizeStr(item1.cells[index].innerText)) -
+        Number(normalizeStr(item2.cells[index].innerText))
+      );
+    } else {
+      return item1.cells[index].innerText.localeCompare(
+        item2.cells[index].innerText,
+      );
     }
   });
+
+  tbody.append(...sortedList);
 });
 
 function normalizeStr(item) {
-  return item.trim().replaceAll('$', '').replaceAll(',', '');
+  return item.trim().replace(/[,$]/g, '');
 }
