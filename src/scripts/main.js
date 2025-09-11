819f563e85501fe8c570e9eb1c6195dfd0b68dc5
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-11 15:35:28 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const titleTable = document.querySelector('table thead');
+const collectionTh = titleTable.querySelectorAll('tr th');
+
+collectionTh.forEach((th, thIndex) => {
+  th.addEventListener('click', () => {
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+    const sortedRows = rows.sort((a, b) => {
+      const cellA = a.cells[thIndex].textContent.trim();
+      const cellB = b.cells[thIndex].textContent.trim();
+
+      if (th.textContent.trim() === 'Salary') {
+        const salaryA = parseFloat(cellA.replace(/[$,]/g, ''));
+        const salaryB = parseFloat(cellB.replace(/[$,]/g, ''));
+
+        return salaryA - salaryB;
+      }
+
+      const numA = parseFloat(cellA.replace(',', '.'));
+      const numB = parseFloat(cellB.replace(',', '.'));
+
+      if (!isNaN(numA) && !isNaN(numB)) {
+        return numA - numB;
+      }
+
+      return cellA.localeCompare(cellB, 'uk', { sensitivity: 'base' });
+    });
+
+    const tbody = document.querySelector('tbody');
+
+    sortedRows.forEach((row) => tbody.appendChild(row));
+  });
+});
