654c19defb7197de775735f98149beb35c0c49d3
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-12 03:08:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,25 @@
 const table = document.querySelector('table');
 const thead = table.tHead;
 const tbody = table.tBodies[0];
-const headers = [...thead.querySelectorAll('tr th')];
 
 thead.addEventListener('click', (e) => {
   const th = e.target.closest('th');
-  const index = headers.indexOf(th);
+  const index = th.cellIndex;
+
+  if (!th) {
+    return;
+  }
 
   const sortedList = [...tbody.rows].sort((item1, item2) => {
-    if (
-      !isNaN(normalizeStr(item1.cells[index].innerText)) &&
-      !isNaN(normalizeStr(item2.cells[index].innerText))
-    ) {
-      return (
-        Number(normalizeStr(item1.cells[index].innerText)) -
-        Number(normalizeStr(item2.cells[index].innerText))
-      );
+    const aRaw = normalizeStr(item1.cells[index].innerText);
+    const bRaw = normalizeStr(item2.cells[index].innerText);
+    const a = parseFloat(aRaw);
+    const b = parseFloat(bRaw);
+    const aIsNum = !Number.isNaN(a);
+    const bIsNum = !Number.isNaN(b);
+
+    if (aIsNum && bIsNum) {
+      return a - b;
     } else {
       return item1.cells[index].innerText.localeCompare(
         item2.cells[index].innerText,
@@ -29,5 +33,5 @@ thead.addEventListener('click', (e) => {
 });
 
 function normalizeStr(item) {
-  return item.trim().replace(/[,$]/g, '');
+  return item.trim().replace(/[^0-9.-]+/g, '');
 }
