a72da0ee2800a37d14787809deaf3d19a8cd9f7e
Vasyliev Vadym <protocol222333@gmail.com>
2025-09-16 13:30:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const allTh = document.querySelectorAll('thead th');
+  const tbody = document.querySelector('tbody');
+  const rows = [...tbody.rows];
+
+  allTh.forEach((th, index) => {
+    th.addEventListener('click', () => {
+      rows.sort((a, b) => {
+        if (index < 2) {
+          return a.cells[index].textContent.localeCompare(
+            b.cells[index].textContent,
+          );
+        } else {
+          return (
+            Number(a.cells[index].textContent.replace(/[,$]/g, '')) -
+            Number(b.cells[index].textContent.replace(/[,$]/g, ''))
+          );
+        }
+      });
+
+      rows.forEach((row) => tbody.append(row));
+    });
+  });
+});
