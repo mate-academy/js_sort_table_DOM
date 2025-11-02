877ddc30a3754fe82d5f0049f32bb35264105757
wzfxc <maksohrimenko1@gmail.com>
2025-11-02 16:12:51 +0200

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('th');
+
+headers.forEach((header) => {
+  header.addEventListener('click', (e) => {
+    const index = [...headers].indexOf(e.currentTarget);
+    const rows = Array.from(table.tBodies[0].rows);
+
+    rows.sort((a, b) => {
+      const valA = a.cells[index].textContent.trim();
+      const valB = b.cells[index].textContent.trim();
+
+      if (!isNaN(valA) && !isNaN(valB)) {
+        return Number(valA) - Number(valB);
+      } else {
+        return valA.localeCompare(valB, 'uk', { numeric: true });
+      }
+    });
+
+    rows.forEach((row) => {
+      table.tBodies[0].appendChild(row);
+    });
+  });
+});
