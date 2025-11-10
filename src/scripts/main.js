4880f159556df74de6e5ab493ffbd6c476ffb7a0
Yuliia Voroniuk <juliasemchishin1545@gmail.com>
2025-11-10 15:14:28 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const theads = [...document.querySelectorAll('thead tr th')];
+
+const tbody = document.querySelector('tbody');
+const tbodyChildren = [...document.querySelectorAll('tbody tr')];
+
+theads.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const sorted = tbodyChildren.sort((a, b) => {
+      let valueA;
+      let valueB;
+
+      // index 3 = salary
+      if (index === 3) {
+        valueA = a.children[index].textContent.split('$')[1].replace(',', '');
+        valueB = b.children[index].textContent.split('$')[1].replace(',', '');
+      } else {
+        valueA = a.children[index].textContent;
+        valueB = b.children[index].textContent;
+      }
+
+      if (!isNaN(+valueA) && !isNaN(+valueB)) {
+        return +valueA - +valueB;
+      }
+
+      return valueA.localeCompare(valueB);
+    });
+
+    tbody.innerHTML = '';
+    sorted.forEach((el) => tbody.append(el));
+  });
+});
