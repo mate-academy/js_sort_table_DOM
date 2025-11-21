68a61763782da84507efcb312c8ebd54d3d9e2c6
Iryna Shevkun <sevkunirina65@gmail.com>
2025-11-21 21:15:36 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tBody = table.querySelector('tbody');
+const tr = [...tBody.querySelectorAll('tr')];
+const th = [...table.querySelectorAll('th')];
+
+th.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    tr.sort((elem1, elem2) => {
+      const firstElem = elem1.children[index].textContent.trim();
+      const secondElem = elem2.children[index].textContent.trim();
+
+      if (index <= 1) {
+        return firstElem.localeCompare(secondElem);
+      }
+
+      return stringToNumber(firstElem) - stringToNumber(secondElem);
+    }).forEach((elem) => {
+      tBody.appendChild(elem);
+    });
+  });
+});
+
+function stringToNumber(string) {
+  if (typeof string !== 'string') {
+    return string;
+  }
+
+  return +string.replace('$', '').replace(',', '');
+}
