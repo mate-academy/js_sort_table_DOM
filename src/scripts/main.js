d60c21396bdc22f7fd1a7fdb6f071de3e206e9ea
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-12-01 01:09:08 +0200

    Sort-table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write code here
+const tableElEmployee = document.querySelector('table');
+
+const thead = tableElEmployee.querySelector('thead');
+const tbody = tableElEmployee.querySelector('tbody');
+
+const thElem = thead.querySelectorAll('th');
+
+thElem.forEach((th, index) => {
+  th.addEventListener('click', () => {
+    const allRowsBody = [...tbody.rows];
+
+    allRowsBody.sort((rowA, rowB) => {
+      const a = rowA.cells[index].textContent.trim();
+      const b = rowB.cells[index].textContent.trim();
+
+      if (normalize(a, index) > normalize(b, index)) {
+        return 1;
+      }
+
+      if (normalize(a, index) < normalize(b, index)) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    allRowsBody.forEach((row) => tbody.append(row));
+  });
+});
+
+function normalize(value, index) {
+  if (index === 2) {
+    return Number(value);
+  }
+
+  if (index === 3) {
+    return Number(value.replace(/[$,]/g, ''));
+  }
+
+  return value.toLowerCase();
+}
