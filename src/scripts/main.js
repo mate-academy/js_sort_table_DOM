4e3ae7cc417c8c5e97ad46ff9fe16b323898a320
Vladyslav <vkobeluh@gmail.com>
2025-12-08 14:29:13 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tHead = table.tHead;
+const tBody = table.tBodies[0];
+
+tHead.addEventListener('click', (e) => {
+  const rows = [...tBody.rows];
+  const th = e.target.closest('th');
+  const thIndex = th.cellIndex;
+
+  rows.sort((firstRow, secondRow) => {
+    const firstValue = firstRow.cells[thIndex].textContent;
+    const secondValue = secondRow.cells[thIndex].textContent;
+
+    if (isNumber(firstValue)) {
+      return validate(firstValue) - validate(secondValue);
+    }
+
+    return firstValue.localeCompare(secondValue);
+  });
+
+  tBody.append(...rows);
+});
+
+function isNumber(value) {
+  if (/\d/.test(value)) {
+    return true;
+  }
+
+  return false;
+}
+
+function validate(value) {
+  return +value.replace(/\D/g, '');
+}
