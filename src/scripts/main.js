fb21c1d80d083f4ab2c936b406e5bab193df862a
Vladyslav <vkobeluh@gmail.com>
2025-12-08 14:41:45 +0200

    Fix solution (attempt 1)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,39 @@
 'use strict';
 
-const table = document.querySelector('table');
-const tHead = table.tHead;
-const tBody = table.tBodies[0];
-
-tHead.addEventListener('click', (e) => {
-  const rows = [...tBody.rows];
-  const th = e.target.closest('th');
-  const thIndex = th.cellIndex;
-
-  rows.sort((firstRow, secondRow) => {
-    const firstValue = firstRow.cells[thIndex].textContent;
-    const secondValue = secondRow.cells[thIndex].textContent;
-
-    if (isNumber(firstValue)) {
-      return validate(firstValue) - validate(secondValue);
-    }
-
-    return firstValue.localeCompare(secondValue);
-  });
-
-  tBody.append(...rows);
-});
-
 function isNumber(value) {
-  if (/\d/.test(value)) {
-    return true;
-  }
-
-  return false;
+  return /\d/.test(value);
 }
 
 function validate(value) {
   return +value.replace(/\D/g, '');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const tHead = table.tHead;
+  const tBody = table.tBodies[0];
+
+  tHead.addEventListener('click', (e) => {
+    const rows = [...tBody.rows];
+    const th = e.target.closest('th');
+
+    if (!th) {
+      return;
+    }
+
+    const thIndex = th.cellIndex;
+
+    rows.sort((firstRow, secondRow) => {
+      const firstValue = firstRow.cells[thIndex].textContent;
+      const secondValue = secondRow.cells[thIndex].textContent;
+
+      if (isNumber(firstValue) && isNumber(secondValue)) {
+        return validate(firstValue) - validate(secondValue);
+      }
+
+      return firstValue.localeCompare(secondValue);
+    });
+
+    tBody.append(...rows);
+  });
+});
