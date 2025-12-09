523bd6d9dd368ac4c8a255899e8f1ae319dfb9a0
Maksym Halushko <gallikillag@gmail.com>
2025-12-09 16:32:48 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const th = thead.querySelectorAll('th');
+const tr = Array.from(tbody.querySelectorAll('tr'));
+
+th.forEach((head, index) => {
+  head.addEventListener('click', () => {
+    tr.sort((rowA, rowB) => {
+      const cellA = rowA.children[index].textContent.trim();
+      const cellB = rowB.children[index].textContent.trim();
+
+      if (index === 2) {
+        return Number(cellA) - Number(cellB);
+      }
+
+      if (index === 3) {
+        const numA = Number(cellA.replace(/,/g, '').replace('$', ''));
+        const numB = Number(cellB.replace(/,/g, '').replace('$', ''));
+
+        return numA - numB;
+      }
+
+      return cellA.localeCompare(cellB);
+    });
+
+    tr.forEach((row) => tbody.append(row));
+  });
+});
