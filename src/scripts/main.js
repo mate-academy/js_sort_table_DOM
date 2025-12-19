759606f740d628cfd410fc73189b51f547e659ec
friranger <65720979+friranger1@users.noreply.github.com>
2025-12-19 21:37:18 +0200

    Implemented table sorting by click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+const table = document.querySelector('table');
+let tbody;
+
+for (const elem of table.children) {
+  if (elem.tagName === 'TBODY') {
+    tbody = elem;
+  }
+}
+
+for (const header of headers) {
+  header.addEventListener('click', (e) => {
+    const thIndex = [...header.parentElement.children].indexOf(header);
+    const rows = [...tbody.children];
+
+    rows.sort((elem1, elem2) => {
+      const valA = elem1.children[thIndex].textContent;
+      const valB = elem2.children[thIndex].textContent;
+
+      if (!isNaN(parseFloat(valA))) {
+        return parseFloat(valA) - parseFloat(valB);
+      } else {
+        return valA.localeCompare(valB);
+      }
+    });
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+}
