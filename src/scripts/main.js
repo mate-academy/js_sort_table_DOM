57538322fa2b25422dff814a6fd80a5a6aa6d4df
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-28 23:22:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const theadTr = document.querySelectorAll('thead tr th');
+
+theadTr.forEach((node) => {
+  node.addEventListener('click', (e) => {
+    const idx = Array.from(theadTr).indexOf(e.currentTarget);
+
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    const tbody = document.querySelector('tbody');
+
+    rows.sort((rowA, rowB) => {
+      const a = rowA.children[idx].textContent.trim();
+      const b = rowB.children[idx].textContent.trim();
+
+      const aNum = Number(a);
+      const bNum = Number(b);
+
+      if (!Number.isNaN(aNum) && !Number.isNaN(bNum)) {
+        return aNum - bNum;
+      }
+
+      return a.localeCompare(b);
+    });
+
+    rows.forEach((r) => tbody.appendChild(r));
+  });
+});
