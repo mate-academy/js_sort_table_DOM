d9355132dd7a43db826ab0773d577725044b9155
Nazarii Lesniak <naziksay@gmail.com>
2026-01-09 14:22:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const tableBody = document.querySelector('tbody');
+const tableHeader = document.querySelector('thead');
+
+function getSalaryValue(salaryString) {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+}
+
+tableHeader.addEventListener('click', (eventClick) => {
+  const th = eventClick.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const listEmployee = [...tableBody.querySelectorAll('tr')];
+
+  listEmployee.sort((firstRow, secondRow) => {
+    const firstValue = firstRow.cells[th.cellIndex].textContent;
+    const secondValue = secondRow.cells[th.cellIndex].textContent;
+
+    if (th.cellIndex === 2 || th.cellIndex === 3) {
+      return getSalaryValue(firstValue) - getSalaryValue(secondValue);
+    }
+
+    return firstValue.localeCompare(secondValue);
+  });
+
+  tableBody.append(...listEmployee);
+});
