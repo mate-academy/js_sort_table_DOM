63e80da520d2f0a97ed63923951e5f2143d25eb3
Yar14k <tabachuk1406@gmail.com>
2026-01-12 21:01:19 +0100

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const headers = document.querySelectorAll('th');
+const rows = Array.from(tbody.querySelectorAll('tr'));
+
+headers.forEach((header) => {
+  header.addEventListener('click', (e) => {
+    rows.sort((a, b) => {
+      const aText = a.children[e.target.cellIndex].textContent.trim();
+      const bText = b.children[e.target.cellIndex].textContent.trim();
+
+      const aNum = Number(aText);
+      const bNum = Number(bText);
+
+      if (isNaN(aNum) || isNaN(bNum)) {
+        return aText.localeCompare(bText);
+      } else {
+        return aNum - bNum;
+      }
+    });
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
