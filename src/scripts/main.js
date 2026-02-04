c0259150b2f63c49dec07716f3d1137ea6057384
Anastasia Sutkova <nxxqar@gmail.com>
2026-02-05 00:44:24 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+
+table.querySelector('thead').addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const columnIndex = th.cellIndex;
+  const type = th.textContent.trim();
+  const rows = [...tbody.rows];
+  const cleanValue = (val) => val.replace(/[$,]/g, '');
+
+  rows.sort((rowA, rowB) => {
+    const valA = rowA.cells[columnIndex].textContent.trim();
+    const valB = rowB.cells[columnIndex].textContent.trim();
+
+    if (type === 'Age' || type === 'Salary') {
+      return Number(cleanValue(valA)) - Number(cleanValue(valB));
+    }
+
+    return valA.localeCompare(valB);
+  });
+
+  tbody.append(...rows);
+});
