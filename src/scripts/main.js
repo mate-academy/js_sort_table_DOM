262cb401523cb787d79eff3a378eb7f32e2af3d5
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-02-11 00:35:31 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+
+table.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const rows = [...tbody.rows];
+
+  const ind = e.target.cellIndex;
+
+  const sortedRows = rows.sort((a, b) => {
+    const astr = a.cells[ind].textContent.replace(/[^\d]/g, '');
+    const bstr = b.cells[ind].textContent.replace(/[^\d]/g, '');
+
+    if (astr.length > 0 && bstr.length > 0) {
+      return +astr - +bstr;
+    }
+
+    if (astr.length === 0 || bstr.length === 0) {
+      return a.cells[ind].textContent.localeCompare(b.cells[ind].textContent);
+    }
+  });
+
+  tbody.append(...sortedRows);
+});
