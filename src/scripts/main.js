dc820dc60dfe7d38f86ecb007175818b6627468f
Anastasiia Tkachenko <nastya240509@gmail.com>
2026-02-22 23:34:13 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
+
+table.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const index = e.target.cellIndex;
+  const rows = Array.from(tbody.querySelectorAll('tr'));
+
+  const isNumericColumn = index === 2 || index === 3;
+
+  rows.sort((a, b) => {
+    const aValue = a.children[index].textContent.trim();
+    const bValue = b.children[index].textContent.trim();
+
+    if (isNumericColumn) {
+      return (
+        Number(aValue.replace(/[$,]/g, '')) -
+        Number(bValue.replace(/[$,]/g, ''))
+      );
+    }
+
+    return aValue.localeCompare(bValue);
+  });
+
+  tbody.append(...rows);
+});
