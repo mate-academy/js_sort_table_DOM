23b2d7f0823a480e221371030a04b4fd826cac6c
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-26 11:02:13 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,73 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tHead = table.tHead;
+const tbody = Array.from(table.tBodies[0].children);
+
+tHead.addEventListener('click', (e) => {
+  const target = e.target;
+  const tbodyNow = table.tBodies[0];
+
+  if (target.textContent === 'Age') {
+    const number = target.cellIndex;
+
+    const newTbody = tbody.sort((first, second) => {
+      const firstFinished = Number(first.children[number].textContent);
+      const secondFinished = Number(second.children[number].textContent);
+
+      return firstFinished - secondFinished;
+    });
+
+    newTbody.forEach((oneTr) => {
+      tbodyNow.append(oneTr);
+    });
+  }
+
+  if (target.textContent === 'Salary') {
+    const number = target.cellIndex;
+
+    const newTbody = tbody.sort((first, second) => {
+      const firstFinished = Number(
+        first.children[number].textContent.slice(1).split(',').join(''),
+      );
+      const secondFinished = Number(
+        second.children[number].textContent.slice(1).split(',').join(''),
+      );
+
+      return firstFinished - secondFinished;
+    });
+
+    newTbody.forEach((oneTr) => {
+      tbodyNow.append(oneTr);
+    });
+  }
+
+  if (target.textContent === 'Position') {
+    const number = target.cellIndex;
+    const newTbody = tbody.sort((first, second) => {
+      const firstFinished = first.children[number].textContent;
+      const secondFinished = second.children[number].textContent;
+
+      return firstFinished.localeCompare(secondFinished);
+    });
+
+    newTbody.forEach((oneTr) => {
+      tbodyNow.append(oneTr);
+    });
+  }
+
+  if (target.textContent === 'Name') {
+    const number = target.cellIndex;
+
+    const newTbody = tbody.sort((first, second) => {
+      const firstFinished = first.children[number].textContent;
+      const secondFinished = second.children[number].textContent;
+
+      return firstFinished.localeCompare(secondFinished);
+    });
+
+    newTbody.forEach((oneTr) => {
+      tbodyNow.append(oneTr);
+    });
+  }
+});
