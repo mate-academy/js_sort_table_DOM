9ba02c596747761baffaaf966a7e9d73b570b6ec
yuliana111007-debug <yuliana111007@gmail.com>
2026-03-07 23:45:20 +0200

    need advice

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const headers = table.querySelectorAll('thead th');
+
+headers.forEach((th) => {
+  th.addEventListener('click', (e) => {
+    const thClicked = e.currentTarget;
+    const index = thClicked.cellIndex;
+    const elToSort = Array.from(table.querySelectorAll('tbody tr'));
+
+    elToSort.sort((a, b) => {
+      const textA = a.children[index].textContent.trim();
+      const textB = b.children[index].textContent.trim();
+      const numA = Number(textA);
+      const numB = Number(textB);
+
+      if (!Number.isNaN(numA) && !Number.isNaN(numB)) {
+        return numA - numB;
+      } else {
+        return textA.localeCompare(textB);
+      }
+    });
+
+    const tbody = table.querySelector('tbody');
+
+    tbody.innerHTML = '';
+    tbody.append(...elToSort);
+  });
+});
