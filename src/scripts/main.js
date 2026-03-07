c2116f2edb64fe81098b3c2874e9094eb2eddf00
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-03-07 19:44:02 +0200

    feat: sorting by selected data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+const header = document.querySelector('thead');
+
+header.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  if (th) {
+    const sortBy = th.cellIndex;
+
+    const sortData = (row1, row2) => {
+      const row1Content = row1.cells[sortBy].textContent;
+      const row2Content = row2.cells[sortBy].textContent;
+
+      return row1Content.localeCompare(row2Content, undefined, {
+        numeric: true,
+      });
+    };
+
+    const body = Array.from(tbody.querySelectorAll('tr'));
+
+    body.sort(sortData);
+
+    body.forEach((tr) => {
+      tbody.append(tr);
+    });
+  }
+});
