e0ea771898ff664705650c5706e62217a43d6782
TiFAu <turchin.anton2@gmail.com>
2026-03-14 00:01:45 +0200

    added first soluution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+const tbody = table.querySelector('tbody');
+const thThead = [...thead.querySelectorAll('th')];
+
+// eslint-disable-next-line no-shadow
+thead.addEventListener('click', (event) => {
+  const tr = [...tbody.querySelectorAll('tr')];
+  const sorting = event.target.closest('th');
+  const indexSorting = thThead.indexOf(sorting);
+
+  tr.sort((element1, element2) => {
+    return element1.children[indexSorting].textContent.localeCompare(
+      element2.children[indexSorting].textContent,
+    );
+  }).forEach((element) => tbody.appendChild(element));
+});
