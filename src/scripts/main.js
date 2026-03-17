0c7165895a1ee726ea22fecd9c653cebd36842b8
Taras Demianiuk <demtaras1@gamil.com>
2026-03-17 15:26:21 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+const tHead = table.querySelectorAll('th');
+
+function sortTable(columnIndex) {
+  const tBody = table.tBodies[0];
+  const rows = Array.from(tBody.querySelectorAll('tr'));
+
+  const sortedRows = rows.sort((a, b) => {
+    const cellA = a.children[columnIndex].textContent.trim();
+    const cellB = b.children[columnIndex].textContent.trim();
+
+    return cellA.localeCompare(cellB, undefined, { numeric: true });
+  });
+
+  tBody.append(...sortedRows);
+}
+
+tHead.forEach((header) => {
+  header.addEventListener('click', () => {
+    sortTable(header.cellIndex);
+  });
+});
