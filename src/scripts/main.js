63f168e9e6dc46a8cc6e1b81a20550302b5a6dac
Artem <muha13artem@gmail.com>
2026-03-22 22:03:53 +0200

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  const index = e.target.cellIndex;
+
+  const rowsArray = Array.from(tbody.rows);
+
+  rowsArray.sort((rowA, rowB) => {
+    const contentA = rowA.cells[index].textContent.trim();
+    const contentB = rowB.cells[index].textContent.trim();
+
+    const cleanA = contentA.replace(/[^0-9.-]+/g, '');
+    const cleanB = contentB.replace(/[^0-9.-]+/g, '');
+
+    const isNumber =
+      cleanA !== '' && !isNaN(cleanA) && cleanB !== '' && !isNaN(cleanB);
+
+    if (isNumber) {
+      return Number(cleanA) - Number(cleanB);
+    }
+
+    return contentA.localeCompare(contentB);
+  });
+
+  tbody.append(...rowsArray);
+});
