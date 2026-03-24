38c7a342aa7d51c8ae6237ebff91764213bebb32
Olena Sobolieva <soboleva13as@gmail.com>
2026-03-24 18:36:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
 // write code here
+
+const tableHeadings = document.querySelectorAll('th');
+
+function tryParseNumber(raw) {
+  const cleaned = raw
+    .replace(/\s/g, '')
+    .replace(',', '.')
+    .replace(/[^\d.-]/g, '');
+  const n = parseFloat(cleaned);
+
+  return Number.isNaN(n) ? null : n;
+}
+
+tableHeadings.forEach((heading) => {
+  heading.addEventListener('click', () => {
+    const columnIndex = Array.from(heading.parentNode.children).indexOf(
+      heading,
+    );
+    const tbody = document.querySelector('tbody');
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+
+    rows.sort((a, b) => {
+      const rawA = a.children[columnIndex].textContent.trim() || '';
+      const rawB = b.children[columnIndex].textContent.trim() || '';
+
+      const numA = tryParseNumber(rawA);
+      const numB = tryParseNumber(rawB);
+
+      if (numA !== null && numB !== null) {
+        return numA - numB;
+      }
+
+      return rawA.localeCompare(rawB, 'uk', { numeric: true });
+    });
+    tbody.innerHTML = '';
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
