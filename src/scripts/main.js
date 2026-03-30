39261485cafa7f7a8f3cd9f1eb40d2b2063ad6e6
Grigoriy <m9258923028@gmail.com>
2026-03-30 16:51:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
 // write code here
+const tbodyContainer = document.querySelector('tbody');
+const trItemes = [...tbodyContainer.querySelectorAll('tr')];
+const listTitles = document.querySelector('thead');
+
+const getCleanItemes = (iteme, index) => {
+  const cleanIteme = iteme.cells[index].textContent;
+
+  return Number(cleanIteme.replace(/[$,]/g, ''));
+};
+
+function normalizeListObj(row) {
+  return row.map((el) => {
+    return {
+      name: el.cells[0].textContent.toLowerCase(),
+      position: el.cells[1].textContent.toLowerCase(),
+      age: getCleanItemes(el, 2),
+      salary: getCleanItemes(el, 3),
+      node: el,
+    };
+  });
+}
+normalizeListObj(trItemes);
+
+function sortList(list, key) {
+  const newList = list.sort((a, b) => {
+    if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+      return a[key] - b[key];
+    } else if (a[key] === b[key]) {
+      return 0;
+    } else {
+      return a[key] < b[key] ? -1 : 1;
+    }
+  });
+
+  newList.forEach((element) => {
+    return tbodyContainer.append(element.node);
+  });
+
+  return newList;
+}
+
+const sortedData = sortList(normalizeListObj(trItemes), 'name');
+
+listTitles.addEventListener('click', (togleTitle) => {
+  const checkedTitle = togleTitle.target.closest('th');
+  const titleValue = checkedTitle.textContent.toLowerCase();
+
+  if (!checkedTitle) {
+    return;
+  }
+
+  sortList(sortedData, titleValue);
+});
