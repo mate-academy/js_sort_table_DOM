7111f8108e339e0d6143ac02128c02757b06b188
Yurii-Leniv <supusman32@gmail.com>
2026-03-30 17:29:59 +0300

    fsdgdgye

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write code here
+const head = document.querySelectorAll('thead th');
+const list = document.querySelectorAll('tbody tr');
+
+for (let i = 0; i < head.length; i++) {
+  head[i].addEventListener('click', () => {
+    for (let k = 0; k < list.length; k++) {
+      for (let j = k; j < list.length;j++) {
+        if (i < 2) {
+          if (list[k].children[i].textContent >list[j].children[i].textContent) {
+            const tempHTML = list[k].innerHTML;
+            list[k].innerHTML = list[j].innerHTML;
+            list[j].innerHTML = tempHTML;
+          }
+        }
+
+        if (i === 2) {
+          if (Number(list[k].children[i].textContent) > Number(list[j].children[i].textContent)) {
+            const tempHTML = list[k].innerHTML;
+            list[k].innerHTML = list[j].innerHTML;
+            list[j].innerHTML = tempHTML;
+          }
+        }
+
+        if (i === 3) {
+          if (toNumber(list[k].children[i].textContent) > toNumber(list[j].children[i].textContent)) {
+            const tempHTML = list[k].innerHTML;
+            list[k].innerHTML = list[j].innerHTML;
+            list[j].innerHTML = tempHTML;
+          }
+        }
+      }
+    }
+  });
+}
+
+function toNumber(num) {
+  num = num.slice(1);
+  num = Number(num.split(',').join(''));
+  return num;
+}
