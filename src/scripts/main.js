2bd590b39806f8fef91dca39cb0650dc35dabe7c
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-03 14:05:24 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+const tbody = table.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  /**
+   * @type {HTMLTableCellElement}
+   */
+  const cellHead = e.target.closest('th');
+  const indexColumn = cellHead.cellIndex;
+  const isNumberColumn =
+    cellHead.textContent.trim() === 'Age' ||
+    cellHead.textContent.trim() === 'Salary';
+
+  const bodyRows = [...tbody.rows];
+
+  bodyRows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[indexColumn].textContent.trim();
+    const cellB = rowB.cells[indexColumn].textContent.trim();
+
+    if (isNumberColumn) {
+      return strToNumber(cellA) - strToNumber(cellB);
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  tbody.append(...bodyRows);
+});
+
+function strToNumber(str) {
+  return Number(str.replace(/[^\d.-]/g, ''));
+}
