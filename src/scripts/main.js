e18b6d3f98883136fde6a08d9530a7f6c013c12f
Ruslan <kryzhanskiyruslan@gmail.com>
2026-04-07 00:49:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  const currentTarget = e.target;
+
+  if (currentTarget.tagName === 'TH') {
+    const index = currentTarget.cellIndex;
+
+    const array = [...tbody.rows];
+
+    array.sort((a, b) => {
+      const aValue = a.cells[index].textContent.replace(/[$,]/g, '');
+      const bValue = b.cells[index].textContent.replace(/[$,]/g, '');
+
+      if (!isNaN(aValue) && !isNaN(bValue)) {
+        return aValue - bValue;
+      }
+
+      return aValue.localeCompare(bValue);
+    });
+
+    array.forEach((elem) => {
+      tbody.appendChild(elem);
+    });
+  }
+});
