e4623b64683be078c44472da5fa4c9978f37e7e0
Maksym <maxim.golya@gmail.com>
2026-04-15 17:41:00 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+
+table.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  if (!th) {
+    return;
+  }
+
+  const index = th.cellIndex;
+  const tbody = table.querySelector('tbody');
+  const rows = [...tbody.querySelectorAll('tr')];
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.children[index];
+    const cellB = rowB.children[index];
+    const valA = cellA.textContent;
+    const valB = cellB.textContent;
+    const cleanA = valA.replace(/[^0-9]/g, '');
+    const cleanB = valB.replace(/[^0-9]/g, '');
+    const numA = Number(cleanA);
+    const numB = Number(cleanB);
+
+    if (cleanA !== '' && cleanB !== '') {
+      return numA - numB;
+    } else {
+      return valA.trim().localeCompare(valB.trim());
+    }
+  });
+
+  tbody.append(...rows);
+});
