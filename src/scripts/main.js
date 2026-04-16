1a957f4b37056724868f8950cba60994bc189360
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-04-16 15:19:04 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('thead th');
+
+headers.forEach((th, index) => {
+  th.addEventListener('click', () => {
+    sortByColumn(index);
+  });
+});
+
+function sortByColumn(colIndex) {
+  const table = document.querySelector('table');
+  const tbody = table.querySelector('tbody');
+
+  const rowsArray = Array.from(tbody.rows);
+
+  rowsArray.sort((a, b) => {
+    const contentA = a.cells[colIndex].textContent;
+    const contentB = b.cells[colIndex].textContent;
+    const numA = parseFloat(contentA.replace(/[$,]/g, ''));
+    const numB = parseFloat(contentB.replace(/[$,]/g, ''));
+
+    if (!isNaN(numA) && !isNaN(numB)) {
+      return numA - numB;
+    }
+
+    return contentA.localeCompare(contentB);
+  });
+
+  tbody.append(...rowsArray);
+}
