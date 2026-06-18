086d75ca0b6724f37d405a29246543ae45771dfe
Denys Boiko <big4ever6996@gmail.com>
2026-06-18 17:30:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const sortEmployeeTable = document.querySelector('thead');
+
+sortEmployeeTable.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const colIndex = e.target.cellIndex;
+
+  const tableBody = document.querySelector('tbody');
+  const tableCells = Array.from(tableBody.querySelectorAll('tr'));
+
+  tableCells.sort((rowA, rowB) => {
+    const textA = rowA.cells[colIndex].textContent;
+    const textB = rowB.cells[colIndex].textContent;
+
+    return textA.localeCompare(textB, undefined, { numeric: true });
+  });
+
+  for (const row of tableCells) {
+    tableBody.append(row);
+  }
+});
