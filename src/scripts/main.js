1f03ff90a21c35807991b74e343651a41312a956
Tendeeer <leifisisidj747@gmail.com>
2026-06-21 16:45:41 +0300

    table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (e) => {
+  if (e.target.tagName === 'TH') {
+    const index = e.target.cellIndex;
+    const rowsArray = Array.from(tbody.querySelectorAll('tr'));
+
+    rowsArray.sort((rowA, rowB) => {
+      const valueA = rowA.cells[index].textContent.trim();
+      const valueB = rowB.cells[index].textContent.trim();
+
+      if (index === 2) {
+        return Number(valueA) - Number(valueB);
+      }
+
+      if (index === 3) {
+        return (
+          Number(valueA.replace(/[$,]/g, '')) -
+          Number(valueB.replace(/[$,]/g, ''))
+        );
+      }
+
+      return valueA.localeCompare(valueB);
+    });
+
+    tbody.innerHTML = '';
+
+    rowsArray.forEach((row) => tbody.appendChild(row));
+  }
+});
