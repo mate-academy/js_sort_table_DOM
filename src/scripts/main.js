ec667bf905b2503e7a903ff52d692082d6b382be
Ihor Balakhmei <balakhmei.dev@gmail.com>
2026-06-25 23:37:08 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+
+headers.forEach((header) => {
+  header.addEventListener('click', (evt) => {
+    const tbody = document.querySelector('tbody');
+    const columnNumber = evt.target.cellIndex;
+    const data = Array.from(tbody.rows);
+
+    data.sort((a, b) => {
+      const aValue = a.cells[columnNumber].innerText;
+      const bValue = b.cells[columnNumber].innerText;
+      const cleanNumber = (str) => +str.replace(/[$,]/g, '');
+
+      const aNum = cleanNumber(aValue);
+      const bNum = cleanNumber(bValue);
+
+      if (!isNaN(aNum) && !isNaN(bNum)) {
+        return aNum - bNum;
+      }
+
+      return aValue.localeCompare(bValue);
+    });
+
+    tbody.append(...data);
+  });
+});
