a2216caa0554e534953856f2c1e0f2ebfe9af3c6
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-07-10 18:51:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('thead tr');
+const tbody = document.querySelector('tbody');
+
+table.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const rows = [...document.querySelectorAll('tbody tr')];
+  const index = e.target.cellIndex;
+
+  rows.sort((a, b) => {
+    const first = a.cells[index].textContent;
+    const second = b.cells[index].textContent;
+
+    const fn = first.replace('$', '').replaceAll(',', '');
+    const sn = second.replace('$', '').replaceAll(',', '');
+
+    if (!isNaN(fn) && !isNaN(sn)) {
+      return Number(fn) - Number(sn);
+    }
+
+    return first.localeCompare(second);
+  });
+
+  tbody.append(...rows);
+});
