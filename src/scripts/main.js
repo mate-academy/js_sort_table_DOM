8b9108dcda6156d32f6d491aa5b40373ce9c5cb4
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-28 22:37:19 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
-'use strict';
+// 'use strict';
 
-// write code here
+// const table = document.querySelector('table');
+
+// // table.addEventListener('click', (e) => {
+// //   if (e.target.closest('thead')) {
+// //     const ind = [...table.rows[0].cells].indexOf(e.target);
+// //     // const values = [...table.tBodies[0].children].map((row) => {
+// //     //   if (isNaN(+row.cells[ind].textContent)) {
+// //     //     return row.cells[ind].textContent;
+// //     //   } else {
+// //     //     return +row.cells[ind].textContent;
+// //     //   }
+// //     // });
+// //     // // console.log(values);
+// //   }
+// // });
