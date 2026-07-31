e33708f0179c3590cf57ff0125d76fce1d239335
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-31 21:05:30 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,10 @@ const table = document.querySelector('table');
 table.addEventListener('click', (e) => {
   if (e.target.closest('thead')) {
     const ind = [...table.rows[0].cells].indexOf(e.target);
-    let values = [...table.tBodies[0].children].map((row) => {
-      if (isNaN(+row.cells[ind].textContent)) {
-        return row.cells[ind].textContent;
-      } else {
-        return +row.cells[ind].textContent;
-      }
-    });
+    const rows = [...table.tBodies[0].rows].sort((rowA, rowB) => {
+      const a = rowA.cells[ind].textContent;
+      const b = rowB.cells[ind].textContent;
 
-    values = values.sort((a, b) => {
       if (typeof a === 'string') {
         if (a.includes('$')) {
           return (
@@ -28,8 +23,6 @@ table.addEventListener('click', (e) => {
       return a - b;
     });
 
-    for (let i = 0; i < [...table.tBodies[0].rows].length; i++) {
-      [...table.tBodies[0].rows][i].children[ind].textContent = values[i];
-    }
+    table.tBodies[0].replaceChildren(...rows);
   }
 });
