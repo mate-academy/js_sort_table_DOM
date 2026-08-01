cec48978084b8c8d6013c080ac8f3a4aaed46951
Yelyzaveta Romanovska <lizarom2608@gmail.com>
2026-08-01 21:58:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write code here
+const table = document.querySelector('table');
+const tableBody = table.querySelector('tbody');
+
+table.querySelector('thead').addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName !== 'TH') {
+    return;
+  }
+
+  let sortColumn = -1;
+
+  for (let i = 0; i < target.parentElement.children.length; i++) {
+    if (target.parentElement.children[i] === target) {
+      sortColumn = i;
+      break;
+    }
+  }
+
+  if (sortColumn < 0) {
+    return;
+  }
+
+  const sorted = [...tableBody.rows].sort((row1, row2) => {
+    let row1Column = row1.children[sortColumn].textContent;
+    let row2Column = row2.children[sortColumn].textContent;
+
+    if (/^\$/.test(row1Column)) {
+      row1Column = +row1Column.replace(/\$|,/g, '');
+      row2Column = +row2Column.replace(/\$|,/g, '');
+    }
+
+    return compare(row1Column, row2Column);
+  });
+
+  tableBody.append(...sorted);
+
+  function compare(elem1, elem2) {
+    switch (typeof elem1) {
+      case 'string':
+        return elem1.localeCompare(elem2);
+      case 'number':
+        return elem1 - elem2;
+    }
+  }
+});
