7d5bed9e31754ae689f82cf8e9bf925fb5f36415
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-08-01 19:11:07 +0300

    solution: sort table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write code here
+function sortTable() {
+  const table = document.querySelector('table');
+  const headTh = table.querySelectorAll('th');
+
+  headTh.forEach((target, index) => {
+    target.addEventListener('click', () => sortType(index));
+  });
+}
+
+const sortType = (columnIndex) => {
+  const tbody = document.querySelector('tbody');
+  const tr = Array.from(tbody.children);
+
+  tr.sort((left, right) => {
+    const leftValue = left.children[columnIndex].textContent.trim();
+    const rightValue = right.children[columnIndex].textContent.trim();
+
+    if (columnIndex === 2) {
+      return parseInt(leftValue) - parseInt(rightValue);
+    }
+
+    if (columnIndex === 3) {
+      const leftNumber = parseInt(leftValue.replace(/[^\d]/g, ''));
+      const rightNumber = parseInt(rightValue.replace(/[^\d]/g, ''));
+
+      return leftNumber - rightNumber;
+    }
+
+    return leftValue.localeCompare(rightValue);
+  });
+
+  tr.forEach((row) => tbody.appendChild(row));
+};
+
+sortTable();
