81b98dbe04518842c8562c6da6dbaef0212ac61b
Maksym Babalich <17033001m@gmail.com>
2026-08-05 22:06:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,44 +1,36 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  if (!e.target.closest('th')) {
+  const tH = e.target.closest('th');
+
+  if (!tH) {
     return;
   }
 
   const table = document.querySelector('table');
-  const rows = table.rows;
-  const positionTh = e.target.cellIndex;
+  const columnIndex = tH.cellIndex;
 
-  const contentColums = [...rows]
-    .map((row) => row.cells[positionTh].textContent)
-    .splice(1)
-    .splice(0, rows.length - 2);
+  const rows = [...table.rows].slice(1);
 
-  contentColums.sort((a, b) => {
-    const isText =
-      e.target.textContent === 'Position' || e.target.textContent === 'Name';
+  rows.sort((rowA, rowB) => {
+    let valueA = rowA.cells[columnIndex].textContent;
+    let valueB = rowB.cells[columnIndex].textContent;
 
-    if (isText) {
-      return a.localeCompare(b);
+    if (columnIndex === 0 || columnIndex === 1) {
+      return valueA.localeCompare(valueB);
     }
 
-    if (e.target.textContent === 'Salary') {
-      const numberA = a.slice(1).replaceAll(',', '');
-      const numberB = b.slice(1).replaceAll(',', '');
+    if (columnIndex === 3) {
+      valueA = Number(valueA.replaceAll('$', '').replaceAll(',', ''));
+      valueB = Number(valueB.replaceAll('$', '').replaceAll(',', ''));
 
-      return numberA - numberB;
+      return valueA - valueB;
     }
 
-    const numA = Number(a);
-    const numB = Number(b);
-
-    return numA - numB;
+    return Number(valueA) - Number(valueB);
   });
 
-  let indexContent = 0;
-
-  for (let i = 1; i < rows.length - 1; i++) {
-    rows[i].cells[positionTh].textContent = contentColums[indexContent];
-    indexContent++;
+  for (const row of rows) {
+    table.appendChild(row);
   }
 });
