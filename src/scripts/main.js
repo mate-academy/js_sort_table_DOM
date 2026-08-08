fc06e952249e5bd67d8a1f952ac2104291f174e9
Nik200309 <nikitapidlypnij@gmail.com>
2026-08-08 14:38:20 +0300

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+thead.addEventListener('click', (ev) => {
+  const clickedEl = ev.target.closest('th, td');
+
+  if (!clickedEl) {
+    return;
+  }
+
+  const parentEl = clickedEl.parentElement;
+
+  const index = [...parentEl.children].indexOf(clickedEl);
+
+  const rows = [...tbody.rows];
+
+  rows.sort((rowA, rowB) => {
+    const cellA = rowA.cells[index].textContent;
+    const cellB = rowB.cells[index].textContent;
+
+    const numA = parseFloat(cellA.replace(/[^0-9.-]/g, ''));
+    const numB = parseFloat(cellB.replace(/[^0-9.-]/g, ''));
+
+    if (!isNaN(numA) && !isNaN(numB)) {
+      return numA - numB;
+    }
+
+    return cellA.localeCompare(cellB);
+  });
+
+  tbody.append(...rows);
+});
