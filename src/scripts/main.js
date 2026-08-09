d5407e758b6fc132493d9a0297555b363a27c36e
pparys93 <pparys93@gmail.com>
2026-08-09 22:41:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const headers = document.querySelectorAll('th');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const tbody = document.querySelector('tbody');
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+    const sortRows = rows.sort((a, b) => {
+      const aValue = a.querySelectorAll('td')[index].textContent.trim();
+      const bValue = b.querySelectorAll('td')[index].textContent.trim();
+      const cleanNum = (str) => parseFloat(str.replace(/[^0-9.-]/g, ''));
+
+      if (!isNaN(cleanNum(aValue)) && !isNaN(cleanNum(bValue))) {
+        return cleanNum(aValue) - cleanNum(bValue);
+      }
+
+      return aValue.localeCompare(bValue);
+    });
+
+    tbody.append(...sortRows);
+  });
+});
