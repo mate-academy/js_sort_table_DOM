870ad3a69d0bce397d829685cb46908cc02e709e
polina3107 <polinadavidenko3107@gmail.com>
2026-08-20 22:46:08 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const headers = [...document.querySelectorAll('thead th')];
+const trs = [...document.querySelectorAll('tbody tr')];
+const head = document.querySelector('thead');
+
+const names = headers.map((header) => {
+  return header.textContent;
+});
+
+head.addEventListener('click', (e) => {
+  const index = names.indexOf(e.target.firstChild.textContent);
+
+  const column = trs.map((tr) => {
+    return tr.children[index].textContent;
+  });
+
+  const sorted = column.sort((el1, el2) => {
+    return toNum(el1) - toNum(el2) || el1.localeCompare(el2);
+  });
+
+  trs.forEach((tr, i) => {
+    tr.children[index].textContent = sorted[i];
+  });
+});
+
+function toNum(el) {
+  return el.replaceAll('$', '').replaceAll(',', '');
+}
