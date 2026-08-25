57265b9547cecbdadbba12880ecf1dc2cbc935c1
Natalia Onufrienko <natalia_anamalia@icloud.com>
2026-08-25 12:19:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write code here
+const headers = document.querySelectorAll('thead th');
+const tbody = document.querySelector('tbody');
+
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+
+    rows.sort((rowA, rowB) => {
+      const valueA = rowA.children[index].textContent.trim();
+      const valueB = rowB.children[index].textContent.trim();
+
+      return valueA.localeCompare(valueB, undefined, {
+        numeric: true,
+      });
+    });
+
+    tbody.append(...rows);
+  });
+});
