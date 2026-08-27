0cb51fac05793fd962211579dc01af0b884f7fe0
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-27 19:03:27 +0300

    improvement solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,20 +8,25 @@ thead.addEventListener('click', (clickEvent) => {
   }
 
   const th = clickEvent.target;
-
   const columnIndex = th.cellIndex;
 
   const tbody = document.querySelector('tbody');
   const tr = tbody.querySelectorAll('tr');
-
   const row = [...tr];
 
+  const isAscending = th.dataset.order !== 'asc';
+
+  th.dataset.order = isAscending ? 'asc' : 'desc';
+
   row.sort((rowA, rowB) => {
-    return rowA.cells[columnIndex].textContent.localeCompare(
-      rowB.cells[columnIndex].textContent,
-      undefined,
-      { numeric: true },
-    );
+    const cellA = rowA.cells[columnIndex].textContent.trim();
+    const cellB = rowB.cells[columnIndex].textContent.trim();
+
+    const compareResult = cellA.localeCompare(cellB, undefined, {
+      numeric: true,
+    });
+
+    return isAscending ? compareResult : -compareResult;
   });
 
   tbody.append(...row);
