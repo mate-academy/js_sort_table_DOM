34f32cf46ade5877ccf504f3b2915085dee82afc
Yevhenii Dmytriienko <dmitrik24042007@gmail.com>
2026-08-27 18:46:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const thead = document.querySelector('thead');
+
+thead.addEventListener('click', (clickEvent) => {
+  if (clickEvent.target.tagName !== 'TH') {
+    return;
+  }
+
+  const th = clickEvent.target;
+
+  const columnIndex = th.cellIndex;
+
+  const tbody = document.querySelector('tbody');
+  const tr = tbody.querySelectorAll('tr');
+
+  const row = [...tr];
+
+  row.sort((rowA, rowB) => {
+    return rowA.cells[columnIndex].textContent.localeCompare(
+      rowB.cells[columnIndex].textContent,
+      undefined,
+      { numeric: true },
+    );
+  });
+
+  tbody.append(...row);
+});
