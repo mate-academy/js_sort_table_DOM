8405ba84f0d5e8f5832c392a86b272d2dc4ca87c
Ruslan Sydorenko <letovletov111@gmail.com>
2026-09-08 09:36:27 +0200

    sort table task DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const cleanSalary = (salaryString) => {
+  return Number(salaryString.replaceAll('$', '').replaceAll(',', ''));
+};
+
+const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector('thead');
+const headers = Array.from(tableHead.rows[0].cells);
+const allRows = Array.from(tableBody.rows);
+
+tableHead.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'TH') {
+    return;
+  }
+
+  const index = headers.indexOf(e.target);
+
+  allRows.sort((row1, row2) => {
+    const firstRowContent = row1.cells[index].textContent;
+    const secondRowContent = row2.cells[index].textContent;
+
+    if (index === 3) {
+      const number1 = cleanSalary(firstRowContent);
+      const number2 = cleanSalary(secondRowContent);
+
+      return number1 - number2;
+    }
+
+    if (index === 2) {
+      return Number(firstRowContent) - Number(secondRowContent);
+    }
+
+    return firstRowContent.localeCompare(secondRowContent);
+  });
+
+  for (const row of allRows) {
+    tableBody.append(row);
+  }
+});
