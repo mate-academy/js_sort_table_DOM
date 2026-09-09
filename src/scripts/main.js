e16f485a1d6eca01231b1ae1a2ee9343db64e51f
Jonereh <gnomushka0@gmail.com>
2026-09-09 18:39:03 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+const tableHeaders = document.querySelectorAll('thead th');
+const rows = document.querySelectorAll('tbody tr');
+const rowsArray = Array.from(rows);
+const tableBody = document.querySelector('tbody');
+
+tableHeaders.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    rowsArray.sort((rowA, rowB) => {
+      const cellA = rowA.children[index].textContent.trim();
+      const cellB = rowB.children[index].textContent.trim();
+
+      if (index === 2) {
+        return +cellA - +cellB;
+      }
+
+      if (index === 3) {
+        return (
+          +cellA.replace('$', '').replace(',', '') -
+          +cellB.replace('$', '').replace(',', '')
+        );
+      }
+
+      return cellA.localeCompare(cellB);
+    });
+
+    rowsArray.forEach((row) => {
+      tableBody.appendChild(row);
+    });
+  });
+});
