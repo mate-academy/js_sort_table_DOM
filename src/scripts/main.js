7f2c1fa011bbcb4074a7da9d5c4bfc931e1baa82
Your Name <you@example.com>
2026-09-13 01:06:42 +0300

    Solution_v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write code here
+const head = document.querySelector('thead');
+const headers = head.querySelectorAll('th');
+
+const table = document.querySelector('tbody');
+const rows = table.querySelectorAll('tr');
+
+function makeColumn(list, count) {
+  const column = [];
+
+  list.forEach((row) => {
+    const cells = row.cells;
+
+    column.push(cells[count].textContent);
+  });
+
+  const sortedColumn = column.sort();
+
+  for (let i = 0; i < list.length; i++) {
+    rows[i].cells[count].textContent = sortedColumn[i];
+  }
+
+  return rows;
+}
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const counter = [...headers].indexOf(header);
+
+    makeColumn(rows, counter);
+  });
+});
