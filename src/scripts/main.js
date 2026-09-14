fad03e3e6665803d144e24591783d17be4c47211
Your Name <you@example.com>
2026-09-14 23:45:07 +0300

    Solution_v1.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,19 @@ function sortTable(count) {
   const table = document.querySelector('tbody');
   const rows = [...table.querySelectorAll('tr')];
 
-  rows.sort(
-    (a, b) =>
-      a.cells[count].textContent.trim() - b.cells[count].textContent.trim(),
-  );
+  rows.sort((a, b) => {
+    const aElem = a.cells[count].textContent.trim();
+    const bElem = b.cells[count].textContent.trim();
+
+    const aElemNum = Number(aElem.replace(/[$,]/g, ''));
+    const bElemNum = Number(bElem.replace(/[$,]/g, ''));
+
+    if (!Number.isNaN(aElemNum) && !Number.isNaN(bElemNum)) {
+      return aElemNum - bElemNum;
+    }
+
+    return aElem.localeCompare(bElem);
+  });
 
   rows.forEach((row) => table.appendChild(row));
 }
